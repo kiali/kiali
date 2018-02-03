d65157960014f8f336de6343c40cc14d974ac323
John Mazzitelli <mazz@redhat.com>
2018-02-03 12:57:48 -0500

    update hack script

--- hack/get-go-dep.sh
+++ hack/get-go-dep.sh
@@ -1,102 +1,96 @@
 #!/bin/sh
 
-PROJECT_NAME="dep"
+DEP_NAME="dep"
+DEP_BIN_DIR="unknown"
+GET_UTIL="unknown"
+DEP_DIST="unknown"
 
-# LGOBIN represents the local bin location: can be either GOBIN, if set, or the GOPATH/bin.
-
-LGOBIN=""
-
-fail() {
-   echo "$1"
-   exit 1
-}
-
-verifyGoInstallation() {
-   GO=$(which go)
+validateGoLang() {
+   $(which go > /dev/null)
    if [ "$?" = "1" ]; then
-      fail "$PROJECT_NAME needs go. Please install it first."
+      fail "$DEP_NAME needs GoLang to be installed. Please install it."
    fi
    if [ -z "$GOPATH" ]; then
-      fail "$PROJECT_NAME needs environment variable "'$GOPATH'". Set it before continue."
+      fail "$DEP_NAME needs the environment variable GOPATH to be set. Please set it."
    fi
    if [ -n "$GOBIN" ]; then
       if [ ! -d "$GOBIN" ]; then
-         fail "$GOBIN "'($GOBIN)'" folder not found. Please create it before continue."
+         fail "The environment variable GOBIN was set but does not refer to an existing directory: $GOBIN"
       fi
-      LGOBIN="$GOBIN"
+      DEP_BIN_DIR="$GOBIN"
    else
       if [ ! -d "$GOPATH/bin" ]; then
-         fail "$GOPATH/bin "'($GOPATH/bin)'" folder not found. Please create it before continue."
+         fail "The bin directory under GOPATH is missing: $GOPATH/bin"
       fi
-      LGOBIN="$GOPATH/bin"
+      DEP_BIN_DIR="$GOPATH/bin"
    fi
-   echo "* Will install $PROJECT_NAME into the bin directory $LGOBIN"
+   echo "* $DEP_NAME will be installed into the directory $DEP_BIN_DIR"
 }
 
-initArch() {
-   ARCH=$(uname -m)
-   case $ARCH in
-      armv5*) ARCH="armv5";;
-      armv6*) ARCH="armv6";;
-      armv7*) ARCH="armv7";;
-      aarch64) ARCH="arm64";;
-      x86) ARCH="386";;
-      x86_64) ARCH="amd64";;
-      i686) ARCH="386";;
-      i386) ARCH="386";;
-   esac
-   echo "* ARCH=$ARCH"
-}
-
-initOS() {
-   OS=$(echo `uname`|tr '[:upper:]' '[:lower:]')
-
-   case "$OS" in
-      # Minimalist GNU for Windows
-      mingw*) OS='windows';;
-      msys*) OS='windows';;
-   esac
-   echo "* OS=$OS"
-}
-
-initDownloadTool() {
+validateGetUtil() {
    if type "curl" > /dev/null; then
-      DOWNLOAD_TOOL="curl"
+      GET_UTIL="curl"
    elif type "wget" > /dev/null; then
-      DOWNLOAD_TOOL="wget"
+      GET_UTIL="wget"
    else
-      fail "You need curl or wget as download tool. Please install it first before continuing"
+      fail "Neither 'curl' or 'wget' is installed on this machine. Please install one."
    fi
-   echo "* Using $DOWNLOAD_TOOL as download tool"
+   echo "* Will use $GET_UTIL to download artifacts from the web"
 }
 
-get() {
+validateMachineDetails() {
+   local arch=$(uname -m)
+   case $arch in
+      aarch64) arch="arm64" ;;
+      armv5*)  arch="armv5" ;;
+      armv6*)  arch="armv6" ;;
+      armv7*)  arch="armv7" ;;
+      i386)    arch="386"   ;;
+      i686)    arch="386"   ;;
+      x86)     arch="386"   ;;
+      x86_64)  arch="amd64" ;;
+   esac
+
+   local os=$(echo `uname` | tr '[:upper:]' '[:lower:]')
+   case "$os" in
+      mingw*) os='windows' ;;
+      msys*)  os='windows' ;;
+   esac
+
+   DEP_DIST="dep-$os-$arch"
+   echo "* $DEP_NAME distribution file name for your platform is expected to be $DEP_DIST"
+}
+
+setUrlResponseToEnvVar() {
+   # $1 is an env var whose value will be that of the http response
+   # $2 is the URL whose response string will be used as the value of the env var passed in $1
    local url="$2"
    local body
    local httpStatusCode
+   local httpResponse
    echo "* Getting $url"
-   if [ "$DOWNLOAD_TOOL" = "curl" ]; then
+   if [ "$GET_UTIL" = "curl" ]; then
       httpResponse=$(curl -sL --write-out HTTPSTATUS:%{http_code} "$url")
       httpStatusCode=$(echo $httpResponse | tr -d '\n' | sed -e 's/.*HTTPSTATUS://')
       body=$(echo "$httpResponse" | sed -e 's/HTTPSTATUS\:.*//g')
-   elif [ "$DOWNLOAD_TOOL" = "wget" ]; then
+   elif [ "$GET_UTIL" = "wget" ]; then
       tmpFile=$(mktemp)
       body=$(wget --server-response --content-on-error -q -O - "$url" 2> $tmpFile || true)
       httpStatusCode=$(cat $tmpFile | awk '/^  HTTP/{print $2}')
    fi
    if [ "$httpStatusCode" != 200 ]; then
-      echo "Request failed with http status code $httpStatusCode"
-      fail "Body: $body"
+      echo "HTTP request failed with http status code $httpStatusCode"
+      fail "HTTP response: $body"
    fi
    eval "$1='$body'"
 }
 
-getFile() {
+getFileFromUrl() {
    local url="$1"
    local filePath="$2"
-   if [ "$DOWNLOAD_TOOL" = "curl" ]; then
+   if [ "$GET_UTIL" = "curl" ]; then
       httpStatusCode=$(curl -s -w '%{http_code}' -L "$url" -o "$filePath")
-   elif [ "$DOWNLOAD_TOOL" = "wget" ]; then
+   elif [ "$GET_UTIL" = "wget" ]; then
       body=$(wget --server-response --content-on-error -q -O "$filePath" "$url")
       httpStatusCode=$(cat $tmpFile | awk '/^  HTTP/{print $2}')
    fi
@@ -104,64 +98,58 @@ getFile() {
 }
 
 downloadFile() {
-   DEP_DIST="dep-$OS-$ARCH"
-   echo "* Binary distribution for your platform is expected to be $DEP_DIST"
-   DEP_TMP_FILE="/tmp/$DEP_DIST"
-   LATEST_RELEASE_URL="https://api.github.com/repos/golang/$PROJECT_NAME/releases/latest"
-   echo "* Trying to find the latest release via github API: $LATEST_RELEASE_URL"
-   get LATEST_RELEASE_JSON $LATEST_RELEASE_URL
-   # || true forces this command to not catch error if grep does not find anything
-   DOWNLOAD_URL=$(echo "$LATEST_RELEASE_JSON" | grep 'browser_' | cut -d\" -f4 | grep "${DEP_DIST}\$") || true
-   if [ -z "$DOWNLOAD_URL" ]; then
-      fail "There is no dist for your system: $OS $ARCH"
+   local depDistFile="${DEP_BIN_DIR}/dep"
+   local latestReleaseUrl="https://api.github.com/repos/golang/dep/releases/latest"
+   echo "* Trying to find the latest release via github API: $latestReleaseUrl"
+   setUrlResponseToEnvVar LATEST_RELEASE_JSON $latestReleaseUrl
+   local downloadUrl=$(echo "$LATEST_RELEASE_JSON" | grep 'browser_' | cut -d\" -f4 | grep "${DEP_DIST}\$") || true
+   if [ -z "$downloadUrl" ]; then
+      fail "There is no distribution for your system: $DEP_DIST"
    else
-      echo "* Downloading the latest binary dist from $DOWNLOAD_URL"
-      httpStatusCode=$(getFile "$DOWNLOAD_URL" "$DEP_TMP_FILE")
+      echo "* Downloading the latest $DEP_NAME binary distribution from $downloadUrl"
+      local httpStatusCode=$(getFileFromUrl "$downloadUrl" "$depDistFile")
       if [ "$httpStatusCode" -ne 200 ]; then
-         fail "Failed to download dist. status code=$httpStatusCode"
+         fail "Failed to download the binary distribution. status code=$httpStatusCode"
       fi
    fi
+
+   chmod +x $depDistFile
+   echo "* $DEP_NAME distribution downloaded to: $depDistFile"
 }
 
-installFile() {
-   DEP_DIST="dep-$OS-$ARCH"
-   DEP_TMP_FILE="/tmp/$DEP_DIST"
-   echo "* Copying $DEP_TMP_FILE to $LGOBIN"
-   chmod +x $DEP_TMP_FILE
-   cp "$DEP_TMP_FILE" "${LGOBIN}/dep"
-   rm -f $DEP_TMP_FILE
+validateBinaryDist() {
+   set +e
+   $(which dep > /dev/null)
+   if [ "$?" = "1" ]; then
+      fail "dep not found. Did you add $DEP_BIN_DIR to your PATH?"
+   fi
+   set -e
+   local depVersion=$(dep version)
+   echo $depVersion
+   echo "* $DEP_NAME has been installed successfully"
 }
 
-bye() {
+fail() {
+   echo "$1"
+   exit 1
+}
+
+terminate() {
    result=$?
    if [ "$result" != "0" ]; then
-      echo "Failed to install $PROJECT_NAME"
+      echo "Failed to install $DEP_NAME"
    fi
    exit $result
 }
 
-testVersion() {
-   set +e
-   DEP="$(which $PROJECT_NAME)"
-   if [ "$?" = "1" ]; then
-      fail "$PROJECT_NAME not found. Did you add $LGOBIN to your PATH?"
-   fi
-   set -e
-   _DEP_VERSION=$($PROJECT_NAME version)
-   echo $_DEP_VERSION
-   echo "* $PROJECT_NAME has been installed successfully"
-}
 
+# START HERE
 
-# Execution
-
-#Stop execution on any error
-trap "bye" EXIT
-verifyGoInstallation
+trap "terminate" EXIT
+trap "fail Abort" SIGQUIT SIGINT SIGSTOP
+validateGoLang
 set -e
-initArch
-initOS
-initDownloadTool
+validateGetUtil
+validateMachineDetails
 downloadFile
-installFile
-testVersion
+validateBinaryDist
