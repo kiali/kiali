9ea73508eeb5999f1fe715e27643b781d57fe995
John Mazzitelli <mazz@redhat.com>
2018-02-09 16:59:24 -0500

    some refactoring to get file system routes defined in routing package (#12)

--- routing/router.go
+++ routing/router.go
@@ -1,20 +1,33 @@
 package routing
 
 import (
+	"net/http"
+
 	"github.com/gorilla/mux"
+
+	"github.com/swift-sunshine/swscore/config"
 )
 
-func NewRouter() *mux.Router {
+func NewRouter(conf *config.Config) *mux.Router {
 
 	router := mux.NewRouter().StrictSlash(true)
 
-	for _, route := range paths {
+	// Build our API server routes and install them.
+	routes := NewRoutes()
+	for _, route := range routes.Routes {
 		router.
 			Methods(route.Method).
 			Path(route.Pattern).
 			Name(route.Name).
 			Handler(route.HandlerFunc)
 	}
+
+	// Build our console routes by first creating the file server handler that will serve
+	// the webapp js files and other static content. Then tell the router about our fixed
+	// routes which pass all static file requests to the file handler.
+	fileServerHandler := http.FileServer(http.Dir(conf.Server.Static_Content_Root_Directory))
+	router.PathPrefix("/console").Handler(http.StripPrefix("/console", fileServerHandler))
+	router.PathPrefix("/").Handler(fileServerHandler)
 
 	return router
 }
