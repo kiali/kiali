6c73748dc1b4422d73a07f5cb49eeb030e44f653
Xavier Canal i Masjuan <xeviknal@gmail.com>
2018-02-12 19:01:04 +0100

    Preparing backend to respond in JSON format

---
+++ handlers/base.go
@@ -0,0 +1,18 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func RespondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+func RespondWithError(w http.ResponseWriter, code int, message string) {
+	RespondWithJSON(w, code, map[string]string{"error": message})
+}
