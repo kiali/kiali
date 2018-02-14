31dd5e9184faf13bb3725bf4a726d517eadb973b
Alberto Jesus Gutierrez Juanes <aljesusg@gmail.com>
2018-02-14 17:13:30 +0100

    Add husky, lint-staged and prettier for lint tasks

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,5 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
-);
+ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
 registerServiceWorker();
