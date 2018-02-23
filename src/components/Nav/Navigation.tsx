d40b9d282c3a8283e8dbaface8efa887e5592686
Lucas Ponce <ponce.ballesteros@gmail.com>
2018-02-23 21:23:34 +0100

    SWS-132: Fix fluid navigation - Bring back PatternFly React Vertical Navigation Component - Workaround issue with react-router-dom Link - Simplify Routes declaration and management - Add allowSyntheticDefaultImports flag to work with ProtTypes

--- src/components/Nav/Navigation.tsx
+++ src/components/Nav/Navigation.tsx
@@ -1,38 +1,67 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom';
-import * as cx from 'classnames';
+import { VerticalNav } from 'patternfly-react';
+import PropTypes from 'prop-types';
+import { Route, Switch, Redirect } from 'react-router-dom';
+
+import ServiceDetailsPage from '../../pages/ServiceDetails/ServiceDetailsPage';
+import ServiceGraphPage from '../../pages/ServiceGraph/ServiceGraphPage';
+import HomePage from '../../pages/Home/HomePage';
+import ServiceListPage from '../../pages/ServiceList/ServiceListPage';
+
+const homePath = '/';
+const homeTitle = 'Overview';
+const serviceGraphPath = '/service-graph';
+const serviceGraphTitle = 'Graph';
+const servicesPath = '/services';
+const servicesTitle = 'Services';
+
+const pfLogo = require('../../img/logo-alt.svg');
+const pfBrand = require('../../img/brand-alt.svg');
 
 class Navigation extends React.Component {
-  itemClass(href: string) {
-    return cx({
-      'list-group-item': true,
-      active: window.location.pathname === href
-    });
+  static contextTypes = {
+    router: PropTypes.object
+  };
+
+  constructor(props: any) {
+    super(props);
+    this.navigateTo = this.navigateTo.bind(this);
+  }
+
+  navigateTo(e: any) {
+    console.log(JSON.stringify(e));
+    if (e.title === serviceGraphTitle) {
+      this.context.router.history.push(serviceGraphPath);
+    } else if (e.title === servicesTitle) {
+      this.context.router.history.push(servicesPath);
+    } else {
+      this.context.router.history.push(homePath);
+    }
   }
 
   render() {
     return (
-      <div className="nav-pf-vertical">
-        <ul className="list-group">
-          <li className={this.itemClass('/')}>
-            <Link to="/">
-              <span className="fa fa-dashboard" data-toggle="tooltip" title="Overview" />
-              <span className="list-group-item-value">Overview</span>
-            </Link>
-          </li>
-          <li className={this.itemClass('/service-graph')}>
-            <Link to="/service-graph">
-              <span className="fa pficon-topology" data-toggle="tooltip" title="Graph" />
-              <span className="list-group-item-value">Graph</span>
-            </Link>
-          </li>
-          <li className={this.itemClass('/services')}>
-            <Link to="/services">
-              <span className="fa pficon-service" data-toggle="tooltip" title="Services" />
-              <span className="list-group-item-value">Services</span>
-            </Link>
-          </li>
-        </ul>
+      <div>
+        <VerticalNav>
+          <VerticalNav.Masthead title="Swift Sunshine">
+            <VerticalNav.Brand iconImg={pfLogo} titleImg={pfBrand} />
+          </VerticalNav.Masthead>
+          <VerticalNav.Item
+            title={homeTitle}
+            iconClass="fa fa-dashboard"
+            onClick={this.navigateTo}
+            initialActive={true}
+          />
+          <VerticalNav.Item title={serviceGraphTitle} iconClass="fa pficon-topology" onClick={this.navigateTo} />
+          <VerticalNav.Item title={servicesTitle} iconClass="fa pficon-service" onClick={this.navigateTo} />
+        </VerticalNav>
+        <Switch>
+          <Route path={serviceGraphPath} component={ServiceGraphPage} />
+          <Route path={servicesPath} component={ServiceListPage} />
+          <Route path="/namespaces/:namespace/services/:service" component={ServiceDetailsPage} />
+          <Route path={homePath} exact={true} component={HomePage} />
+          <Redirect to={homePath} />
+        </Switch>
       </div>
     );
   }
