e8a8a81925bd685eadb15dee9032a34a379b8cb7
Joel Takvorian <joel.takvorian@qaraywa.net>
2018-02-26 14:24:39 +0100

    SWS-131 Distinguish routes for static files versus paths to be managed... (#64)
    * SWS-131 Distinguish routes for static files versus paths to be managed by client-side router
    
    * Client-side routes are now prefixed with /console
    
    * Use / path for static files

--- routing/router.go
+++ routing/router.go
@@ -8,6 +8,7 @@ import (
 	"github.com/swift-sunshine/swscore/config"
 )
 
+// NewRouter creates the router with all API routes and the static files handler
 func NewRouter(conf *config.Config) *mux.Router {
 
 	router := mux.NewRouter().StrictSlash(true)
@@ -22,11 +23,16 @@ func NewRouter(conf *config.Config) *mux.Router {
 			Handler(route.HandlerFunc)
 	}
 
-	// Build our console routes by first creating the file server handler that will serve
+	// All client-side routes are prefixed with /console.
+	// They are forwarded to index.html and will be handled by react-router.
+	router.PathPrefix("/console").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, conf.Server.StaticContentRootDirectory+"/index.html")
+	})
+
+	// Build our static files routes by first creating the file server handler that will serve
 	// the webapp js files and other static content. Then tell the router about our fixed
 	// routes which pass all static file requests to the file handler.
 	fileServerHandler := http.FileServer(http.Dir(conf.Server.StaticContentRootDirectory))
-	router.PathPrefix("/console").Handler(http.StripPrefix("/console", fileServerHandler))
 	router.PathPrefix("/").Handler(fileServerHandler)
 
 	return router
