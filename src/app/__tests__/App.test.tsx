1e7b70d4d4e136547f8cbc0cbd2f757a2d47c36a
Viet Nguyen <vnguyen@redhat.com>
2018-03-07 09:12:25 -0800

    Restore main app test

--- src/app/__tests__/App.test.tsx
+++ src/app/__tests__/App.test.tsx
@@ -2,8 +2,20 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from '../App';
 
-// Skipping full App rendering for now until we properly mock Cytoscape
-test.skip('renders App without crashing', () => {
+const axios = require('axios');
+const MockAdapter = require('axios-mock-adapter');
+
+// Mock graph component due to direct DOM/div access
+jest.mock('../../components/CytoscapeLayout/CytoscapeLayout');
+
+const mock = new MockAdapter(axios);
+mock.onAny().reply(200);
+
+process.env.REACT_APP_NAME = 'swsui-test';
+process.env.REACT_APP_VERSION = '1.0.1';
+process.env.REACT_APP_GIT_HASH = '89323';
+
+it('renders full App without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
