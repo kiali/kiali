cd47c056335a90d0fba4cb7a80a0f7ba36166755
Joel Takvorian <jtakvori@redhat.com>
2018-03-07 11:54:21 +0100

    SWS-261 match backend changes for metrics model
    Keep the existing layout and display

--- src/types/Metrics.ts
+++ src/types/Metrics.ts
@@ -1,10 +1,29 @@
-export interface MetricValue {
-  value: number;
+export interface Metrics {
+  health: Health;
+  metrics: Map<String, MetricGroup>;
+  histograms: Map<String, Histogram>;
 }
 
-export interface MetricHistogram {
-  average: number;
-  median: number;
-  percentile95: number;
-  percentile99: number;
+export interface Health {
+  healthyReplicas: number;
+  totalReplicas: number;
 }
+
+export interface Histogram {
+  average: MetricGroup;
+  median: MetricGroup;
+  percentile95: MetricGroup;
+  percentile99: MetricGroup;
+}
+
+export interface MetricGroup {
+  matrix: TimeSeries[];
+}
+
+export interface TimeSeries {
+  metric: Map<String, String>;
+  values: Datapoint[];
+}
+
+// First is timestamp, second is value
+export type Datapoint = [number, number];
