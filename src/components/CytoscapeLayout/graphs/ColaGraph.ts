fc7111e0e3140bf75be1a623d7a1f9c3005b0eba
mwringe <mwringe@redhat.com>
2018-03-09 17:36:24 -0500

    Slight tweak to how cola is rendered

--- src/components/CytoscapeLayout/graphs/ColaGraph.ts
+++ src/components/CytoscapeLayout/graphs/ColaGraph.ts
@@ -3,7 +3,9 @@ import { GraphType } from './GraphType';
 export class ColaGraph implements GraphType {
   static getLayout() {
     return {
-      name: 'cola'
+      name: 'cola',
+      animate: false,
+      flow: { axis: 'y' }
     };
   }
 }
