dc3b852678dd0cd5af6719afe360613d501177ca
jshaughn <jshaughn@redhat.com>
2018-03-09 14:25:58 -0500

    SWS-221 Simple Time Filter - introduce a consolidated graph filter component to give   us a single toolbar for graph options and maintain some   l+f parity with the service list view (and pfly in general)

---
+++ src/components/GraphFilter/LayoutButtonGroup.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import { ButtonGroup, Button } from 'patternfly-react';
+import PropTypes from 'prop-types';
+
+type LayoutButtonGroupProps = {
+  initialLayout: string;
+  onClick: PropTypes.func;
+};
+
+type LayoutButtonGroupState = {
+  current: string;
+};
+
+export class LayoutButtonGroup extends React.Component<LayoutButtonGroupProps, LayoutButtonGroupState> {
+  constructor(props: LayoutButtonGroupProps) {
+    super(props);
+    this.state = {
+      current: props.initialLayout
+    };
+  }
+
+  layout = (e: any) => {
+    this.setState({ current: e.target.id });
+    this.props.onClick(e.target.id);
+  };
+
+  render() {
+    return (
+      <ButtonGroup>
+        <Button onClick={this.layout} active={this.state.current === 'breadthfirst'} id="breadthfirst">
+          Breadthfirst
+        </Button>
+        <Button onClick={this.layout} active={this.state.current === 'cola'} id="cola">
+          Cola
+        </Button>
+        <Button onClick={this.layout} active={this.state.current === 'dagre'} id="dagre">
+          Dagre
+        </Button>
+      </ButtonGroup>
+    );
+  }
+}
