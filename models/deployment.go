bc3b7d475ca9d2c1a921a5ecdcbc64ac5e0dc28d
Xavier Canal i Masjuan <xeviknal@gmail.com>
2018-03-16 15:32:01 +0100

    SWS-253: Embed autoscalers in each deployment

--- models/deployment.go
+++ models/deployment.go
@@ -1,11 +1,13 @@
 package models
 
 import (
-	"k8s.io/api/apps/v1beta1"
 	"time"
+
+	"k8s.io/api/apps/v1beta1"
+	"k8s.io/api/autoscaling/v1"
 )
 
-type Deployments []Deployment
+type Deployments []*Deployment
 type Deployment struct {
 	Name                string            `json:"name"`
 	Labels              map[string]string `json:"labels"`
@@ -13,6 +15,7 @@ type Deployment struct {
 	Replicas            int32             `json:"replicas"`
 	AvailableReplicas   int32             `json:"available_replicas"`
 	UnavailableReplicas int32             `json:"unavailable_replicas"`
+	Autoscaler          Autoscaler        `json:"autoscaler"`
 }
 
 func (deployments *Deployments) Parse(ds *v1beta1.DeploymentList) {
@@ -23,7 +26,7 @@ func (deployments *Deployments) Parse(ds *v1beta1.DeploymentList) {
 	for _, deployment := range ds.Items {
 		casted := Deployment{}
 		casted.Parse(&deployment)
-		*deployments = append(*deployments, casted)
+		*deployments = append(*deployments, &casted)
 	}
 }
 
@@ -34,4 +37,18 @@ func (deployment *Deployment) Parse(d *v1beta1.Deployment) {
 	deployment.Replicas = d.Status.Replicas
 	deployment.AvailableReplicas = d.Status.AvailableReplicas
 	deployment.UnavailableReplicas = d.Status.UnavailableReplicas
+}
+
+func (deployments *Deployments) AddAutoscalers(as *v1.HorizontalPodAutoscalerList) {
+	if as == nil {
+		return
+	}
+
+	for _, deployment := range *deployments {
+		for _, autoscaler := range as.Items {
+			if deployment.Name == autoscaler.Spec.ScaleTargetRef.Name {
+				deployment.Autoscaler.Parse(&autoscaler)
+			}
+		}
+	}
 }
