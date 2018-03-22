89f9d8c1547117c9da7361e4aab10bd022ac0ff2
Jay Shaughnessy <jshaughn@users.noreply.github.com>
2018-03-22 15:11:29 -0400

    KIALI-396 Fix edge summary rendering, and simplify impl. (#168)
    - also, prevent link to "unknown" source service

--- src/pages/ServiceGraph/SummaryPanelEdge.tsx
+++ src/pages/ServiceGraph/SummaryPanelEdge.tsx
@@ -9,15 +9,6 @@ import graphUtils from '../../utils/graphing';
 
 type SummaryPanelEdgeState = {
   loading: boolean;
-  sourceService: string;
-  sourceServiceName: string;
-  sourceNamespace: string;
-  sourceVersion: string;
-  dest: string;
-  destService: string;
-  destServiceName: string;
-  destNamespace: string;
-  destVersion: string;
   reqRates: [string, number][];
   errRates: [string, number][];
 };
@@ -33,82 +24,58 @@ export default class SummaryPanelEdge extends React.Component<SummaryPanelPropTy
   constructor(props: SummaryPanelPropType) {
     super(props);
 
-    const edge = this.props.data.summaryTarget;
-    const source = edge.source();
-    const sourceService = source.data('service');
-    const sourceSplit = sourceService.split('.');
-    const dest = edge.target();
-    const destService = dest.data('service');
-    const destSplit = dest.data('service').split('.');
-
     this.state = {
       loading: true,
-      sourceService: sourceService,
-      sourceServiceName: sourceSplit[0],
-      sourceNamespace: sourceSplit.length < 2 ? 'unknown' : sourceSplit[1],
-      sourceVersion: source.data('version'),
-      dest: dest.data('service'),
-      destService: destService,
-      destServiceName: destSplit[0],
-      destNamespace: destSplit[1],
-      destVersion: dest.data('version'),
       reqRates: [],
       errRates: []
     };
   }
 
   componentDidMount() {
-    const options = {
-      version: this.state.destVersion,
-      'byLabelsIn[]': 'source_service,source_version',
-      rateInterval: this.props.rateInterval
-    };
-    API.getServiceMetrics(this.state.destNamespace, this.state.destServiceName, options)
-      .then(response => {
-        const data: M.Metrics = response['data'];
-        const metrics: Map<String, M.MetricGroup> = data.metrics;
-        const reqRates = this.getRates(metrics['request_count_in'], 'RPS');
-        const errRates = this.getRates(metrics['request_error_count_in'], 'Error');
+    this.updateRpsChart(this.props);
+  }
 
-        this.setState({
-          loading: false,
-          reqRates: reqRates,
-          errRates: errRates
-        });
-      })
-      .catch(error => {
-        this.setState({ loading: false });
-        console.error(error);
-        // this.props.onError(error);
-      });
+  componentWillReceiveProps(nextProps: SummaryPanelPropType) {
+    if (nextProps.data.summaryTarget !== this.props.data.summaryTarget) {
+      this.updateRpsChart(nextProps);
+    }
   }
 
   render() {
     const edge = this.props.data.summaryTarget;
+    const source = edge.source();
+    const sourceService = source.data('service');
+    const sourceVersion = source.data('version');
+    const sourceSplit = sourceService.split('.');
+    const sourceServiceName = sourceSplit[0];
+    const sourceNamespace = sourceSplit.length < 2 ? 'unknown' : sourceSplit[1];
+    const dest = edge.target();
+    const destService = dest.data('service');
+    const destVersion = dest.data('version');
+    const destSplit = dest.data('service').split('.');
+    const destServiceName = destSplit[0];
+    const destNamespace = destSplit[1];
     const rate = this.safeRate(edge.data('rate'));
     const rate3xx = this.safeRate(edge.data('rate3XX'));
     const rate4xx = this.safeRate(edge.data('rate4XX'));
     const rate5xx = this.safeRate(edge.data('rate5XX'));
-    const sourceLink = (
-      <a href={`../namespaces/${this.state.sourceNamespace}/services/${this.state.sourceService}`}>
-        {this.state.sourceServiceName}
-      </a>
-    );
-    const destLink = (
-      <a href={`../namespaces/${this.state.destNamespace}/services/${this.state.destService}`}>
-        {this.state.destServiceName}
-      </a>
-    );
+    const sourceLink = <a href={`../namespaces/${sourceNamespace}/services/${sourceService}`}>{sourceServiceName}</a>;
+    const destLink = <a href={`../namespaces/${destNamespace}/services/${destService}`}>{destServiceName}</a>;
 
     return (
       <div className="panel panel-default" style={SummaryPanelEdge.panelStyle}>
-        <div className="panel-heading">Edge Source: {sourceLink}</div>
+        <div hidden={sourceServiceName === 'unknown'} className="panel-heading">
+          Edge Source: {sourceLink}
+        </div>
+        <div hidden={sourceServiceName !== 'unknown'} className="panel-heading">
+          Edge Source: unknown
+        </div>
         <div className="panel-body">
-          <p>{this.renderLabels(this.state.sourceNamespace, this.state.sourceVersion)}</p>
+          <p>{this.renderLabels(sourceNamespace, sourceVersion)}</p>
         </div>
         <div className="panel-heading">Edge Dest: {destLink}</div>
         <div className="panel-body">
-          <p>{this.renderLabels(this.state.destNamespace, this.state.destVersion)}</p>
+          <p>{this.renderLabels(destNamespace, destVersion)}</p>
           <hr />
           <RateTable
             title="Traffic (requests per second):"
@@ -125,6 +92,42 @@ export default class SummaryPanelEdge extends React.Component<SummaryPanelPropTy
       </div>
     );
   }
+
+  private updateRpsChart = (props: SummaryPanelPropType) => {
+    const edge = props.data.summaryTarget;
+    const source = edge.source();
+    const sourceService = source.data('service');
+    const sourceVersion = source.data('version');
+    const dest = edge.target();
+    const destVersion = dest.data('version');
+    const destSplit = dest.data('service').split('.');
+    const destServiceName = destSplit[0];
+    const destNamespace = destSplit[1];
+
+    const options = {
+      version: destVersion,
+      'byLabelsIn[]': 'source_service,source_version',
+      rateInterval: this.props.rateInterval
+    };
+    API.getServiceMetrics(destNamespace, destServiceName, options)
+      .then(response => {
+        const data: M.Metrics = response['data'];
+        const metrics: Map<String, M.MetricGroup> = data.metrics;
+        const reqRates = this.getRates(metrics['request_count_in'], 'RPS', sourceService, sourceVersion);
+        const errRates = this.getRates(metrics['request_error_count_in'], 'Error', sourceService, sourceVersion);
+
+        this.setState({
+          loading: false,
+          reqRates: reqRates,
+          errRates: errRates
+        });
+      })
+      .catch(error => {
+        this.setState({ loading: false });
+        console.error(error);
+        // this.props.onError(error);
+      });
+  };
 
   private safeRate = (s: string) => {
     return s === undefined ? 0.0 : parseFloat(s);
@@ -146,16 +149,18 @@ export default class SummaryPanelEdge extends React.Component<SummaryPanelPropTy
     return <RpsChart label="Request Average" dataRps={this.state.reqRates} dataErrors={this.state.errRates} />;
   };
 
-  private getRates = (mg: M.MetricGroup, title: string): [string, number][] => {
+  private getRates = (
+    mg: M.MetricGroup,
+    title: string,
+    sourceService: string,
+    sourceVersion: string
+  ): [string, number][] => {
     const tsa: M.TimeSeries[] = mg.matrix;
     let series: M.TimeSeries[] = [];
 
     for (let i = 0; i < tsa.length; ++i) {
       const ts = tsa[i];
-      if (
-        ts.metric['source_service'] === this.state.sourceService &&
-        ts.metric['source_version'] === this.state.sourceVersion
-      ) {
+      if (ts.metric['source_service'] === sourceService && ts.metric['source_version'] === sourceVersion) {
         series.push(ts);
       }
     }
