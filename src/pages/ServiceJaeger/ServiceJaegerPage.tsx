e7fcb9edd0d35d12ab26e6b8b43a1eb9aa734363
Alberto Gutierrez <aljesusg@gmail.com>
2018-03-28 11:32:56 +0200

    [KIALI-170]Iframe with Jaeger

---
+++ src/pages/ServiceJaeger/ServiceJaegerPage.tsx
@@ -0,0 +1,80 @@
+import * as React from 'react';
+import Iframe from 'react-iframe';
+import * as API from '../../services/Api';
+import { ToastNotification, ToastNotificationList } from 'patternfly-react';
+
+type ServiceJaegerState = {
+  height: string;
+  width: string;
+  jaegerURL: string;
+  error: boolean;
+  errorMsg: string;
+};
+
+class ServiceJaegerPage extends React.Component<{}, ServiceJaegerState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = { width: '0 px', jaegerURL: '', height: '0 px', error: false, errorMsg: '' };
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+  }
+
+  componentWillMount() {
+    API.getJaegerInfo()
+      .then(response => {
+        let data = response['data'];
+        this.setState({
+          jaegerURL: data.url
+        });
+      })
+      .catch(error => {
+        this.setState({
+          error: true,
+          errorMsg: 'Could not connect to server'
+        });
+        console.log(error);
+      });
+  }
+
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener('resize', this.updateWindowDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions() {
+    this.setState({ width: window.innerWidth - 300 + 'px', height: window.innerHeight - 100 + 'px' });
+  }
+
+  render() {
+    return (
+      <div className="container-fluid container-pf-nav-pf-vertical">
+        {this.state.error ? (
+          <ToastNotificationList>
+            <ToastNotification type="danger">
+              <span>
+                <strong>Error </strong>
+                {this.state.errorMsg}
+              </span>
+            </ToastNotification>
+          </ToastNotificationList>
+        ) : null}
+        <div className="container-fluid container-cards-pf">
+          <Iframe
+            url={this.state.jaegerURL}
+            width={this.state.width}
+            height={this.state.height}
+            id="jaegerUI"
+            display="block"
+            allowFullScreen={true}
+            style={{ verticalOverflow: false }}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ServiceJaegerPage;
