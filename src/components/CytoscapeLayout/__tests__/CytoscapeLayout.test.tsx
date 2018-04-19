54d7b1f9cb706ee5d1efc70b3117d5488496c4d4
Cainã Costa <cainan.costa@gmail.com>
2018-04-18 21:10:31 -0300

    Fix failing test

--- src/components/CytoscapeLayout/__tests__/CytoscapeLayout.test.tsx
+++ src/components/CytoscapeLayout/__tests__/CytoscapeLayout.test.tsx
@@ -22,7 +22,7 @@ describe('CytographLayout component test', () => {
   it('should set correct elements data', () => {
     const myLayout: Layout = { name: 'breadthfirst' };
     const myDuration: Duration = { value: '5m' };
-    const badgeStatus: BadgeStatus = { showCBs: true };
+    const badgeStatus: BadgeStatus = { showCBs: false };
 
     const wrapper = shallow(
       <CytoscapeLayout
