1fbf5173f733f0233eee9f523d05f17d810bb700
jshaughn <jshaughn@redhat.com>
2018-05-14 16:29:59 -0400

    KIALI-447 Add latency to edge summary

---
+++ src/components/SummaryPanel/LatencyChart.tsx
@@ -0,0 +1,59 @@
+import * as React from 'react';
+import { AreaChart } from 'patternfly-react';
+import { PfColors } from '../../components/Pf/PfColors';
+
+type LatencyChartTypeProp = {
+  label: string;
+  latAvg: [string, number][];
+  latMed: [string, number][];
+  lat95: [string, number][];
+  lat99: [string, number][];
+};
+
+export default class LatencyChart extends React.Component<LatencyChartTypeProp, {}> {
+  constructor(props: LatencyChartTypeProp) {
+    super(props);
+  }
+
+  render() {
+    const axis: any = {
+      x: {
+        show: false,
+        type: 'timeseries',
+        tick: {
+          fit: true,
+          count: 15,
+          multiline: false,
+          format: '%H:%M:%S'
+        }
+      },
+      y: { show: false }
+    };
+
+    const chartData = {
+      x: 'x',
+      columns: (this.props.latAvg as [string, number][])
+        .concat(this.props.latMed as [string, number][])
+        .concat(this.props.lat95 as [string, number][])
+        .concat(this.props.lat99 as [string, number][]),
+      type: 'area-spline',
+      hide: ['Average', '95th', '99th']
+    };
+
+    return (
+      <>
+        <div>
+          <strong>{this.props.label}:</strong>
+        </div>
+        <AreaChart
+          size={{ height: 80 }}
+          color={{ pattern: [PfColors.Black, PfColors.Green, PfColors.Blue, PfColors.Orange] }}
+          legend={{ show: true }}
+          grid={{ y: { show: false } }}
+          axis={axis}
+          data={chartData}
+        />
+      </>
+    );
+  }
+}
