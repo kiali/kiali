ff809917ee82c96c27b674ca4114f70a1abc7759
Josejulio Martínez <finwemartinez@gmail.com>
2018-05-18 12:09:30 -0500

    KIALI-727 Migrate from npm to yarn (#348)
    - Update travis and related scripts to use yarn instead of npm
    - Updated README
    - Adding a test to check to warn if package.json is out of sync
      with yarn.lock
    - Upgraded some required dependencies

--- run_itest.sh
+++ run_itest.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-if [ ${TRAVIS_PULL_REQUEST} = "false" ] ||  [ -z ${TRAVIS_PULL_REQUEST} ]; 
+if [ ${TRAVIS_PULL_REQUEST} = "false" ] ||  [ -z ${TRAVIS_PULL_REQUEST} ];
 then
   echo "Must be run from a PR."
   exit 0
 fi
 
 # launch Kiali in the background
-npm run kiali &
+yarn kiali &
 APP_PID=$!
 
 # wait for app
 timeout 10s bash -c "until curl -I -L  --silent  --output /dev/null http://localhost:5003; do if [ "$?" -eq 0 ]; then continue; fi; sleep 1; done"
 
-npm run itest
+yarn itest
 
 kill -9 ${APP_PID}
 
