ee1ddd94ef06d01fe498ce0ce4267ed7fcf47c42
Josejulio Martínez <jmartine@redhat.com>
2018-05-18 15:05:14 -0500

    Add tests for MessageCenter action and reducer

---
+++ src/actions/__tests__/MessageCenterAction.test.ts
@@ -0,0 +1,161 @@
+import { MessageCenterActions, MessageCenterActionKeys } from '../MessageCenterActions';
+import { MessageType } from '../../types/MessageCenter';
+
+import thunk from 'redux-thunk';
+import configureMockStore from 'redux-mock-store';
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+describe('MessageCenterAtions', () => {
+  it('should add a message', () => {
+    const expectedAction = {
+      type: MessageCenterActionKeys.ADD_MESSAGE,
+      content: 'my message',
+      groupId: 'great-messages',
+      messageType: MessageType.WARNING
+    };
+    expect(MessageCenterActions.addMessage('my message', 'great-messages', MessageType.WARNING)).toEqual(
+      expectedAction
+    );
+  });
+  it('should remove a single message', () => {
+    const expectedAction = {
+      type: MessageCenterActionKeys.REMOVE_MESSAGE,
+      messageId: [5]
+    };
+    expect(MessageCenterActions.removeMessage(5)).toEqual(expectedAction);
+  });
+  it('should remove multiple messages', () => {
+    const expectedAction = {
+      type: MessageCenterActionKeys.REMOVE_MESSAGE,
+      messageId: [5, 6, 8]
+    };
+    expect(MessageCenterActions.removeMessage([5, 6, 8])).toEqual(expectedAction);
+  });
+  it('should mark as read a single message', () => {
+    const expectedAction = {
+      type: MessageCenterActionKeys.MARK_MESSAGE_AS_READ,
+      messageId: [3]
+    };
+    expect(MessageCenterActions.markAsRead(3)).toEqual(expectedAction);
+  });
+  it('should mark as read multiple messages', () => {
+    const expectedAction = {
+      type: MessageCenterActionKeys.MARK_MESSAGE_AS_READ,
+      messageId: [1, 2, 3, 4]
+    };
+    expect(MessageCenterActions.markAsRead([1, 2, 3, 4])).toEqual(expectedAction);
+  });
+  it('should toggle group', () => {
+    const expectedAction = {
+      type: MessageCenterActionKeys.TOGGLE_GROUP,
+      groupId: 'my-awesome-group'
+    };
+    expect(MessageCenterActions.toggleGroup('my-awesome-group')).toEqual(expectedAction);
+  });
+  it('should hide a single notification', () => {
+    const expectedAction = {
+      type: MessageCenterActionKeys.HIDE_NOTIFICATION,
+      messageId: [2]
+    };
+    expect(MessageCenterActions.hideNotification(2)).toEqual(expectedAction);
+  });
+  it('should hide a multiple notifications', () => {
+    const expectedAction = {
+      type: MessageCenterActionKeys.HIDE_NOTIFICATION,
+      messageId: [8, 9, 7]
+    };
+    expect(MessageCenterActions.hideNotification([8, 9, 7])).toEqual(expectedAction);
+  });
+  it('should show the message center', () => {
+    const expectedAction = {
+      type: MessageCenterActionKeys.SHOW
+    };
+    expect(MessageCenterActions.showMessageCenter()).toEqual(expectedAction);
+  });
+  it('should hide the message center', () => {
+    const expectedAction = {
+      type: MessageCenterActionKeys.HIDE
+    };
+    expect(MessageCenterActions.hideMessageCenter()).toEqual(expectedAction);
+  });
+  it('should toggle the exapended state of the message center', () => {
+    const expectedAction = {
+      type: MessageCenterActionKeys.TOGGLE_EXPAND
+    };
+    expect(MessageCenterActions.togleExpandedMessageCenter()).toEqual(expectedAction);
+  });
+  it('should open a closed message center', () => {
+    const expectedActions = [
+      {
+        type: MessageCenterActionKeys.SHOW
+      }
+    ];
+    const store = mockStore({ messageCenter: { hidden: true } });
+    return store.dispatch(MessageCenterActions.toggleMessageCenter()).then(() => {
+      expect(store.getActions()).toEqual(expectedActions);
+    });
+  });
+  it('should close an opened message center', () => {
+    const expectedActions = [
+      {
+        type: MessageCenterActionKeys.HIDE
+      }
+    ];
+    const store = mockStore({ messageCenter: { hidden: false } });
+    return store.dispatch(MessageCenterActions.toggleMessageCenter()).then(() => {
+      expect(store.getActions()).toEqual(expectedActions);
+    });
+  });
+  it('should only mark selected group as read', () => {
+    const expectedActions = [
+      {
+        type: MessageCenterActionKeys.MARK_MESSAGE_AS_READ,
+        messageId: [1, 2, 3]
+      }
+    ];
+    const store = mockStore({
+      messageCenter: {
+        groups: [
+          {
+            id: 'my-group',
+            messages: [{ id: 1 }, { id: 2 }, { id: 3 }]
+          },
+          {
+            id: 'other',
+            messages: [{ id: 5 }, { id: 6 }, { id: 7 }]
+          }
+        ]
+      }
+    });
+    return store.dispatch(MessageCenterActions.markGroupAsRead('my-group')).then(() => {
+      expect(store.getActions()).toEqual(expectedActions);
+    });
+  });
+  it('should only clear messages of selected group', () => {
+    const expectedActions = [
+      {
+        type: MessageCenterActionKeys.REMOVE_MESSAGE,
+        messageId: [5, 6, 7]
+      }
+    ];
+    const store = mockStore({
+      messageCenter: {
+        groups: [
+          {
+            id: 'my-group',
+            messages: [{ id: 1 }, { id: 2 }, { id: 3 }]
+          },
+          {
+            id: 'other',
+            messages: [{ id: 5 }, { id: 6 }, { id: 7 }]
+          }
+        ]
+      }
+    });
+    return store.dispatch(MessageCenterActions.clearGroup('other')).then(() => {
+      expect(store.getActions()).toEqual(expectedActions);
+    });
+  });
+});
---
+++ src/reducers/__tests__/MessageCenterReducer.test.ts
@@ -0,0 +1,477 @@
+import MessageCenter from '../MessageCenter';
+import { MessageCenterActionKeys } from '../../actions/MessageCenterActions';
+import { MessageType } from '../../types/MessageCenter';
+
+describe('MessageCenter reducer', () => {
+  const RealDate = Date;
+
+  const mockDate = date => {
+    global.Date = jest.fn(() => date) as any;
+    return date;
+  };
+
+  afterEach(() => {
+    global.Date = RealDate;
+  });
+
+  it('should return the initial state', () => {
+    expect(MessageCenter(undefined, {})).toEqual({
+      expanded: false,
+      expandedGroupId: 'default',
+      groups: [
+        {
+          id: 'default',
+          messages: [],
+          title: 'Default'
+        }
+      ],
+      hidden: true,
+      nextId: 0
+    });
+  });
+  it('should handle ADD_MESSAGE', () => {
+    const date = mockDate(new Date());
+    expect(
+      MessageCenter(
+        {
+          expanded: false,
+          expandedGroupId: 'default',
+          groups: [
+            {
+              id: 'default',
+              messages: [],
+              title: 'Default'
+            }
+          ],
+          hidden: true,
+          nextId: 0
+        },
+        {
+          type: MessageCenterActionKeys.ADD_MESSAGE,
+          groupId: 'default',
+          content: 'my new message',
+          messageType: MessageType.WARNING
+        }
+      )
+    ).toEqual({
+      expanded: false,
+      expandedGroupId: 'default',
+      groups: [
+        {
+          id: 'default',
+          messages: [
+            {
+              id: 0,
+              seen: false,
+              show_notification: true,
+              content: 'my new message',
+              type: MessageType.WARNING,
+              created: date
+            }
+          ],
+          title: 'Default'
+        }
+      ],
+      hidden: true,
+      nextId: 1
+    });
+  });
+  it('should handle REMOVE_MESSAGE', () => {
+    const date = mockDate(new Date());
+    expect(
+      MessageCenter(
+        {
+          expanded: false,
+          expandedGroupId: 'default',
+          groups: [
+            {
+              id: 'default',
+              messages: [
+                {
+                  id: 0,
+                  seen: false,
+                  show_notification: true,
+                  content: 'my new message',
+                  type: MessageType.WARNING,
+                  created: date
+                },
+                {
+                  id: 1,
+                  seen: true,
+                  show_notification: false,
+                  content: 'other message',
+                  type: MessageType.ERROR,
+                  created: date
+                },
+                {
+                  id: 2,
+                  seen: true,
+                  show_notification: false,
+                  content: 'other',
+                  type: MessageType.INFO,
+                  created: date
+                }
+              ],
+              title: 'Default'
+            }
+          ],
+          hidden: true,
+          nextId: 1
+        },
+        {
+          type: MessageCenterActionKeys.REMOVE_MESSAGE,
+          messageId: [0, 2]
+        }
+      )
+    ).toEqual({
+      expanded: false,
+      expandedGroupId: 'default',
+      groups: [
+        {
+          id: 'default',
+          messages: [
+            {
+              id: 1,
+              seen: true,
+              show_notification: false,
+              content: 'other message',
+              type: MessageType.ERROR,
+              created: date
+            }
+          ],
+          title: 'Default'
+        }
+      ],
+      hidden: true,
+      nextId: 1
+    });
+  });
+  it('should handle MARK_MESSAGE_AS_READ', () => {
+    const date = mockDate(new Date());
+    expect(
+      MessageCenter(
+        {
+          expanded: false,
+          expandedGroupId: 'default',
+          groups: [
+            {
+              id: 'default',
+              messages: [
+                {
+                  id: 0,
+                  seen: false,
+                  show_notification: true,
+                  content: 'my new message',
+                  type: MessageType.WARNING,
+                  created: date
+                },
+                {
+                  id: 1,
+                  seen: true,
+                  show_notification: false,
+                  content: 'other message',
+                  type: MessageType.ERROR,
+                  created: date
+                },
+                {
+                  id: 2,
+                  seen: false,
+                  show_notification: false,
+                  content: 'other',
+                  type: MessageType.INFO,
+                  created: date
+                }
+              ],
+              title: 'Default'
+            }
+          ],
+          hidden: true,
+          nextId: 1
+        },
+        {
+          type: MessageCenterActionKeys.MARK_MESSAGE_AS_READ,
+          messageId: [0, 1]
+        }
+      )
+    ).toEqual({
+      expanded: false,
+      expandedGroupId: 'default',
+      groups: [
+        {
+          id: 'default',
+          messages: [
+            {
+              id: 0,
+              seen: true,
+              show_notification: false,
+              content: 'my new message',
+              type: MessageType.WARNING,
+              created: date
+            },
+            {
+              id: 1,
+              seen: true,
+              show_notification: false,
+              content: 'other message',
+              type: MessageType.ERROR,
+              created: date
+            },
+            {
+              id: 2,
+              seen: false,
+              show_notification: false,
+              content: 'other',
+              type: MessageType.INFO,
+              created: date
+            }
+          ],
+          title: 'Default'
+        }
+      ],
+      hidden: true,
+      nextId: 1
+    });
+  });
+  it('should handle HIDE_NOTIFICATION', () => {
+    const date = mockDate(new Date());
+    expect(
+      MessageCenter(
+        {
+          expanded: false,
+          expandedGroupId: 'default',
+          groups: [
+            {
+              id: 'default',
+              messages: [
+                {
+                  id: 0,
+                  seen: false,
+                  show_notification: true,
+                  content: 'my new message',
+                  type: MessageType.WARNING,
+                  created: date
+                },
+                {
+                  id: 1,
+                  seen: false,
+                  show_notification: true,
+                  content: 'other message',
+                  type: MessageType.ERROR,
+                  created: date
+                }
+              ],
+              title: 'Default'
+            }
+          ],
+          hidden: true,
+          nextId: 1
+        },
+        {
+          type: MessageCenterActionKeys.HIDE_NOTIFICATION,
+          messageId: [0]
+        }
+      )
+    ).toEqual({
+      expanded: false,
+      expandedGroupId: 'default',
+      groups: [
+        {
+          id: 'default',
+          messages: [
+            {
+              id: 0,
+              seen: false,
+              show_notification: false,
+              content: 'my new message',
+              type: MessageType.WARNING,
+              created: date
+            },
+            {
+              id: 1,
+              seen: false,
+              show_notification: true,
+              content: 'other message',
+              type: MessageType.ERROR,
+              created: date
+            }
+          ],
+          title: 'Default'
+        }
+      ],
+      hidden: true,
+      nextId: 1
+    });
+  });
+  it('should handle SHOW', () => {
+    expect(
+      MessageCenter(
+        {
+          expanded: false,
+          expandedGroupId: 'default',
+          groups: [
+            {
+              id: 'default',
+              messages: [],
+              title: 'Default'
+            }
+          ],
+          hidden: true,
+          nextId: 0
+        },
+        {
+          type: MessageCenterActionKeys.SHOW
+        }
+      )
+    ).toEqual({
+      expanded: false,
+      expandedGroupId: 'default',
+      groups: [
+        {
+          id: 'default',
+          messages: [],
+          title: 'Default'
+        }
+      ],
+      hidden: false,
+      nextId: 0
+    });
+  });
+  it('should handle HIDE', () => {
+    expect(
+      MessageCenter(
+        {
+          expanded: false,
+          expandedGroupId: 'default',
+          groups: [
+            {
+              id: 'default',
+              messages: [],
+              title: 'Default'
+            }
+          ],
+          hidden: false,
+          nextId: 0
+        },
+        {
+          type: MessageCenterActionKeys.HIDE
+        }
+      )
+    ).toEqual({
+      expanded: false,
+      expandedGroupId: 'default',
+      groups: [
+        {
+          id: 'default',
+          messages: [],
+          title: 'Default'
+        }
+      ],
+      hidden: true,
+      nextId: 0
+    });
+  });
+  it('should handle TOGGLE_EXPAND', () => {
+    expect(
+      MessageCenter(
+        {
+          expanded: false,
+          expandedGroupId: 'default',
+          groups: [
+            {
+              id: 'default',
+              messages: [],
+              title: 'Default'
+            }
+          ],
+          hidden: false,
+          nextId: 0
+        },
+        {
+          type: MessageCenterActionKeys.TOGGLE_EXPAND
+        }
+      )
+    ).toEqual({
+      expanded: true,
+      expandedGroupId: 'default',
+      groups: [
+        {
+          id: 'default',
+          messages: [],
+          title: 'Default'
+        }
+      ],
+      hidden: false,
+      nextId: 0
+    });
+  });
+  it('should handle TOGGLE_GROUP to hide a group', () => {
+    expect(
+      MessageCenter(
+        {
+          expanded: false,
+          expandedGroupId: 'default',
+          groups: [
+            {
+              id: 'default',
+              messages: [],
+              title: 'Default'
+            }
+          ],
+          hidden: false,
+          nextId: 0
+        },
+        {
+          type: MessageCenterActionKeys.TOGGLE_GROUP,
+          groupId: 'default'
+        }
+      )
+    ).toEqual({
+      expanded: false,
+      expandedGroupId: undefined,
+      groups: [
+        {
+          id: 'default',
+          messages: [],
+          title: 'Default'
+        }
+      ],
+      hidden: false,
+      nextId: 0
+    });
+  });
+  it('should handle TOGGLE_GROUP to show a group', () => {
+    expect(
+      MessageCenter(
+        {
+          expanded: false,
+          expandedGroupId: undefined,
+          groups: [
+            {
+              id: 'default',
+              messages: [],
+              title: 'Default'
+            }
+          ],
+          hidden: false,
+          nextId: 0
+        },
+        {
+          type: MessageCenterActionKeys.TOGGLE_GROUP,
+          groupId: 'default'
+        }
+      )
+    ).toEqual({
+      expanded: false,
+      expandedGroupId: 'default',
+      groups: [
+        {
+          id: 'default',
+          messages: [],
+          title: 'Default'
+        }
+      ],
+      hidden: false,
+      nextId: 0
+    });
+  });
+});
