3035ae2791711d558b4af8c5677501f70b390208
jshaughn <jshaughn@redhat.com>
2018-07-23 16:29:06 -0400

    KIALI-1223 [1.0] graph, update unused_service appender - rename to unused_node appender - migrate from service-pods approach to deployments approach - enable unused_node appender - update tests - config impact:   - replace obsolete ServiceFilterLabelName with AppLabelName   - rename VersionFilterLabelName to VersionLabelName   - bump supported istio from 0.8+ to 1.0+ - service impact   - remove incorrect GetServicePods method, use GetPods instead, as needed - also:   - fix issue in compound group nodes (set the new fields, not just the old "service" fields)

---
+++ graph/appender/unused_node.go
@@ -0,0 +1,96 @@
+package appender
+
+import (
+	"k8s.io/api/apps/v1beta1"
+
+	"github.com/kiali/kiali/config"
+	"github.com/kiali/kiali/graph"
+	"github.com/kiali/kiali/kubernetes"
+	"github.com/kiali/kiali/log"
+)
+
+type UnusedNodeAppender struct {
+	GraphType string
+	Versioned bool
+}
+
+// AppendGraph implements Appender
+func (a UnusedNodeAppender) AppendGraph(trafficMap graph.TrafficMap, namespace string) {
+	istioClient, err := kubernetes.NewClient()
+	checkError(err)
+
+	deployments, err := istioClient.GetDeployments(namespace)
+	checkError(err)
+
+	a.addUnusedNodes(trafficMap, namespace, deployments)
+}
+
+func (a UnusedNodeAppender) addUnusedNodes(trafficMap graph.TrafficMap, namespace string, deployments *v1beta1.DeploymentList) {
+	unusedTrafficMap := a.buildUnusedTrafficMap(trafficMap, namespace, deployments)
+
+	// If trafficMap is empty just populate it with the unused nodes and return
+	if len(trafficMap) == 0 {
+		for k, v := range unusedTrafficMap {
+			trafficMap[k] = v
+		}
+		return
+	}
+
+	// Integrate the unused nodes into the existing traffic map
+	for _, v := range unusedTrafficMap {
+		addUnusedNodeToTrafficMap(trafficMap, v)
+	}
+}
+
+func (a UnusedNodeAppender) buildUnusedTrafficMap(trafficMap graph.TrafficMap, namespace string, deployments *v1beta1.DeploymentList) graph.TrafficMap {
+	unusedTrafficMap := graph.NewTrafficMap()
+	appLabel := config.Get().AppLabelName
+	versionLabel := config.Get().VersionLabelName
+	for _, d := range deployments.Items {
+		labels := d.GetLabels()
+		app := graph.UnknownApp
+		version := graph.UnknownVersion
+		if v, ok := labels[appLabel]; ok {
+			app = v
+		}
+		if v, ok := labels[versionLabel]; ok {
+			version = v
+		}
+		id := graph.Id(namespace, d.Name, app, version, a.GraphType, a.Versioned)
+		if _, found := trafficMap[id]; !found {
+			if _, found = unusedTrafficMap[id]; !found {
+				log.Debugf("Adding unused node for deployment [%s] with labels [%v]", d.Name, labels)
+				node := graph.NewNode(id, namespace, d.Name, app, version)
+				node.Metadata = map[string]interface{}{"rate": 0.0, "rateOut": 0.0, "isUnused": true}
+				unusedTrafficMap[id] = &node
+			}
+		}
+	}
+	return unusedTrafficMap
+}
+
+func addUnusedNodeToTrafficMap(trafficMap graph.TrafficMap, unusedNode *graph.Node) {
+	// add unused node to traffic map
+	trafficMap[unusedNode.ID] = unusedNode
+
+	// Add a "sibling" edge to any node with an edge to the same app
+	for _, n := range trafficMap {
+		findAndAddSibling(n, unusedNode)
+	}
+}
+
+func findAndAddSibling(parent, unusedNode *graph.Node) {
+	if unusedNode.App == graph.UnknownApp {
+		return
+	}
+
+	found := false
+	for _, edge := range parent.Edges {
+		if found = edge.Dest.App == unusedNode.App; found {
+			break
+		}
+	}
+	if found {
+		parent.AddEdge(unusedNode)
+	}
+}
