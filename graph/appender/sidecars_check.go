37d7346c46296068d5406107bc3c383f275abcbe
Jay Shaughnessy <jshaughn@users.noreply.github.com>
2018-07-24 17:15:45 -0400

    KIALI-1225 [1.0] graph, update missing-sidecar appender (#378)
    - update to 1.0 nodes
    - add support for workload nodes
      - restrict appPreferred nodes to requiring app+version labels
    - enable appender
    
    also:
    - fix issue with group compound nodes, only add for versioned app or appPreferred

--- graph/appender/sidecars_check.go
+++ graph/appender/sidecars_check.go
@@ -3,13 +3,19 @@ package appender
 import (
 	"fmt"
 
+	"k8s.io/apimachinery/pkg/labels"
+
 	"github.com/kiali/kiali/config"
 	"github.com/kiali/kiali/graph"
 	"github.com/kiali/kiali/kubernetes"
+	"github.com/kiali/kiali/log"
 	"github.com/kiali/kiali/services/business/checkers"
 )
 
-type SidecarsCheckAppender struct{}
+type SidecarsCheckAppender struct {
+	GraphType string
+	Versioned bool
+}
 
 // AppendGraph implements Appender
 func (a SidecarsCheckAppender) AppendGraph(trafficMap graph.TrafficMap, _ string) {
@@ -23,26 +29,69 @@ func (a SidecarsCheckAppender) AppendGraph(trafficMap graph.TrafficMap, _ string
 	a.applySidecarsChecks(trafficMap, k8s)
 }
 
-func (a SidecarsCheckAppender) applySidecarsChecks(trafficMap graph.TrafficMap, k8s *kubernetes.IstioClient) {
+func (a *SidecarsCheckAppender) applySidecarsChecks(trafficMap graph.TrafficMap, k8s *kubernetes.IstioClient) {
 	appLabel := config.Get().AppLabelName
 	versionLabel := config.Get().VersionLabelName
 
-	for _, s := range trafficMap {
-		// TODO FIX s.ServiceName, s.Name --> s.App to compile
-		if s.App == graph.UnknownApp {
-			return
+	for _, n := range trafficMap {
+		// dead nodes have no pods
+		if isDead, ok := n.Metadata["isDead"]; ok && isDead.(bool) {
+			continue
 		}
 
-		pods, err := k8s.GetPods(s.Namespace, fmt.Sprintf("%s=%s,%s=%s", appLabel, s.App, versionLabel, s.Version))
+		// get the pods for the node, either by app+version labels, or workload deployment
+		var podLabels string
+		var err error
+		switch a.GraphType {
+		case graph.GraphTypeApp:
+			podLabels = a.getAppLabels(appLabel, n.App, versionLabel, n.Version)
+		case graph.GraphTypeWorkload:
+			podLabels, err = a.getWorkloadLabels(n.Namespace, n.Workload, k8s)
+			if err != nil {
+				continue
+			}
+		case graph.GraphTypeAppPreferred:
+			if n.App != graph.UnknownApp && (!a.Versioned || n.Version != graph.UnknownVersion) {
+				podLabels = a.getAppLabels(appLabel, n.App, versionLabel, n.Version)
+			} else {
+				podLabels, err = a.getWorkloadLabels(n.Namespace, n.Workload, k8s)
+				if err != nil {
+					continue
+				}
+			}
+		}
+
+		pods, err := k8s.GetPods(n.Namespace, podLabels)
 		checkError(err)
 
+		if len(pods.Items) == 0 {
+			log.Warningf("Sidecar check found no pods Checking sidecars node [%s] num pods [%v]", n.ID, len(pods.Items))
+		}
+
+		// check each pod for sidecar, stop and flag at first pod missing sidecar
 		checker := checkers.PodChecker{Pods: pods.Items}
 		validations := checker.Check()
 
-		sidecarsOk := true
 		for _, check := range validations {
-			sidecarsOk = sidecarsOk && check.Valid
+			if !check.Valid {
+				n.Metadata["hasMissingSC"] = true
+				break
+			}
 		}
-		s.Metadata["hasMissingSC"] = !sidecarsOk
 	}
+}
+
+func (a *SidecarsCheckAppender) getAppLabels(appLabel, app, versionLabel, version string) string {
+	if a.Versioned {
+		return fmt.Sprintf("%s=%s,%s=%s", appLabel, app, versionLabel, version)
+	}
+	return fmt.Sprintf("%s=%s", appLabel, app)
+}
+
+func (a *SidecarsCheckAppender) getWorkloadLabels(namespace, workload string, k8s *kubernetes.IstioClient) (string, error) {
+	deployment, err := k8s.GetDeployment(namespace, workload)
+	if err != nil {
+		return "", err
+	}
+	return labels.Set(deployment.Spec.Selector.MatchLabels).String(), nil
 }
--- graph/options/options.go
+++ graph/options/options.go
@@ -179,7 +179,10 @@ func parseAppenders(params url.Values, o Options) []appender.Appender {
 		//appenders = append(appenders, appender.IstioAppender{})
 	}
 	if csl == AppenderAll || strings.Contains(csl, "sidecars_check") {
-		//appenders = append(appenders, appender.SidecarsCheckAppender{})
+		appenders = append(appenders, appender.SidecarsCheckAppender{
+			GraphType: o.GraphType,
+			Versioned: o.Versioned,
+		})
 	}
 
 	return appenders
