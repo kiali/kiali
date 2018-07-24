8c3e589406f5dc8ad790d44fa18ac1fe91309db5
Josejulio Martínez <finwemartinez@gmail.com>
2018-07-24 15:50:55 -0500

    KIALI-1250 Fixes test errors (#536)
    * KIALI-1250 Fixes test errors
    
    * Fix enum usage

--- src/utils/Enum.ts
+++ src/utils/Enum.ts
@@ -1,7 +1,7 @@
-export const fromValue = <EnumType>(enumType: EnumType, value: any, defaultValue: any) => {
-  const found: EnumType = enumType[value] as EnumType;
-  if (found !== undefined) {
-    return found;
+export const fromValue = (enumType: any, value: any, defaultValue: any) => {
+  const enumKey = Object.keys(enumType).find(key => enumType[key] === value);
+  if (enumKey !== undefined) {
+    return enumType[enumKey];
   }
   return defaultValue;
 };
