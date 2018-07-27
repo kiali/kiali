b91d98416d350847cfaab00ccbe066a0957360f4
John Mazzitelli <mazz@redhat.com>
2018-07-27 15:43:18 -0400

    rename the app so not the same as service name

--- hack/istio/download-istio.sh
+++ hack/istio/download-istio.sh
@@ -19,7 +19,7 @@
 # The version used by the getLatestIstio script - if empty, gets the latest version
 # TODO when Istio 1.0 is released, remove the version string (right side) - we
 # want this set to empty string so by default we pick up the latest version.
-ISTIO_VERSION=1.0.0-snapshot.1
+ISTIO_VERSION=1.0.0-snapshot.2
 
 # process command line args
 while [[ $# -gt 0 ]]; do
