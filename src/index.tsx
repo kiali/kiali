faa74038fe4534875e9c1aa7c9b778ea1c9597e4
Mike Thompson <mithomps@redhat.com>
2018-08-16 14:28:43 -0700

    Trivial: Remove service worker from build since it is not being used.

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './app/App';
-import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
-registerServiceWorker();
