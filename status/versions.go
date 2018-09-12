61b2fbbc98e91922fa94be8a9da386be01dfe6d9
John Mazzitelli <mazz@redhat.com>
2018-09-12 09:48:37 -0400

    add tests - fix some things the tests found

--- status/versions.go
+++ status/versions.go
@@ -21,8 +21,8 @@ type externalService func() (*ExternalServiceInfo, error)
 var (
 	// Example Maistra version is:
 	//   redhat@redhat-docker.io/maistra-0.1.0-1-3a136c90ec5e308f236e0d7ebb5c4c5e405217f4-unknown
-	maistraVersionExpr = regexp.MustCompile(".*maistra-([0-9]+\\.[0-9]+\\.[0-9]+).*")
-	istioVersionExpr   = regexp.MustCompile(".*([0-9]+\\.[0-9]+\\.[0-9]+).*")
+	maistraVersionExpr = regexp.MustCompile("maistra-([0-9]+\\.[0-9]+\\.[0-9]+)")
+	istioVersionExpr   = regexp.MustCompile("([0-9]+\\.[0-9]+\\.[0-9]+)")
 )
 
 func getVersions() {
@@ -66,7 +66,6 @@ func validateVersion(istioReq string, installedVersion string) bool {
 }
 
 func istioVersion() (*ExternalServiceInfo, error) {
-	product := ExternalServiceInfo{Name: "Unknown", Version: "Unknown"}
 	istioConfig := config.Get().ExternalServices.Istio
 	resp, err := http.Get(istioConfig.UrlServiceVersion)
 	if err == nil {
@@ -74,47 +73,53 @@ func istioVersion() (*ExternalServiceInfo, error) {
 		body, err := ioutil.ReadAll(resp.Body)
 		if err == nil {
 			rawVersion := string(body)
-
-			// First see if we detect Maistra. If it is not Maistra, see if it is upstream Istio.
-			// If it is neither then it is some unknown Istio implementation that we do not support.
-
-			maistraVersionStringArr := maistraVersionExpr.FindStringSubmatch(rawVersion)
-			if maistraVersionStringArr != nil {
-				log.Debugf("Detected Maistra version [%v]", rawVersion)
-				if len(maistraVersionStringArr) > 1 {
-					product.Name = "Maistra"
-					product.Version = maistraVersionStringArr[1] // get regex group #1 ,which is the "#.#.#" version string
-					if !validateVersion(config.MaistraVersionSupported, product.Version) {
-						info.WarningMessages = append(info.WarningMessages, "Maistra version "+product.Version+" is not supported, the version should be "+config.MaistraVersionSupported)
-					}
-
-					// we know this is Maistra - either a supported or unsupported version - return now
-					return &product, nil
-				}
-			}
-
-			istioVersionStringArr := istioVersionExpr.FindStringSubmatch(rawVersion)
-			if istioVersionStringArr != nil {
-				log.Debugf("Detected Istio version [%v]", rawVersion)
-				if len(istioVersionStringArr) > 1 {
-					product.Name = "Istio"
-					product.Version = istioVersionStringArr[1] // get regex group #1 ,which is the "#.#.#" version string
-					if !validateVersion(config.IstioVersionSupported, product.Version) {
-						info.WarningMessages = append(info.WarningMessages, "Istio version "+product.Version+" is not supported, the version should be "+config.IstioVersionSupported)
-					}
-					// we know this is Istio upstream - either a supported or unsupported version - return now
-					return &product, nil
-				}
-			}
-
-			log.Debugf("Detected unknown Istio implementation version [%v]", rawVersion)
-			product.Name = "Unknown Istio Implementation"
-			product.Version = rawVersion
-			info.WarningMessages = append(info.WarningMessages, "Unknown Istio implementation version "+product.Version+" is not recognized, thus not supported.")
-			return &product, nil
+			product, err := parseIstioRawVersion(rawVersion)
+			return product, err
 		}
 	}
 	return nil, err
+}
+
+func parseIstioRawVersion(rawVersion string) (*ExternalServiceInfo, error) {
+	product := ExternalServiceInfo{Name: "Unknown", Version: "Unknown"}
+
+	// First see if we detect Maistra. If it is not Maistra, see if it is upstream Istio.
+	// If it is neither then it is some unknown Istio implementation that we do not support.
+
+	maistraVersionStringArr := maistraVersionExpr.FindStringSubmatch(rawVersion)
+	if maistraVersionStringArr != nil {
+		log.Debugf("Detected Maistra version [%v]", rawVersion)
+		if len(maistraVersionStringArr) > 1 {
+			product.Name = "Maistra"
+			product.Version = maistraVersionStringArr[1] // get regex group #1 ,which is the "#.#.#" version string
+			if !validateVersion(config.MaistraVersionSupported, product.Version) {
+				info.WarningMessages = append(info.WarningMessages, "Maistra version "+product.Version+" is not supported, the version should be "+config.MaistraVersionSupported)
+			}
+
+			// we know this is Maistra - either a supported or unsupported version - return now
+			return &product, nil
+		}
+	}
+
+	istioVersionStringArr := istioVersionExpr.FindStringSubmatch(rawVersion)
+	if istioVersionStringArr != nil {
+		log.Debugf("Detected Istio version [%v]", rawVersion)
+		if len(istioVersionStringArr) > 1 {
+			product.Name = "Istio"
+			product.Version = istioVersionStringArr[1] // get regex group #1 ,which is the "#.#.#" version string
+			if !validateVersion(config.IstioVersionSupported, product.Version) {
+				info.WarningMessages = append(info.WarningMessages, "Istio version "+product.Version+" is not supported, the version should be "+config.IstioVersionSupported)
+			}
+			// we know this is Istio upstream - either a supported or unsupported version - return now
+			return &product, nil
+		}
+	}
+
+	log.Debugf("Detected unknown Istio implementation version [%v]", rawVersion)
+	product.Name = "Unknown Istio Implementation"
+	product.Version = rawVersion
+	info.WarningMessages = append(info.WarningMessages, "Unknown Istio implementation version "+product.Version+" is not recognized, thus not supported.")
+	return &product, nil
 }
 
 type p8sResponseVersion struct {
