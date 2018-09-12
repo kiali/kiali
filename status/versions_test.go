6e0f268160f72cd8cf19de200a646d9097f8eb1e
John Mazzitelli <mazz@redhat.com>
2018-09-12 09:48:37 -0400

    add tests - fix some things the tests found

--- status/versions_test.go
+++ status/versions_test.go
@@ -4,6 +4,81 @@ import (
 	"testing"
 )
 
+func TestParseIstioRawVersion(t *testing.T) {
+	type versionsToTestStruct struct {
+		rawVersion string
+		name       string
+		version    string
+		supported  bool
+	}
+
+	// see config.go/[Maistra,Istio]VersionSupported for what versions are supported
+	versionsToTest := []versionsToTestStruct{
+		{
+			rawVersion: "redhat@redhat-docker.io/maistra-0.1.0-1-3a13-unknown",
+			name:       "Maistra",
+			version:    "0.1.0",
+			supported:  true,
+		},
+		{
+			rawVersion: "foobar-maistra-11.12.13-wotgorilla?",
+			name:       "Maistra",
+			version:    "11.12.13",
+			supported:  true,
+		},
+		{
+			rawVersion: "foobar-maistra-0.0.987-wotgorilla?",
+			name:       "Maistra",
+			version:    "0.0.987",
+			supported:  false,
+		},
+		{
+			rawVersion: "foo-istio-1.2.3-bar",
+			name:       "Istio",
+			version:    "1.2.3",
+			supported:  true,
+		},
+		{
+			rawVersion: "foo-istio-10.11.122-bar",
+			name:       "Istio",
+			version:    "10.11.122",
+			supported:  true,
+		},
+		{
+			rawVersion: "foo-istio-0.123.789-bar",
+			name:       "Istio",
+			version:    "0.123.789",
+			supported:  false,
+		},
+		{
+			rawVersion: "some-unknown-version-string",
+			name:       "Unknown Istio Implementation",
+			version:    "some-unknown-version-string",
+			supported:  false,
+		},
+	}
+
+	for _, versionToTest := range versionsToTest {
+		info.WarningMessages = []string{} // reset before we test
+		p, err := parseIstioRawVersion(versionToTest.rawVersion)
+		if err != nil {
+			t.Errorf("Got an error trying to validate [%+v]: %+v", versionToTest, err)
+		}
+		if p.Name != versionToTest.name {
+			t.Errorf("Cannot validate [%+v] - name is incorrect: %+v", versionToTest, p)
+		}
+		if p.Version != versionToTest.version {
+			t.Errorf("Cannot validate [%+v] - version is incorrect: %+v", versionToTest, p)
+		}
+		if versionToTest.supported && len(info.WarningMessages) > 0 {
+			t.Errorf("Version [%+v] is supported but the parsed version [%+v] caused a warning: %+v", versionToTest, p, info.WarningMessages)
+		}
+		if !versionToTest.supported && len(info.WarningMessages) == 0 {
+			t.Errorf("Version [%+v] is not supported but the parsed version [%+v] did not cause a warning", versionToTest, p)
+		}
+	}
+}
+
 func TestValidateVersion(t *testing.T) {
 	result := validateVersion(">= 0.7.1", "0.7.1")
 
