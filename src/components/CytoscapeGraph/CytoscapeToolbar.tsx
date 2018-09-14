08f4076a99eedc42e3a6c36adc83d20fc224864d
jshaughn <jshaughn@redhat.com>
2018-09-14 15:20:00 -0400

    KIALI-1172 Migrate layout selection to cy toolbar - removed from graph settings dropdown - add new buttons to cy toolbar - remove unnecessary and poorly named GraphType interface

--- src/components/CytoscapeGraph/CytoscapeToolbar.tsx
+++ src/components/CytoscapeGraph/CytoscapeToolbar.tsx
@@ -1,13 +1,19 @@
 import * as React from 'react';
-import { Button, Icon } from 'patternfly-react';
+import { ButtonGroup, Button, Icon } from 'patternfly-react';
 import { style } from 'typestyle';
 import { PfColors } from '../Pf/PfColors';
 import * as CytoscapeGraphUtils from './CytoscapeGraphUtils';
+import { Layout } from '../../types/GraphFilter';
+import { ColaGraph } from './graphs/ColaGraph';
+import { CoseGraph } from './graphs/CoseGraph';
+import { DagreGraph } from './graphs/DagreGraph';
 
 type CytoscapeToolbarProps = {
   cytoscapeGraphRef: any;
   isLegendActive: boolean;
-  toggleLegend(): void;
+  activeLayout: Layout;
+  onLayoutChange: (layout: Layout) => void;
+  toggleLegend: () => void;
 };
 
 const cytoscapeToolbarStyle = style({
@@ -17,7 +23,7 @@ const cytoscapeToolbarStyle = style({
   borderColor: PfColors.Black500,
   backgroundColor: PfColors.White
 });
-const cytoscapeToolbarButtonStyle = style({ marginLeft: '10px' });
+const cytoscapeToolbarPadStyle = style({ marginLeft: '10px' });
 
 const ZOOM_STEP = 0.2;
 
@@ -25,19 +31,56 @@ export class CytoscapeToolbar extends React.PureComponent<CytoscapeToolbarProps>
   render() {
     return (
       <div className={cytoscapeToolbarStyle}>
-        <Button onClick={this.zoomIn}>
-          <Icon type="fa" name="plus" />
-        </Button>
-        <Button onClick={this.zoomOut} className={cytoscapeToolbarButtonStyle}>
-          <Icon type="fa" name="minus" />
-        </Button>
-        <Button onClick={this.fit} className={cytoscapeToolbarButtonStyle}>
+        <ButtonGroup>
+          <Button onClick={this.zoomIn}>
+            <Icon type="fa" name="plus" />
+          </Button>
+
+          <Button onClick={this.zoomOut}>
+            <Icon type="fa" name="minus" />
+          </Button>
+        </ButtonGroup>
+
+        <Button onClick={this.fit} className={cytoscapeToolbarPadStyle}>
           <div className="glyphicon glyphicon-fullscreen" />
         </Button>
+
+        <ButtonGroup className={cytoscapeToolbarPadStyle}>
+          <Button
+            onClick={() => {
+              this.props.onLayoutChange(DagreGraph.getLayout());
+            }}
+            title={DagreGraph.getLayout().name}
+            active={this.props.activeLayout.name === DagreGraph.getLayout().name}
+          >
+            <div className="fa pficon-infrastructure fa-rotate-270" />
+          </Button>
+
+          <Button
+            onClick={() => {
+              this.props.onLayoutChange(CoseGraph.getLayout());
+            }}
+            title={CoseGraph.getLayout().name}
+            active={this.props.activeLayout.name === CoseGraph.getLayout().name}
+          >
+            <div className="fa pficon-topology" /> 1
+          </Button>
+
+          <Button
+            onClick={() => {
+              this.props.onLayoutChange(ColaGraph.getLayout());
+            }}
+            title={ColaGraph.getLayout().name}
+            active={this.props.activeLayout.name === ColaGraph.getLayout().name}
+          >
+            <div className="fa pficon-topology" /> 2
+          </Button>
+        </ButtonGroup>
+
         <Button
           onClick={this.props.toggleLegend}
           active={this.props.isLegendActive}
-          className={cytoscapeToolbarButtonStyle}
+          className={cytoscapeToolbarPadStyle}
         >
           Legend
         </Button>
--- src/components/CytoscapeGraph/graphs/GraphType.ts
+++
@@ -1,4 +0,0 @@
-export interface GraphType {
-  // @todo this is just a Marker Interface until we can figure out a Static Method I/F
-  // static getLayout(): any;
-}
