9fb633df550da6d9fa0e4cc4e381f578a7bcf77d
Xavier Canal i Masjuan <xeviknal@gmail.com>
2018-09-20 11:41:38 +0200

    KIALI-1510 Add new assertions to workload endpoints e2e tests

--- tests/e2e/tests/test_kiali_workload_endpoint.py
+++ tests/e2e/tests/test_kiali_workload_endpoint.py
@@ -10,7 +10,11 @@ def test_workload_list_endpoint(kiali_client):
     workload_list = kiali_client.workload_list(namespace=bookinfo_namespace)
     assert workload_list != None
     for workload in workload_list.get('workloads'):
-        assert workload.get('istioSidecar') == True
+      assert workload != None
+      assert workload.get('name') != None and workload.get('name') != ''
+      assert workload.get('istioSidecar') == True
+      assert workload.get('appLabel') == True
+      assert workload.get('versionLabel') == True
 
 def test_workload_details(kiali_client):
     bookinfo_namespace = conftest.get_bookinfo_endpoint()
@@ -19,15 +23,35 @@ def test_workload_details(kiali_client):
     assert workload != None
     assert WORKLOAD_TO_VALIDATE in workload.get('name')
     assert WORKLOAD_TYPE in workload.get('type')
+    assert 'labels' in workload
+    assert 'templateAnnotations' in workload
+    assert 'pods' in workload
+    assert 'services' in workload
 
 def test_workload_metrics(kiali_client):
     bookinfo_namespace = conftest.get_bookinfo_endpoint()
 
     workload = kiali_client.workload_metrics(namespace=bookinfo_namespace, workload=WORKLOAD_TO_VALIDATE)
-    assert workload != None
-    metrics = workload.get('source').get('metrics')
-    assert 'request_count_in' in metrics
-    assert 'request_count_out' in metrics
+    for direction in ['dest', 'source']:
+      assert workload != None
+
+      metrics = workload.get(direction).get('metrics')
+      assert 'request_count_in' in metrics
+      assert 'request_count_out' in metrics
+      assert 'request_error_count_in' in metrics
+      assert 'request_error_count_out' in metrics
+      assert 'tcp_received_in' in metrics
+      assert 'tcp_received_out' in metrics
+      assert 'tcp_sent_in' in metrics
+      assert 'tcp_sent_out' in metrics
+
+      histograms = workload.get(direction).get('histograms')
+      assert 'request_duration_in' in histograms
+      assert 'request_duration_out' in histograms
+      assert 'request_size_in' in histograms
+      assert 'request_size_out' in histograms
+      assert 'response_size_in' in histograms
+      assert 'response_size_out' in histograms
 
 def test_workload_health(kiali_client):
     bookinfo_namespace = conftest.get_bookinfo_endpoint()
@@ -35,6 +59,7 @@ def test_workload_health(kiali_client):
     workload = kiali_client.workload_health(namespace=bookinfo_namespace, workload=WORKLOAD_TO_VALIDATE)
     assert workload != None
     assert WORKLOAD_TO_VALIDATE in workload.get('deploymentStatus').get('name')
+    assert 'requests' in workload
 
 def test_workload_istio_validations(kiali_client):
     bookinfo_namespace = conftest.get_bookinfo_endpoint()
