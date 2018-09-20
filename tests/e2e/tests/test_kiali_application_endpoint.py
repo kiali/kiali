161dfc314081031df47b54280fdbef310b651b4a
Xavier Canal i Masjuan <xeviknal@gmail.com>
2018-09-20 10:48:58 +0200

    KIALI-1510 Add new assertions to service endpoints e2e tests

--- tests/e2e/tests/test_kiali_application_endpoint.py
+++ tests/e2e/tests/test_kiali_application_endpoint.py
@@ -5,7 +5,6 @@ APPLICATION_TO_VALIDATE = 'productpage'
 
 PARAMS = {'graphType': 'versionedApp', 'duration': '60s'}
 
-
 def test_application_list_endpoint(kiali_client):
     bookinfo_namespace = conftest.get_bookinfo_endpoint()
 
--- tests/e2e/tests/test_kiali_service_endpoint.py
+++ tests/e2e/tests/test_kiali_service_endpoint.py
@@ -8,9 +8,17 @@ SERVICE_TO_VALIDATE = 'reviews'
 def test_service_list_endpoint(kiali_client):
     bookinfo_namespace = conftest.get_bookinfo_endpoint()
 
-    assert kiali_client.service_list(namespace=bookinfo_namespace).get('namespace').get('name') == bookinfo_namespace
-    number_services = len (kiali_client.service_list(namespace=bookinfo_namespace).get('services'))
-    assert (number_services == BOOKINFO_EXPECTED_SERVICES) or (number_services == BOOKINFO_EXPECTED_SERVICES_MONGODB)
+    service_list_json = kiali_client.service_list(namespace=bookinfo_namespace)
+    assert service_list_json.get('namespace').get('name') == bookinfo_namespace
+
+    services = service_list_json.get('services')
+    assert (len (services) == BOOKINFO_EXPECTED_SERVICES) or (len (services) == BOOKINFO_EXPECTED_SERVICES_MONGODB)
+
+    for service in services:
+      assert service != None
+      assert service.get('name') != None and service.get('name') != ''
+      assert service.get('istioSidecar') == True
+      assert service.get('appLabel') == True
 
 def test_service_detail_endpoint(kiali_client):
     bookinfo_namespace = conftest.get_bookinfo_endpoint()
@@ -18,24 +26,45 @@ def test_service_detail_endpoint(kiali_client):
     service_details = kiali_client.service_details(namespace=bookinfo_namespace, service=SERVICE_TO_VALIDATE)
     assert service_details != None
     assert service_details.get('istioSidecar') == True
+    assert 'ports' in service_details.get('service')
+    assert 'labels' in service_details.get('service')
     assert 'workloads' in service_details
     assert 'service' in service_details
+    assert 'endpoints' in service_details
+    assert 'virtualServices' in service_details
+    assert 'destinationRules' in service_details
+    assert 'health' in service_details
 
 def test_service_metrics_endpoint(kiali_client):
     bookinfo_namespace = conftest.get_bookinfo_endpoint()
 
-    service_metrics = kiali_client.service_metrics(namespace=bookinfo_namespace, service=SERVICE_TO_VALIDATE).get('dest')
-    assert 'metrics' in service_metrics
+    service = kiali_client.service_metrics(namespace=bookinfo_namespace, service=SERVICE_TO_VALIDATE)
+    for direction in ['dest', 'source']:
+      assert service != None
+
+      metrics = service.get(direction).get('metrics')
+      assert 'request_count_in' in metrics
+      assert 'request_error_count_in' in metrics
+      assert 'tcp_received_in' in metrics
+      assert 'tcp_sent_in' in metrics
+
+      histograms = service.get(direction).get('histograms')
+      assert 'request_duration_in' in histograms
+      assert 'request_size_in' in histograms
+      assert 'response_size_in' in histograms
 
 def test_service_health_endpoint(kiali_client):
     bookinfo_namespace = conftest.get_bookinfo_endpoint()
 
     service_health = kiali_client.service_health(namespace=bookinfo_namespace, service=SERVICE_TO_VALIDATE)
     assert service_health != None
+
     envoy = service_health.get('envoy')
     assert envoy != None
     assert 'inbound' in envoy
     assert 'outbound' in envoy
+
+    assert 'requests' in service_health
 
 def test_service_validations_endpoint(kiali_client):
     bookinfo_namespace = conftest.get_bookinfo_endpoint()
@@ -43,4 +72,3 @@ def test_service_validations_endpoint(kiali_client):
     service_validations = kiali_client.service_validations(namespace=bookinfo_namespace, service=SERVICE_TO_VALIDATE)
     assert service_validations != None
     assert len(service_validations.get('pod')) > 0
-
