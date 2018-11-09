2c31e7004e05a99ec56014dd21068c4d8cbb04e1
John Mazzitelli <mazz@redhat.com>
2018-11-09 16:55:35 -0500

    be able to ask for your own credentials so "admin/admin" is not necessarily the default. pass in empty string for KIALI_USERNAME and KIALI_PASSWORD to have the script ask the user for them.

--- deploy/openshift/deploy-kiali-to-openshift.sh
+++ deploy/openshift/deploy-kiali-to-openshift.sh
@@ -22,6 +22,23 @@ JAEGER_URL="${JAEGER_URL:-http://jaeger-query-istio-system.127.0.0.1.nip.io}"
 GRAFANA_URL="${GRAFANA_URL:-http://grafana-istio-system.127.0.0.1.nip.io}"
 VERBOSE_MODE="${VERBOSE_MODE:-3}"
 
+# the "=" inside ${} is on purpose - if the envvar is explicitly set to empty string, it will stay that way
+KIALI_USERNAME="${KIALI_USERNAME=admin}"
+KIALI_PASSPHRASE="${KIALI_PASSPHRASE=admin}"
+
+# If the username or passphrase is empty, we are being told that we need to ask the user
+if [ "$KIALI_USERNAME" == "" ]; then
+  KIALI_USERNAME=$(read -p 'What do you want to use for the Kiali Username: ' val && echo -n $val)
+fi
+if [ "$KIALI_PASSPHRASE" == "" ]; then
+  KIALI_PASSPHRASE=$(read -sp 'What do you want to use for the Kiali Passphrase: ' val && echo -n $val)
+  echo
+fi
+
+# base64 encode the credentials
+KIALI_USERNAME_BASE64="$(echo -n ${KIALI_USERNAME} | base64)"
+KIALI_PASSPHRASE_BASE64="$(echo -n ${KIALI_PASSPHRASE} | base64)"
+
 # Make sure we have access to all required tools
 
 if which 'oc' > /dev/null 2>&1 ; then
@@ -48,7 +65,7 @@ YAML_DIR=$(dirname $(readlink -f "$0"))
 
 echo Deploying Kiali to OpenShift project ${NAMESPACE}
 cat ${YAML_DIR}/configmap.yaml | VERSION_LABEL=${VERSION_LABEL} JAEGER_URL=${JAEGER_URL} GRAFANA_URL=${GRAFANA_URL}ISTIO_NAMESPACE=${NAMESPACE}  envsubst | oc create -n ${NAMESPACE} -f -
-cat ${YAML_DIR}/secret.yaml | VERSION_LABEL=${VERSION_LABEL} envsubst | oc create -n ${NAMESPACE} -f -
+cat ${YAML_DIR}/secret.yaml | VERSION_LABEL=${VERSION_LABEL} KIALI_USERNAME_BASE64=${KIALI_USERNAME_BASE64} KIALI_PASSPHRASE_BASE64=${KIALI_PASSPHRASE_BASE64} envsubst | oc create -n ${NAMESPACE} -f -
 cat ${YAML_DIR}/serviceaccount.yaml | VERSION_LABEL=${VERSION_LABEL} envsubst | oc create -n ${NAMESPACE} -f -
 cat ${YAML_DIR}/service.yaml | VERSION_LABEL=${VERSION_LABEL} envsubst | oc create -n ${NAMESPACE} -f -
 cat ${YAML_DIR}/route.yaml | VERSION_LABEL=${VERSION_LABEL} envsubst | oc create -n ${NAMESPACE} -f -
