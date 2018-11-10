29233476ac2fbb26da69abdca7e59443cd462190
John Mazzitelli <mazz@redhat.com>
2018-11-10 09:58:36 -0500

    stream from github directly rather than write the files. this is going to help when we want to use this script for a one-command-installer

--- deploy/openshift/deploy-kiali-to-openshift.sh
+++ deploy/openshift/deploy-kiali-to-openshift.sh
@@ -18,16 +18,8 @@
 # is installed and in your PATH.
 ##############################################################################
 
-IMAGE_NAME="${IMAGE_NAME:-kiali/kiali}"
-IMAGE_VERSION="${IMAGE_VERSION:-v0.9.0}"
-VERSION_LABEL="${VERSION_LABEL:-$IMAGE_VERSION}"
-IMAGE_PULL_POLICY_TOKEN="${IMAGE_PULL_POLICY_TOKEN:-imagePullPolicy: Always}"
-NAMESPACE="${NAMESPACE:-istio-system}"
-JAEGER_URL="${JAEGER_URL:-http://jaeger-query-istio-system.127.0.0.1.nip.io}"
-GRAFANA_URL="${GRAFANA_URL:-http://grafana-istio-system.127.0.0.1.nip.io}"
-VERBOSE_MODE="${VERBOSE_MODE:-3}"
-
-# the "=" inside ${} is on purpose - if the envvar is explicitly set to empty string, it will stay that way
+# Ask the user for the credentials if necessary.
+# (note: the "=" inside ${} is on purpose - if the envvar is explicitly empty, it will stay that way)
 KIALI_USERNAME="${KIALI_USERNAME=admin}"
 KIALI_PASSPHRASE="${KIALI_PASSPHRASE=admin}"
 
@@ -40,9 +32,17 @@ if [ "$KIALI_PASSPHRASE" == "" ]; then
   echo
 fi
 
-# base64 encode the credentials
-KIALI_USERNAME_BASE64="$(echo -n ${KIALI_USERNAME} | base64)"
-KIALI_PASSPHRASE_BASE64="$(echo -n ${KIALI_PASSPHRASE} | base64)"
+export IMAGE_NAME="${IMAGE_NAME:-kiali/kiali}"
+export IMAGE_VERSION="${IMAGE_VERSION:-v0.9.0}"
+export VERSION_LABEL="${VERSION_LABEL:-$IMAGE_VERSION}"
+export IMAGE_PULL_POLICY_TOKEN="${IMAGE_PULL_POLICY_TOKEN:-imagePullPolicy: Always}"
+export NAMESPACE="${NAMESPACE:-istio-system}"
+export ISTIO_NAMESPACE="${ISTIO_NAMESPACE:-$NAMESPACE}"
+export JAEGER_URL="${JAEGER_URL:-http://jaeger-query-istio-system.127.0.0.1.nip.io}"
+export GRAFANA_URL="${GRAFANA_URL:-http://grafana-istio-system.127.0.0.1.nip.io}"
+export VERBOSE_MODE="${VERBOSE_MODE:-3}"
+export KIALI_USERNAME_BASE64="$(echo -n ${KIALI_USERNAME} | base64)"
+export KIALI_PASSPHRASE_BASE64="$(echo -n ${KIALI_PASSPHRASE} | base64)"
 
 # Make sure we have access to all required tools
 
@@ -64,33 +64,24 @@ fi
 # It is assumed the yaml files are in the same location as this script.
 # Figure out where that is using a method that is valid for bash and sh.
 
-YAML_DIR=$(dirname $(readlink -f "$0"))
-
-# If we are missing one or more of the yaml files, pull them down
-
-for yaml in configmap secret serviceaccount service route deployment clusterrole clusterrolebinding ingress
-do
-  yaml_file="${yaml}.yaml"
-  if [ ! -f "${YAML_DIR}/${yaml_file}" ]; then
-    yaml_url="https://raw.githubusercontent.com/kiali/kiali/${VERSION_LABEL}/deploy/openshift/${yaml_file}"
-    echo "Downloading ${yaml_file} from ${yaml_url}"
-    curl -s -f -o "${YAML_DIR}/${yaml_file}" $yaml_url
-    if [ ! -f "${YAML_DIR}/${yaml_file}" ]; then
-      echo "Missig YAML file ${yaml_file} and cannot download it."
-      exit 1
-    fi
-  fi
-done
+YAML_DIR=${YAML_DIR:-$(dirname $(readlink -f "$0"))}
 
 # Now deploy all the Kiali components to OpenShift
-
+# If we are missing one or more of the yaml files, download them
 echo Deploying Kiali to OpenShift project ${NAMESPACE}
-cat ${YAML_DIR}/configmap.yaml | VERSION_LABEL=${VERSION_LABEL} JAEGER_URL=${JAEGER_URL} GRAFANA_URL=${GRAFANA_URL}ISTIO_NAMESPACE=${NAMESPACE}  envsubst | oc create -n ${NAMESPACE} -f -
-cat ${YAML_DIR}/secret.yaml | VERSION_LABEL=${VERSION_LABEL} KIALI_USERNAME_BASE64=${KIALI_USERNAME_BASE64} KIALI_PASSPHRASE_BASE64=${KIALI_PASSPHRASE_BASE64} envsubst | oc create -n ${NAMESPACE} -f -
-cat ${YAML_DIR}/serviceaccount.yaml | VERSION_LABEL=${VERSION_LABEL} envsubst | oc create -n ${NAMESPACE} -f -
-cat ${YAML_DIR}/service.yaml | VERSION_LABEL=${VERSION_LABEL} envsubst | oc create -n ${NAMESPACE} -f -
-cat ${YAML_DIR}/route.yaml | VERSION_LABEL=${VERSION_LABEL} envsubst | oc create -n ${NAMESPACE} -f -
-cat ${YAML_DIR}/deployment.yaml | IMAGE_NAME=${IMAGE_NAME} IMAGE_VERSION=${IMAGE_VERSION} NAMESPACE=${NAMESPACE} VERSION_LABEL=${VERSION_LABEL} VERBOSE_MODE=${VERBOSE_MODE} IMAGE_PULL_POLICY_TOKEN=${IMAGE_PULL_POLICY_TOKEN} envsubst |oc create -n ${NAMESPACE} -f -
-cat ${YAML_DIR}/clusterrole.yaml | VERSION_LABEL=${VERSION_LABEL} envsubst | oc create -n ${NAMESPACE} -f -
-cat ${YAML_DIR}/clusterrolebinding.yaml | VERSION_LABEL=${VERSION_LABEL} NAMESPACE=${NAMESPACE} envsubst | oc create -n ${NAMESPACE} -f -
-cat ${YAML_DIR}/ingress.yaml | VERSION_LABEL=${VERSION_LABEL} envsubst | oc create -n ${NAMESPACE} -f -
+for yaml in secret configmap serviceaccount clusterrole clusterrolebinding deployment service route ingress
+do
+  yaml_file="${YAML_DIR}/${yaml}.yaml"
+  if [ -f "${yaml_file}" ]; then
+    echo "Using YAML file: ${yaml_file}"
+    cat ${yaml_file} | envsubst | oc create -n ${NAMESPACE} -f -
+  else
+    yaml_url="https://raw.githubusercontent.com/kiali/kiali/${VERSION_LABEL}/deploy/openshift/${yaml_file}"
+    echo "Using YAML downloaded from: ${yaml_url}"
+    curl ${yaml_url} | envsubst | oc create -n ${NAMESPACE} -f -
+  fi
+  if [ "$?" != "0" ]; then
+    echo "Failed to deploy to OpenShift. Aborting."
+    exit 1
+  fi
+done
