10b84940cb6c4da837834a014b64351aa67589e1
John Mazzitelli <mazz@redhat.com>
2018-11-11 08:11:40 -0500

    tweek script - mainly to prefix "ERROR:" to error messages.

--- deploy/openshift/deploy-kiali-to-openshift.sh
+++ deploy/openshift/deploy-kiali-to-openshift.sh
@@ -21,7 +21,7 @@
 # The credentials can be specified either as already base64 encoded, or in plain text.
 # If the username or passphrase plain text variable is set but empty, the user will be asked for a value.
 if [ "${KIALI_USERNAME_BASE64}" == "" ]; then
-  KIALI_USERNAME="${KIALI_USERNAME=admin}" # (note: the "=" inside ${} is on purpose
+  KIALI_USERNAME="${KIALI_USERNAME=admin}" # note: the "=" inside ${} is on purpose
   if [ "$KIALI_USERNAME" == "" ]; then
     KIALI_USERNAME=$(read -p 'What do you want to use for the Kiali Username: ' val && echo -n $val)
   fi
@@ -29,7 +29,7 @@ if [ "${KIALI_USERNAME_BASE64}" == "" ]; then
 fi
 
 if [ "${KIALI_PASSPHRASE_BASE64}" == "" ]; then
-  KIALI_PASSPHRASE="${KIALI_PASSPHRASE=admin}" # (note: the "=" inside ${} is on purpose
+  KIALI_PASSPHRASE="${KIALI_PASSPHRASE=admin}" # note: the "=" inside ${} is on purpose
   if [ "$KIALI_PASSPHRASE" == "" ]; then
     KIALI_PASSPHRASE=$(read -sp 'What do you want to use for the Kiali Passphrase: ' val && echo -n $val)
     echo
@@ -54,14 +54,14 @@ export KIALI_PASSPHRASE_BASE64
 if which 'oc' > /dev/null 2>&1 ; then
   echo "oc is here: $(which oc)"
 else
-  echo "You do not have 'oc' in your PATH. Please install it and retry."
+  echo "ERROR: You do not have 'oc' in your PATH. Please install it and retry."
   exit 1
 fi
 
 if which 'envsubst' > /dev/null 2>&1 ; then
   echo "envsubst is here: $(which envsubst)"
 else
-  echo "You do not have 'envsubst' in your PATH. Please install it and retry."
+  echo "ERROR: You do not have 'envsubst' in your PATH. Please install it and retry."
   echo "If you are on MacOS, you can get this by installing the gettext package"
   exit 1
 fi
@@ -92,7 +92,7 @@ YAML_DIR=${YAML_DIR:-$(dirname $(readlink -f "$0"))}
 
 # Now deploy all the Kiali components to OpenShift
 # If we are missing one or more of the yaml files, download them
-echo Deploying Kiali to OpenShift project ${NAMESPACE}
+echo "Deploying Kiali to OpenShift project ${NAMESPACE}"
 for yaml in secret configmap serviceaccount clusterrole clusterrolebinding deployment service route ingress
 do
   yaml_file="${yaml}.yaml"
@@ -107,7 +107,7 @@ do
     ${downloader} ${yaml_url} | envsubst | oc create -n ${NAMESPACE} -f -
   fi
   if [ "$?" != "0" ]; then
-    echo "Failed to deploy to OpenShift. Aborting."
+    echo "ERROR: Failed to deploy to OpenShift. Aborting."
     exit 1
   fi
 done
