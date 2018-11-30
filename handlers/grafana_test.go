cb6251e7db53ee213442904ebae885984e1e794b
Joel Takvorian <jtakvori@redhat.com>
2018-11-30 10:49:31 +0100

    KIALI-2048 Don't panic on Grafana error
    - Better handling of grafana errors & unmarshalling
    - Propagate error message
    - More tests

--- handlers/grafana_test.go
+++ handlers/grafana_test.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"encoding/json"
 	"net/http"
 	"testing"
 
@@ -9,6 +10,16 @@ import (
 
 	"github.com/kiali/kiali/config"
 )
+
+var dashboard = []map[string]interface{}{
+	map[string]interface{}{
+		"url": "the_path",
+	},
+}
+
+var anError = map[string]string{
+	"message": "unauthorized",
+}
 
 func TestGetGrafanaInfoDisabled(t *testing.T) {
 	conf := config.NewConfig()
@@ -19,9 +30,7 @@ func TestGetGrafanaInfoDisabled(t *testing.T) {
 			ClusterIP: "fromservice",
 			Ports: []v1.ServicePort{
 				v1.ServicePort{Port: 3000}}}, nil
-	}, func(_, _ string) (string, error) {
-		return "/dash", nil
-	})
+	}, buildDashboardSupplier(dashboard, 200))
 	assert.Nil(t, err)
 	assert.Equal(t, http.StatusNoContent, code)
 	assert.Nil(t, info)
@@ -36,12 +45,11 @@ func TestGetGrafanaInfoFromConfig(t *testing.T) {
 			ExternalIPs: []string{"fromservice"},
 			Ports: []v1.ServicePort{
 				v1.ServicePort{Port: 3000}}}, nil
-	}, func(_, _ string) (string, error) {
-		return "/dash", nil
-	})
+	}, buildDashboardSupplier(dashboard, 200))
 	assert.Nil(t, err)
 	assert.Equal(t, http.StatusOK, code)
 	assert.Equal(t, "http://fromconfig:3001", info.URL)
+	assert.Equal(t, "the_path", info.WorkloadDashboardPath)
 }
 
 func TestGetGrafanaInfoNoExternalIP(t *testing.T) {
@@ -52,9 +60,42 @@ func TestGetGrafanaInfoNoExternalIP(t *testing.T) {
 			ExternalIPs: []string{},
 			Ports: []v1.ServicePort{
 				v1.ServicePort{Port: 3000}}}, nil
-	}, func(_, _ string) (string, error) {
-		return "/dash", nil
-	})
+	}, buildDashboardSupplier(dashboard, 200))
 	assert.NotNil(t, err)
 	assert.Equal(t, http.StatusServiceUnavailable, code)
+}
+
+func TestGetGrafanaInfoGetError(t *testing.T) {
+	conf := config.NewConfig()
+	conf.ExternalServices.Grafana.URL = "http://fromconfig:3001"
+	config.Set(conf)
+	_, code, err := getGrafanaInfo(func(_, _ string) (*v1.ServiceSpec, error) {
+		return &v1.ServiceSpec{
+			ExternalIPs: []string{"fromservice"},
+			Ports: []v1.ServicePort{
+				v1.ServicePort{Port: 3000}}}, nil
+	}, buildDashboardSupplier(anError, 401))
+	assert.Equal(t, "Error from Grafana (401): unauthorized", err.Error())
+	assert.Equal(t, 500, code)
+}
+
+func TestGetGrafanaInfoInvalidDashboard(t *testing.T) {
+	conf := config.NewConfig()
+	conf.ExternalServices.Grafana.URL = "http://fromconfig:3001"
+	config.Set(conf)
+	_, code, err := getGrafanaInfo(func(_, _ string) (*v1.ServiceSpec, error) {
+		return &v1.ServiceSpec{
+			ExternalIPs: []string{"fromservice"},
+			Ports: []v1.ServicePort{
+				v1.ServicePort{Port: 3000}}}, nil
+	}, buildDashboardSupplier("unexpected response", 200))
+	assert.NotNil(t, err)
+	assert.Equal(t, 500, code)
+}
+
+func buildDashboardSupplier(jSon interface{}, code int) dashboardSupplier {
+	return func(_, _ string) ([]byte, int, error) {
+		bytes, err := json.Marshal(jSon)
+		return bytes, code, err
+	}
 }
