2dd85b32370eb35d9f474af3a34c2a8deb26fc4d
Jay Shaughnessy <jshaughn@users.noreply.github.com>
2018-12-19 16:20:29 -0500

    KIALI-2172 Fix error traffic aggregation in service graph reducer (#751)
    * KIALI-2172 Fix error traffic aggregation in service graph reducer
    Now aggregates the 3xx/4xx/5xx edge traffic rate and not just the total.
    - added a test
    - Instead of adding 2xx aggregation in the service graph reducer I removed it from the edge metadata as it isn't needed.

--- handlers/graph.go
+++ handlers/graph.go
@@ -264,6 +264,15 @@ func addServiceGraphTraffic(target, source *graph.Edge) {
 	switch protocol {
 	case "http":
 		addToMetadataValue(target.Metadata, "rate", source.Metadata["rate"].(float64))
+		if val, ok := source.Metadata["rate3xx"]; ok {
+			addToMetadataValue(target.Metadata, "rate3xx", val.(float64))
+		}
+		if val, ok := source.Metadata["rate4xx"]; ok {
+			addToMetadataValue(target.Metadata, "rate4xx", val.(float64))
+		}
+		if val, ok := source.Metadata["rate5xx"]; ok {
+			addToMetadataValue(target.Metadata, "rate5xx", val.(float64))
+		}
 	case "tcp":
 		addToMetadataValue(target.Metadata, "tcpSentRate", source.Metadata["tcpSentRate"].(float64))
 	default:
@@ -473,10 +482,10 @@ func addHttpTraffic(trafficMap graph.TrafficMap, val float64, code, sourceWlNs, 
 		handleMisconfiguredLabels(dest, destApp, destVer, val, o)
 	}
 
+	// note, we don't track 2xx because it's not used downstream and can be easily
+	// calculated: 2xx = (rate - 3xx - 4xx - 5xx)
 	var ck string
 	switch {
-	case strings.HasPrefix(string(code), "2"):
-		ck = "rate2xx"
 	case strings.HasPrefix(string(code), "3"):
 		ck = "rate3xx"
 	case strings.HasPrefix(string(code), "4"):
@@ -484,12 +493,15 @@ func addHttpTraffic(trafficMap graph.TrafficMap, val float64, code, sourceWlNs, 
 	case strings.HasPrefix(string(code), "5"):
 		ck = "rate5xx"
 	}
-	addToMetadataValue(edge.Metadata, ck, val)
-	addToMetadataValue(edge.Metadata, "rate", val)
-
 	addToMetadataValue(source.Metadata, "rateOut", val)
-	addToMetadataValue(dest.Metadata, ck, val)
+
+	addToMetadataValue(edge.Metadata, "rate", val)
 	addToMetadataValue(dest.Metadata, "rate", val)
+
+	if ck != "" {
+		addToMetadataValue(edge.Metadata, ck, val)
+		addToMetadataValue(dest.Metadata, ck, val)
+	}
 
 	return source, dest
 }
