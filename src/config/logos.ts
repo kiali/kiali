d715c12bcb217ceba0f19e7e8d204e422ae456e1
Joel Takvorian <joel.takvorian@qaraywa.net>
2019-03-18 12:24:49 +0100

    KIALI-2340 Add Go logo (#1048)

--- src/config/logos.ts
+++ src/config/logos.ts
@@ -7,5 +7,6 @@ export const kialiLogo = require('../assets/img/logo-alt.svg');
 export const runtimesLogoProviders = {
   'Vert.x': () => require('../assets/img/vertx-logo.png'),
   'Node.js': () => require('../assets/img/nodejs-logo.png'),
-  Thorntail: () => require('../assets/img/thorntail-logo.png')
+  Thorntail: () => require('../assets/img/thorntail-logo.png'),
+  Go: () => require('../assets/img/go-logo.png')
 };
