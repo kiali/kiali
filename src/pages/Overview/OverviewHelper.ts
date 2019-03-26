795657b4c804a1d00b691be27e2cb35eb25ab536
Xavier Canal i Masjuan <xeviknal@gmail.com>
2019-03-26 15:35:59 +0100

    Fixing circular import

--- src/pages/Overview/OverviewHelper.ts
+++ src/pages/Overview/OverviewHelper.ts
@@ -1,43 +1,5 @@
 import { OverviewType } from './OverviewToolbar';
-import { FilterSelected } from '../../components/Filters/StatefulFilters';
-import { FiltersAndSorts } from './FiltersAndSorts';
-import { FAILURE, DEGRADED, HEALTHY } from '../../types/Health';
 
 export const switchType = <T, U, V>(type: OverviewType, caseApp: T, caseService: U, caseWorkload: V): T | U | V => {
   return type === 'app' ? caseApp : type === 'service' ? caseService : caseWorkload;
-};
-
-export const summarizeHealthFilters = () => {
-  const healthFilters = FilterSelected.getSelected().filter(f => f.category === FiltersAndSorts.healthFilter.title);
-  if (healthFilters.length === 0) {
-    return {
-      noFilter: true,
-      showInError: true,
-      showInWarning: true,
-      showInSuccess: true
-    };
-  }
-  let showInError = false,
-    showInWarning = false,
-    showInSuccess = false;
-  healthFilters.forEach(f => {
-    switch (f.value) {
-      case FAILURE.name:
-        showInError = true;
-        break;
-      case DEGRADED.name:
-        showInWarning = true;
-        break;
-      case HEALTHY.name:
-        showInSuccess = true;
-        break;
-      default:
-    }
-  });
-  return {
-    noFilter: false,
-    showInError: showInError,
-    showInWarning: showInWarning,
-    showInSuccess: showInSuccess
-  };
 };
