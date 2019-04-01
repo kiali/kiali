50598dd15594ba2c67e8952491512186ee84f8c0
Joel Takvorian <joel.takvorian@qaraywa.net>
2019-04-01 10:11:47 +0200

    KIALI-2488 Add dashboards logos (#1098)
    * KIALI-2488 Add dashboards logos
    
    (JVM and MicroProfile)
    
    * Update snapshot

--- src/config/logos.ts
+++ src/config/logos.ts
@@ -8,5 +8,7 @@ export const runtimesLogoProviders = {
   'Vert.x': () => require('../assets/img/vertx-logo.png'),
   'Node.js': () => require('../assets/img/nodejs-logo.png'),
   Thorntail: () => require('../assets/img/thorntail-logo.png'),
-  Go: () => require('../assets/img/go-logo.png')
+  Go: () => require('../assets/img/go-logo.png'),
+  MicroProfile: () => require('../assets/img/microprofile-logo.png'),
+  JVM: () => require('../assets/img/java-logo.png')
 };
