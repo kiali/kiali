128891ae819930eb3dd1463808c1f863ba229536
mattmahoneyrh <mmahoney@redhat.com>
2019-04-11 16:41:10 -0400

    KIALI-2648 - Login Strategies

--- tests/e2e/tests/test_kiali_configmap.py
+++ tests/e2e/tests/test_kiali_configmap.py
@@ -1,7 +1,10 @@
 import pytest
 import tests.conftest as conftest
-from utils.command_exec import command_exec
+import time
+from subprocess import PIPE, Popen
 from kiali import KialiClient
+from utils.command_exec import command_exec
+from utils.timeout import timeout
 
 
 STRATEGY_LOGIN = 'login'
@@ -14,7 +17,7 @@ AUTH_NOAUTH = 'no-auth'
 STRATEGY_LIST = ['login', 'anonymous', 'openshift']
 WEB_ROOT_LIST = ['/']
 
-def __test_auth_anonymous():
+def test_auth_anonymous():
     try:
         assert change_configmap_with_new_value(element_name='strategy:', list=STRATEGY_LIST,
             new_value=STRATEGY_ANONYMOUS, current_configmap_file=conftest.CURRENT_CONFIGMAP_FILE,
@@ -26,6 +29,43 @@ def __test_auth_anonymous():
         # Return Auth strategy back to 'login'
         create_configmap_and_wait_for_kiali(conftest.CURRENT_CONFIGMAP_FILE)
 
+def test_auth_openshift():
+
+    kiali_hostname = conftest.get_kiali_hostname()
+    cookie_file = "./tmp_cookie_file"
+
+    try:
+        assert change_configmap_with_new_value(element_name='strategy:', list=STRATEGY_LIST,
+            new_value=STRATEGY_OPENSHIFT, current_configmap_file=conftest.CURRENT_CONFIGMAP_FILE,
+            new_configmap_file=conftest.NEW_CONFIG_MAP_FILE)
+
+        # Create token cookie file
+        cmd = "curl -v -k POST -c {} -d 'access_token='$(oc whoami -t)'&expires_in=86400&scope=user%3Afull&token_type=Bearer' https://{}/api/authenticate".format(cookie_file,  kiali_hostname)
+        with timeout(seconds=120, error_message='Timed out waiting getting token'):
+            while True:
+                stdout, stderr = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE).communicate()
+                if 'username' in stdout.decode():
+                    break
+
+                time.sleep(2)
+
+        # Make the API request using token cookie
+        cmd = "curl -v -k -b {} https://{}/api/namespaces".format(cookie_file, kiali_hostname)
+        with timeout(seconds=120, error_message='Timed out waiting getting token'):
+            while True:
+                stdout, stderr = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE).communicate()
+                if "istio-system" in stdout.decode():
+                    break
+
+                time.sleep(2)
+
+        cmd = "rm -f {}".format(cookie_file)
+        Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE).communicate()
+
+    finally:
+        # Return Auth strategy back to 'login'
+         create_configmap_and_wait_for_kiali(conftest.CURRENT_CONFIGMAP_FILE)
+
 def __test_change_web_root(kiali_client):
     new_web_root_value = '/e2e'
 
@@ -34,8 +74,13 @@ def __test_change_web_root(kiali_client):
             new_value=new_web_root_value, current_configmap_file=conftest.CURRENT_CONFIGMAP_FILE,
             new_configmap_file=conftest.NEW_CONFIG_MAP_FILE)
 
-        assert kiali_client.request(plain_url="e2e/api/namespaces", path=None, params=None)
+        with timeout(seconds=180, error_message='Timed out waiting for API call'):
+            while True:
+                response = kiali_client.request(plain_url=new_web_root_value + "/api/namespaces", path=None, params=None)
+                if response.status_code == 200:
+                    break
 
+                time.sleep(2)
     finally:
         # Return web_root back to '/'
         create_configmap_and_wait_for_kiali(conftest.CURRENT_CONFIGMAP_FILE)
@@ -43,16 +88,19 @@ def __test_change_web_root(kiali_client):
 ##
 
 def do_auth_strategy_test(auth_type):
-    config = conftest.__get_environment_config__(conftest.ENV_FILE)
-    swagger = config.get('kiali_swagger_address')
-    hostname = config.get('kiali_hostname')
+    swagger = conftest.get_kiali_swagger_address()
+    hostname = conftest.get_kiali_hostname()
 
     if AUTH_NOAUTH in auth_type:
-        kiali_client = KialiClient(hostname=hostname, auth_type=AUTH_NOAUTH, verify=False, swagger_address=swagger)
-        response = kiali_client.request(method_name='namespaceList', path=None, params=None)
 
-        # Make API call - expected to pass with no authentication
-        assert response.status_code == 200, "Unexpected status code \'response.status_code\'"
+        with timeout(seconds=180, error_message='Timed out waiting for API call to complete'):
+            while True:
+                kiali_client = KialiClient(hostname=hostname, auth_type=AUTH_NOAUTH, verify=False, swagger_address=swagger)
+                response = kiali_client.request(method_name='namespaceList', path=None, params=None)
+                if response.status_code == 200:
+                    break
+
+                time.sleep(2)
     else:
         assert False, "To Do"
 
