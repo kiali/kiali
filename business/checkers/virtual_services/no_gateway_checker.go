2eac4e24fbdf2a2f05d28c8075eb9c42454ff9b8
Michael Burman <miburman@redhat.com>
2019-05-29 18:01:51 +0300

    KIALI-2947 Fix detection of valid+false pair of gateways in VS

--- business/checkers/virtual_services/no_gateway_checker.go
+++ business/checkers/virtual_services/no_gateway_checker.go
@@ -2,7 +2,9 @@ package virtual_services
 
 import (
 	"strconv"
+	"strings"
 
+	"github.com/kiali/kiali/config"
 	"github.com/kiali/kiali/kubernetes"
 	"github.com/kiali/kiali/models"
 )
@@ -16,12 +18,45 @@ type NoGatewayChecker struct {
 func (s NoGatewayChecker) Check() ([]*models.IstioCheck, bool) {
 	validations := make([]*models.IstioCheck, 0)
 
-	valid, index := kubernetes.ValidateVirtualServiceGateways(s.VirtualService.GetSpec(), s.GatewayNames, s.VirtualService.GetObjectMeta().Namespace, s.VirtualService.GetObjectMeta().ClusterName)
-	if !valid {
-		path := "spec/gateways[" + strconv.Itoa(index) + "]"
-		validation := models.Build("virtualservices.nogateway", path)
-		validations = append(validations, &validation)
-	}
+	s.ValidateVirtualServiceGateways(s.VirtualService.GetSpec(), s.VirtualService.GetObjectMeta().Namespace, s.VirtualService.GetObjectMeta().ClusterName, &validations)
 
-	return validations, valid
+	return validations, len(validations) == 0
+}
+
+// ValidateVirtualServiceGateways checks all VirtualService gateways (except mesh, which is reserved word) and checks that they're found from the given list of gatewayNames. Also return index of missing gatways to show clearer error path in editor
+func (s NoGatewayChecker) ValidateVirtualServiceGateways(spec map[string]interface{}, namespace, clusterName string, validations *[]*models.IstioCheck) {
+	if clusterName == "" {
+		clusterName = config.Get().ExternalServices.Istio.IstioIdentityDomain
+	}
+	if gatewaysSpec, found := spec["gateways"]; found {
+		if gateways, ok := gatewaysSpec.([]interface{}); ok {
+		GatewaySearch:
+			for index, g := range gateways {
+				if gate, ok := g.(string); ok {
+					if gate == "mesh" {
+						continue GatewaySearch
+					}
+					var hostname string
+					if strings.Contains(gate, "/") {
+						parts := strings.Split(gate, "/")
+						hostname = kubernetes.Host{
+							Service:   parts[1],
+							Namespace: parts[0],
+							Cluster:   clusterName,
+						}.String()
+					} else {
+						hostname = kubernetes.ParseHost(gate, namespace, clusterName).String()
+					}
+					for gw := range s.GatewayNames {
+						if found := kubernetes.FilterByHost(hostname, gw, namespace); found {
+							continue GatewaySearch
+						}
+					}
+					path := "spec/gateways[" + strconv.Itoa(index) + "]"
+					validation := models.Build("virtualservices.nogateway", path)
+					*validations = append(*validations, &validation)
+				}
+			}
+		}
+	}
 }
