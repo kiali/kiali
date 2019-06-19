5d354057c135a4431339565faee2eccd7084df61
Guilherme Baufaker Rêgo <gbaufake@redhat.com>
2019-06-19 11:16:36 +0200

    Adding Tests on Kiali Operator

--- operator/molecule/test-auth/tests/test_auth.py
+++
@@ -1,8 +0,0 @@
-import os
-import testinfra.utils.ansible_runner
-testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
-
-
-def test_sos_report_package(host):
-    print(host)
