47178f5f02a959209f76096db8a3085ba6091ed1
Xavier Canal i Masjuan <xeviknal@gmail.com>
2019-10-09 11:26:05 +0200

    Migrate DebugInformation modal to PF4 (#1455)
    * Migrate DebugInformation modal to PF4 - WIP
    
    * Adding background transparent to inline alert

--- src/components/DebugInformation/DebugInformation.tsx
+++ src/components/DebugInformation/DebugInformation.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
-import { Modal, Icon, Button, Alert } from 'patternfly-react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { style } from 'typestyle';
 import _ from 'lodash';
@@ -11,6 +10,7 @@ import { serverConfig } from '../../config';
 import { ComputedServerConfig } from '../../config/ServerConfig';
 import { AuthConfig } from '../../types/Auth';
 import { KialiAppState } from '../../store/Store';
+import { Alert, AlertActionCloseButton, AlertVariant, Button, ButtonVariant, Modal } from '@patternfly/react-core';
 
 enum CopyStatus {
   NOT_COPIED, // We haven't copied the current output
@@ -106,41 +106,40 @@ export class DebugInformation extends React.PureComponent<DebugInformationProps,
     }
 
     return (
-      <Modal show={this.state.show} onHide={this.close}>
-        <Modal.Header>
-          <button className="close" onClick={this.close} aria-hidden="true" aria-label="Close">
-            <Icon type="pf" name="close" />
-          </button>
-          <Modal.Title>Debug information</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          {this.state.copyStatus === CopyStatus.COPIED && (
-            <Alert type="success" onDismiss={this.hideAlert}>
-              Debug information has been copied to your clipboard.
-            </Alert>
-          )}
-          {this.state.copyStatus === CopyStatus.OLD_COPY && (
-            <Alert type="warning" onDismiss={this.hideAlert}>
-              Debug information was copied to your clipboard, but is outdated now. It could be caused by new data
-              received by auto refresh timers.
-            </Alert>
-          )}
-          <span>Please include this information when opening a bug.</span>
+      <Modal
+        isSmall={true}
+        isOpen={this.state.show}
+        onClose={this.close}
+        title="Debug information"
+        actions={[
+          <Button onClick={this.close}>Close</Button>,
           <CopyToClipboard onCopy={this.copyCallback} text={renderDebugInformation()} options={copyToClipboardOptions}>
-            <textarea
-              ref={this.textareaRef}
-              className={textAreaStyle}
-              readOnly={true}
-              value={renderDebugInformation()}
-            />
+            <Button variant={ButtonVariant.primary}>Copy</Button>
           </CopyToClipboard>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={this.close}>Close</Button>
-          <CopyToClipboard onCopy={this.copyCallback} text={renderDebugInformation()} options={copyToClipboardOptions}>
-            <Button bsStyle="primary">Copy</Button>
-          </CopyToClipboard>
-        </Modal.Footer>
+        ]}
+      >
+        {this.state.copyStatus === CopyStatus.COPIED && (
+          <Alert
+            style={{ marginBottom: '20px' }}
+            title="Debug information has been copied to your clipboard."
+            variant={AlertVariant.success}
+            isInline={true}
+            action={<AlertActionCloseButton onClose={this.hideAlert} />}
+          />
+        )}
+        {this.state.copyStatus === CopyStatus.OLD_COPY && (
+          <Alert
+            style={{ marginBottom: '20px' }}
+            title="Debug information was copied to your clipboard, but is outdated now. It could be caused by new data received by auto refresh timers."
+            variant={AlertVariant.warning}
+            isInline={true}
+            action={<AlertActionCloseButton onClose={this.hideAlert} />}
+          />
+        )}
+        <span>Please include this information when opening a bug.</span>
+        <CopyToClipboard onCopy={this.copyCallback} text={renderDebugInformation()} options={copyToClipboardOptions}>
+          <textarea ref={this.textareaRef} className={textAreaStyle} readOnly={true} value={renderDebugInformation()} />
+        </CopyToClipboard>
       </Modal>
     );
   }
