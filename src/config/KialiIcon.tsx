12e8588ae94eae52c53daf28f7eaf4f8f4d3da21
Mike Thompson <mtho11@users.noreply.github.com>
2019-10-18 18:08:12 -0700

    Add additional icons to KialiIcon.tsx and make width of icons 10px. (#1499)

--- src/config/KialiIcon.tsx
+++ src/config/KialiIcon.tsx
@@ -13,21 +13,32 @@ import {
   CodeBranchIcon,
   BoltIcon,
   LockIcon,
-  LockOpenIcon
+  LockOpenIcon,
+  BlueprintIcon,
+  AngleDoubleUpIcon,
+  AngleDoubleDownIcon
 } from '@patternfly/react-icons';
+import { style } from 'typestyle';
+
+const iconStyle = style({
+  width: '10px'
+});
 
 export const KialiIcon = {
-  Info: () => <InfoAltIcon />,
-  Ok: () => <OkIcon color={PfColors.Green400} />,
-  Warning: () => <WarningTriangleIcon color={PfColors.Orange400} />,
-  Error: () => <ErrorCircleOIcon color={PfColors.Red100} />,
-  Unknown: () => <UnknownIcon />,
-  Topology: () => <TopologyIcon />,
-  Service: () => <ServiceIcon />,
-  Applications: () => <ApplicationsIcon />,
-  Bundle: () => <BundleIcon />,
-  CircuitBreaker: () => <BoltIcon />,
-  MissingSidecar: () => <CodeBranchIcon />,
-  MtlsLock: () => <LockIcon />,
-  MtlsUnlock: () => <LockOpenIcon />
+  Info: () => <InfoAltIcon className={iconStyle} />,
+  Ok: () => <OkIcon className={iconStyle} color={PfColors.Green400} />,
+  Warning: () => <WarningTriangleIcon className={iconStyle} color={PfColors.Orange400} />,
+  Error: () => <ErrorCircleOIcon className={iconStyle} color={PfColors.Red100} />,
+  Unknown: () => <UnknownIcon className={iconStyle} />,
+  Topology: () => <TopologyIcon className={iconStyle} />,
+  Services: () => <ServiceIcon className={iconStyle} />,
+  Applications: () => <ApplicationsIcon className={iconStyle} />,
+  Workloads: () => <BundleIcon className={iconStyle} />,
+  VirtualService: () => <BlueprintIcon className={iconStyle} />,
+  CircuitBreaker: () => <BoltIcon className={iconStyle} />,
+  MissingSidecar: () => <CodeBranchIcon className={iconStyle} />,
+  MtlsLock: () => <LockIcon className={iconStyle} />,
+  MtlsUnlock: () => <LockOpenIcon className={iconStyle} />,
+  AngleDoubleUp: () => <AngleDoubleUpIcon className={iconStyle} />,
+  AngleDoubleDown: () => <AngleDoubleDownIcon className={iconStyle} />
 };
