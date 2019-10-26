2f65a9e77c97b0fe4c86a11ebfe6d4573201754a
Alberto Gutierrez <aljesusg@gmail.com>
2019-10-26 22:31:45 +0200

    Move localtimes to another style (#1508)

--- src/components/Time/LocalTime.tsx
+++ src/components/Time/LocalTime.tsx
@@ -1,19 +1,52 @@
 import * as React from 'react';
+import { Tooltip } from '@patternfly/react-core';
+import { KialiIcon } from '../../config/KialiIcon';
 
 interface TimeProps {
   time: string;
 }
+
+const monthAbbrs = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+const formatDate = (date: string) => {
+  const mdate = new Date(date);
+  const monthStr = monthAbbrs[mdate.getMonth()];
+
+  let a = 'am';
+  let hours = mdate.getHours();
+  if (hours > 12) {
+    hours -= 12;
+    a = 'pm';
+  }
+
+  const minuteStr = mdate
+    .getMinutes()
+    .toString()
+    .padStart(2, '00');
+  let timeStr = `${hours}:${minuteStr} ${a}`;
+  if (mdate.getFullYear() !== new Date().getFullYear()) {
+    timeStr = `${mdate.getFullYear()} ${timeStr}`;
+  }
+
+  return `${monthStr} ${mdate.getDate()}, ${timeStr}`;
+};
 
 export default class LocalTime extends React.Component<TimeProps> {
   render() {
     let renderedTime: string;
 
     if (this.props.time) {
-      renderedTime = new Date(this.props.time).toLocaleString();
+      renderedTime = formatDate(this.props.time);
     } else {
       renderedTime = '-';
     }
 
-    return renderedTime;
+    return (
+      <Tooltip content={<>{this.props.time}</>}>
+        <span>
+          {KialiIcon.LocalTime({})} {renderedTime}
+        </span>
+      </Tooltip>
+    );
   }
 }
