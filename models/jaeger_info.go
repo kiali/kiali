d45fc4d6522df373421d2b0d95ff95c326cf0e25
Alberto Gutierrez <aljesusg@gmail.com>
2019-11-15 13:07:36 +0100

    Add Jaeger configuration for namespaceSelector (#1801)

--- models/jaeger_info.go
+++ models/jaeger_info.go
@@ -2,5 +2,6 @@ package models
 
 // JaegerInfo provides information to access Jaeger UI
 type JaegerInfo struct {
-	URL string `json:"url"`
+	URL               string `json:"url"`
+	NamespaceSelector bool   `json:"namespace_selector"`
 }
