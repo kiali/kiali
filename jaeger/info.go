57e648033cdfdd21dfb9ec0ac71a0a8bdddaa8a8
Alberto Gutierrez <aljesusg@gmail.com>
2020-01-20 11:36:51 +0100

    Fix 2117-Traces tab should be visible just with InClusterURL configured

--- jaeger/info.go
+++ jaeger/info.go
@@ -1,7 +1,6 @@
 package jaeger
 
 import (
-	"errors"
 	"net/http"
 	"net/url"
 	"path"
@@ -17,29 +16,24 @@ func getJaegerInfo(client http.Client, endpoint *url.URL) (*JaegerInfo, int, err
 		return nil, http.StatusNoContent, nil
 	}
 
-	externalUrl := jaegerConfig.URL
-	if externalUrl == "" {
-		return nil, http.StatusServiceUnavailable, errors.New("Jaeger URL is not set in Kiali configuration")
-	}
-
-	if jaegerConfig.InClusterURL == "" {
-		return nil, http.StatusServiceUnavailable, errors.New("Jaeger URL in cluster is not set in Kiali configuration")
-	}
-
-	u := endpoint
-	u.Path = path.Join(u.Path, "/api/services")
-
-	_, code, err := makeRequest(client, u.String(), nil)
-	if err != nil || code != 200 {
+	u, err := url.Parse(jaegerConfig.InClusterURL)
+	if err != nil {
 		integration = false
-		error = "Error with internal connection with Jaeger"
-		if err != nil {
-			error += ": " + err.Error()
+		error = "Error parsing in cluster url for Jaeger : " + err.Error()
+	} else {
+		u.Path = path.Join(u.Path, "/api/services")
+		_, code, err := makeRequest(client, u.String(), nil)
+		if err != nil || code != 200 {
+			integration = false
+			error = "Error with internal connection with Jaeger"
+			if err != nil {
+				error += ": " + err.Error()
+			}
 		}
 	}
 
 	info := &JaegerInfo{
-		URL:                externalUrl,
+		URL:                jaegerConfig.URL,
 		NamespaceSelector:  jaegerConfig.NamespaceSelector,
 		Integration:        integration,
 		IntegrationMessage: error,
