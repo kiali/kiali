b9a38ca354c9589bde2e2a977c3521db6e3c132b
jshaughn <jshaughn@redhat.com>
2020-03-26 13:30:20 -0400

    Add GRPC status as dashboards available aggregation/filters
    Backports https://github.com/kiali/kiali/issues/2485
    
    # Conflicts:
    #	models/dashboards.go

--- models/dashboards_test.go
+++ models/dashboards_test.go
@@ -13,11 +13,12 @@ func TestPrepareIstioDashboard(t *testing.T) {
 	dashboard := PrepareIstioDashboard("Outbound", "source", "destination")
 
 	assert.Equal(dashboard.Title, "Outbound Metrics")
-	assert.Len(dashboard.Aggregations, 6)
+	assert.Len(dashboard.Aggregations, 7)
 	assert.Equal(model.Aggregation{Label: "source_version", DisplayName: "Local version"}, dashboard.Aggregations[0])
 	assert.Equal(model.Aggregation{Label: "destination_service_name", DisplayName: "Remote service"}, dashboard.Aggregations[1])
 	assert.Equal(model.Aggregation{Label: "destination_app", DisplayName: "Remote app"}, dashboard.Aggregations[2])
 	assert.Equal(model.Aggregation{Label: "destination_version", DisplayName: "Remote version"}, dashboard.Aggregations[3])
 	assert.Equal(model.Aggregation{Label: "response_code", DisplayName: "Response code"}, dashboard.Aggregations[4])
-	assert.Equal(model.Aggregation{Label: "response_flags", DisplayName: "Response flags"}, dashboard.Aggregations[5])
+	assert.Equal(model.Aggregation{Label: "grpc_response_status", DisplayName: "GRPC status"}, dashboard.Aggregations[5])
+	assert.Equal(model.Aggregation{Label: "response_flags", DisplayName: "Response flags"}, dashboard.Aggregations[6])
 }
