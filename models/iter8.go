a51a49b962a2a15fcf372dc2257882cbac8be346
Lucas Ponce <ponce.ballesteros@gmail.com>
2020-04-01 12:28:25 +0200

    Fix bad time parsing

--- models/iter8.go
+++ models/iter8.go
@@ -111,8 +111,15 @@ func (i *Iter8ExperimentDetail) Parse(iter8Object kubernetes.Iter8Experiment) {
 		TrafficStepSize:      spec.TrafficControl.TrafficStepSize,
 	}
 
-	startTimeString := time.Unix(0, status.StartTimeStamp*int64(1000000)).Format(time.RFC1123)
-	endTimeString := time.Unix(0, status.EndTimestamp*int64(1000000)).Format(time.RFC1123)
+	startTimeString := ""
+	endTimeString := ""
+	if status.StartTimeStamp > 0 {
+		startTimeString = time.Unix(0, status.StartTimeStamp).Format(time.RFC1123)
+	}
+	if status.EndTimestamp > 0 {
+		endTimeString = time.Unix(0, status.EndTimestamp).Format(time.RFC1123)
+	}
+
 	targetServiceNamespace := spec.TargetService.Namespace
 	if targetServiceNamespace == "" {
 		targetServiceNamespace = iter8Object.GetObjectMeta().Namespace
