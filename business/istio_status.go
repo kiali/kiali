e2c36a20ff5106fc5d1b6c802bcac8338f958f0d
Xavier Canal i Masjuan <xeviknal@gmail.com>
2020-04-21 10:54:57 +0200

    Differenciate Mixer-based or Istiod-based deployments

--- business/istio_status.go
+++ business/istio_status.go
@@ -26,7 +26,7 @@ type ComponentStatus struct {
 	// required: true
 	Status string `json:"status"`
 
-	// When true, the component is part of istio core. Otherwise, it is an addon
+	// When true, the component is necessary for Istio to function. Otherwise, it is an addon
 	//
 	// example:  true
 	// required: true
@@ -41,42 +41,89 @@ const (
 	NotFound  string = "NotFound"
 )
 
-// List of workloads part of a Istio deployment and if whether it is mandatory or not
-var components = map[string]bool{
-	// Core components, mandatory
-	"istio-egressgateway":  true,
-	"istio-ingressgateway": true,
-	"istiod":               true,
-	// Addon components, not mandatory
-	// Kiali not included.
-	"grafana":    false,
-	"jaeger":     false,
-	"prometheus": false,
+// List of workloads part of a Istio deployment and if whether it is mandatory or not.
+// It follows the default profile
+var components = map[string]map[string]bool{
+	"monolith": {
+		"istio-egressgateway":  false,
+		"istio-ingressgateway": true,
+		"istiod":               true,
+		"grafana":              false,
+		"istio-tracing":        false,
+		"prometheus":           true,
+	},
+	"mixer": {
+		"istio-citadel":          true,
+		"istio-egressgateway":    false,
+		"istio-galley":           true,
+		"istio-ingressgateway":   true,
+		"istio-pilot":            true,
+		"istio-policy":           true,
+		"istio-sidecar-injector": true,
+		"istio-telemetry":        true,
+		"grafana":                false,
+		"istio-tracing":          false,
+		"prometheus":             true,
+	},
 }
 
 func (iss *IstioStatusService) GetStatus() (IstioComponentStatus, error) {
-	isc := IstioComponentStatus{}
-
 	if !config.Get().ExternalServices.Istio.IstioStatusEnabled {
-		return isc, nil
+		return IstioComponentStatus{}, nil
 	}
-
-	cf := map[string]bool{}
 
 	// Fetching workloads from control plane namespace
 	ds, error := iss.k8s.GetDeployments(config.Get().IstioNamespace)
 	if error != nil {
-		return isc, error
+		return IstioComponentStatus{}, error
 	}
 
-	// Map workloads there by app name
+	arch := iss.detectArchitecture(ds)
+	if arch != "monolith" && arch != "mixer" {
+		return IstioComponentStatus{}, nil
+	}
+
+	return iss.getStatusOf(arch, ds)
+}
+
+func (iss *IstioStatusService) detectArchitecture(ds []apps_v1.Deployment) string {
+	monArch := false
+	mixArch := false
+
 	for _, d := range ds {
-		appName := d.Labels[config.Get().IstioLabels.AppLabelName]
+		appName := d.Name
 		if appName == "" {
 			continue
 		}
 
-		isCore, found := components[appName]
+		monArch = monArch || appName == "istiod"
+		mixArch = mixArch || appName == "istio-pilot"
+	}
+
+	arch := "notfound"
+	if monArch && !mixArch {
+		arch = "monolith"
+	} else if !monArch && mixArch {
+		arch = "mixer"
+	} else if monArch && mixArch {
+		arch = "multiple"
+	}
+
+	return arch
+}
+
+func (iss *IstioStatusService) getStatusOf(arch string, ds []apps_v1.Deployment) (IstioComponentStatus, error) {
+	isc := IstioComponentStatus{}
+	cf := map[string]bool{}
+
+	// Map workloads there by app name
+	for _, d := range ds {
+		appName := d.Name
+		if appName == "" {
+			continue
+		}
+
+		isCore, found := components[arch][appName]
 		if !found {
 			continue
 		}
@@ -84,17 +131,19 @@ func (iss *IstioStatusService) GetStatus() (IstioComponentStatus, error) {
 		// Component found
 		cf[appName] = true
 
-		// Check status
-		isc = append(isc, ComponentStatus{
-			Name:   appName,
-			Status: GetDeploymentStatus(d),
-			IsCore: isCore,
-		},
-		)
+		if status := GetDeploymentStatus(d); status != Healthy {
+			// Check status
+			isc = append(isc, ComponentStatus{
+				Name:   appName,
+				Status: status,
+				IsCore: isCore,
+			},
+			)
+		}
 	}
 
 	// Add missing deployments
-	for comp, isCore := range components {
+	for comp, isCore := range components[arch] {
 		if _, found := cf[comp]; !found {
 			isc = append(isc, ComponentStatus{
 				Name:   comp,
--- business/istio_status_test.go
+++ business/istio_status_test.go
@@ -66,7 +66,7 @@ func TestComponentRunning(t *testing.T) {
 	assert.Equal(Healthy, status)
 }
 
-func TestAllComp(t *testing.T) {
+func TestMonolithComp(t *testing.T) {
 	assert := assert.New(t)
 
 	conf := config.NewConfig()
@@ -84,12 +84,58 @@ func TestAllComp(t *testing.T) {
 
 	icsl, error := iss.GetStatus()
 	assert.NoError(error)
-	assertComponent(assert, icsl, "istio-egressgateway", Healthy, true)
 	assertComponent(assert, icsl, "istio-ingressgateway", NotFound, true)
-	assertComponent(assert, icsl, "istiod", Healthy, true)
 	assertComponent(assert, icsl, "grafana", Unhealthy, false)
-	assertComponent(assert, icsl, "jaeger", Unhealthy, false)
-	assertComponent(assert, icsl, "prometheus", NotFound, false)
+	assertComponent(assert, icsl, "istio-tracing", Unhealthy, false)
+	assertComponent(assert, icsl, "prometheus", NotFound, true)
+
+	// Don't return healthy deployments
+	assertComponent(assert, icsl, "istio-egressgateway", Healthy, false)
+	assertComponent(assert, icsl, "istiod", Healthy, true)
+
+	// Don't return status of mixer deployment
+	assertNotPresent(assert, icsl, "istio-citadel")
+	assertNotPresent(assert, icsl, "istio-galley")
+	assertNotPresent(assert, icsl, "istio-pilot")
+	assertNotPresent(assert, icsl, "istio-policy")
+	assertNotPresent(assert, icsl, "istio-telemetry")
+}
+
+func TestMixerComp(t *testing.T) {
+	assert := assert.New(t)
+
+	conf := config.NewConfig()
+	config.Set(conf)
+
+	pods := []apps_v1.Deployment{
+		fakeDeploymentWithStatus("istio-citadel", map[string]string{"app": "citadel", "istio": "citadel"}, healthyStatus),
+		fakeDeploymentWithStatus("istio-egressgateway", map[string]string{"app": "istio-egressgateway", "istio": "egressgateway"}, healthyStatus),
+		fakeDeploymentWithStatus("istio-pilot", map[string]string{"app": "pilot", "istio": "pilot"}, healthyStatus),
+		fakeDeploymentWithStatus("grafana", map[string]string{"app": "grafana"}, unhealthyStatus),
+		fakeDeploymentWithStatus("istio-tracing", map[string]string{"app": "jaeger"}, unhealthyStatus),
+	}
+
+	k8s := mockDeploymentCall(pods)
+	iss := IstioStatusService{k8s: k8s}
+
+	icsl, error := iss.GetStatus()
+	assert.NoError(error)
+	assertComponent(assert, icsl, "istio-galley", NotFound, true)
+	assertComponent(assert, icsl, "istio-ingressgateway", NotFound, true)
+	assertComponent(assert, icsl, "istio-policy", NotFound, true)
+	assertComponent(assert, icsl, "istio-sidecar-injection", NotFound, true)
+	assertComponent(assert, icsl, "istio-telemetry", NotFound, true)
+	assertComponent(assert, icsl, "grafana", Unhealthy, false)
+	assertComponent(assert, icsl, "istio-tracing", Unhealthy, false)
+	assertComponent(assert, icsl, "prometheus", NotFound, true)
+
+	// Don't return healthy deployments
+	assertNotPresent(assert, icsl, "istio-citadel")
+	assertNotPresent(assert, icsl, "istio-egressgateway")
+	assertNotPresent(assert, icsl, "istio-pilot")
+
+	// Don't return status of mixer deployment
+	assertNotPresent(assert, icsl, "istiod")
 }
 
 func assertComponent(assert *assert.Assertions, icsl IstioComponentStatus, name string, status string, isCore bool) {
@@ -97,6 +143,14 @@ func assertComponent(assert *assert.Assertions, icsl IstioComponentStatus, name 
 		if ics.Name == name {
 			assert.Equal(status, ics.Status)
 			assert.Equal(isCore, ics.IsCore)
+		}
+	}
+}
+
+func assertNotPresent(assert *assert.Assertions, icsl IstioComponentStatus, name string) {
+	for _, ics := range icsl {
+		if ics.Name == name {
+			assert.NotEqual(name, ics.Name)
 		}
 	}
 }
