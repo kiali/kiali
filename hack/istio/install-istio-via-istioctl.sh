f7ee318f2436f5db501b4605db21276875727872
John Mazzitelli <mazz@redhat.com>
2020-05-09 04:03:23 -0400

    [hack] support 1.6 as well as 1.5 - mtls options changed (#2755)

--- hack/istio/install-istio-via-istioctl.sh
+++ hack/istio/install-istio-via-istioctl.sh
@@ -239,6 +239,14 @@ if [[ "${CLIENT_EXE}" = *"oc" ]]; then
   else
     CNI_OPTIONS="--set components.cni.enabled=true --set components.cni.namespace=kube-system --set values.cni.cniBinDir=/var/lib/cni/bin --set values.cni.cniConfDir=/etc/cni/multus/net.d --set values.cni.chained=false --set values.cni.cniConfFileName=istio-cni.conf --set values.sidecarInjectorWebhook.injectedAnnotations.k8s\.v1\.cni\.cncf\.io/networks=istio-cni"
     TELEMETRY_OPTIONS="--set components.telemetry.k8s.resources.requests.memory=100Mi --set components.telemetry.k8s.resources.requests.cpu=50m"
+
+    # Istio 1.5 used global.mtls.auto, 1.6 renamed it
+    if ${ISTIOCTL} --remote=false version | grep -q "1\.5" ; then
+      MTLS_OPTIONS="--set values.global.mtls.auto=${MTLS}"
+    else
+      MTLS_OPTIONS="--set values.meshConfig.enableAutoMtls=${MTLS}"
+    fi
+    MTLS_OPTIONS="${MTLS_OPTIONS} --set values.global.controlPlaneSecurityEnabled=${MTLS}"
   fi
 fi
 
@@ -288,8 +296,7 @@ for s in \
    "${_KIALI_TAG_ARG}" \
    "--set addonComponents.tracing.enabled=${DASHBOARDS_ENABLED}" \
    "--set addonComponents.grafana.enabled=${DASHBOARDS_ENABLED}" \
-   "--set values.meshConfig.enableAutoMtls=${MTLS}" \
-   "--set values.global.controlPlaneSecurityEnabled=${MTLS}" \
+   "${MTLS_OPTIONS}" \
    "--set values.gateways.istio-egressgateway.enabled=${ISTIO_EGRESSGATEWAY_ENABLED}" \
    "${CNI_OPTIONS}" \
    "${TELEMETRY_OPTIONS}" \
