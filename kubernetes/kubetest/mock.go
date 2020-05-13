a24f97e37740614b202b2285867c736857be9b50
Yew-Huey Liu <yhliu@us.ibm.com>
2020-05-12 21:38:19 -0400

    Update mock.go

--- kubernetes/kubetest/mock.go
+++ kubernetes/kubetest/mock.go
@@ -500,7 +500,7 @@ func (o *K8SClientMock) DeleteIter8Experiment(namespace string, name string) err
 	return args.Error(0)
 }
 
-func (o *K8SClientMock) Iter8ConfigMap () ([]string, error) {
+func (o *K8SClientMock) Iter8ConfigMap() ([]string, error) {
 	args := o.Called()
 	return args.Get(0).([]string), args.Error(1)
 }
