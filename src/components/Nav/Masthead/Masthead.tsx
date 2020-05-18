1190065b7cd8c68f9da908079fa076e5c48f1361
Joel Takvorian <jtakvori@redhat.com>
2020-05-18 11:32:05 +0200

    Use new patternfly spinner
    This is using the new spinner.
    Fixes https://github.com/kiali/kiali/issues/2679

--- src/components/Nav/Masthead/Masthead.tsx
+++ src/components/Nav/Masthead/Masthead.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
-
+import { style } from 'typestyle';
 import { Toolbar as ToolbarNext, ToolbarGroup, ToolbarItem } from '@patternfly/react-core';
+
 import { default as MeshMTLSStatus } from '../../../components/MTls/MeshMTLSStatus';
 import { default as IstioStatus } from '../../IstioStatus/IstioStatus';
 import PfSpinner from '../../PfSpinner';
@@ -8,11 +9,17 @@ import UserDropdown from './UserDropdown';
 import HelpDropdown from './HelpDropdown';
 import MessageCenterTriggerContainer from '../../../components/MessageCenter/MessageCenterTrigger';
 
+const leftGroup = style({
+  position: 'absolute',
+  left: 210,
+  top: 28
+});
+
 class Masthead extends React.Component {
   render() {
     return (
       <ToolbarNext>
-        <ToolbarGroup>
+        <ToolbarGroup className={leftGroup}>
           <PfSpinner />
         </ToolbarGroup>
         <ToolbarGroup>
