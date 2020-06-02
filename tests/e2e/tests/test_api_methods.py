2362562514567725f81522ab80bae7391a43104d
Praneeth Bajjuri <pbajjuri@redhat.com>
2020-06-02 16:04:36 -0400

    Adding new test cases to "api_methods"

--- tests/e2e/tests/test_api_methods.py
+++ tests/e2e/tests/test_api_methods.py
@@ -10,7 +10,7 @@ def before_all_tests(kiali_client):
     swagger = kiali_client.swagger_parser.swagger
     swagger_method_list= []
     tested_method_list = ['root','jaegerInfo', 'grafanaInfo', 'getPermissions', 'getStatus', 'getConfig', 'authenticate',
-                          'namespaceList', 'namespaceMetrics','namespaceHealth',
+                          'namespaceList', 'namespaceMetrics','namespaceHealth','istioStatus','iter8Experiments',
                           'istioConfigList', 'istioConfigDetails', 'istioConfigCreate', 'istioConfigDelete', 'objectValidations', ''
                           'serviceList', 'serviceDetails', 'serviceMetrics', 'serviceHealth',
                           'appHealth', 'appList', 'appDetails', 'appMetrics',
@@ -114,6 +114,12 @@ def test_grafana_info(kiali_client):
     
 def test_get_permissions(kiali_client):
     evaluate_response(kiali_client, method_name='getPermissions')
+    
+def test_iter8_experiments(kiali_client):
+    evaluate_response(kiali_client, method_name='iter8Experiments')
+
+def test_istio_status(kiali_client):
+    evaluate_response(kiali_client, method_name='istioStatus')  
     
 def test_get_status(kiali_client):
     evaluate_response(kiali_client, method_name='getStatus')
