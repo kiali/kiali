a5a1dcd7948796725eea5314b57af61ed002ab43
John Mazzitelli <mazz@redhat.com>
2020-06-05 12:55:09 -0400

    [hack] some scripts we can use for testing (#2871)
    * [hack] some scripts we can use for testing
    1. hack/istio/install-travel-agency-demo.sh
    Another microservice demo with added benefit of a couple defined operations in telemetry.
    2. hack/jwt-[en,de]code.sh
    Two scripts that let us test passing in and examining JWT tokens. Useful to make sure hacking JWT tokens is not possible. Also useful to pass in when testing Kiali APIs as authenticated users.

---
+++ hack/jwt-decode.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+
+#
+# JWT Decoder Bash Script
+#
+# To run, pipe the JWT encoded token (found in the Kiali cookie) like this:
+#   echo -n $MY_JWT_TOKEN | ./jwt-decode.sh
+#
+
+secret='kiali'
+
+while [ $# -gt 0 ]; do
+  key="$1"
+  case $key in
+    -k|--key)
+      secret="$2"
+      shift;shift
+      ;;
+    *)
+      echo "Unknown argument [$key]. Aborting."
+      exit 1
+      ;;
+  esac
+done
+
+base64_encode()
+{
+	declare input=${1:-$(</dev/stdin)}
+	# Use `tr` to URL encode the output from base64.
+	printf '%s' "${input}" | base64 | tr -d '=' | tr '/+' '_-' | tr -d '\n'
+}
+
+base64_decode()
+{
+	declare input=${1:-$(</dev/stdin)}
+	# A standard base64 string should always be `n % 4 == 0`. We made the base64
+	# string URL safe when we created the JWT, which meant removing the `=`
+	# signs that are there for padding. Now we must add them back to get the
+	# proper length.
+	remainder=$((${#input} % 4));
+	if [ $remainder -eq 1 ];
+	then
+		>2& echo "fatal error. base64 string is unexepcted length"
+	elif [[ $remainder -eq 2 || $remainder -eq 3 ]];
+	then
+		input="${input}$(for i in `seq $((4 - $remainder))`; do printf =; done)"
+	fi
+	printf '%s' "${input}" | base64 --decode
+}
+
+verify_signature()
+{
+	declare header_and_payload=${1}
+	expected=$(echo "${header_and_payload}" | hmacsha256_encode | base64_encode)
+	actual=${2}
+
+	if [ "${expected}" = "${actual}" ]
+	then
+		echo "Signature is valid"
+	else
+		echo "Signature is NOT valid"
+	fi
+}
+
+hmacsha256_encode()
+{
+	declare input=${1:-$(</dev/stdin)}
+	printf '%s' "${input}" | openssl dgst -binary -sha256 -hmac "${secret}"
+}
+
+# Read the token from stdin
+declare token=${1:-$(</dev/stdin)};
+
+IFS='.' read -ra pieces <<< "$token"
+
+declare header=${pieces[0]}
+declare payload=${pieces[1]}
+declare signature=${pieces[2]}
+
+echo "Header"
+echo "${header}" | base64_decode | jq
+echo "Payload"
+echo "${payload}" | base64_decode | jq
+
+verify_signature "${header}.${payload}" "${signature}"
+
---
+++ hack/jwt-encode.sh
@@ -0,0 +1,164 @@
+#!/usr/bin/env bash
+
+set -u
+
+#
+# JWT Encoder Bash Script
+#
+# Use this to build a JWT token which can then be passed to the Kiali API. This is used to validate the security
+# mechanisms within Kiali that do not allow for Kiali to accept hacked JWT tokens.
+#
+
+secret="${KEY:-kiali}"
+cookie="${COOKIE:-kiali-token}"
+ncookie="${NCOOKIE:-kiali-token-openid-nonce}"
+
+# Kiali issuers: kiali-openshift, kiali-token, kiali-open-id, kiali-login, kiali-ldap
+iss="${ISS:-kiali-open-id}"
+
+# When the session expires in epoch seconds - by default, expire it next Saturday
+exp="${EXP:-$(date --date 'next Sat' '+%s')}"
+
+# Session ID
+sid="${SID:-}"
+
+# Subject
+sub="${SUB:-admin@example.com}"
+
+# Nonce Code - e.g. l8LEwQHzQa0U&q0
+nonce="${NONCE:-}"
+
+# URL is left undefined - only when defined will a request be made
+#url=http://192.168.99.100/kiali/api/namespaces
+
+# process command line args
+while [ $# -gt 0 ]; do
+  key="$1"
+  case $key in
+    -c|--cookie)
+      cookie="$2"
+      shift;shift
+      ;;
+    -e|--expires)
+      exp="$2"
+      shift;shift
+      ;;
+    -i|--issuer)
+      iss="$2"
+      shift;shift
+      ;;
+    -k|--key)
+      secret="$2"
+      shift;shift
+      ;;
+    -n|--nonce)
+      nonce="$2"
+      shift;shift
+      ;;
+    -nc|--nonce-cookie)
+      ncookie="$2"
+      shift;shift
+      ;;
+    -sid|--sessionid)
+      sid="$2"
+      shift;shift
+      ;;
+    -sub|--subject)
+      sub="$2"
+      shift;shift
+      ;;
+    -u|--url)
+      url="$2"
+      shift;shift
+      ;;
+    -h|--help)
+      cat <<HELPMSG
+Valid command line arguments:
+  -c|--cookie <cookie name>: If -u is used, this is the name of the cookie whose value is the JWT token. Default: ${cookie}
+  -e|--expires <epoch secs>: Expiration date of the JWT token. Default: ${exp}
+  -i|--issuer <issuer>: The issuer ("iss") to be used in the JWT payload. Default: ${iss}
+  -k|--key <signing key string>: The key used to encrypt the JWT token. Default: ${secret}
+  -n|--nonce <code>: If -u is used, this is the nonce code Default: ${nonce}
+  -nc|--nonce-cookie <cookie name>: If -u is used, this is the name of the nonce cookie. Default: ${ncookie}
+  -sid|--sessionid <id>: The long session ID text.
+  -sub|--subject <name>: The subject name. Default: ${sub}
+  -u|--url <url>: if specified, this script will make a curl command to this URL passing the JWT token to it.
+  -h|--help : This message.
+HELPMSG
+      exit 1
+      ;;
+    *)
+      echo "Unknown argument [$key]. Aborting."
+      exit 1
+      ;;
+  esac
+done
+
+header='{
+	"alg": "HS256",
+	"typ": "JWT"
+}'
+
+payload="{"
+if [ ! -z ${exp} ]; then payload="${payload} ${comma:-}\"exp\": ${exp}"; comma=", "; fi
+if [ ! -z ${iss} ]; then payload="${payload} ${comma:-}\"iss\": \"${iss}\""; comma=", "; fi
+if [ ! -z ${sid} ]; then payload="${payload} ${comma:-}\"sid\": \"${sid}\""; comma=", "; fi
+if [ ! -z ${sub} ]; then payload="${payload} ${comma:-}\"sub\": \"${sub}\""; comma=", "; fi
+payload="${payload} }"
+
+# Use jq to set the dynamic `iat` field on the header using the current time.
+# This is not used - the env var being check is never true (unless the caller sets it)
+if [ "${SET_IAT_IN_HEADER:-}" == "true" ]; then
+header=$(
+	echo "${header}" | jq --arg time_str "$(date +%s)" \
+	'
+	($time_str | tonumber) as $time_num
+	| .iat=$time_num
+	'
+)
+fi
+
+base64_encode()
+{
+	declare input=${1:-$(</dev/stdin)}
+	# Use `tr` to URL encode the output from base64.
+	printf '%s' "${input}" | base64 | tr -d '=' | tr '/+' '_-' | tr -d '\n'
+}
+
+json() {
+	declare input=${1:-$(</dev/stdin)}
+	printf '%s' "${input}" | jq -c .
+}
+
+hmacsha256_sign()
+{
+	declare input=${1:-$(</dev/stdin)}
+	printf '%s' "${input}" | openssl dgst -binary -sha256 -hmac "${secret}"
+}
+
+header_base64=$(echo "${header}" | json | base64_encode)
+payload_base64=$(echo "${payload}" | json | base64_encode)
+
+header_payload=$(echo "${header_base64}.${payload_base64}")
+signature=$(echo "${header_payload}" | hmacsha256_sign | base64_encode)
+
+jwt_token="${header_payload}.${signature}"
+echo "${jwt_token}"
+
+if [ ! -z "${url}" ]; then
+  echo "=================SUBMITING REQUEST:"
+  echo "URL:     ${url}"
+  echo "SECRET:  ${secret}"
+  echo "COOKIE:  ${cookie}"
+  echo "NCOOKIE: ${ncookie}"
+  echo "ISSUER:  ${iss}"
+  echo "EXPIRES: ${exp}"
+  echo "SUBJECT: ${sub}"
+  echo "NONCE:   ${nonce}"
+  echo "SID:     ${sid}"
+  echo "=================FULL JWT JSON:"
+  echo -n ${jwt_token} | ./jwt-decode.sh -k "${secret}"
+  echo "=================RESULTS:"
+  if [ ! -z "${nonce}" ]; then nonce_cookie_arg="; ${ncookie}=${nonce}"; fi
+  curl -v -k --cookie "${cookie}=${jwt_token}${nonce_cookie_arg:-}" ${url}
+fi
