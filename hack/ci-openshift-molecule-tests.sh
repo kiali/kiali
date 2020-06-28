5d886e0f632ab2c4c85b1c4662ca5b672fd10883
John Mazzitelli <mazz@redhat.com>
2020-06-28 13:44:10 -0400

    if not logged in, then rebuild the cluster, destroying one if one already exists

--- hack/ci-openshift-molecule-tests.sh
+++ hack/ci-openshift-molecule-tests.sh
@@ -10,6 +10,8 @@
 # If you do not have an OpenShift cluster, then when this script runs it will attempt
 # to install one on AWS. In this case, you must have an AWS account set up and an OpenShift
 # pull secret available. See the aws-openshift.sh and aws-openshift-istio.sh hack scripts for details.
+# WARNING! If you are not logged into an AWS cluster but you already have an AWS cluster deployed,
+# this script will destroy that cluster and rebuild a new one.
 #
 
 while [[ $# -gt 0 ]]; do
@@ -48,25 +50,23 @@ script_root="$( cd "$(dirname "$0")" ; pwd -P )"
 hack_dir="$script_root"
 
 if ! oc whoami > /dev/null 2>&1; then
-  if ! ${hack_dir}/aws-openshift.sh status; then
-    if [ "${INSTALL_SERVICE_MESH}" == "false" -a "${INSTALL_UPSTREAM_ISTIO}" == "false" ]; then
-      echo "Molecule tests require either Service Mesh or upstream Istio to be installed."
-      exit 1
-    fi
+  if [ "${INSTALL_SERVICE_MESH}" == "false" -a "${INSTALL_UPSTREAM_ISTIO}" == "false" ]; then
+    echo "Molecule tests require either Service Mesh or upstream Istio to be installed."
+    exit 1
+  fi
 
-    ${hack_dir}/aws-openshift-istio.sh \
-      --install-service-mesh "${INSTALL_SERVICE_MESH}" \
-      --install-upstream-istio "${INSTALL_UPSTREAM_ISTIO}" \
-      --install-bookinfo "false"
-    if [ "$?" != "0" ]; then
-      echo "Failed to install the AWS cluster."
-      exit 1
-    fi
+  ${hack_dir}/aws-openshift-istio.sh \
+    --install-service-mesh "${INSTALL_SERVICE_MESH}" \
+    --install-upstream-istio "${INSTALL_UPSTREAM_ISTIO}" \
+    --install-bookinfo "false"
+  if [ "$?" != "0" ]; then
+    echo "Failed to install the AWS cluster."
+    exit 1
+  fi
 
-    # If we are installing Service Mesh, uninstall Kiali since the molecule tests will want to install its own
-    if [ "${INSTALL_SERVICE_MESH}" == "true" ]; then
-      ${hack_dir}/istio/install-sm11.sh k-uninstall
-    fi
+  # If we are installing Service Mesh, uninstall Kiali since the molecule tests will want to install its own
+  if [ "${INSTALL_SERVICE_MESH}" == "true" ]; then
+    ${hack_dir}/istio/install-sm11.sh k-uninstall
   fi
 
   oc login -u kiali -p kiali https://$(${hack_dir}/aws-openshift.sh api-host)
