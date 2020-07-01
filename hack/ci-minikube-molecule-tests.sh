bafd3581ca87e6db20d2420ceaa9533dcce95b8d
John Mazzitelli <mazz@redhat.com>
2020-07-01 09:45:16 -0400

    [hack] fix some things that are broke when running cron jobs  (#2953)
    * [hack] fix some bugs in k8s-minikube.sh
    
    * [hack] switch to the minikube profile context
    
    * do not pass interactive/terminal flags to docker - there is no need for it and they break the make target when running in cron

--- hack/ci-minikube-molecule-tests.sh
+++ hack/ci-minikube-molecule-tests.sh
@@ -6,6 +6,8 @@
 #
 # This will start a minikube cluster via the hack script k8s-minikube.sh.
 #
+# This assumes 'kubectl' and 'minikube' is in the PATH.
+#
 
 # Where this script is - all our hack files are assumed to be in here
 script_root="$( cd "$(dirname "$0")" ; pwd -P )"
@@ -14,6 +16,13 @@ hack_dir="$script_root"
 # the minikube hack script command
 minikube_profile="ci"
 minikube_sh="${hack_dir}/k8s-minikube.sh --minikube-profile ${minikube_profile}"
+
+# make sure we switch contexts if we can so we are pointing to the current cluster
+if [ "$(kubectl config current-context)" != "${minikube_profile}" ]; then
+  if ! kubectl config use-context ${minikube_profile}; then
+    echo "There is no kubectl context named [${minikube_profile}]. This likely means we will start one next."
+  fi
+fi
 
 if ! ${minikube_sh} status; then
   ${minikube_sh} start --dex-enabled true -kv 1.18.0
@@ -24,4 +33,4 @@ if ! ${minikube_sh} status; then
   ${minikube_sh} istio
 fi
 
-${hack_dir}/run-molecule-tests.sh --cluster-type minikube --minikube-profile ${minikube_profile}
+${hack_dir}/run-molecule-tests.sh --cluster-type minikube --minikube-profile ${minikube_profile} --color false
