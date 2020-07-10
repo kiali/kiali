4dbb3f3a92054185eabed3d65c45817a9de4358b
Ryan-Dmello <ryan.dmello1@ibm.com>
2020-07-10 22:23:08 +0530

    Enabling travis build for ppc64le (#2968)
    * Adding ppc64le support
    
    * Updating Readme, to set environment variable SWAGGER_VERSION
    
    * Making change to re-trigger build
    
    * Incorporating review change
    
    * Making change to re-trigger build

--- make/Makefile.build.mk
+++ make/Makefile.build.mk
@@ -87,7 +87,11 @@ run:
 ## swagger-install: Install swagger from github
 swagger-install:
 	@echo "Installing swagger binary to ${GOPATH}/bin..."
-	@curl https://github.com/go-swagger/go-swagger/releases/download/v0.22.0/swagger_linux_${GOARCH} --create-dirs -Lo ${GOPATH}/bin/swagger && chmod +x ${GOPATH}/bin/swagger
+ifeq ($(GOARCH), ppc64le)
+	curl https://github.com/go-swagger/go-swagger/archive/v${SWAGGER_VERSION}.tar.gz --create-dirs -Lo /tmp/v${SWAGGER_VERSION}.tar.gz && tar -xzf /tmp/v${SWAGGER_VERSION}.tar.gz -C /tmp/ && src_dir='pwd' && cd /tmp/go-swagger-${SWAGGER_VERSION} && go install ./cmd/swagger && cd ${src_dir}
+else
+	curl https://github.com/go-swagger/go-swagger/releases/download/v${SWAGGER_VERSION}/swagger_linux_${GOARCH} --create-dirs -Lo ${GOPATH}/bin/swagger && chmod +x ${GOPATH}/bin/swagger
+endif
 
 ## swagger-validate: Validate that swagger.json is correctly. Runs `swagger validate` internally
 swagger-validate:
