9f23d21e808819bd80276e26e7507fc6228a6216
John Mazzitelli <mazz@redhat.com>
2020-07-30 14:29:30 -0400

    update to travel agency v2

--- hack/istio/install-travel-agency-demo.sh
+++ hack/istio/install-travel-agency-demo.sh
@@ -5,9 +5,11 @@
 : ${CLIENT_EXE:=oc}
 : ${NAMESPACE_AGENCY:=travel-agency}
 : ${NAMESPACE_PORTAL:=travel-portal}
+: ${NAMESPACE_CONTROL:=travel-control}
 : ${ENABLE_OPERATION_METRICS:=false}
 : ${INSTALL_VERSION:=v1}
 : ${DELETE_DEMO:=false}
+: ${SHOW_GUI:=false}
 
 while [ $# -gt 0 ]; do
   key="$1"
@@ -28,12 +30,8 @@ while [ $# -gt 0 ]; do
       INSTALL_VERSION="$2"
       shift;shift
       ;;
-    -na|--namespace-agency)
-      NAMESPACE_AGENCY="$2"
-      shift;shift
-      ;;
-    -np|--namespace-portal)
-      NAMESPACE_PORTAL="$2"
+    -sg|--show-gui)
+      SHOW_GUI="$2"
       shift;shift
       ;;
     -h|--help)
@@ -42,9 +40,8 @@ Valid command line arguments:
   -c|--client: either 'oc' or 'kubectl'
   -d|--delete: either 'true' or 'false'. If 'true' the travel agency demo will be deleted, not installed.
   -eo|--enable-operation-metrics: either 'true' or 'false' (default is false). Only works on Istio 1.6 installed in istio-system.
-  -na|--namespace-agency: where to install the travel agency demo resources
-  -np|--namespace-portal: where to install the travel portal demo resources
   -iv|--install-version: either 'v1' or 'v2' (default is v1)
+  -sg|--show-gui: do not install anything, but bring up the travel agency GUI in a browser window
 HELPMSG
       exit 1
       ;;
@@ -55,10 +52,19 @@ HELPMSG
   esac
 done
 
+if [ "${SHOW_GUI}" == "true" ]; then
+  echo "Will not install anything. Creating port-forward now. (control-c to exit)"
+  echo "Point your browser to here: "
+  echo "  http://localhost:8080"
+  ${CLIENT_EXE} port-forward svc/control 8080:8080 -n travel-control
+  exit 0
+fi
+
 echo Will deploy Travel Agency using these settings:
 echo CLIENT_EXE=${CLIENT_EXE}
 echo NAMESPACE_AGENCY=${NAMESPACE_AGENCY}
 echo NAMESPACE_PORTAL=${NAMESPACE_PORTAL}
+echo NAMESPACE_CONTROL=${NAMESPACE_CONTROL}
 echo ENABLE_OPERATION_METRICS=${ENABLE_OPERATION_METRICS}
 echo INSTALL_VERSION=${INSTALL_VERSION}
 
@@ -78,9 +84,14 @@ if [ "${DELETE_DEMO}" == "true" ]; then
     ${CLIENT_EXE} adm policy remove-scc-from-group privileged system:serviceaccounts:${NAMESPACE_PORTAL}
     ${CLIENT_EXE} adm policy remove-scc-from-group anyuid system:serviceaccounts:${NAMESPACE_PORTAL}
     ${CLIENT_EXE} delete network-attachment-definition istio-cni -n ${NAMESPACE_PORTAL}
+
+    ${CLIENT_EXE} adm policy remove-scc-from-group privileged system:serviceaccounts:${NAMESPACE_CONTROL}
+    ${CLIENT_EXE} adm policy remove-scc-from-group anyuid system:serviceaccounts:${NAMESPACE_CONTROL}
+    ${CLIENT_EXE} delete network-attachment-definition istio-cni -n ${NAMESPACE_CONTROL}
   fi
   ${CLIENT_EXE} delete namespace ${NAMESPACE_AGENCY}
   ${CLIENT_EXE} delete namespace ${NAMESPACE_PORTAL}
+  ${CLIENT_EXE} delete namespace ${NAMESPACE_CONTROL}
   exit 0
 fi
 
@@ -116,18 +127,35 @@ EOF
   fi
 fi
 
+if ! ${CLIENT_EXE} get namespace ${NAMESPACE_CONTROL} 2>/dev/null; then
+  ${CLIENT_EXE} create namespace ${NAMESPACE_CONTROL}
+  ${CLIENT_EXE} label namespace ${NAMESPACE_CONTROL} istio-injection=enabled
+  if [ "${CLIENT_EXE}" == "oc" ]; then
+    ${CLIENT_EXE} adm policy add-scc-to-group privileged system:serviceaccounts:${NAMESPACE_CONTROL}
+    ${CLIENT_EXE} adm policy add-scc-to-group anyuid system:serviceaccounts:${NAMESPACE_CONTROL}
+    cat <<EOF | ${CLIENT_EXE} -n ${NAMESPACE_CONTROL} create -f -
+apiVersion: "k8s.cni.cncf.io/v1"
+kind: NetworkAttachmentDefinition
+metadata:
+  name: istio-cni
+EOF
+  fi
+fi
+
 # Deploy the demo v1
 
 if [ "${INSTALL_VERSION}" == "v1" ]; then
-  ${CLIENT_EXE} apply -f <(curl -L https://raw.githubusercontent.com/lucasponce/travel-comparison-demo/master/travel_agency.yaml) -n ${NAMESPACE_AGENCY}
-  ${CLIENT_EXE} apply -f <(curl -L https://raw.githubusercontent.com/lucasponce/travel-comparison-demo/master/travel_portal.yaml) -n ${NAMESPACE_PORTAL}
+  ${CLIENT_EXE} apply -f <(curl -L https://raw.githubusercontent.com/lucasponce/travel-comparison-demo/v2/travel_agency.yaml) -n ${NAMESPACE_AGENCY}
+  ${CLIENT_EXE} apply -f <(curl -L https://raw.githubusercontent.com/lucasponce/travel-comparison-demo/v2/travel_portal.yaml) -n ${NAMESPACE_PORTAL}
+  ${CLIENT_EXE} apply -f <(curl -L https://raw.githubusercontent.com/lucasponce/travel-comparison-demo/v2/travel_control.yaml) -n ${NAMESPACE_CONTROL}
 fi
 
 # Deploy the demo v2
 
 if [ "${INSTALL_VERSION}" == "v2" ]; then
-  ${CLIENT_EXE} apply -f <(curl -L https://raw.githubusercontent.com/lucasponce/travel-comparison-demo/master/travel_agency_v2.yaml) -n ${NAMESPACE_AGENCY}
-  ${CLIENT_EXE} apply -f <(curl -L https://raw.githubusercontent.com/lucasponce/travel-comparison-demo/master/travel_portal.yaml) -n ${NAMESPACE_PORTAL}
+  ${CLIENT_EXE} apply -f <(curl -L https://raw.githubusercontent.com/lucasponce/travel-comparison-demo/v2/travel_agency_v2.yaml) -n ${NAMESPACE_AGENCY}
+  ${CLIENT_EXE} apply -f <(curl -L https://raw.githubusercontent.com/lucasponce/travel-comparison-demo/v2/travel_portal.yaml) -n ${NAMESPACE_PORTAL}
+  ${CLIENT_EXE} apply -f <(curl -L https://raw.githubusercontent.com/lucasponce/travel-comparison-demo/v2/travel_control.yaml) -n ${NAMESPACE_CONTROL}
 fi
 
 # Set up metric classification
