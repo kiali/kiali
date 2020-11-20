4ce1e59798fe467871ec02e1555d8f08fb33e7b9
John Mazzitelli <mazz@redhat.com>
2020-11-20 14:12:39 -0500

    refactor - ensure buildx is available, enabled, and correct version

--- make/Makefile.container.mk
+++ make/Makefile.container.mk
@@ -64,26 +64,50 @@ endif
 container-push: container-push-kiali-quay
 
 # Ensure "docker buildx" is available and enabled. For more details, see: https://github.com/docker/buildx/blob/master/README.md
+# This does a few things:
+#  1. Makes sure docker is in PATH
+#  2. Downloads and installs buildx if no version of buildx is installed yet
+#  3. Makes sure any installed buildx is a required version or newer
+#  4. Makes sure the user has enabled buildx (either by default or by setting DOCKER_CLI_EXPERIMENTAL env var to 'enabled')
+#  Thus, this target will only ever succeed if a required (or newer) version of 'docker buildx' is available and enabled.
 .ensure-docker-buildx:
-	@required_buildx_version="v0.4.2"; \
-	current_buildx_version=$$(DOCKER_CLI_EXPERIMENTAL="enabled" docker buildx version); \
-	if [[ "$${current_buildx_version}" == *"$${required_buildx_version}"* ]]; then \
-	  echo "'docker buildx' is available and enabled."; \
+	@if ! which docker > /dev/null 2>&1; then echo "'docker' is not in your PATH."; exit 1; fi
+	@required_buildx_version="0.4.2"; \
+	if ! DOCKER_CLI_EXPERIMENTAL="enabled" docker buildx version > /dev/null 2>&1 ; then \
+	  buildx_download_url="https://github.com/docker/buildx/releases/download/$${required_buildx_version}/buildx-$${required_buildx_version}.${GOOS}-${GOARCH}"; \
+	  echo "You do not have 'docker buildx' installed. Will now download from [$${buildx_download_url}] and install it to [${HOME}/.docker/cli-plugins]."; \
+	  mkdir -p ${HOME}/.docker/cli-plugins; \
+	  curl -L --output ${HOME}/.docker/cli-plugins/docker-buildx "$${buildx_download_url}"; \
+	  chmod a+x ${HOME}/.docker/cli-plugins/docker-buildx; \
+	  installed_version="$$(DOCKER_CLI_EXPERIMENTAL="enabled" docker buildx version || echo "unknown")"; \
+	  if docker buildx version > /dev/null 2>&1; then \
+	    echo "'docker buildx' has been installed and is enabled [version=$${installed_version}]"; \
+	  else \
+	    echo "An attempt to install 'docker buildx' has been made but it either failed or is not enabled by default. [version=$${installed_version}]"; \
+	    echo "Set DOCKER_CLI_EXPERIMENTAL=enabled to enable it."; \
+	    exit 1; \
+	  fi \
+	fi; \
+	current_buildx_version="$$(DOCKER_CLI_EXPERIMENTAL=enabled docker buildx version 2>/dev/null | sed -E 's/.*v([0-9]+\.[0-9]+\.[0-9]+).*/\1/g')"; \
+	is_valid_buildx_version="$$(if [ "$$(printf $${required_buildx_version}\\n$${current_buildx_version} | sort -V | head -n1)" == "$${required_buildx_version}" ]; then echo "true"; else echo "false"; fi)"; \
+	if [ "$${is_valid_buildx_version}" == "true" ]; then \
+	  echo "A valid version of 'docker buildx' is available: $${current_buildx_version}"; \
 	else \
-		buildx_download_url=https://github.com/docker/buildx/releases/download/$${required_buildx_version}/buildx-$${required_buildx_version}.${GOOS}-${GOARCH}; \
-		echo "You do not have 'docker buildx' installed. Will now download from $${buildx_download_url} and install it to [${HOME}/.docker/cli-plugins]."; \
-		mkdir -p ${HOME}/.docker/cli-plugins; \
-		curl -L --output ${HOME}/.docker/cli-plugins/docker-buildx "$${buildx_download_url}"; \
-		chmod a+x ${HOME}/.docker/cli-plugins/docker-buildx; \
-		DOCKER_CLI_EXPERIMENTAL="enabled" docker buildx version; \
-		echo "'docker buildx' is available and enabled."; \
+	  echo "You have an older version of 'docker buildx' that is not compatible. Please upgrade to at least v$${required_buildx_version}"; \
+	  exit 1; \
+	fi; \
+	if docker buildx version > /dev/null 2>&1; then \
+	  echo "'docker buildx' is enabled"; \
+	else \
+	  echo "'docker buildx' is not enabled. Set DOCKER_CLI_EXPERIMENTAL=enabled if you want to use it."; \
+	  exit 1; \
 	fi
 
 # Ensure a local builder for multi-arch build. For more details, see: https://github.com/docker/buildx/blob/master/README.md#building-multi-platform-images
 .ensure-buildx-builder: .ensure-docker-buildx
-	@if ! DOCKER_CLI_EXPERIMENTAL="enabled" docker buildx inspect kiali-builder > /dev/null 2>&1; then \
+	@if ! docker buildx inspect kiali-builder > /dev/null 2>&1; then \
 	  echo "The buildx builder instance named 'kiali-builder' does not exist. Creating one now."; \
-	  if ! DOCKER_CLI_EXPERIMENTAL="enabled" docker buildx create --name=kiali-builder --driver-opt=image=moby/buildkit:v0.8.0-rc2; then \
+	  if ! docker buildx create --name=kiali-builder --driver-opt=image=moby/buildkit:v0.8.0-rc2; then \
 	    echo "Failed to create the buildx builder 'kiali-builder'"; \
 	    exit 1; \
 	  fi \
@@ -93,9 +117,9 @@ container-push: container-push-kiali-quay
 	  if ! docker run --privileged --rm tonistiigi/binfmt --install all; then \
 	    echo "Failed to ensure QEMU is set up. This build will be allowed to continue, but it may fail at a later step."; \
 	  fi \
-	fi; \
+	fi
 
 ## container-multi-arch-push-kiali-quay: Pushes the Kiali multi-arch image to quay.
 container-multi-arch-push-kiali-quay: .ensure-buildx-builder .prepare-kiali-image-files
 	@echo Pushing Kiali multi-arch image to ${QUAY_TAG} using docker buildx
-	DOCKER_CLI_EXPERIMENTAL="enabled" docker buildx build --push --builder=kiali-builder $(foreach arch,${TARGET_ARCHS},--platform=linux/${arch}) $(foreach tag,${QUAY_TAG},--tag=${tag}) -f ${OUTDIR}/docker/Dockerfile-multi-arch ${OUTDIR}/docker
+	docker buildx build --push --builder=kiali-builder $(foreach arch,${TARGET_ARCHS},--platform=linux/${arch}) $(foreach tag,${QUAY_TAG},--tag=${tag}) -f ${OUTDIR}/docker/Dockerfile-multi-arch ${OUTDIR}/docker
