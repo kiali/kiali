6dbc4eff8f45a535293f0962dd8d575c02baa62d
Xavier Canal i Masjuan <xeviknal@gmail.com>
2020-11-23 11:08:21 +0100

    Lintfix

--- business/istio_status_test.go
+++ business/istio_status_test.go
@@ -361,7 +361,7 @@ func mockServer(mr *mux.Router) *httptest.Server {
 	return httptest.NewServer(mr)
 }
 
-func addAddOnRoute(mr *mux.Router, mu sync.Mutex, url string, statusCode int, callNum *int) {
+func addAddOnRoute(mr *mux.Router, mu *sync.Mutex, url string, statusCode int, callNum *int) {
 	mr.HandleFunc(url, func(w http.ResponseWriter, r *http.Request) {
 		mu.Lock()
 		if callNum != nil {
@@ -382,7 +382,7 @@ func mockAddOnCalls(addons map[string]addOnsSetup) *mux.Router {
 	var mu sync.Mutex
 	mr := mux.NewRouter()
 	for _, addon := range addons {
-		addAddOnRoute(mr, mu, addon.Url, addon.StatusCode, addon.CallCount)
+		addAddOnRoute(mr, &mu, addon.Url, addon.StatusCode, addon.CallCount)
 	}
 	return mr
 }
