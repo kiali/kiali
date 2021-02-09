90b25c587ccb7428f07d327dd7a7dc168fc3c2e7
Jay Shaughnessy <jshaughn@users.noreply.github.com>
2021-02-09 16:36:08 -0500

    Update envoy response flags that we know about (#2080)
    * Update envoy response flags that we know about for the Flags tab in graph side-panel.
    
    https://github.com/kiali/kiali/issues/3685

--- src/utils/ResponseFlags.ts
+++ src/utils/ResponseFlags.ts
@@ -1,15 +1,20 @@
-// The Envoy flags can be found here:
-// https://github.com/envoyproxy/envoy/blob/master/source/common/stream_info/utility.cc
+// The Envoy response flags can be found here (search for %RESPONSE_FLAGS% on the page):
+// https://www.envoyproxy.io/docs/envoy/latest/configuration/observability/access_log/usage
 const responseFlags = {
   DC: { code: '500', help: 'Downstream connection termination', short: 'Downstream cx term' },
   DI: { help: 'Delayed via fault injection' },
+  DPE: {
+    help: ' Downstream request had an HTTP protocol error (is port type correct?)',
+    short: 'Downstream invalid HTTP'
+  },
   FI: { help: 'Aborted via fault injection' },
+  IH: { code: '400', help: 'Invalid value for a strictly-checked header', short: 'Invalid header value' },
   LH: { code: '503', help: 'Local service failed health check request', short: 'Failed health check' },
   LR: { code: '503', help: 'Connection local reset' },
   NR: { code: '404', help: 'No route configured (check DestinationRule or VirtualService)', short: 'No route' },
   RL: { code: '429', help: 'Ratelimited locally by the HTTP rate limit filter', short: 'Rate limit' },
   RLSE: { help: 'Rate limited service error' },
-  SI: { help: 'Stream idle timeout' },
+  SI: { code: '408', help: 'Stream idle timeout' },
   UAEX: { help: 'Unauthorized external service' },
   UC: { code: '503', help: 'Upstream connection termination', short: 'Upstream cx term' },
   UF: {
@@ -18,9 +23,14 @@ const responseFlags = {
     short: 'Upstream cx failure'
   },
   UH: { code: '503', help: 'No healthy upstream hosts in upstream cluster', short: 'No healthy upstream' },
+  UMSDR: { help: 'Upstream request reached max stream duration' },
   UO: { code: '503', help: 'Upstream overflow (circuit breaker open)', short: 'Circuit breaker open' },
   UR: { code: '503', help: 'Upstream remote reset' },
-  URX: { code: '503', help: 'Upstream retry limit exceeded', short: 'Upstream retry limit' },
+  URX: {
+    code: '503',
+    help: 'Upstream retry limit (HTTP) or Max connect attempts (TCP) exceeded',
+    short: 'Upstream retry/connect limit'
+  },
   UT: { code: '504', help: 'Upstream request timeout' }
 };
 
