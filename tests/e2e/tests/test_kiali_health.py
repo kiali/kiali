e22bcf308e44e8385b8cb6ab86c5706d4ead6ffe
pbajjuri20 <pbajjuri@redhat.com>
2021-03-26 13:56:30 -0400

    add health_negative_testcases

--- tests/e2e/tests/test_kiali_health.py
+++ tests/e2e/tests/test_kiali_health.py
@@ -3,6 +3,8 @@ import tests.conftest as conftest
 
 bookinfo_namespace = conftest.get_bookinfo_namespace()
 
+INVALID_PARAMS_NAMESPACE_HEALTH = {'namespace':'invalid'}
+
 
 def test_workload_health_deployment(kiali_client):
     workload_type = 'Deployment'
@@ -43,6 +45,84 @@ def test_namespace_health_app(kiali_client):
     response = get_response(kiali_client, method_name='namespaceHealth', path={'namespace':bookinfo_namespace}, params={'type':type_, 'rateInterval':'60s'})
 
     assert response.json().get('details') is not None
+
+def test_namespace_health_app_invalid_namespace_negative(kiali_client):
+
+    response = get_response(kiali_client, method_name='namespaceHealth', path=INVALID_PARAMS_NAMESPACE_HEALTH, params={'type':'app','rateInterval':'60s'},status_code_expected=403)
+
+def test_namespace_health_service_invalid_namespace_negative(kiali_client):
+
+    response = get_response(kiali_client, method_name='namespaceHealth', path=INVALID_PARAMS_NAMESPACE_HEALTH, params={'type':'service','rateInterval':'60s'},status_code_expected=403)
+
+def test_namespace_health_workload_invalid_namespace_negative(kiali_client):
+
+    response = get_response(kiali_client, method_name='namespaceHealth', path=INVALID_PARAMS_NAMESPACE_HEALTH, params={'type':'workload','rateInterval':'60s'},status_code_expected=403)
+
+def test_namespace_health_app_invalid_namespace_invalid_rateinterval_negative(kiali_client):
+
+    INVALID_APP_QUERY_PARAMS_NAMESPACE_HEALTH = {'type':'app','rateInterval':'invalid'}
+
+    response = get_response(kiali_client, method_name='namespaceHealth', path=INVALID_PARAMS_NAMESPACE_HEALTH, params=INVALID_APP_QUERY_PARAMS_NAMESPACE_HEALTH,status_code_expected=403)
+
+def test_namespace_health_service_invalid_namespace_invalid_rateinterval_negative(kiali_client):
+
+    INVALID_SERVICE_QUERY_PARAMS_NAMESPACE_HEALTH = {'type':'service','rateInterval':'invalid'}
+
+    response = get_response(kiali_client, method_name='namespaceHealth', path=INVALID_PARAMS_NAMESPACE_HEALTH, params=INVALID_SERVICE_QUERY_PARAMS_NAMESPACE_HEALTH,status_code_expected=403)
+
+def test_namespace_health_workload_invalid_namespace_invalid_rateinterval_negative(kiali_client):
+
+    INVALID_WORKLOAD_QUERY_PARAMS_NAMESPACE_HEALTH = {'type':'workload','rateInterval':'invalid'}
+
+    response = get_response(kiali_client, method_name='namespaceHealth', path=INVALID_PARAMS_NAMESPACE_HEALTH, params=INVALID_WORKLOAD_QUERY_PARAMS_NAMESPACE_HEALTH,status_code_expected=403)
+
+def test_namespace_health_invalid_type_negative(kiali_client):
+
+    INVALID_RATEINTERVALQUERY_PARAMS_NAMESPACE_HEALTH = {'type':'invalid','rateInterval':'60s'}
+
+    response = get_response(kiali_client, method_name='namespaceHealth', path={'namespace':bookinfo_namespace}, params=INVALID_RATEINTERVALQUERY_PARAMS_NAMESPACE_HEALTH,status_code_expected=400)
+
+def test_namespace_health_invalid_type_invalid_rateinterval_negative(kiali_client):
+
+    INVALID_TYPE_QUERY_PARAMS_NAMESPACE_HEALTH = {'type':'invalid','rateInterval':'invalid'}
+
+    response = get_response(kiali_client, method_name='namespaceHealth', path={'namespace':bookinfo_namespace}, params=INVALID_TYPE_QUERY_PARAMS_NAMESPACE_HEALTH,status_code_expected=400)
+
+def test_namespace_health_invalid_negative(kiali_client):
+
+    INVALID_QUERY_PARAMS_NAMESPACE_HEALTH = {'type':'invalid','rateInterval':'invalid'}
+
+    response = get_response(kiali_client, method_name='namespaceHealth', path=INVALID_PARAMS_NAMESPACE_HEALTH, params=INVALID_QUERY_PARAMS_NAMESPACE_HEALTH,status_code_expected=400)
+
+def test_namespace_health_invalid_namespace_invalid_type_negative(kiali_client):
+
+    INVALID_TYPE_QUERY_PARAMS_NAMESPACE_HEALTH = {'type':'invalid','rateInterval':'60s'}
+
+    response = get_response(kiali_client, method_name='namespaceHealth', path=INVALID_PARAMS_NAMESPACE_HEALTH, params=INVALID_TYPE_QUERY_PARAMS_NAMESPACE_HEALTH,status_code_expected=400)
+
+def test_namespace_health_app_invalid_rateinterval_negative(kiali_client):
+
+    INVALID_APP_QUERY_PARAMS_NAMESPACE_HEALTH = {'type':'app','rateInterval':'invalid'}
+
+    response = get_response(kiali_client, method_name='namespaceHealth', path={'namespace':bookinfo_namespace}, params=INVALID_APP_QUERY_PARAMS_NAMESPACE_HEALTH,status_code_expected=500)
+
+def test_namespace_health_service_invalid_rateinterval_negative(kiali_client):
+
+    INVALID_SERVICE_QUERY_PARAMS_NAMESPACE_HEALTH = {'type':'service','rateInterval':'invalid'}
+
+    response = get_response(kiali_client, method_name='namespaceHealth', path={'namespace':bookinfo_namespace}, params=INVALID_SERVICE_QUERY_PARAMS_NAMESPACE_HEALTH,status_code_expected=500)
+
+def test_namespace_health_workload_invalid_rateinterval_negative(kiali_client):
+
+    INVALID_WORKLOAD_QUERY_PARAMS_NAMESPACE_HEALTH = {'type':'workload','rateInterval':'invalid'}
+
+    response = get_response(kiali_client, method_name='namespaceHealth', path={'namespace':bookinfo_namespace}, params=INVALID_WORKLOAD_QUERY_PARAMS_NAMESPACE_HEALTH,status_code_expected=500)
+
+
+def test_workload_health_invalid_replicaset_negative(kiali_client):
+    workload_type = 'ReplicaSet'
+    workload_name = 'details-v1'
+    response = get_response(kiali_client, method_name='workloadHealth', path={'namespace':bookinfo_namespace, 'workload':workload_name}, params={'type':workload_type, 'rateInterval':'60s'},status_code_expected=502)
 
 
 #############
