915cd97a256417066478b8b1fd9116f9981eae4b
Lucas Ponce <ponce.ballesteros@gmail.com>
2021-04-09 09:03:04 +0200

    Add MissingSidecar tooltip (#2114)
    * Add MissingSidecar tooltip
    
    * Fix grammar suggestion

--- src/components/MissingSidecar/MissingSidecar.tsx
+++ src/components/MissingSidecar/MissingSidecar.tsx
@@ -3,6 +3,8 @@ import { Tooltip, TooltipPosition } from '@patternfly/react-core';
 import { IconType } from '@patternfly/react-icons/dist/js/createIcon';
 import { isIstioNamespace } from 'config/ServerConfig';
 import { icons } from 'config';
+import { KialiIcon } from '../../config/KialiIcon';
+import { infoStyle } from '../../styles/DropdownStyles';
 
 type MissingSidecarProps = {
   text: string;
@@ -17,7 +19,8 @@ type MissingSidecarProps = {
 class MissingSidecar extends React.Component<MissingSidecarProps, {}> {
   static defaultProps = {
     text: 'Missing Sidecar',
-    textTooltip: 'Missing Sidecar',
+    textTooltip:
+      'Istio sidecar container not found in Pod(s). Check if the istio-injection label/annotation is correctly set on the namespace/workload.',
     tooltip: false,
     icon: icons.istio.missingSidecar.icon,
     color: icons.istio.missingSidecar.color
@@ -29,7 +32,18 @@ class MissingSidecar extends React.Component<MissingSidecarProps, {}> {
     const iconComponent = (
       <span style={style} {...otherProps}>
         {React.createElement(icon, { style: { color: color } })}
-        {!tooltip && <span style={{ marginLeft: '5px' }}>{text}</span>}
+        {!tooltip && (
+          <span style={{ marginLeft: '5px' }}>
+            {text}
+            <Tooltip
+              key={`tooltip_missing_sidecar`}
+              position={TooltipPosition.top}
+              content={<div style={{ textAlign: 'left' }}>{textTooltip}</div>}
+            >
+              <KialiIcon.Info className={infoStyle} />
+            </Tooltip>
+          </span>
+        )}
       </span>
     );
 
@@ -38,7 +52,7 @@ class MissingSidecar extends React.Component<MissingSidecarProps, {}> {
     }
 
     return tooltip ? (
-      <Tooltip content={<>{textTooltip}</>} position={TooltipPosition.right}>
+      <Tooltip content={<div style={{ textAlign: 'left' }}>{textTooltip}</div>} position={TooltipPosition.right}>
         {iconComponent}
       </Tooltip>
     ) : (
