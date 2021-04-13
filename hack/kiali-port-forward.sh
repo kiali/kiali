15b6878b061352f3eb338ea8c867112fd2590ca9
John Mazzitelli <mazz@redhat.com>
2021-04-13 13:03:55 -0400

    be able to port forward multiple kiali servers (#3875)

--- hack/kiali-port-forward.sh
+++ hack/kiali-port-forward.sh
@@ -11,6 +11,38 @@
 # This assumes you have kubectl or oc in your path.
 ##############################################################################
 
+set -u
+
+while [[ $# -gt 0 ]]; do
+  key="$1"
+  case $key in
+    -kc|--kubernetes-context)  K8S_CONTEXT="$2";   shift;shift ;;
+    -lp|--local-port)          LOCAL_PORT="$2";    shift;shift ;;
+    -rp|--remote-port)         REMOTE_PORT="$2";   shift;shift ;;
+    -h|--help )
+      cat <<HELPMSG
+$0 [option...]
+Valid options:
+  -kc|--kubernetes-context
+      The kubectl context to use when connecting to the k8s cluster.
+      Default: the current kubectl context
+  -lp|--local-port
+      The local port the proxy will listen to.
+      Your local machine must not have anything bound to this port.
+      Default: 20001
+  -rp|--remote-port
+      The remote port to forward to. This is the port Kiali is listening to.
+      Default: 20001
+HELPMSG
+      exit 1
+      ;;
+    *)
+      echo "ERROR: Unknown argument [$key]. Aborting."
+      exit 1
+      ;;
+  esac
+done
+
 if ! which kubectl > /dev/null 2>&1; then
   if ! which oc > /dev/null 2>&1; then
     echo "You must have either kubectl or oc in your PATH"
@@ -23,18 +55,20 @@ else
 fi
 
 ISTIO_NAMESPACE=${ISTIO_NAMESPACE:-istio-system}
-KIALI_PORT=${KIALI_PORT:-20001}
+LOCAL_PORT=${LOCAL_PORT:-20001}
+REMOTE_PORT=${REMOTE_PORT:-20001}
 
 # optionally takes a single argument - a kube context name (e.g. "kind-east" or "minikube")
 CONTEXT_ARG=""
-if [ "${1}" != "" ]; then
-  echo "Using context: ${1}"
-  CONTEXT_ARG="--context ${1}"
+if [ "${K8S_CONTEXT:-}" != "" ]; then
+  echo "Using context: ${K8S_CONTEXT}"
+  CONTEXT_ARG="--context ${K8S_CONTEXT}"
 else
   echo "Using current context: $(${CLIENT_EXE} config current-context)"
 fi
 
-echo "Forwarding port ${KIALI_PORT} to the Kiali server. This runs in foreground, press Control-C to kill it."
-echo "To access Kiali, point your browser to http://localhost:${KIALI_PORT}/kiali/console"
+echo "Forwarding local port [${LOCAL_PORT}] to Kiali server port [${REMOTE_PORT}]. This runs in foreground, press Control-C to kill it."
+echo "To access Kiali, point your browser to http://localhost:${LOCAL_PORT}/kiali/console"
 
-${CLIENT_EXE} ${CONTEXT_ARG} -n ${ISTIO_NAMESPACE} port-forward $(${CLIENT_EXE} ${CONTEXT_ARG} -n ${ISTIO_NAMESPACE} get pod -l app.kubernetes.io/name=kiali -o jsonpath='{.items[0].metadata.name}') ${KIALI_PORT}:${KIALI_PORT}
+${CLIENT_EXE} ${CONTEXT_ARG} -n ${ISTIO_NAMESPACE} port-forward $(${CLIENT_EXE} ${CONTEXT_ARG} -n ${ISTIO_NAMESPACE} get pod -l app.kubernetes.io/name=kiali -o jsonpath='{.items[0].metadata.name}') ${LOCAL_PORT}:${REMOTE_PORT}
+
