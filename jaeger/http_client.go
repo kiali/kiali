e544a74275751f642335f2b4ba8cfebfaf33c8dd
Lucas Ponce <ponce.ballesteros@gmail.com>
2021-04-27 12:19:20 +0200

    Fix missing units in jaeger query

--- jaeger/http_client.go
+++ jaeger/http_client.go
@@ -53,7 +53,14 @@ func getTraceDetailHTTP(client http.Client, endpoint *url.URL, traceID string) (
 }
 
 func queryTracesHTTP(client http.Client, u *url.URL) (*JaegerResponse, error) {
-	resp, code, reqError := makeRequest(client, u.String(), nil)
+	// HTTP and GRPC requests co-exist, but when minDuration is present, for HTTP it requires a unit (ms)
+	// https://github.com/kiali/kiali/issues/3939
+	if u.Query().Get("minDuration") != "" {
+		query := u.Query()
+		query.Set("minDuration", u.Query().Get("minDuration") + "ms")
+		u.RawQuery = query.Encode()
+	}
+ 	resp, code, reqError := makeRequest(client, u.String(), nil)
 	if reqError != nil {
 		log.Errorf("Jaeger query error: %s [code: %d, URL: %v]", reqError, code, u)
 		return &JaegerResponse{}, reqError
