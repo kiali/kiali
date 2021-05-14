4bf4e7313485bcc2b2ebbd253226dad58b300dcf
Edgar Hernández <23639005+israel-hdez@users.noreply.github.com>
2021-05-14 15:14:15 -0500

    Update the README.adoc file
    * Remove outdated parts
    * Reorganize so that a development setup can be achieved by a top-bottom read rather than having to jump to different sections as needed.

--- make/Makefile.container.mk
+++ make/Makefile.container.mk
@@ -44,8 +44,12 @@ else
 	podman push ${QUAY_TAG}
 endif
 
+## container-push-operator-quay: Pushes the operator image to quay.
+container-push-operator-quay:
+	$(MAKE) -C "${ROOTDIR}/operator" -e "OPERATOR_QUAY_TAG=${OPERATOR_QUAY_TAG}" push
+
 ## container-push: Pushes all container images to quay
-container-push: container-push-kiali-quay
+container-push: container-push-kiali-quay container-push-operator-quay
 
 # Ensure "docker buildx" is available and enabled. For more details, see: https://github.com/docker/buildx/blob/master/README.md
 # This does a few things:
