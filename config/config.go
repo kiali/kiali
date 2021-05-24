7899977d0e75e9a7405e4828dd80d444f4ece987
Jay Shaughnessy <jshaughn@users.noreply.github.com>
2021-05-24 17:11:58 -0400

    Introduce Graph Find/Hide presets (#4020)
    https://github.com/kiali/kiali/issues/3708

--- config/config.go
+++ config/config.go
@@ -311,11 +311,24 @@ type DeploymentConfig struct {
 // defaults to the namespace configured for IstioNamespace (which itself defaults to 'istio-system').
 type IstioComponentNamespaces map[string]string
 
+// GraphFindOption defines a single Graph Find/Hide Option
+type GraphFindOption struct {
+	Description string `yaml:"description,omitempty" json:"description,omitempty"`
+	Expression  string `yaml:"expression,omitempty" json:"expression,omitempty"`
+}
+
+// GraphUIDefaults defines UI Defaults specific to the UI Graph
+type GraphUIDefaults struct {
+	FindOptions []GraphFindOption `yaml:"find_options,omitempty" json:"findOptions,omitempty"`
+	HideOptions []GraphFindOption `yaml:"hide_options,omitempty" json:"hideOptions,omitempty"`
+}
+
 // UIDefaults defines default settings configured for the UI
 type UIDefaults struct {
-	MetricsPerRefresh string   `yaml:"metrics_per_refresh,omitempty" json:"metricsPerRefresh,omitempty"`
-	Namespaces        []string `yaml:"namespaces,omitempty" json:"namespaces,omitempty"`
-	RefreshInterval   string   `yaml:"refresh_interval,omitempty" json:"refreshInterval,omitempty"`
+	Graph             GraphUIDefaults `yaml:"graph,omitempty" json:"graph,omitempty"`
+	MetricsPerRefresh string          `yaml:"metrics_per_refresh,omitempty" json:"metricsPerRefresh,omitempty"`
+	Namespaces        []string        `yaml:"namespaces,omitempty" json:"namespaces,omitempty"`
+	RefreshInterval   string          `yaml:"refresh_interval,omitempty" json:"refreshInterval,omitempty"`
 }
 
 // KialiFeatureFlags available from the CR
@@ -491,6 +504,32 @@ func NewConfig() (c *Config) {
 		KialiFeatureFlags: KialiFeatureFlags{
 			IstioInjectionAction: true,
 			UIDefaults: UIDefaults{
+				Graph: GraphUIDefaults{
+					FindOptions: []GraphFindOption{
+						{
+							Description: "Find: slow edges (> 1s)",
+							Expression:  "rt > 1000",
+						},
+						{
+							Description: "Find: unhealthy nodes",
+							Expression:  "! healthy",
+						},
+						{
+							Description: "Find: unknown nodes",
+							Expression:  "name = unknown",
+						},
+					},
+					HideOptions: []GraphFindOption{
+						{
+							Description: "Hide: healthy nodes",
+							Expression:  "healthy",
+						},
+						{
+							Description: "Hide: unknown nodes",
+							Expression:  "name = unknown",
+						},
+					},
+				},
 				MetricsPerRefresh: "1m",
 				Namespaces:        make([]string, 0),
 				RefreshInterval:   "15s",
