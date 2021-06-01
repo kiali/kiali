8dded09e754d908c315d4fbe85738e4555922306
Jay Shaughnessy <jshaughn@users.noreply.github.com>
2021-06-01 11:51:51 -0400

    Get the trace span selections to be visible (#2168)
    Get the trace span selections to be visible by getting the h-scroll inside the select dropdown.
    - Additional fix ensures full trace name is available in trace detail, via tooltip
    
    https://github.com/kiali/kiali/issues/3586

--- src/pages/Graph/SummaryPanelTraceDetails.tsx
+++ src/pages/Graph/SummaryPanelTraceDetails.tsx
@@ -72,6 +72,10 @@ const spanSelectStyle = style({
       paddingTop: 3,
       paddingBottom: 3
     },
+    '& > ul': {
+      maxWidth: '100%',
+      overflowY: 'hidden'
+    },
     '& > ul > li > button': {
       fontSize: 'var(--graph-side-panel--font-size)',
       paddingTop: 3,
@@ -130,11 +134,11 @@ class SummaryPanelTraceDetails extends React.Component<Props, State> {
         </span>
         <div>
           {tracesDetailsURL ? (
-            <Tooltip content={'View trace details'}>
+            <Tooltip content={`View trace details for: ${info.name()}`}>
               <Link to={tracesDetailsURL}>{title}</Link>
             </Tooltip>
           ) : (
-            title
+            <Tooltip content={`${info.name()}`}>{title}</Tooltip>
           )}
           <div>
             {info.numErrors !== 0 && (
