d85530980b7924632191001c3b71a195b9c6a878
John Mazzitelli <mazz@redhat.com>
2021-06-22 15:39:58 -0400

    [hack] be able to enable OLM in minikube (#4053)
    * [hack] be able to enable OLM in minikube (so we can eventually test the kiali operator from OperatorHub.io)
    
    * add some code to the ci script to enable testing the olm operator.
    If you pass in "-oe true" the tests will actually fail. that will require updates to the molecule tests. But for now,
    we will allow this but the default is "false" so it still works as it did before.
    
    * install olm and kiali properly
    
    * be able to rebuild the cluster from scratch.
    helpful to cron jobs
    
    * don't wait forever to avoid an infinite loop (which is bad when this is run as a cron job - don't ask me how I know)
    
    * split out the check for existence and establishment

--- hack/ci-minikube-molecule-tests.sh
+++ hack/ci-minikube-molecule-tests.sh
@@ -30,6 +30,16 @@ Options:
 -me|--minikube-exe <path to minikube>
     The 'minikube' command, if not in PATH then must be a full path.
     Default: minikube
+
+-oe|--olm-enabled <true|false>
+    If true, install OLM into the cluster and test the operator as installed by OLM
+    This has no effect if the cluster is already built. To ensure OLM is enabled,
+    you can pass in "--rebuild-cluster true" to start a new cluster with OLM.
+    Default: false
+
+-rc|--rebuild-cluster <true|false>
+    If true, any existing cluster will be destroyed and a new one will be rebuilt.
+    Default: false
 HELP
 }
 
@@ -37,10 +47,12 @@ HELP
 while [[ $# -gt 0 ]]; do
   key="$1"
   case $key in
-    -ce|--client-exe)             CLIENT_EXE="$2";     shift;shift; ;;
-    -dorp|--docker-or-podman)     DORP="$2";           shift;shift; ;;
-    -me|--minikube-exe)           MINIKUBE_EXE="$2";   shift;shift; ;;
-    -h|--help)                    helpmsg; exit 1;     shift; ;;
+    -ce|--client-exe)             CLIENT_EXE="$2";      shift;shift; ;;
+    -dorp|--docker-or-podman)     DORP="$2";            shift;shift; ;;
+    -me|--minikube-exe)           MINIKUBE_EXE="$2";    shift;shift; ;;
+    -oe|--olm-enabled)            OLM_ENABLED="$2";     shift;shift; ;;
+    -rc|--rebuild-cluster)        REBUILD_CLUSTER="$2"; shift;shift; ;;
+    -h|--help)                    helpmsg; exit 1;      shift; ;;
     *) echo "Unknown argument: [$key]. Aborting."; helpmsg; exit 1 ;;
   esac
 done
@@ -48,10 +60,17 @@ done
 CLIENT_EXE="${CLIENT_EXE:-kubectl}"
 DORP="${DORP:-docker}"
 MINIKUBE_EXE="${MINIKUBE_EXE:-minikube}"
+OLM_ENABLED="${OLM_ENABLED:-false}"
+REBUILD_CLUSTER="${REBUILD_CLUSTER:-false}"
 
 # the minikube hack script command
 minikube_profile="ci"
 minikube_sh="${hack_dir}/k8s-minikube.sh --minikube-profile ${minikube_profile} --minikube-exe ${MINIKUBE_EXE} --client-exe ${CLIENT_EXE}"
+
+if [ "${REBUILD_CLUSTER}" == "true" ]; then
+  echo "Destroying any existing cluster to ensure a new one will be rebuilt."
+  ${minikube_sh} delete
+fi
 
 # make sure we switch contexts if we can so we are pointing to the current cluster
 if [ "$(${CLIENT_EXE} config current-context)" != "${minikube_profile}" ]; then
@@ -60,15 +79,45 @@ if [ "$(${CLIENT_EXE} config current-context)" != "${minikube_profile}" ]; then
   fi
 fi
 
+if [ "${OLM_ENABLED}" == "true" ]; then
+  operator_installer_arg="--operator-installer skip"
+  olm_enabled_arg="--olm-enabled true"
+else
+  operator_installer_arg="--operator-installer helm"
+  olm_enabled_arg="--olm-enabled false"
+fi
+
 if ! ${minikube_sh} status; then
-  ${minikube_sh} start --dex-enabled true
+
+  ${minikube_sh} start --dex-enabled true ${olm_enabled_arg}
   if ! ${minikube_sh} status; then
     echo "Failed to install the minikube cluster."
     exit 1
   fi
   ${minikube_sh} istio
+
+  if [ "${OLM_ENABLED}" == "true" ]; then
+    echo "Installing Kiali Operator"
+    ${CLIENT_EXE} create -f https://operatorhub.io/install/stable/kiali.yaml
+
+    echo -n "Waiting for Kiali CRD to be created."
+    timeout 1h bash -c "until ${CLIENT_EXE} get crd kialis.kiali.io >& /dev/null; do echo -n '.' ; sleep 3; done"
+    echo
+
+    echo "Waiting for Kiali CRD to be established."
+    ${CLIENT_EXE} wait --for condition=established --timeout=300s crd kialis.kiali.io
+
+    echo "Configuring the Kiali operator to allow ad hoc images and ad hoc namespaces."
+    operator_namespace="$(${CLIENT_EXE} get deployments --all-namespaces  | grep kiali-operator | cut -d ' ' -f 1)"
+    for env_name in ALLOW_AD_HOC_KIALI_NAMESPACE ALLOW_AD_HOC_KIALI_IMAGE; do
+      ${CLIENT_EXE} -n ${operator_namespace} patch $(${CLIENT_EXE} -n ${operator_namespace} get csv -o name | grep kiali) --type=json -p "[{'op':'replace','path':"/spec/install/spec/deployments/0/spec/template/spec/containers/0/env/$(${CLIENT_EXE} -n ${operator_namespace} get $(${CLIENT_EXE} -n ${operator_namespace} get csv -o name | grep kiali) -o jsonpath='{.spec.install.spec.deployments[0].spec.template.spec.containers[0].env[*].name}' | tr ' ' '\n' | cat --number | grep ${env_name} | cut -f 1 | xargs echo -n | cat - <(echo "-1") | bc)/value",'value':"\"true\""}]"
+    done
+
+    echo "Waiting for the Kiali Operator to be ready."
+    ${CLIENT_EXE} wait -n ${operator_namespace} --for=condition=ready --timeout=300s $(${CLIENT_EXE} get pod -n ${operator_namespace} -l app.kubernetes.io/name=kiali-operator -o name)
+  fi
 else
   ${minikube_sh} resetclock
 fi
 
-${hack_dir}/run-molecule-tests.sh --cluster-type minikube --minikube-profile ${minikube_profile} --color false --minikube-exe ${MINIKUBE_EXE} --client-exe ${CLIENT_EXE} -dorp ${DORP}
+${hack_dir}/run-molecule-tests.sh --cluster-type minikube --minikube-profile ${minikube_profile} --color false --minikube-exe ${MINIKUBE_EXE} --client-exe ${CLIENT_EXE} -dorp ${DORP} ${operator_installer_arg}
