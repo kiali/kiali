345f44592eb56d7da136f245ab3d52639a09ca21
jshaughn <jshaughn@redhat.com>
2021-11-03 15:07:07 -0400

    Fix kiali.io links given new website

--- deploy/kubernetes/deploy-kiali-to-kubernetes.sh
+++ deploy/kubernetes/deploy-kiali-to-kubernetes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "This script is no longer used to install Kiali. Instead, use the Kiali Operator."
-echo "For more details, see https://www.kiali.io/gettingstarted"
+echo "For more details, see https://kiali.io/docs/installation/installation-guide/"
 exit 1
