53ec8e9a6b41d25214c2f36d853d8966fe7643c2
shireenf-ibm <shireenf@ibm.com>
2021-11-08 12:47:29 +0200

    reverting file

---
+++ business/checkers/authorization_policies_checker.go
@@ -0,0 +1,66 @@
+package checkers
+
+import (
+	core_v1 "k8s.io/api/core/v1"
+
+	"github.com/kiali/kiali/business/checkers/authorization"
+	"github.com/kiali/kiali/business/checkers/common"
+	"github.com/kiali/kiali/kubernetes"
+	"github.com/kiali/kiali/models"
+)
+
+const AuthorizationPolicyCheckerType = "authorizationpolicy"
+
+type AuthorizationPolicyChecker struct {
+	AuthorizationPolicies   []kubernetes.IstioObject
+	Namespace               string
+	Namespaces              models.Namespaces
+	ServiceEntries          []kubernetes.IstioObject
+	ExportedServiceEntries  []kubernetes.IstioObject
+	Services                []core_v1.Service
+	WorkloadList            models.WorkloadList
+	MtlsDetails             kubernetes.MTLSDetails
+	VirtualServices         []kubernetes.IstioObject
+	ExportedVirtualServices []kubernetes.IstioObject
+	RegistryStatus          []*kubernetes.RegistryStatus
+}
+
+func (a AuthorizationPolicyChecker) Check() models.IstioValidations {
+	validations := models.IstioValidations{}
+
+	// Individual validations
+	for _, authPolicy := range a.AuthorizationPolicies {
+		validations.MergeValidations(a.runChecks(authPolicy))
+	}
+
+	// Group Validations
+	validations.MergeValidations(authorization.MtlsEnabledChecker{
+		Namespace:             a.Namespace,
+		AuthorizationPolicies: a.AuthorizationPolicies,
+		MtlsDetails:           a.MtlsDetails,
+	}.Check())
+
+	return validations
+}
+
+// runChecks runs all the individual checks for a single mesh policy and appends the result into validations.
+func (a AuthorizationPolicyChecker) runChecks(authPolicy kubernetes.IstioObject) models.IstioValidations {
+	policyName := authPolicy.GetObjectMeta().Name
+	key, rrValidation := EmptyValidValidation(policyName, authPolicy.GetObjectMeta().Namespace, AuthorizationPolicyCheckerType)
+	serviceHosts := kubernetes.ServiceEntryHostnames(append(a.ServiceEntries, a.ExportedServiceEntries...))
+
+	enabledCheckers := []Checker{
+		common.SelectorNoWorkloadFoundChecker(AuthorizationPolicyCheckerType, authPolicy, a.WorkloadList),
+		authorization.NamespaceMethodChecker{AuthorizationPolicy: authPolicy, Namespaces: a.Namespaces.GetNames()},
+		authorization.NoHostChecker{AuthorizationPolicy: authPolicy, Namespace: a.Namespace, Namespaces: a.Namespaces,
+			ServiceEntries: serviceHosts, Services: a.Services, VirtualServices: a.VirtualServices, RegistryStatus: a.RegistryStatus},
+	}
+
+	for _, checker := range enabledCheckers {
+		checks, validChecker := checker.Check()
+		rrValidation.Checks = append(rrValidation.Checks, checks...)
+		rrValidation.Valid = rrValidation.Valid && validChecker
+	}
+
+	return models.IstioValidations{key: rrValidation}
+}
