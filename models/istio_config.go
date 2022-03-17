9efb3e350d3fa3775a871ac2bb93da845cbb7f7b
Leandro Beretta <lea.beretta@gmail.com>
2022-03-17 13:56:43 -0300

    reordering alphabetically and adding envoy filter help messages (#4815)

--- models/istio_config.go
+++ models/istio_config.go
@@ -35,18 +35,17 @@ type IstioConfigDetails struct {
 	Namespace  Namespace `json:"namespace"`
 	ObjectType string    `json:"objectType"`
 
-	DestinationRule *networking_v1alpha3.DestinationRule `json:"destinationRule"`
-	EnvoyFilter     *networking_v1alpha3.EnvoyFilter     `json:"envoyFilter"`
-	Gateway         *networking_v1alpha3.Gateway         `json:"gateway"`
-	ServiceEntry    *networking_v1alpha3.ServiceEntry    `json:"serviceEntry"`
-	Sidecar         *networking_v1alpha3.Sidecar         `json:"sidecar"`
-	VirtualService  *networking_v1alpha3.VirtualService  `json:"virtualService"`
-	WorkloadEntry   *networking_v1alpha3.WorkloadEntry   `json:"workloadEntry"`
-	WorkloadGroup   *networking_v1alpha3.WorkloadGroup   `json:"workloadGroup"`
-
 	AuthorizationPolicy   *security_v1beta.AuthorizationPolicy   `json:"authorizationPolicy"`
+	DestinationRule       *networking_v1alpha3.DestinationRule   `json:"destinationRule"`
+	EnvoyFilter           *networking_v1alpha3.EnvoyFilter       `json:"envoyFilter"`
+	Gateway               *networking_v1alpha3.Gateway           `json:"gateway"`
 	PeerAuthentication    *security_v1beta.PeerAuthentication    `json:"peerAuthentication"`
 	RequestAuthentication *security_v1beta.RequestAuthentication `json:"requestAuthentication"`
+	ServiceEntry          *networking_v1alpha3.ServiceEntry      `json:"serviceEntry"`
+	Sidecar               *networking_v1alpha3.Sidecar           `json:"sidecar"`
+	VirtualService        *networking_v1alpha3.VirtualService    `json:"virtualService"`
+	WorkloadEntry         *networking_v1alpha3.WorkloadEntry     `json:"workloadEntry"`
+	WorkloadGroup         *networking_v1alpha3.WorkloadGroup     `json:"workloadGroup"`
 
 	Permissions           ResourcePermissions `json:"permissions"`
 	IstioValidation       *IstioValidation    `json:"validation"`
@@ -62,29 +61,6 @@ type IstioConfigHelp struct {
 
 // IstioConfigHelpMessages represents the help messages for a given Istio object type
 var IstioConfigHelpMessages = map[string][]IstioConfigHelp{
-	"virtualservices": {
-		{ObjectField: "spec.hosts", Message: "The destination hosts to which traffic is being sent. Could be a DNS name with wildcard prefix or an IP address. Depending on the platform, short-names can also be used instead of a FQDN (i.e. has no dots in the name)."},
-		{ObjectField: "spec.gateways", Message: "The names of gateways and sidecars that should apply these routes. Gateways in other namespaces may be referred to by <gateway namespace>/<gateway name>; specifying a gateway with no namespace qualifier is the same as specifying the VirtualService’s namespace. To apply the rules to both gateways and sidecars, specify mesh as one of the gateway names."},
-		{ObjectField: "spec.http", Message: "An ordered list of route rules for HTTP traffic."},
-		{ObjectField: "spec.exportTo", Message: "A list of namespaces to which this virtual service is exported. Exporting a virtual service allows it to be used by sidecars and gateways defined in other namespaces."},
-		{ObjectField: "spec.http.match", Message: "Match conditions to be satisfied for the rule to be activated."},
-		{ObjectField: "spec.http.route", Message: "A HTTP rule can either redirect or forward (default) traffic. The forwarding target can be one of several versions of a service. Weights associated with the service version determine the proportion of traffic it receives."},
-		{ObjectField: "spec.http.route.destination.host", Message: "The name of a service from the service registry. Service names are looked up from the platform’s service registry (e.g., Kubernetes services, Consul services, etc.) and from the hosts declared by ServiceEntry."},
-		{ObjectField: "spec.http.route.destination.subset", Message: "The name of a subset within the service. Applicable only to services within the mesh. The subset must be defined in a corresponding DestinationRule."},
-	},
-	"destinationrules": {
-		{ObjectField: "spec.host", Message: "The name of a service from the service registry. Rules defined for services that do not exist in the service registry will be ignored."},
-		{ObjectField: "spec.trafficPolicy", Message: "Traffic policies to apply (load balancing policy, connection pool sizes, outlier detection)."},
-		{ObjectField: "spec.subsets", Message: "One or more named sets that represent individual versions of a service. Traffic policies can be overridden at subset level."},
-		{ObjectField: "spec.exportTo", Message: "A list of namespaces to which this destination rule is exported. The resolution of a destination rule to apply to a service occurs in the context of a hierarchy of namespaces. This feature provides a mechanism for service owners and mesh administrators to control the visibility of destination rules across namespace boundaries. If no namespaces are specified then the destination rule is exported to all namespaces by default."},
-	},
-	"gateways": {
-		{ObjectField: "spec.servers", Message: "A list of server specifications."},
-		{ObjectField: "spec.selector", Message: "One or more labels that indicate a specific set of pods/VMs on which this gateway configuration should be applied. By default workloads are searched across all namespaces based on label selectors."},
-		{ObjectField: "spec.servers.port", Message: "The port on which the proxy should listen for incoming connections."},
-		{ObjectField: "spec.servers.hosts", Message: "One or more hosts exposed by this gateway. While typically applicable to HTTP services, it can also be used for TCP services using TLS with SNI."},
-		{ObjectField: "spec.servers.tls", Message: "Set of TLS related options that govern the server’s behavior. Use these options to control if all http requests should be redirected to https, and the TLS modes to use."},
-	},
 	"authorizationpolicies": {
 		{ObjectField: "spec.selector", Message: "Optional. The selector decides where to apply the authorization policy. The selector will match with workloads in the same namespace as the authorization policy. If the authorization policy is in the root namespace, the selector will additionally match with workloads in all namespaces."},
 		{ObjectField: "spec.selector.matchLabels", Message: "One or more labels that indicate a specific set of pods/VMs on which a policy should be applied."},
@@ -93,6 +69,27 @@ var IstioConfigHelpMessages = map[string][]IstioConfigHelp{
 		{ObjectField: "spec.rules.to", Message: "Optional. to specifies the operation of a request. If not set, any operation is allowed."},
 		{ObjectField: "spec.rules.when", Message: "Optional. when specifies a list of additional conditions of a request. If not set, any condition is allowed."},
 		{ObjectField: "spec.action", Message: "Optional. The action to take if the request is matched with the rules. Default is ALLOW if not specified."},
+	},
+	"destinationrules": {
+		{ObjectField: "spec.host", Message: "The name of a service from the service registry. Rules defined for services that do not exist in the service registry will be ignored."},
+		{ObjectField: "spec.trafficPolicy", Message: "Traffic policies to apply (load balancing policy, connection pool sizes, outlier detection)."},
+		{ObjectField: "spec.subsets", Message: "One or more named sets that represent individual versions of a service. Traffic policies can be overridden at subset level."},
+		{ObjectField: "spec.exportTo", Message: "A list of namespaces to which this destination rule is exported. The resolution of a destination rule to apply to a service occurs in the context of a hierarchy of namespaces. This feature provides a mechanism for service owners and mesh administrators to control the visibility of destination rules across namespace boundaries. If no namespaces are specified then the destination rule is exported to all namespaces by default."},
+	},
+	"envoyfilters": {
+		{ObjectField: "spec.workloadSelector", Message: "Criteria used to select the specific set of pods/VMs on which this patch configuration should be applied. If omitted, the set of patches in this configuration will be applied to all workload instances in the same namespace."},
+		{ObjectField: "spec.configPatches", Message: "One or more patches with match conditions."},
+		{ObjectField: "spec.configPatches.applyTo", Message: "Specifies where in the Envoy configuration, the patch should be applied."},
+		{ObjectField: "spec.configPatches.match", Message: "Match on listener/route configuration/cluster."},
+		{ObjectField: "spec.configPatches.patch", Message: "The patch to apply along with the operation."},
+		{ObjectField: "spec.priority", Message: "riority defines the order in which patch sets are applied within a context. When one patch depends on another patch, the order of patch application is significant."},
+	},
+	"gateways": {
+		{ObjectField: "spec.servers", Message: "A list of server specifications."},
+		{ObjectField: "spec.selector", Message: "One or more labels that indicate a specific set of pods/VMs on which this gateway configuration should be applied. By default workloads are searched across all namespaces based on label selectors."},
+		{ObjectField: "spec.servers.port", Message: "The port on which the proxy should listen for incoming connections."},
+		{ObjectField: "spec.servers.hosts", Message: "One or more hosts exposed by this gateway. While typically applicable to HTTP services, it can also be used for TCP services using TLS with SNI."},
+		{ObjectField: "spec.servers.tls", Message: "Set of TLS related options that govern the server’s behavior. Use these options to control if all http requests should be redirected to https, and the TLS modes to use."},
 	},
 	"sidecars": {
 		{ObjectField: "spec.workloadSelector", Message: "Criteria used to select the specific set of pods/VMs on which this Sidecar configuration should be applied. If omitted, the Sidecar configuration will be applied to all workload instances in the same namespace."},
@@ -118,6 +115,16 @@ var IstioConfigHelpMessages = map[string][]IstioConfigHelp{
 		{ObjectField: "spec.endpoints", Message: "One or more endpoints associated with the service. Only one of endpoints or workloadSelector can be specified."},
 		{ObjectField: "spec.workloadSelector", Message: "Applicable only for MESH_INTERNAL services. Only one of endpoints or workloadSelector can be specified."},
 		{ObjectField: "spec.exportTo", Message: "A list of namespaces to which this service is exported. Exporting a service allows it to be used by sidecars, gateways and virtual services defined in other namespaces. This feature provides a mechanism for service owners and mesh administrators to control the visibility of services across namespace boundaries."},
+	},
+	"virtualservices": {
+		{ObjectField: "spec.hosts", Message: "The destination hosts to which traffic is being sent. Could be a DNS name with wildcard prefix or an IP address. Depending on the platform, short-names can also be used instead of a FQDN (i.e. has no dots in the name)."},
+		{ObjectField: "spec.gateways", Message: "The names of gateways and sidecars that should apply these routes. Gateways in other namespaces may be referred to by <gateway namespace>/<gateway name>; specifying a gateway with no namespace qualifier is the same as specifying the VirtualService’s namespace. To apply the rules to both gateways and sidecars, specify mesh as one of the gateway names."},
+		{ObjectField: "spec.http", Message: "An ordered list of route rules for HTTP traffic."},
+		{ObjectField: "spec.exportTo", Message: "A list of namespaces to which this virtual service is exported. Exporting a virtual service allows it to be used by sidecars and gateways defined in other namespaces."},
+		{ObjectField: "spec.http.match", Message: "Match conditions to be satisfied for the rule to be activated."},
+		{ObjectField: "spec.http.route", Message: "A HTTP rule can either redirect or forward (default) traffic. The forwarding target can be one of several versions of a service. Weights associated with the service version determine the proportion of traffic it receives."},
+		{ObjectField: "spec.http.route.destination.host", Message: "The name of a service from the service registry. Service names are looked up from the platform’s service registry (e.g., Kubernetes services, Consul services, etc.) and from the hosts declared by ServiceEntry."},
+		{ObjectField: "spec.http.route.destination.subset", Message: "The name of a subset within the service. Applicable only to services within the mesh. The subset must be defined in a corresponding DestinationRule."},
 	},
 	"workloadentries": {
 		{ObjectField: "spec.address", Message: "Address associated with the network endpoint without the port."},
