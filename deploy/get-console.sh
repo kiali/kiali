7bb26b1f246051b797566e7b8a7b3f244ecd3e8e
John Mazzitelli <mazz@redhat.com>
2022-03-22 14:55:02 -0400

    this should be the only change necessary in the make files for the kiali-ui merge.

--- deploy/get-console.sh
+++ deploy/get-console.sh
@@ -8,23 +8,22 @@
 # See the main Makefile for more info.
 
 DIR=$(dirname $0)/..
-CONSOLE_DIR=${CONSOLE_LOCAL_DIR:-$DIR/../kiali-ui}
+CONSOLE_DIR=${CONSOLE_LOCAL_DIR:-$DIR/frontend}
 
 mkdir -p $DIR/_output/docker
 
 # Some sanity checks of CONSOLE_DIR. Some checks are naive.
 if [ -z "$CONSOLE_DIR" ]; then
-  echo "You must set the CONSOLE_LOCAL_DIR environment variable to the path where the kiali-ui source code is located."
-  echo "If you don't have the kiali-ui source code, download it from the kiali/kiali-ui GitHub repository."
+  echo "You must set the CONSOLE_LOCAL_DIR environment variable to the path where the UI source code is located."
   exit 1
 elif [ ! -f "$CONSOLE_DIR/package.json" ]; then
   echo "CONSOLE_DIR is $CONSOLE_DIR"
-  echo "Apparently, this CONSOLE_DIR does not contain the kiali-ui"
+  echo "Apparently, this CONSOLE_DIR does not contain the kiali UI"
   exit 1
 elif [ ! -d "$CONSOLE_DIR/build" ] || [ -z "$(ls -A $CONSOLE_DIR/build)" ]; then
   echo "CONSOLE_DIR is $CONSOLE_DIR"
-  echo "Apparently, the kiali-ui is not built."
-  echo "Build the front-end by running 'yarn && yarn build' inside the kiali-ui directory"
+  echo "Apparently, the kiali UI is not built."
+  echo "Build the front-end by running 'yarn && yarn build' inside the kiali UI directory"
   exit 1
 fi
 
