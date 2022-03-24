d6d9b92b79e259634e57098a26cd24806206910b
Xunzhuo <mixdeers@gmail.com>
2022-03-25 00:51:58 +0800

    chore: update cmd
    Signed-off-by: Xunzhuo <mixdeers@gmail.com>

--- make/Makefile.build.mk
+++ make/Makefile.build.mk
@@ -36,8 +36,8 @@ build-ui:
 	@cd ${ROOTDIR}/frontend && yarn install && yarn run build
 
 ## build-ui-test: Runs the yarn commands to build the dev frontend UI and runs the UI tests
-build-ui-test:
-	@cd ${ROOTDIR}/frontend && yarn && yarn build && yarn test
+build-ui-test: build-ui
+	@cd ${ROOTDIR}/frontend && yarn run test
 
 ## build-linux-multi-arch: Build Kiali binary with arch suffix for multi-arch
 build-linux-multi-arch:
