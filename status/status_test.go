1ccc1d02c71d82232ca0b0c75bde1f5a649e50a6
Xunzhuo <mixdeers@gmail.com>
2022-03-26 00:12:56 +0800

    fix: add read lock when getting status (#4856)
    Signed-off-by: Xunzhuo <mixdeers@gmail.com>

--- status/status_test.go
+++ status/status_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 )
 
+// test multiple goroutines to write the same key with different values
 func TestMultiplePutStatus(t *testing.T) {
 	counter := 100
 	wg := sync.WaitGroup{}
@@ -17,6 +18,24 @@ func TestMultiplePutStatus(t *testing.T) {
 		k, v := "key", fmt.Sprintf("%d", rand.Intn(1000000))
 		go func(k, v string) {
 			Put(k, v)
+			wg.Done()
+		}(k, v)
+	}
+	wg.Wait()
+}
+
+// test multiple goroutines to read the key while writing the same key with different values
+func TestMultipleGetStatus(t *testing.T) {
+	counter := 100
+	wg := sync.WaitGroup{}
+
+	wg.Add(counter)
+	// get status while writing different values with the same key
+	for i := 0; i < counter; i++ {
+		k, v := "key", fmt.Sprintf("%d", rand.Intn(1000000))
+		go func(k, v string) {
+			Put(k, v)
+			GetStatus(k)
 			wg.Done()
 		}(k, v)
 	}
