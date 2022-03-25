a89b205cb02bfe8395341eda36b00a4a5e4fdc45
Hayk Hovsepyan <hhovsepy@redhat.com>
2022-03-25 11:25:08 +0100

    Converting e2e tests into Integration tests for Kiali, initial commit.

---
+++ tests/integration/kiali_urls_test.go
@@ -0,0 +1,17 @@
+package integration
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/kiali/kiali/tests/integration/util"
+)
+
+func TestKialiStatus(t *testing.T) {
+	assert := assert.New(t)
+	response, err := util.KialiStatus()
+
+	assert.Nil(err)
+	assert.True(response)
+}
