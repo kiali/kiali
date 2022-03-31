da5455adfb3b433b77fa141ce93e3698a2322cfc
Hayk Hovsepyan <hhovsepy@redhat.com>
2022-03-31 13:21:09 +0200

    Added README and structure.

--- tests/integration/tests/kiali_urls_test.go
+++ tests/integration/tests/kiali_urls_test.go
@@ -1,16 +1,16 @@
-package integration
+package tests
 
 import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 
-	"github.com/kiali/kiali/tests/integration/util"
+	"github.com/kiali/kiali/tests/integration/utils"
 )
 
 func TestKialiStatus(t *testing.T) {
 	assert := assert.New(t)
-	response, statusCode, err := util.KialiStatus()
+	response, statusCode, err := utils.KialiStatus()
 
 	assert.Nil(err)
 	assert.True(response)
--- tests/integration/utils/kiali_client.go
+++ tests/integration/utils/kiali_client.go
@@ -1,4 +1,4 @@
-package util
+package utils
 
 import (
 	"net/http"
