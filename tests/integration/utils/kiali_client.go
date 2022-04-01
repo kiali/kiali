33d76fd4e47cb3523ee5878a78bc48d3ae3d9d8f
Hayk Hovsepyan <hhovsepy@redhat.com>
2022-04-01 13:16:40 +0200

    Added support of auth strategy in Kiali.

--- tests/integration/utils/kiali_client.go
+++ tests/integration/utils/kiali_client.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/url"
 	"os"
@@ -12,27 +13,66 @@ import (
 	"github.com/kiali/kiali/util/httputil"
 )
 
-var kialiURL string
-var kialiToken string
-var kialiCookies []*http.Cookie
+type KialiClient struct {
+	kialiURL     string
+	kialiToken   string
+	kialiCookies []*http.Cookie
+	authStrategy string
+}
 
-func init() {
-	kialiURL = os.Getenv("URL")
-	kialiToken = os.Getenv("TOKEN")
-	if kialiURL == "" || kialiToken == "" {
-		log.Fatalf("URL and TOKEN environment variables are required. Kiali URL in 'https://kiali-hostname' and TOKEN in 'sha256~' format.")
+type AuthStrategy struct {
+	Strategy string `json:"strategy"`
+}
+
+var client = *NewKialiClient()
+
+func NewKialiClient() (c *KialiClient) {
+	c = &KialiClient{
+		kialiURL: os.Getenv("URL"),
+	}
+	if c.kialiURL == "" {
+		log.Fatalf("URL environment variable is required. Kiali URL in 'https://kiali-hostname' format.")
 		return
 	}
-	tokenResult, tokenCookies := GetCookies()
-	if !tokenResult || tokenCookies == nil {
-		log.Fatalf("Unable to login to the Kiali: %s by provided token: %s", kialiURL, kialiToken)
+	if strategy, err := c.KialiAuthStrategy(); err == nil {
+		c.authStrategy = strategy
+		if strategy == config.AuthStrategyOpenshift {
+			c.kialiToken = os.Getenv("TOKEN")
+			if c.kialiToken == "" {
+				log.Fatalf("TOKEN environment variable is required by Kiali Auth strategy.")
+				return
+			}
+			tokenResult, tokenCookies := c.GetCookies()
+			if !tokenResult || tokenCookies == nil {
+				log.Fatalf("Unable to login to the Kiali: %s by provided token: %s", c.kialiURL, c.kialiToken)
+				return
+			}
+			c.kialiCookies = tokenCookies
+		}
+	} else {
+		log.Fatalf("Unable to check Kiali auth strategy, Err: %s", err)
 		return
 	}
-	kialiCookies = tokenCookies
+	return
+}
+
+func (c *KialiClient) KialiAuthStrategy() (string, error) {
+	body, _, _, err := httputil.HttpGet(c.kialiURL+"/api/auth/info", c.GetAuth(), 10*time.Second, nil, nil)
+	if err == nil {
+		authStrategy := new(AuthStrategy)
+		err = json.Unmarshal(body, &authStrategy)
+		if err == nil {
+			return authStrategy.Strategy, nil
+		} else {
+			return "", err
+		}
+	} else {
+		return "", err
+	}
 }
 
 func KialiStatus() (bool, int, error) {
-	_, code, _, err := httputil.HttpGet(kialiURL+"/api/istio/status", GetAuth(), 10*time.Second, nil, kialiCookies)
+	_, code, _, err := httputil.HttpGet(client.kialiURL+"/api/istio/status", client.GetAuth(), 10*time.Second, nil, client.kialiCookies)
 	if err == nil {
 		return true, code, nil
 	} else {
@@ -40,23 +80,29 @@ func KialiStatus() (bool, int, error) {
 	}
 }
 
-func GetAuth() *config.Auth {
-	return &config.Auth{
-		Token:              kialiToken,
-		Type:               config.AuthTypeBearer,
-		InsecureSkipVerify: true,
+func (c *KialiClient) GetAuth() *config.Auth {
+	if c.authStrategy == config.AuthStrategyOpenshift {
+		return &config.Auth{
+			Token:              c.kialiToken,
+			Type:               config.AuthTypeBearer,
+			InsecureSkipVerify: true,
+		}
+	} else {
+		return &config.Auth{
+			InsecureSkipVerify: true,
+		}
 	}
 }
 
-func GetCookies() (bool, []*http.Cookie) {
-	auth := GetAuth()
+func (c *KialiClient) GetCookies() (bool, []*http.Cookie) {
+	auth := c.GetAuth()
 	requestParams := url.Values{}
 	requestParams.Set("access_token", auth.Token)
 	requestParams.Set("expires_in", "86400")
 	customHeaders := map[string]string{
 		"Content-Type": "application/x-www-form-urlencoded",
 	}
-	_, code, cookies, err := httputil.HttpPost(kialiURL+"/api/authenticate", auth, strings.NewReader(requestParams.Encode()), 10*time.Second, customHeaders)
+	_, code, cookies, err := httputil.HttpPost(c.kialiURL+"/api/authenticate", auth, strings.NewReader(requestParams.Encode()), 10*time.Second, customHeaders)
 	if code == 200 && err == nil && cookies != nil {
 		return true, cookies
 	}
