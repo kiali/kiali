4aa44c034e08b9b41dcc43a9226304c32fe3cd89
jshaughn <jshaughn@redhat.com>
2022-04-06 08:54:44 -0400

    -allow certain "hot cve" dependencies to allow minor/patch updates   - lodash   - moment - use fixed versions for react and react-dom - ongoing issues with "yarn test"   - revert some bumps to go back to our previous versions     - revert react-scripts and redux-mock-store     - babel-loader back to 8.1.0 in resolutions   - given the update to react 17 update "enzyme-adapter-react-16" to @wojtekmaj/enzyme-adapter-react-17": "^0.6.7"     - this is "unofficial" react 17 support

--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -1,7 +1,7 @@
 import * as Enzyme from 'enzyme';
 require('jest-localstorage-mock');
 require('jest-canvas-mock');
-const Adapter = require('enzyme-adapter-react-16');
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 var JSDOM = require('jsdom').JSDOM;
 
