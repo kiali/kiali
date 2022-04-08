74dc53a9a062b1dcb73b4489c87e268c40597ecd
Alberto Gutierrez <agutierr@redhat.com>
2022-04-08 15:42:36 +0200

    Merged Kiali UI and Kiali Server can point to a single commit (#4943)

--- frontend/src/types/StatusState.ts
+++ frontend/src/types/StatusState.ts
@@ -1,7 +1,6 @@
 export enum StatusKey {
-  KIALI_CORE_COMMIT_HASH = 'Kiali core commit hash',
-  KIALI_CORE_VERSION = 'Kiali core version',
-  KIALI_CONSOLE_VERSION = 'Kiali console version',
+  KIALI_CORE_COMMIT_HASH = 'Kiali commit hash',
+  KIALI_CORE_VERSION = 'Kiali version',
   KIALI_CONTAINER_VERSION = 'Kiali container version',
   KIALI_STATE = 'Kiali state'
 }
