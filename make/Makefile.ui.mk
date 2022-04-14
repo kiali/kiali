a89dd5f7635af6395aa22f20559c0b3caf11074a
Nick Fox <nfox@redhat.com>
2022-04-14 09:47:01 -0400

    Always add/replace proxy line with `make yarn-start`

--- make/Makefile.ui.mk
+++ make/Makefile.ui.mk
@@ -4,13 +4,11 @@
 
 ## yarn-start: Run the UI in a local process that is separate from the backend. Set YARN_START_URL to update package.json.
 yarn-start:
-	@if ! (cat ${ROOTDIR}/frontend/package.json | grep -q "\"proxy\":"); then \
-	  if [ -z "${YARN_START_URL}" ]; then \
-	    echo "${ROOTDIR}/frontend/package.json does not have a 'proxy' setting and you did not set YARN_START_URL. Aborting."; \
-	    exit 1; \
-	  else \
-	    sed -i "2 i \ \ \"proxy\": \"${YARN_START_URL}\"," ${ROOTDIR}/frontend/package.json; \
-	  fi; \
+	@if [ -z "${YARN_START_URL}" ]; then \
+		echo "${ROOTDIR}/frontend/package.json does not have a 'proxy' setting and you did not set YARN_START_URL. Aborting."; \
+		exit 1; \
+	else \
+		sed -i "2 i \ \ \"proxy\": \"${YARN_START_URL}\"," ${ROOTDIR}/frontend/package.json; \
 	fi
 	@echo "'yarn start' will use this proxy setting: $$(grep proxy ${ROOTDIR}/frontend/package.json)"
 	@cd ${ROOTDIR}/frontend && yarn start
