e8a85cf93f3cebc5791cd2aebc36a26e77f1276c
Xunzhuo <mixdeers@gmail.com>
2022-04-21 19:29:10 +0800

    feat: refactor expired clients with channel (#4855)
    Signed-off-by: Xunzhuo <mixdeers@gmail.com>

--- kubernetes/client_factory_test.go
+++ kubernetes/client_factory_test.go
@@ -1,69 +1,109 @@
 package kubernetes
 
 import (
+	"fmt"
+	"math/rand"
+	"sync"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd/api"
 )
 
-// TestClientExpiry Verify that clients expire
-func TestClientExpiry(t *testing.T) {
+// TestClientExpiration Verify the details that clients expire are correct
+func TestClientExpiration(t *testing.T) {
+
 	istioConfig := rest.Config{}
-	clientFactory, _ := getClientFactory(&istioConfig, time.Millisecond*100)
+	clientFactory := newClientFactory(&istioConfig)
 
-	clientEntries := clientFactory.clientEntries
 	// Make sure we are starting off with an empty set of clients
-	mutex.RLock()
-	assert.Equal(t, 0, len(clientEntries))
-	mutex.RUnlock()
+	assert.Equal(t, 0, clientFactory.getClientsLength())
 
-	mutex.Lock()
-	// Create a single initial test client
-	clientEntries["foo"] = &clientEntry{
-		created: time.Now(),
+	// Create a single initial test clients
+	authInfo := api.NewAuthInfo()
+	authInfo.Token = "foo-token"
+	_, err := clientFactory.getRecycleClient(authInfo, 100*time.Millisecond)
+	if err != nil {
+		assert.Nil(t, err)
 	}
-	mutex.Unlock()
 
 	// Verify we have the client
-	mutex.RLock()
-	assert.Equal(t, 1, len(clientEntries))
-	_, found := clientEntries["foo"]
-	mutex.RUnlock()
+	assert.Equal(t, 1, clientFactory.getClientsLength())
+	_, found := clientFactory.hasClient(authInfo)
 	assert.True(t, found)
 
 	// Sleep for a bit and add another client
-	time.Sleep(time.Millisecond * 25)
-	mutex.Lock()
-	clientEntries["bar"] = &clientEntry{
-		created: time.Now(),
+	time.Sleep(time.Millisecond * 60)
+	authInfo1 := api.NewAuthInfo()
+	authInfo1.Token = "bar-token"
+	_, err = clientFactory.getRecycleClient(authInfo1, 100*time.Millisecond)
+	if err != nil {
+		assert.Nil(t, err)
 	}
-	mutex.Unlock()
 
 	// Verify we have both the foo and bar clients
-	mutex.RLock()
-	assert.Equal(t, 2, len(clientEntries))
-	_, found = clientEntries["foo"]
+	assert.Equal(t, 2, clientFactory.getClientsLength())
+	_, found = clientFactory.hasClient(authInfo)
 	assert.True(t, found)
-	_, found = clientEntries["bar"]
+	_, found = clientFactory.hasClient(authInfo1)
 	assert.True(t, found)
-	mutex.RUnlock()
 
 	// Wait for foo to be expired
-	time.Sleep(time.Millisecond * 100)
+	time.Sleep(time.Millisecond * 60)
 	// Verify the client has been removed
-	mutex.RLock()
-	assert.Equal(t, 1, len(clientEntries))
-	_, found = clientEntries["foo"]
+	assert.Equal(t, 1, clientFactory.getClientsLength())
+	_, found = clientFactory.hasClient(authInfo)
 	assert.False(t, found)
-	_, found = clientEntries["bar"]
+	_, found = clientFactory.hasClient(authInfo1)
 	assert.True(t, found)
-	mutex.RUnlock()
 
 	// Wait for bar to be expired
-	time.Sleep(time.Millisecond * 125)
-	mutex.RLock()
-	assert.Equal(t, 0, len(clientEntries))
-	mutex.RUnlock()
+	time.Sleep(time.Millisecond * 60)
+	assert.Equal(t, 0, clientFactory.getClientsLength())
+}
+
+// TestConcurrentClientExpiration Verify Concurrent clients are expired correctly
+func TestConcurrentClientExpiration(t *testing.T) {
+	istioConfig := rest.Config{}
+	clientFactory := newClientFactory(&istioConfig)
+	count := 100
+
+	wg := sync.WaitGroup{}
+	wg.Add(count)
+
+	for i := 0; i < count; i++ {
+		go func() {
+			authInfo := api.NewAuthInfo()
+			authInfo.Token = fmt.Sprintf("%d", rand.Intn(10000000000))
+			if _, err := clientFactory.getRecycleClient(authInfo, 10*time.Millisecond); err != nil {
+				assert.Nil(t, err)
+			}
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+	time.Sleep(3 * time.Second)
+
+	assert.Equal(t, 0, clientFactory.getClientsLength())
+}
+
+// TestConcurrentClientFactory test Concurrently create ClientFactory
+func TestConcurrentClientFactory(t *testing.T) {
+	istioConfig := rest.Config{}
+	count := 100
+
+	wg := sync.WaitGroup{}
+	wg.Add(count)
+
+	for i := 0; i < count; i++ {
+		go func() {
+			newClientFactory(&istioConfig)
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
 }
