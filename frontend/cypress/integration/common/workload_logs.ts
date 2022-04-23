dbd31ae138db6bf5dcf3a2e4072ecb6ac1f18224
Edgar Hernández <23639005+israel-hdez@users.noreply.github.com>
2022-04-22 19:53:43 -0500

    Feedback: Don't use variables for cypress.

--- frontend/cypress/integration/common/workload_logs.ts
+++ frontend/cypress/integration/common/workload_logs.ts
@@ -41,32 +41,31 @@ When("I choose to show 10 lines of logs", () => {
 });
 
 When("I select only the {string} container", containerName => {
-    let containerListForm = cy.get("[data-test=workload-logs-pod-containers]");
-    containerListForm.get('[type=checkbox]:checked').uncheck();
-
-    let podLabel = containerListForm.get("label").contains(containerName);
-    podLabel.invoke('attr', 'for')
-        .then(labelFor => {
-            cy.get(`#${labelFor}`).check();
-        });
+    cy.get("[data-test=workload-logs-pod-containers]").within(() => {
+        cy.get('[type=checkbox]:checked').uncheck();
+        cy.get("label").contains(containerName).invoke('attr', 'for')
+            .then(labelFor => {
+                cy.get(`#${labelFor}`).check();
+            });
+    });
 });
 
-When("I enable visualization of spans", function () {
+When("I enable visualization of spans", () => {
     cy.get("#spans-show-").check();
 });
 
 Then("I should see the {string} container listed", (containerName) => {
-    let containerListForm = cy.get("[data-test=workload-logs-pod-containers]");
-
-    let podLabel = containerListForm.get("label").contains(containerName);
-    podLabel.should('have.text', containerName);
+    cy.get("[data-test=workload-logs-pod-containers]")
+        .get("label")
+        .contains(containerName)
+        .should('have.text', containerName);
 });
 
 Then("the {string} container should be checked", containerName => {
-    let containerListForm = cy.get("[data-test=workload-logs-pod-containers]");
-    let podLabel = containerListForm.get("label").contains(containerName);
-
-    podLabel.invoke('attr', 'for')
+    cy.get("[data-test=workload-logs-pod-containers]")
+        .get("label")
+        .contains(containerName)
+        .invoke('attr', 'for')
         .then(labelFor => {
             cy.get(`#${labelFor}`).should('be.checked');
         });
@@ -77,46 +76,50 @@ Then("I should see some {string} pod selected in the pod selector", podNamePrefi
 });
 
 Then("the log pane should only show log lines containing {string}", filterText => {
-    let logPane = cy.get('#logsText');
-    logPane.get('p').each(line => {
-        expect(line).to.contain(filterText);
-    });
+    cy.get('#logsText')
+        .get('p')
+        .each(line => {
+            expect(line).to.contain(filterText);
+        });
 });
 
 Then("the log pane should only show log lines not containing {string}", filterText => {
-    let logPane = cy.get('#logsText');
-    logPane.get('p').each(line => {
-        expect(line).to.not.contain(filterText);
-    });
+    cy.get('#logsText')
+        .get('p')
+        .each(line => {
+            expect(line).to.not.contain(filterText);
+        });
 });
 
 Then("the log pane should show only {int} lines of logs of each selected container", numberOfLinesPerContainer => {
-    let containerListForm = cy.get("[data-test=workload-logs-pod-containers]");
-    let containersEnabled = containerListForm.get('[type=checkbox]:checked');
-
-    containersEnabled.its('length').then(numContainersEnabled => {
-        let logPane = cy.get('#logsText');
-        logPane.get('p').its('length').should('eq', numContainersEnabled * numberOfLinesPerContainer);
-    });
+    cy.get("[data-test=workload-logs-pod-containers]")
+        .get('[type=checkbox]:checked')
+        .its('length')
+        .then(numContainersEnabled => {
+            cy.get('#logsText')
+                .get('p')
+                .its('length')
+                .should('eq', numContainersEnabled * numberOfLinesPerContainer);
+        });
 });
 
 Then("the log pane should only show logs for the {string} container", containerName => {
-    let containerListForm = cy.get("[data-test=workload-logs-pod-containers]");
+    cy.get("[data-test=workload-logs-pod-containers]")
+        .get("label")
+        .contains(containerName)
+        .then($podLabel => {
+            let logColor = $podLabel[0].style.color;
 
-    let podLabel = containerListForm.get("label").contains(containerName);
-    podLabel.then($podLabel => {
-        let logColor = $podLabel[0].style.color;
-
-        let logPane = cy.get('#logsText');
-        logPane.get('p').each(line => {
-            expect(line[0].style.color).to.equal(logColor);
-        })
-    })
+            cy.get('#logsText')
+                .get('p')
+                .each(line => {
+                    expect(line[0].style.color).to.equal(logColor);
+                });
+        });
 });
 
-Then("the log pane should show spans", function () {
+Then("the log pane should show spans", () => {
     cy.get("label[for=spans-show-]").invoke('css', 'color').then(spansColor => {
-        let logPane = cy.get('#logsText');
-        logPane.get('p').should('have.css', 'color', spansColor);
+        cy.get('#logsText').get('p').should('have.css', 'color', spansColor);
     });
 });
