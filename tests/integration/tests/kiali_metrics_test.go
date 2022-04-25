f0d8b411da60382fd7fc737bb3b68762d5167202
Hayk Hovsepyan <hhovsepy@redhat.com>
2022-04-25 18:19:03 +0200

    fixed metrics tests

--- tests/integration/tests/kiali_metrics_test.go
+++ tests/integration/tests/kiali_metrics_test.go
@@ -2,6 +2,9 @@ package tests
 
 import (
 	"testing"
+	"time"
+
+	"k8s.io/apimachinery/pkg/util/wait"
 
 	"github.com/stretchr/testify/assert"
 
@@ -11,28 +14,35 @@ import (
 func TestNamespaceMetrics(t *testing.T) {
 	assert := assert.New(t)
 	params := map[string]string{"filters": "tcp_sent,tcp_received"}
-	metrics, err := utils.NamespaceMetrics(utils.BOOKINFO, params)
 
-	assert.Nil(err)
-	assert.NotEmpty(metrics)
-	assert.NotEmpty(metrics.TcpSent)
-	assert.NotEmpty(metrics.TcpSent[0].Datapoints)
-	assert.NotEmpty(metrics.TcpReceived)
-	assert.NotEmpty(metrics.TcpReceived[0].Datapoints)
+	pollErr := wait.Poll(time.Second, time.Minute, func() (bool, error) {
+		metrics, err := utils.NamespaceMetrics(utils.BOOKINFO, params)
+		assert.Nil(err)
+		assert.NotEmpty(metrics)
+		return CheckMetrics(metrics.TcpSent, metrics.TcpReceived), nil
+	})
+	assert.Nil(pollErr, "Metrics should be returned")
 }
 
 func TestServiceMetrics(t *testing.T) {
 	assert := assert.New(t)
 	name := "ratings"
 	params := map[string]string{"filters": "request_count,request_duration_millis,request_error_count"}
-	metrics, err := utils.ServiceMetrics(utils.BOOKINFO, name, params)
 
-	assert.Nil(err)
-	assert.NotEmpty(metrics)
-	assert.NotEmpty(metrics.RequestCount)
-	assert.NotEmpty(metrics.RequestCount[0].Datapoints)
-	assert.NotEmpty(metrics.RequestDurationMillis)
-	assert.NotEmpty(metrics.RequestDurationMillis[0].Datapoints)
-	assert.NotEmpty(metrics.RequestErrorCount)
-	assert.NotEmpty(metrics.RequestErrorCount[0].Datapoints)
+	pollErr := wait.Poll(time.Second, time.Minute, func() (bool, error) {
+		metrics, err := utils.ServiceMetrics(utils.BOOKINFO, name, params)
+		assert.Nil(err)
+		assert.NotEmpty(metrics)
+		return CheckMetrics(metrics.RequestCount, metrics.RequestDurationMillis, metrics.RequestErrorCount), nil
+	})
+	assert.Nil(pollErr, "Metrics should be returned")
+}
+
+func CheckMetrics(metrics ...[]utils.MetricJson) bool {
+	for _, metric := range metrics {
+		if len(metric) == 0 || len(metric[0].Datapoints) == 0 {
+			return false
+		}
+	}
+	return true
 }
