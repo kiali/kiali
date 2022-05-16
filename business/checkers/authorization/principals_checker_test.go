839c11ba0055665e1fb03101b7fd2870ee08dad1
Hayk Hovsepyan <hhovsepy@redhat.com>
2022-05-16 16:50:51 +0200

    Added more tests.

--- business/checkers/authorization/principals_checker_test.go
+++ business/checkers/authorization/principals_checker_test.go
@@ -24,6 +24,19 @@ func TestPresentServiceAccount(t *testing.T) {
 	assert.Empty(validations)
 }
 
+func TestEmptyPrincipals(t *testing.T) {
+	assert := assert.New(t)
+
+	validations, valid := PrincipalsChecker{
+		AuthorizationPolicy: *authPolicyWithPrincipals([]string{}),
+		ServiceAccounts:     []string{"cluster.local/ns/bookinfo/sa/default", "cluster.local/ns/bookinfo/sa/test"},
+	}.Check()
+
+	// Well configured object
+	assert.True(valid)
+	assert.Empty(validations)
+}
+
 func TestNotPresentServiceAccount(t *testing.T) {
 	assert := assert.New(t)
 
@@ -42,6 +55,23 @@ func TestNotPresentServiceAccount(t *testing.T) {
 	assert.Equal(models.ErrorSeverity, vals[1].Severity)
 	assert.Error(validations.ConfirmIstioCheckMessage("authorizationpolicy.nodest.principalnotfound", vals[1]))
 	assert.Equal("spec/rules[0]/from[0]/source/principals[1]", vals[1].Path)
+}
+
+func TestEmptyServiceAccount(t *testing.T) {
+	assert := assert.New(t)
+
+	vals, valid := PrincipalsChecker{
+		AuthorizationPolicy: *authPolicyWithPrincipals([]string{"cluster.local/ns/bookinfo/sa/wrong"}),
+		ServiceAccounts:     []string{},
+	}.Check()
+
+	// Wrong host is not present
+	assert.False(valid)
+	assert.NotEmpty(vals)
+	assert.Len(vals, 1)
+	assert.Equal(models.ErrorSeverity, vals[0].Severity)
+	assert.Error(validations.ConfirmIstioCheckMessage("authorizationpolicy.nodest.principalnotfound", vals[0]))
+	assert.Equal("spec/rules[0]/from[0]/source/principals[0]", vals[0].Path)
 }
 
 func authPolicyWithPrincipals(principalsList []string) *security_v1beta.AuthorizationPolicy {
