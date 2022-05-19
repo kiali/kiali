64833abe20b293f36689fb8f6daac4af373f06e7
Jay Shaughnessy <jshaughn@users.noreply.github.com>
2022-05-19 13:55:43 -0400

    Remove storybook tests as part of the PF-React update.  The extensive (#5095)
    transitive deps are making other dep updates difficult, as well as triggering
    several dependabot security warnings.  Moreover, we don't pay attention to
    these tests.

--- frontend/src/stories/__dataStories__/index.ts
+++
@@ -1,5 +0,0 @@
-import { dashboardEnvoy } from './envoyMetrics';
-import { dashboardService, dashboardServiceSpans } from './dashboardService';
-import { dashboardWithlegends } from './dashboardWithLegends';
-
-export { dashboardService, dashboardServiceSpans, dashboardEnvoy, dashboardWithlegends };
