27e2aab4a5b9a004be490af04c743dc3b8b86020
Prathamesh Salunkhe <spratham55@gmail.com>
2022-05-25 09:39:20 +0530

    Add support for authorization policy conditions that was missing
    - Adds support for remote.ip, request.auth.claims conditions
    - Fixes #5109

--- frontend/src/pages/IstioConfigNew/AuthorizationPolicyForm/When/ConditionBuilder.tsx
+++ frontend/src/pages/IstioConfigNew/AuthorizationPolicyForm/When/ConditionBuilder.tsx
@@ -3,7 +3,7 @@ import { cellWidth, ICell, Table, TableBody, TableHeader } from '@patternfly/rea
 // Use TextInputBase like workaround while PF4 team work in https://github.com/patternfly/patternfly-react/issues/4072
 import { Button, TextInputBase as TextInput } from '@patternfly/react-core';
 import { PlusCircleIcon } from '@patternfly/react-icons';
-import { isValidRequestHeaderName } from '../../../../helpers/ValidationHelpers';
+import { isValidRequestHeaderName, isValidRequestAuthClaimName } from '../../../../helpers/ValidationHelpers';
 import { style } from 'typestyle';
 import { PFColors } from '../../../../components/Pf/PfColors';
 import { isValidIp } from '../../../../utils/IstioConfigUtils';
@@ -47,6 +47,7 @@ const noValidKeyStyle = style({
 
 const conditionFixedKeys = [
   'source.ip',
+  'remote.ip',
   'source.namespace',
   'source.principal',
   'request.auth.principal',
@@ -56,6 +57,12 @@ const conditionFixedKeys = [
   'destination.port',
   'connection.sni'
 ];
+
+const conditionIpAddressKeys = [
+  'source.ip',
+  'remote.ip',
+  'destination.ip'
+]
 
 class ConditionBuilder extends React.Component<Props, State> {
   constructor(props: Props) {
@@ -121,6 +128,9 @@ class ConditionBuilder extends React.Component<Props, State> {
     if (key.startsWith('experimental.envoy.filters.')) {
       return true;
     }
+    if (key.startsWith('request.auth.claims[')) {
+      return isValidRequestAuthClaimName(key);
+    }
     return false;
   };
 
@@ -136,7 +146,8 @@ class ConditionBuilder extends React.Component<Props, State> {
     if ((!values || values.length === 0) && (!notValues || notValues.length === 0)) {
       return [true, false, false, 'Values and NotValues cannot be empty'];
     }
-    if (key === 'source.ip' || key === 'destination.ip') {
+
+    if (conditionIpAddressKeys.includes(key)) {
       // If some value is not an IP, then is not valid
       // @ts-ignore
       const valuesValid = values ? !values.some(value => !isValidIp(value)) : true;
