bcb312a1c403d10826ab6a1a36270ca07fd9d178
josunect <josune.cordoba@gmail.com>
2022-06-16 10:57:33 +0100

    hide the warning/error status icon on side panel when there is any validation message

--- frontend/src/pages/IstioConfigDetails/IstioObjectDetails/IstioConfigOverview.tsx
+++ frontend/src/pages/IstioConfigDetails/IstioObjectDetails/IstioConfigOverview.tsx
@@ -115,7 +115,7 @@ class IstioConfigOverview extends React.Component<IstioConfigOverviewProps> {
                 <KialiIcon.Info className={infoStyle} />
               </Tooltip>
               {this.props.istioValidations && (!this.props.statusMessages || this.props.statusMessages.length === 0)
-                && (!this.props.istioValidations.checks || this.props.istioValidations.checks.length == 0)
+                && (!this.props.istioValidations.checks || this.props.istioValidations.checks.length === 0)
                 && (
                 <span className={healthIconStyle}>
                   <ValidationObjectSummary
