2f3af1ad05f22506255d23f2078e2847170ea0a8
josunect <josune.cordoba@gmail.com>
2022-06-30 08:55:00 +0100

    Fix topology generator demo

--- hack/istio/install-topology-generator-demo.sh
+++ hack/istio/install-topology-generator-demo.sh
@@ -10,8 +10,10 @@
 
 : ${CLIENT_EXE:=oc}
 : ${DELETE_DEMOS:=false}
+: ${DELETE_CONFIG:=false}
 : ${TOPO:=topology-generator}
 : ${BASE_URL:=https://raw.githubusercontent.com/kiali/demos/master}
+: ${NUM_NS:=1}
 
 apply_network_attachment() {
   NAME=$1
@@ -20,6 +22,8 @@ apiVersion: "k8s.cni.cncf.io/v1"
 kind: NetworkAttachmentDefinition
 metadata:
   name: istio-cni
+  labels:
+   generated-by: mimik
 NAD
     cat <<SCC | $CLIENT_EXE apply -f -
 apiVersion: security.openshift.io/v1
@@ -51,6 +55,17 @@ install_topology_generator_demo() {
     ${CLIENT_EXE} apply -n ${TOPO}  -f ${BASE_URL}/topology-generator/deploy/generator.yaml
 }
 
+generate_config() {
+
+  if [ "${IS_OPENSHIFT}" == "true" ]; then
+    for (( i=1; i<=$NUM_NS; i++ ))
+        do
+
+            apply_network_attachment n${i}
+    done
+  fi
+}
+
 while [ $# -gt 0 ]; do
   key="$1"
   case $key in
@@ -58,7 +73,15 @@ while [ $# -gt 0 ]; do
       CLIENT_EXE="$2"
       shift;shift
       ;;
-    -d|-delete)
+    -n|--num_ns)
+      NUM_NS="$2"
+      shift;shift
+      ;;
+    -g|--generate_config)
+      GENERATE_CONFIG="$2"
+      shift;shift
+      ;;
+    -d|--delete)
       DELETE_DEMOS="$2"
       shift;shift
       ;;
@@ -66,8 +89,16 @@ while [ $# -gt 0 ]; do
       cat <<HELPMSG
 Valid command line arguments:
   -c|--client: either 'oc' or 'kubectl'
+  -n|--num_ns: number of namespaces to be generate in the generator to apply the network policies
   -d|--delete: if 'true' demos will be deleted; otherwise, they will be installed
+  -g|--generate_config: Generate configuration of the topology based in the number of namespaces (-n)
   -h|--help: this text
+
+Usage:
+  1) Run with no args - or -c - to install the topology generator
+  2) Run the proxy: CLIENT port-forward svc/topology-generator 8080:8080 -n topology-generator
+  3) Go to the app and generate the topology
+  4) Run -g to run the openshift configuration and -n with the number of namespaces created
 HELPMSG
       exit 1
       ;;
@@ -86,12 +117,18 @@ fi
 echo "CLIENT_EXE=${CLIENT_EXE}"
 echo "IS_OPENSHIFT=${IS_OPENSHIFT}"
 
-if [ "${DELETE_DEMOS}" != "true" ]; then
+if [ "${GENERATE_CONFIG}" == "true" ]; then
+
+    echo "Configuring the ${TOPO} app in the ${NUM_NS} namespaces..."
+    generate_config
+
+else
+  if [ "${DELETE_DEMOS}" != "true" ]; then
 
     echo "Installing the ${TOPO} app in the ${TOPO} namespace..."
     install_topology_generator_demo
 
-else
+  else
 
     echo "Deleting the '${TOPO}' app in the '${TOPO}' namespace..."
 
@@ -102,6 +139,7 @@ else
             ${CLIENT_EXE} delete ns ${TOPO}
     fi
 
+  fi
 fi
 
 
