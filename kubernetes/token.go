366a070b221029bdacf4f2130f80c5dffba7ed99
josunect <josune.cordoba@gmail.com>
2022-08-08 16:47:34 +0100

    Error handling for token

--- kubernetes/token.go
+++ kubernetes/token.go
@@ -1,6 +1,11 @@
 package kubernetes
 
-import "io/ioutil"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"time"
+)
 
 // Be careful with how you use this token. This is the Kiali Service Account token, not the user token.
 // We need the Service Account token to access third-party in-cluster services (e.g. Grafana).
@@ -8,18 +13,57 @@ import "io/ioutil"
 const DefaultServiceAccountPath = "/var/run/secrets/kubernetes.io/serviceaccount/token"
 
 var KialiToken string
+var LastRead time.Time
 
 func GetKialiToken() (string, error) {
-	if KialiToken == "" {
+	expired, err := IsTokenExpired()
+	if KialiToken == "" || (expired && err == nil) {
 		if remoteSecret, err := GetRemoteSecret(RemoteSecretData); err == nil {
 			KialiToken = remoteSecret.Users[0].User.Token
 		} else {
-			token, err := ioutil.ReadFile(DefaultServiceAccountPath)
-			if err != nil {
-				return "", err
+			errUpdating := updateKialiToken()
+			if errUpdating != nil {
+				fmt.Errorf("Error updating Kiali token: " + errUpdating.Error())
 			}
-			KialiToken = string(token)
 		}
 	}
 	return KialiToken, nil
+}
+
+func updateKialiToken() error {
+	var err error
+	LastRead, err = getLastModified(DefaultServiceAccountPath)
+	if err != nil {
+		return err
+	}
+	token, errRead := ioutil.ReadFile(DefaultServiceAccountPath)
+	if errRead != nil {
+		fmt.Println(errRead)
+	}
+	KialiToken = string(token)
+	return nil
+}
+
+// Return time for when a file was last modified
+func getLastModified(fileName string) (time.Time, error) {
+
+	file, err := os.Stat(fileName)
+	if err != nil {
+		fmt.Println(err)
+		return time.Time{}, err
+	}
+	return file.ModTime(), nil
+}
+
+// Is token expired is token file has been modified
+func IsTokenExpired() (bool, error) {
+	checkModifiedTime, err := getLastModified(DefaultServiceAccountPath)
+	if err != nil {
+		fmt.Println(err)
+		return false, err
+	}
+	if checkModifiedTime.Unix() > LastRead.Unix() {
+		return true, nil
+	}
+	return false, nil
 }
