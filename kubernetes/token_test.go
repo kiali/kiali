3413b8daf74a35ada8a996d1870c85b83c21e2fd
josunect <josune.cordoba@gmail.com>
2022-08-23 08:01:54 +0100

    Go imports

--- kubernetes/token_test.go
+++ kubernetes/token_test.go
@@ -1,10 +1,11 @@
 package kubernetes
 
 import (
-	"github.com/stretchr/testify/assert"
 	"os"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 const tmpFileTokenExpired = "/tmp/token"
