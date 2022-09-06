974cb353534ab74aedf36182b75a69b3dd76fbb7
Edgar Hernández <23639005+israel-hdez@users.noreply.github.com>
2022-09-06 13:17:00 -0500

    Fix invalid ref

--- frontend/src/components/Refresh/connectRefresh.tsx
+++ frontend/src/components/Refresh/connectRefresh.tsx
@@ -4,13 +4,11 @@ import useRefreshInterval from "../../hooks/refresh";
 // TODO: This is a transition component. Please, avoid using this HOC/function.
 //  Prefer usage of the useRefreshInterval hook for function components, or the RefreshNotifier component for class components.
 export default function connectRefresh<P extends { lastRefreshAt: number }>(Component: React.ComponentType<P>) {
-  return function (props: Omit<P, 'lastRefreshAt'>) {
+  return React.forwardRef((props: Omit<P, 'lastRefreshAt'>, ref) => {
     const refreshing = useRefreshInterval();
 
-    console.log('Rendering connectedRefresh component:', Component, refreshing.lastRefreshAt);
-
     return (
-      <Component {...props as P} lastRefreshAt={refreshing.lastRefreshAt} />
+      <Component {...props as P} ref={ref} lastRefreshAt={refreshing.lastRefreshAt} />
     );
-  };
+  });
 }
