d89595fc4bc17df7a3218557d1ea38472010205a
josunect <josune.cordoba@gmail.com>
2022-09-07 16:14:41 +0100

    Patch virtualservice just if empty

--- hack/istio/install-testing-demos.sh
+++ hack/istio/install-testing-demos.sh
@@ -139,7 +139,10 @@ if [ "${DELETE_DEMOS}" != "true" ]; then
 
   # Some front-end tests have conflicts with the wildcard host in the bookinfo-gateway. Patch it with the host resolved for the traffic generator.
   ISTIO_INGRESS_HOST=$(${CLIENT_EXE} get cm -n bookinfo traffic-generator-config -o jsonpath='{.data.route}' | sed 's|.*//\([^\:]*\).*/.*|\1|')
-  ${CLIENT_EXE} patch VirtualService bookinfo -n bookinfo --type json -p "[{\"op\": \"replace\", \"path\": \"/spec/hosts/0\", \"value\": \"${ISTIO_INGRESS_HOST}\"}]"
+
+  if [ -n "${ISTIO_INGRESS_HOST}" ]; then
+    ${CLIENT_EXE} patch VirtualService bookinfo -n bookinfo --type json -p "[{\"op\": \"replace\", \"path\": \"/spec/hosts/0\", \"value\": \"${ISTIO_INGRESS_HOST}\"}]"
+  fi
 
   for namespace in bookinfo alpha beta
   do
