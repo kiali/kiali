56c0b88b8ef8d175d6fcc0155433797b563fbfcc
Jay Shaughnessy <jshaughn@users.noreply.github.com>
2022-09-16 16:03:35 -0400

    Fix integration test failure related to change in default values for install-istio-via-istioctl.sh (#5475)

--- hack/setup-kind-in-ci.sh
+++ hack/setup-kind-in-ci.sh
@@ -137,7 +137,7 @@ LBCONFIGMAP
 
 infomsg "Installing istio"
 # Apparently you can't set the requests to zero for the proxy so just setting them to some really low number.
-hack/istio/install-istio-via-istioctl.sh --reduce-resources true --client-exe-path "$(which kubectl)"
+hack/istio/install-istio-via-istioctl.sh --reduce-resources true --client-exe-path "$(which kubectl)" -cn "cluster-default" -mid "mesh-default" -net "network-default"
   
 infomsg "Pushing the images into the cluster..."
 make -e DORP="${DORP}" -e CLUSTER_TYPE="kind" -e KIND_NAME="ci" cluster-push-kiali
