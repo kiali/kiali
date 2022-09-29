3efed88456aac62d7e6be23e97d33ef312cca042
Hayk Hovsepyan <hhovsepy@redhat.com>
2022-09-29 16:26:45 +0200

    Add Gateway API node icon into isRoot

--- frontend/src/components/CytoscapeGraph/graphs/GraphStyles.ts
+++ frontend/src/components/CytoscapeGraph/graphs/GraphStyles.ts
@@ -266,11 +266,11 @@ export class GraphStyles {
       if (node.hasWorkloadEntry) {
         badges = `<span class="${NodeIconWorkloadEntry} ${badgeMargin(badges)}"></span> ${badges}`;
       }
-      if (node.isGateway?.gatewayAPIInfo?.hostnames?.length !== undefined) {
-        badges = `<span class="${NodeIconGateway} ${badgeMargin(badges)}"></span> ${badges}`;
-      }
       if (node.isRoot) {
-        if (node.isGateway?.ingressInfo?.hostnames?.length !== undefined) {
+        if (
+          node.isGateway?.ingressInfo?.hostnames?.length !== undefined ||
+          node.isGateway?.gatewayAPIInfo?.hostnames?.length !== undefined
+        ) {
           badges = `<span class="${NodeIconGateway} ${badgeMargin(badges)}"></span> ${badges}`;
         }
         badges = `<span class="${NodeIconRoot} ${badgeMargin(badges)}"></span> ${badges}`;
