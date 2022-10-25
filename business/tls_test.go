4fa03aa599ea431a35e9cb937d81d98d4dca70d2
josunect <josune.cordoba@gmail.com>
2022-10-25 10:57:29 +0100

    Tests fix

--- business/tls_test.go
+++ business/tls_test.go
@@ -37,6 +37,7 @@ func TestMeshStatusEnabled(t *testing.T) {
 	k8s.On("IsGatewayAPI").Return(false)
 	k8s.On("GetNamespaces", mock.AnythingOfType("string")).Return(&core_v1.Namespace{}, nil)
 	k8s.On("GetToken").Return("token")
+	k8s.On("GetTlsMinVersion").Return("")
 
 	TLSService := getTLSService(k8s, false, ns, pa, dr)
 	status, err := TLSService.MeshWidemTLSStatus(context.TODO(), ns)
