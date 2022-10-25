0645185b111165d97fee99abe151ad40aaf550ec
josunect <josune.cordoba@gmail.com>
2022-10-25 09:41:33 +0100

    Remove unused handler

--- handlers/istio_certs.go
+++ handlers/istio_certs.go
@@ -16,18 +16,3 @@ func IstioCerts(w http.ResponseWriter, r *http.Request) {
 	}
 	RespondWithJSON(w, http.StatusOK, certs)
 }
-
-// IstioCerts returns information about internal certificates used by Istio
-func IstioMTLS(w http.ResponseWriter, r *http.Request) {
-	business, err := getBusiness(r)
-	if err != nil {
-		RespondWithError(w, http.StatusInternalServerError, err.Error())
-		return
-	}
-	certs, err := business.IstioCerts.GetTlsMinVersion()
-	if err != nil {
-		handleErrorResponse(w, err)
-		return
-	}
-	RespondWithJSON(w, http.StatusOK, certs)
-}
