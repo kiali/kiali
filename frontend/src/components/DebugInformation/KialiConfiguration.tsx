a02eaa541874de90a7d4b53b99c3ba4f104e17db
josunect <josune.cordoba@gmail.com>
2022-11-08 15:38:46 +0000

    Kiali configuration view

--- frontend/src/components/DebugInformation/KialiConfiguration.tsx
+++ frontend/src/components/DebugInformation/KialiConfiguration.tsx
@@ -1,25 +1,15 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
-import _ from 'lodash';
-import beautify from 'json-beautify';
-
-import authenticationConfig from '../../config/AuthenticationConfig';
 import { serverConfig } from '../../config';
-import { ComputedServerConfig } from '../../config/ServerConfig';
-import { AuthConfig } from '../../types/Auth';
 import { KialiAppState } from '../../store/Store';
 import {
-  Alert,
-  AlertActionCloseButton,
-  AlertVariant,
   Button,
   ButtonVariant,
   Modal,
   ModalVariant
 } from '@patternfly/react-core';
-import {aceOptions} from "../../types/IstioConfigDetails";
-import AceEditor from "react-ace";
+import {ICell, Table, TableBody, TableHeader, TableVariant} from "@patternfly/react-table";
 
 enum CopyStatus {
   NOT_COPIED, // We haven't copied the current output
@@ -27,37 +17,41 @@ enum CopyStatus {
   OLD_COPY // We copied the prev output, but there are changes in the KialiAppState
 }
 
-type DebugInformationProps = {
-  appState: KialiAppState;
-  ref: React.RefObject<any>;
-};
-
 type DebugInformationState = {
   show: boolean;
   copyStatus: CopyStatus;
-};
-
-type DebugInformationData = {
-  backendConfigs: {
-    authenticationConfig: AuthConfig;
-    computedServerConfig: ComputedServerConfig;
-  };
-  currentURL: string;
-  reduxState: KialiAppState;
+  config: Array<string>;
 };
 
 const copyToClipboardOptions = {
   message: 'We failed to automatically copy the text, please use: #{key}, Enter\t'
 };
 
-export class KialiConfiguration extends React.PureComponent<DebugInformationProps, DebugInformationState> {
-  aceEditorRef: React.RefObject<AceEditor>;
+const propsToShow = ["clusters", "gatewayAPIEnabled", "istioConfigMap", "istioIdentityDomain"];
 
-  constructor(props: DebugInformationProps) {
+type Props = {
+  ref: React.RefObject<any>;
+}
+
+export class KialiConfiguration extends React.Component<Props, DebugInformationState> {
+
+  constructor(props: Props) {
     super(props);
-    this.aceEditorRef = React.createRef();
 
-    this.state = { show: false, copyStatus: CopyStatus.NOT_COPIED };
+    let showConfig = [];
+
+    for (const key in serverConfig) {
+      if (propsToShow.includes(key)) {
+        // @ts-ignore
+        showConfig.push(key)
+      }
+    }
+
+    this.state = {
+      show: false,
+      copyStatus: CopyStatus.NOT_COPIED,
+      config: showConfig
+    };
   }
 
   open = () => {
@@ -72,64 +66,20 @@ export class KialiConfiguration extends React.PureComponent<DebugInformationProp
     this.setState({ copyStatus: result ? CopyStatus.COPIED : CopyStatus.NOT_COPIED });
   };
 
-  hideAlert = () => {
-    this.setState({ copyStatus: CopyStatus.NOT_COPIED });
-  };
-
-  componentDidUpdate(prevProps: DebugInformationProps, _prevState: DebugInformationState) {
-    if (this.props.appState !== prevProps.appState && this.state.copyStatus === CopyStatus.COPIED) {
-      this.setState({ copyStatus: CopyStatus.OLD_COPY });
-    }
-  }
-
-  renderHealthConfig = (config: Array<any> | Object | RegExp | string) => {
-    if (Array.isArray(config)) {
-      let arr = [];
-      for (let v of config) {
-        arr.push(this.renderHealthConfig(v) as never);
-      }
-      return arr;
-    }
-    let result = {};
-    for (let [key, value] of Object.entries(config)) {
-      if ((value as Object).constructor.toString().includes('RegExp')) {
-        result[key] = (value as RegExp).toString();
-      } else if (typeof value !== 'object') {
-        result[key] = value;
-      } else {
-        result[key] = this.renderHealthConfig(value);
-      }
-    }
-    return result;
+  private columns = (): ICell[] => {
+    return [{ title: 'Configuration' }, { title: 'Value' }];
   };
 
   render() {
-    const parseConfig = (key: string, value: any) => {
-      // We have to patch some runtime properties  we don't want to serialize
-      if (['cyRef', 'summaryTarget', 'token', 'username'].includes(key)) {
-        return null;
+
+    const config = this.state.config.map((k) => {
+      if (typeof serverConfig[k] === "string") {
+        return [k, serverConfig[k]]
+      } else {
+        return [k, JSON.stringify(serverConfig[k])]
       }
-      if ('healthConfig' === key) {
-        return this.renderHealthConfig(value);
-      }
-      return value;
-    };
-    const renderDebugInformation = _.memoize(() => {
-      const debugInformation: DebugInformationData = {
-        backendConfigs: {
-          authenticationConfig: authenticationConfig,
-          computedServerConfig: serverConfig
-        },
-        currentURL: "",
-        reduxState: this.props.appState
-      };
-      return beautify(debugInformation, parseConfig, 2);
+
     });
-
-    if (!this.state.show) {
-      return null;
-    }
-
     return (
       <Modal
         variant={ModalVariant.small}
@@ -138,44 +88,23 @@ export class KialiConfiguration extends React.PureComponent<DebugInformationProp
         title="Kiali Configuration"
         actions={[
           <Button onClick={this.close}>Close</Button>,
-          <CopyToClipboard onCopy={this.copyCallback} text={renderDebugInformation()} options={copyToClipboardOptions}>
+          <CopyToClipboard onCopy={this.copyCallback} text={this.state.config} options={copyToClipboardOptions}>
             <Button variant={ButtonVariant.primary}>Copy</Button>
           </CopyToClipboard>
         ]}
       >
-        {this.state.copyStatus === CopyStatus.COPIED && (
-          <Alert
-            style={{ marginBottom: '20px' }}
-            title="Debug information has been copied to your clipboard."
-            variant={AlertVariant.success}
-            isInline={true}
-            actionClose={<AlertActionCloseButton onClose={this.hideAlert} />}
-          />
-        )}
-        {this.state.copyStatus === CopyStatus.OLD_COPY && (
-          <Alert
-            style={{ marginBottom: '20px' }}
-            title="Debug information was copied to your clipboard, but is outdated now. It could be caused by new data received by auto refresh timers."
-            variant={AlertVariant.warning}
-            isInline={true}
-            actionClose={<AlertActionCloseButton onClose={this.hideAlert} />}
-          />
-        )}
-        <span>Please include this information when opening a bug:</span>
-        <CopyToClipboard onCopy={this.copyCallback} text={renderDebugInformation()} options={copyToClipboardOptions}>
-          <AceEditor
-            ref={this.aceEditorRef}
-            mode="yaml"
-            theme="eclipse"
-            width={'100%'}
-            //height={height.toString() + 'px'}
-            className={'istio-ace-editor'}
-            wrapEnabled={true}
-            readOnly={true}
-            setOptions={aceOptions || { foldStyle: 'markbegin' }}
-            value={renderDebugInformation()}
-          />
-        </CopyToClipboard>
+        <div>
+
+          <Table
+            header={<></>}
+            variant={TableVariant.compact}
+            cells={this.columns()}
+            rows={config}
+          >
+            <TableHeader />
+            <TableBody />
+          </Table>
+        </div>
       </Modal>
     );
   }
