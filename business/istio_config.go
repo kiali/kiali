cbcf117c683ba5ac77c294b95aaebf86205070f2
josunect <josune.cordoba@gmail.com>
2022-11-18 16:01:46 +0000

    See services

--- business/istio_config.go
+++ business/istio_config.go
@@ -157,7 +157,8 @@ func (in *IstioConfigService) GetIstioConfigList(ctx context.Context, criteria I
 		}
 		registryConfiguration, err := in.businessLayer.RegistryStatus.GetRegistryConfiguration(registryCriteria)
 		if err != nil {
-			return istioConfigList, err
+			log.Warningf("RegistryConfiguration is nil. This is an unexpected case. Is the Kiali cache disabled ?")
+			//return istioConfigList, err
 		}
 		if registryConfiguration == nil {
 			log.Warningf("RegistryConfiguration is nil. This is an unexpected case. Is the Kiali cache disabled ?")
