4d7d251087f538fe125f45fb3d8028f7ea797536
josunect <josune.cordoba@gmail.com>
2022-11-22 09:27:21 +0000

    workloads

--- handlers/workloads.go
+++ handlers/workloads.go
@@ -90,9 +90,9 @@ func WorkloadDetails(w http.ResponseWriter, r *http.Request) {
 
 	includeValidations := false
 
-	//if p.Validate {
-	//	includeValidations = true
-	//}
+	if p.Validate {
+		includeValidations = true
+	}
 
 	var istioConfigValidations = models.IstioValidations{}
 	var errValidations error
