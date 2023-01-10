35428ceca5693345823c742689f37593969333a8
josunect <josune.cordoba@gmail.com>
2023-01-10 15:59:45 +0000

    lint

--- kubernetes/cache/proxy_status.go
+++ kubernetes/cache/proxy_status.go
@@ -2,9 +2,10 @@ package cache
 
 import (
 	"context"
-	"k8s.io/apimachinery/pkg/util/wait"
 	"strings"
 	"time"
+
+	"k8s.io/apimachinery/pkg/util/wait"
 
 	"github.com/kiali/kiali/config"
 	"github.com/kiali/kiali/kubernetes"
