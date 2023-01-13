85be987f902053a64eab3e16f1eb69060eb9937a
josunect <josune.cordoba@gmail.com>
2023-01-13 15:16:42 +0000

    Lint fix

--- handlers/proxy_logging.go
+++ handlers/proxy_logging.go
@@ -14,7 +14,7 @@ import (
 func LoggingUpdate(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 
-	if config.Get().Deployment.ViewOnlyMode == true {
+	if config.Get().Deployment.ViewOnlyMode {
 		RespondWithError(w, http.StatusForbidden, "Logs cannot be edited in View only mode")
 		return
 	}
