f378267c6e6b9a9ee80b8697fb84694f829d6222
Fernando Hoyos <fhoyosle@redhat.com>
2023-02-06 11:08:34 +0100

    Remove Labels in Create K8s Gateway Wizard

--- frontend/src/pages/IstioConfigNew/K8sGatewayForm.tsx
+++ frontend/src/pages/IstioConfigNew/K8sGatewayForm.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react';
 // Use TextInputBase like workaround while PF4 team work in https://github.com/patternfly/patternfly-react/issues/4072
-import { FormGroup, TextInputBase as TextInput } from '@patternfly/react-core';
+import { FormGroup } from '@patternfly/react-core';
 import ListenerBuilder from "./GatewayForm/ListenerBuilder";
 import AddressBuilder from "./GatewayForm/AddressBuilder";
 import ListenerList from "./GatewayForm/ListenerList";
 import AddressList from "./GatewayForm/AddressList";
 import {Address, Listener} from '../../types/IstioObjects';
-import { isValid } from 'utils/Common';
 
 export const K8SGATEWAY = 'K8sGateway';
 export const K8SGATEWAYS = 'k8sgateways';
@@ -18,8 +17,6 @@ type Props = {
 
 // Gateway and Sidecar states are consolidated in the parent page
 export type K8sGatewayState = {
-  workloadSelectorValid: boolean;
-  workloadSelectorLabels: string;
   listeners: Listener[];
   addresses: Address[];
   addListener: Listener;
@@ -28,8 +25,6 @@ export type K8sGatewayState = {
 };
 
 export const initK8sGateway = (): K8sGatewayState => ({
-  workloadSelectorLabels: 'app=gatewayapi',
-  workloadSelectorValid: true,
   listeners: [],
   addresses: [],
   addListener: {
@@ -47,7 +42,7 @@ export const initK8sGateway = (): K8sGatewayState => ({
 });
 
 export const isK8sGatewayStateValid = (g: K8sGatewayState): boolean => {
-  return g.workloadSelectorValid && g.listeners.length > 0;
+  return g.listeners.length > 0;
 };
 
 class K8sGatewayForm extends React.Component<Props, K8sGatewayState> {
@@ -59,46 +54,6 @@ class K8sGatewayForm extends React.Component<Props, K8sGatewayState> {
   componentDidMount() {
     this.setState(this.props.k8sGateway);
   }
-
-  addWorkloadLabels = (value: string, _) => {
-    if (value.length === 0) {
-      this.setState(
-        {
-          workloadSelectorValid: false,
-          workloadSelectorLabels: ''
-        },
-        () => this.props.onChange(this.state)
-      );
-      return;
-    }
-    value = value.trim();
-    const labels: string[] = value.split(',');
-    let isValid = true;
-    // Some smoke validation rules for the labels
-    for (let i = 0; i < labels.length; i++) {
-      const label = labels[i];
-      if (label.indexOf('=') < 0) {
-        isValid = false;
-        break;
-      }
-      const splitLabel: string[] = label.split('=');
-      if (splitLabel.length !== 2) {
-        isValid = false;
-        break;
-      }
-      if (splitLabel[0].trim().length === 0 || splitLabel[1].trim().length === 0) {
-        isValid = false;
-        break;
-      }
-    }
-    this.setState(
-      {
-        workloadSelectorValid: isValid,
-        workloadSelectorLabels: value
-      },
-      () => this.props.onChange(this.state)
-    );
-  };
 
   onAddListener = () => {
     this.setState(
@@ -162,21 +117,6 @@ class K8sGatewayForm extends React.Component<Props, K8sGatewayState> {
   render() {
     return (
       <>
-        <FormGroup
-          fieldId="workloadLabels"
-          label="Labels"
-          helperText="One or more labels to select a workload where the Gateway is applied."
-          helperTextInvalid="Enter a label in the format <label>=<value>. Enter one or multiple labels separated by comma."
-          validated={isValid(this.state.workloadSelectorValid)}
-        >
-          <TextInput
-            id="gwLabels"
-            name="gwLabels"
-            value={this.state.workloadSelectorLabels}
-            onChange={this.addWorkloadLabels}
-            validated={isValid(this.state.workloadSelectorValid)}
-          />
-        </FormGroup>
         <FormGroup label="Listeners" fieldId="listener" isRequired={true}>
           <ListenerBuilder
             onAddListener={listener => {
