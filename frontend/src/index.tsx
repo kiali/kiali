99b2739d41a78cd759bd6b2924759b013d5d33af
Muhammad Rafay <muhammadrafay7655@gmail.com>
2023-02-12 00:08:11 +0500

    optimize frontend code index.tsx file.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,18 +3,29 @@ import * as ReactDOM from 'react-dom';
 import App from './app/App';
 import './styles/index.css';
 
+// Declare a new interface for the Date object to add the new method toLocaleStringWithConditionalDate
 declare global {
   interface Date {
     toLocaleStringWithConditionalDate(): string;
   }
 }
 
+// Extend the Date object to add the new method toLocaleStringWithConditionalDate
 // eslint-disable-next-line no-extend-native
 Date.prototype.toLocaleStringWithConditionalDate = function () {
-  const nowDate = (new Date()).toLocaleDateString();
+  // Get the locale date string for the current date
+  const nowDate = new Date().toLocaleDateString();
+  // Get the locale date string for the date this method is being called on
   const thisDate = this.toLocaleDateString();
 
+  // Return the locale time string if the dates are the same, otherwise return the locale string
   return nowDate === thisDate ? this.toLocaleTimeString() : this.toLocaleString();
 };
 
-ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
+// Get the element with id "root"
+const rootElement = document.getElementById('root');
+
+// If the root element exists, render the App component to it
+if (rootElement) {
+  ReactDOM.render(<App />, rootElement);
+}
