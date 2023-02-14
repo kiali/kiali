f3b555417aa2b550e05165791eb77da0dcd45b3d
jshaughn <jshaughn@redhat.com>
2023-02-13 19:32:41 -0500

    Update swagger file with better header that explains how to regenerate the file when needed.

--- kiali.go
+++ kiali.go
@@ -1,6 +1,15 @@
 // Kiali
 //
-// Kiali project, observability for the Istio service mesh
+// Kiali Project, The Console for Istio Service Mesh
+//
+// NOTE! The Kiali API is not for public use and is not supported for any use outside of the Kiali UI itself.
+// The API can and will change from version to version with no guarantee of backwards compatibility.
+//
+// To generate this API document:
+// ```
+//   > swagger generate spec -o ./swagger.json
+//   > swagger generate markdown --quiet --spec ./swagger.json --output ./kiali_internal_api.md
+// ```
 //
 //	Schemes: http, https
 //	BasePath: /api
