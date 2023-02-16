9c88d031d35927d8f8f2bebb026ba8f95ce52dcc
Leandro Beretta <lea.beretta@gmail.com>
2023-02-16 12:59:01 -0300

    add validation references to gw, fix duplicated gws

--- business/checkers/k8sgateways/multi_match_checker.go
+++ business/checkers/k8sgateways/multi_match_checker.go
@@ -26,27 +26,27 @@ func (m MultiMatchChecker) Check() models.IstioValidations {
 
 		// With addresses
 		for _, address := range g.Spec.Addresses {
-			duplicate, _ := m.findMatchIP(address, g.Name)
+			duplicate, collidingGateways := m.findMatchIP(address, g.Name)
 			if duplicate {
 				// The above is referenced by each one below..
-				currentHostValidation := createError(gatewayRuleName, "k8sgateways.multimatch.ip", gatewayNamespace, "spec/addresses/value")
+				currentHostValidation := createError(gatewayRuleName, "k8sgateways.multimatch.ip", gatewayNamespace, "spec/addresses/value", collidingGateways)
 				validations = validations.MergeValidations(currentHostValidation)
 			}
 		}
 
 		// With listeners
 		for index, listener := range g.Spec.Listeners {
-			duplicate, _ := m.findMatch(listener, g.Name)
+			duplicate, collidingGateways := m.findMatch(listener, g.Name)
 			// Find in a different k8s GW
 			if duplicate {
 				// The above is referenced by each one below..
-				currentHostValidation := createError(gatewayRuleName, "k8sgateways.multimatch.listener", gatewayNamespace, fmt.Sprintf("spec/listeners[%d]/hostname", index))
+				currentHostValidation := createError(gatewayRuleName, "k8sgateways.multimatch.listener", gatewayNamespace, fmt.Sprintf("spec/listeners[%d]/hostname", index), collidingGateways)
 				validations = validations.MergeValidations(currentHostValidation)
 			}
 			// Check for unique listeners in the GW
 			for i, l := range g.Spec.Listeners {
 				if listener.Name != l.Name && l.Hostname != nil && listener.Hostname != nil && *listener.Hostname == *l.Hostname && listener.Port == l.Port && listener.Protocol == l.Protocol {
-					currentHostValidation := createError(gatewayRuleName, "k8sgateways.unique.listener", gatewayNamespace, fmt.Sprintf("spec/listeners[%d]/name", i))
+					currentHostValidation := createError(gatewayRuleName, "k8sgateways.unique.listener", gatewayNamespace, fmt.Sprintf("spec/listeners[%d]/name", i), nil)
 					validations = validations.MergeValidations(currentHostValidation)
 				}
 			}
@@ -57,7 +57,7 @@ func (m MultiMatchChecker) Check() models.IstioValidations {
 }
 
 // Create validation error for k8sgateway object
-func createError(gatewayRuleName string, ruleCode string, namespace string, path string) models.IstioValidations {
+func createError(gatewayRuleName string, ruleCode string, namespace string, path string, references []models.IstioValidationKey) models.IstioValidations {
 	key := models.IstioValidationKey{Name: gatewayRuleName, Namespace: namespace, ObjectType: K8sGatewayCheckerType}
 	checks := models.Build(ruleCode, path)
 	rrValidation := &models.IstioValidation{
@@ -67,40 +67,46 @@ func createError(gatewayRuleName string, ruleCode string, namespace string, path
 		Checks: []*models.IstioCheck{
 			&checks,
 		},
+		References: references,
 	}
 
 	return models.IstioValidations{key: rrValidation}
 }
 
 // findMatch uses a linear search with regexp to check for matching gateway host + port combinations. If this becomes a bottleneck for performance, replace with a graph or trie algorithm.
-func (m MultiMatchChecker) findMatch(listener k8s_networking_v1beta1.Listener, gwName string) (bool, []k8s_networking_v1beta1.Listener) {
-	duplicates := make([]k8s_networking_v1beta1.Listener, 0)
+func (m MultiMatchChecker) findMatch(listener k8s_networking_v1beta1.Listener, gwName string) (bool, []models.IstioValidationKey) {
+	collidingGateways := make([]models.IstioValidationKey, 0)
+
 	for _, gw := range m.K8sGateways {
 		if gw.Name == gwName {
 			continue
 		}
 		for _, l := range gw.Spec.Listeners {
 			if l.Hostname != nil && listener.Hostname != nil && *l.Hostname == *listener.Hostname && l.Port == listener.Port && l.Protocol == listener.Protocol {
-				duplicates = append(duplicates, listener)
+				key := models.IstioValidationKey{Name: gw.Name, Namespace: gw.Namespace, ObjectType: K8sGatewayCheckerType}
+				collidingGateways = append(collidingGateways, key)
 			}
 		}
 
 	}
-	return len(duplicates) > 0, duplicates
+	return len(collidingGateways) > 0, collidingGateways
 }
 
 // Check duplicates IP
-func (m MultiMatchChecker) findMatchIP(address k8s_networking_v1beta1.GatewayAddress, gwName string) (bool, []k8s_networking_v1beta1.GatewayAddress) {
-	duplicates := make([]k8s_networking_v1beta1.GatewayAddress, 0)
+func (m MultiMatchChecker) findMatchIP(address k8s_networking_v1beta1.GatewayAddress, gwName string) (bool, []models.IstioValidationKey) {
+	collidingGateways := make([]models.IstioValidationKey, 0)
+
 	for _, aa := range m.K8sGateways {
 		if aa.Name == gwName {
 			continue
 		}
+
 		for _, a := range aa.Spec.Addresses {
 			if a.Type != nil && address.Type != nil && *a.Type == *address.Type && a.Value == address.Value {
-				duplicates = append(duplicates, address)
+				key := models.IstioValidationKey{Name: aa.Name, Namespace: aa.Namespace, ObjectType: K8sGatewayCheckerType}
+				collidingGateways = append(collidingGateways, key)
 			}
 		}
 	}
-	return len(duplicates) > 0, duplicates
+	return len(collidingGateways) > 0, collidingGateways
 }
