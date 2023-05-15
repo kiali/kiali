cbe8cc0d6b2641566910514f03a916b80f3a0074
Jay Shaughnessy <jshaughn@redhat.com>
2023-05-15 15:49:05 -0400

    Try to fix flakes by ensuring button visibility before click - also, reformatting automatically applied, sorry, but I guess it's necessary.
    https://issues.redhat.com/browse/OSSM-3940
    
    Manual backport of https://github.com/kiali/kiali/pull/6143

--- frontend/cypress/integration/common/wizard_istio_config.ts
+++ frontend/cypress/integration/common/wizard_istio_config.ts
@@ -2,14 +2,16 @@ import { And, Then, When } from '@badeball/cypress-cucumber-preprocessor';
 
 When('user clicks in the {string} Istio config actions', (action: string) => {
   cy.get('button[data-test="config-actions-dropdown"]')
-      .click()
-      .get('#loading_kiali_spinner')
-      .should('not.exist');
+    .should('be.visible')
+    .click()
+    .get('#loading_kiali_spinner')
+    .should('not.exist');
 
   cy.get('a[data-test="create_' + action + '"]')
-      .click()
-      .get('#loading_kiali_spinner')
-      .should('not.exist');
+    .should('be.visible')
+    .click()
+    .get('#loading_kiali_spinner')
+    .should('not.exist');
 });
 
 And('user sees the {string} config wizard', (title: string) => {
@@ -17,7 +19,7 @@ And('user sees the {string} config wizard', (title: string) => {
 });
 
 And('user adds listener', () => {
-  cy.get('button[name="addListener"]').click()
+  cy.get('button[name="addListener"]').should('be.visible').click();
 });
 
 And('user types {string} in the {string} input', (value: string, id: string) => {
@@ -35,49 +37,50 @@ And('user checks validation of the hostname {string} input', (id: string) => {
 });
 
 And('user adds a server to a server list', () => {
-  cy.get('[aria-label="Server List"]').find('button').click();
+  cy.get('[aria-label="Server List"]').find('button').should('be.visible').click();
 });
 
-And('the {string} input should display a warning', (id:string)=>{
+And('the {string} input should display a warning', (id: string) => {
   cy.get(`input[id="${id}"]`).invoke('attr', 'aria-invalid').should('eq', 'true');
 });
 
-And('the {string} input should not display a warning', (id:string)=>{
+And('the {string} input should not display a warning', (id: string) => {
   cy.get(`input[id="${id}"]`).invoke('attr', 'aria-invalid').should('eq', 'false');
 });
 
 And('user creates the istio config', () => {
-  cy.get('button[data-test="create"]')
-      .click()
+  cy.get('button[data-test="create"]').should('be.visible').click();
   it('spinner should disappear', { retries: 3 }, () => {
-    cy.get('#loading_kiali_spinner')
-        .should('not.exist');
+    cy.get('#loading_kiali_spinner').should('not.exist');
   });
 });
 
-And('user chooses {string} mode from the {string} select',(option: string, id: string) => {
+And('user chooses {string} mode from the {string} select', (option: string, id: string) => {
   cy.get(`select[id="${id}"]`).select(option);
 });
 
-And('the {string} message should be displayed',(message: string)=> {
+And('the {string} message should be displayed', (message: string) => {
   cy.get('main').contains(message).should('be.visible');
 });
 
-And('user opens the {string} submenu',(title: string)=>{
-  cy.get('button').contains(title).click();
+And('user opens the {string} submenu', (title: string) => {
+  cy.get('button').contains(title).should('be.visible').click();
 });
 
-Then('the {string} {string} should be listed in {string} namespace', function(type: string, name: string, namespace: string) {
+Then('the {string} {string} should be listed in {string} namespace', function (
+  type: string,
+  name: string,
+  namespace: string
+) {
   cy.get(`[data-test=VirtualItem_Ns${namespace}_${type.toLowerCase()}_${name}] svg`).should('exist');
 });
 
-Then('the preview button should be disabled', () =>{
+Then('the preview button should be disabled', () => {
   cy.get('[data-test="preview"').should('be.disabled');
 });
 
-
-Then('an error message {string} is displayed', (message: string) =>{
-  cy.get('h4').contains(message).should("be.visible");
+Then('an error message {string} is displayed', (message: string) => {
+  cy.get('h4').contains(message).should('be.visible');
 });
 
 Then('the {string} input should be empty', (id: string) => {
