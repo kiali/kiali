e71da6ebb5fa5133b863a616ec8a7c6150d3812c
Filip Brychta <fbrychta@redhat.com>
2023-05-19 21:47:20 +0200

    Adding readme for int tests in a container (#6145)
    Also getting kiali route dynamically

--- tests/integration/run-tests.sh
+++ tests/integration/run-tests.sh
@@ -5,5 +5,5 @@ set -o errexit
 set -o pipefail
 
 oc login --token=${TOKEN} --server=${OCP_API_URL} --insecure-skip-tls-verify
-make test-integration -e URL=${KIALI_ROUTE}
+make test-integration -e URL="https://$(oc get route -n istio-system kiali -o 'jsonpath={.spec.host}')"
 cat tests/integration/junit-rest-report.xml
