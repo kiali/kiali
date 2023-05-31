405b9023266681aabaa7c6221754894a7cedb1cc
Alberto Gutierrez <aljesusg@gmail.com>
2023-05-31 11:02:50 +0200

    Upgrade go to 1.20 & SA1019: rnd.seed deprecated

--- handlers/authentication_test.go
+++ handlers/authentication_test.go
@@ -26,7 +26,7 @@ import (
 // TestStrategyTokenAuthentication checks that a user with no active
 // session is logged in successfully
 func TestStrategyTokenAuthentication(t *testing.T) {
-	rand.Seed(time.Now().UnixNano())
+	rand.New(rand.NewSource(time.Now().UnixNano()))
 	cfg := config.NewConfig()
 	cfg.Auth.Strategy = config.AuthStrategyToken
 	cfg.LoginToken.SigningKey = util.RandomString(16)
@@ -130,7 +130,7 @@ func TestLogout(t *testing.T) {
 // TestStrategyHeaderOidcAuthentication checks that a user with no active
 // session is logged in successfully with an OIDC header
 func TestStrategyHeaderOidcAuthentication(t *testing.T) {
-	rand.Seed(time.Now().UnixNano())
+	rand.New(rand.NewSource(time.Now().UnixNano()))
 	cfg := config.NewConfig()
 	cfg.Auth.Strategy = config.AuthStrategyHeader
 	cfg.LoginToken.SigningKey = util.RandomString(16)
@@ -171,7 +171,7 @@ func TestStrategyHeaderOidcAuthentication(t *testing.T) {
 // TestStrategyHeaderAuthentication checks that a user with no active
 // session is logged in successfully with a header that is NOT OIDC
 func TestStrategyHeaderAuthentication(t *testing.T) {
-	rand.Seed(time.Now().UnixNano())
+	rand.New(rand.NewSource(time.Now().UnixNano()))
 	cfg := config.NewConfig()
 	cfg.Auth.Strategy = config.AuthStrategyHeader
 	cfg.LoginToken.SigningKey = util.RandomString(16)
@@ -212,7 +212,7 @@ func TestStrategyHeaderAuthentication(t *testing.T) {
 // TestStrategyHeaderOidcWithImpersonationAuthentication checks that a user with no active
 // session is logged in successfully with a header that is NOT OIDC
 func TestStrategyHeaderOidcWithImpersonationAuthentication(t *testing.T) {
-	rand.Seed(time.Now().UnixNano())
+	rand.New(rand.NewSource(time.Now().UnixNano()))
 	cfg := config.NewConfig()
 	cfg.Auth.Strategy = config.AuthStrategyHeader
 	cfg.LoginToken.SigningKey = util.RandomString(16)
