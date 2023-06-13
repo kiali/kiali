2a1117b38b54edae0c964ec6b28ac9fed29d055d
matejnesuta <matejnetusa@gmail.com>
2023-06-13 11:56:51 +0200

    sleep_script

--- hack/istio/install-testing-demos.sh
+++ hack/istio/install-testing-demos.sh
@@ -11,56 +11,6 @@ set -eu
 HACK_SCRIPT_DIR="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
 source ${HACK_SCRIPT_DIR}/functions.sh
 
-install_sleep_app() {
-
-  if [ "${ISTIO_DIR}" == "" ]; then
-    ISTIO_DIR=$(ls -dt1 ${SCRIPT_DIR}/../../_output/istio-* | head -n1)
-  fi
-
-  if [ "${IS_OPENSHIFT}" == "true" ]; then
-    ${CLIENT_EXE} get project "sleep" || ${CLIENT_EXE} new-project "sleep"
-  else
-    ${CLIENT_EXE} get ns sleep || ${CLIENT_EXE} create ns sleep
-  fi
-
-  ${CLIENT_EXE} label namespace "sleep" istio-injection=enabled --overwrite=true
-
-  # For OpenShift 4.11, adds default service account in the current ns to use as a user
-  if [ "${IS_OPENSHIFT}" == "true" ]; then
-    ${CLIENT_EXE} adm policy add-scc-to-user anyuid system:serviceaccount:sleep:sleep
-  fi
-
-  if [ "${IS_OPENSHIFT}" == "true" ]; then
-      cat <<NAD | $CLIENT_EXE -n sleep apply -f -
-apiVersion: "k8s.cni.cncf.io/v1"
-kind: NetworkAttachmentDefinition
-metadata:
-  name: istio-cni
-NAD
-    cat <<SCC | $CLIENT_EXE apply -n sleep -f -
-apiVersion: security.openshift.io/v1
-kind: SecurityContextConstraints
-metadata:
-  name: sleep-scc
-runAsUser:
-  type: RunAsAny
-seLinuxContext:
-  type: RunAsAny
-supplementalGroups:
-  type: RunAsAny
-users:
-- "system:serviceaccount:sleep:default"
-- "system:serviceaccount:sleep:sleep"
-SCC
-  fi
-
-  ${CLIENT_EXE} apply -n sleep -f ${ISTIO_DIR}/samples/sleep/sleep.yaml
-
-  if [ "${IS_MAISTRA}" == "true" ]; then
-    prepare_maistra "sleep"
-  fi
-}
-
 SCRIPT_DIR="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
 
 # install the Istio release that was last downloaded (that's the -t option to ls)
@@ -155,6 +105,8 @@ if [ "${DELETE_DEMOS}" != "true" ]; then
     "${SCRIPT_DIR}/install-bookinfo-demo.sh" -tg -in ${ISTIO_NAMESPACE} -a ${ARCH}
     echo "Deploying error rates demo ..."
     "${SCRIPT_DIR}/install-error-rates-demo.sh" -in ${ISTIO_NAMESPACE} -a ${ARCH}
+    echo "Deploying sleep demo ..."
+    "${SCRIPT_DIR}/install-sleep-demo.sh" 
 
   else
     echo "Deploying bookinfo demo..."
@@ -162,10 +114,10 @@ if [ "${DELETE_DEMOS}" != "true" ]; then
 
     echo "Deploying error rates demo..."
     "${SCRIPT_DIR}/install-error-rates-demo.sh" -c kubectl -in ${ISTIO_NAMESPACE} -a ${ARCH}
-  fi
 
-  echo "Installing the 'sleep' app in the 'sleep' namespace..."
-  install_sleep_app
+    echo "Deploying sleep demo ..."
+    "${SCRIPT_DIR}/install-sleep-demo.sh" -c kubectl
+  fi
 
   # Some front-end tests have conflicts with the wildcard host in the bookinfo-gateway. Patch it with the host resolved for the traffic generator.
   ISTIO_INGRESS_HOST=$(${CLIENT_EXE} get cm -n bookinfo traffic-generator-config -o jsonpath='{.data.route}' | sed 's|.*//\([^\:]*\).*/.*|\1|')
@@ -180,24 +132,16 @@ else
   # Delete everything - don't abort on error, just keep going and try to delete everything
   set +e
 
-  echo "Deleting the 'sleep' app in the 'sleep' namespace..."
-  ${CLIENT_EXE} delete -n sleep -f ${ISTIO_DIR}/samples/sleep/sleep.yaml
-  if [ "${IS_MAISTRA}" == "true" ]; then
-    ${CLIENT_EXE} delete smm default -n "sleep" --ignore-not-found=true
-  fi
   if [ "${IS_OPENSHIFT}" == "true" ]; then
-    ${CLIENT_EXE} delete network-attachment-definition istio-cni -n sleep
-    ${CLIENT_EXE} delete scc sleep-scc
-    ${CLIENT_EXE} delete project sleep
-  fi
-  ${CLIENT_EXE} delete namespace sleep
-
-  if [ "${IS_OPENSHIFT}" == "true" ]; then
+    echo "Deleting sleep demo ..."
+    "${SCRIPT_DIR}/install-sleep-demo.sh" --delete-sleep true
     echo "Deleting bookinfo demo ..."
     "${SCRIPT_DIR}/install-bookinfo-demo.sh" --delete-bookinfo true
     echo "Deleting error rates demo ..."
     "${SCRIPT_DIR}/install-error-rates-demo.sh" --delete true
   else
+    echo "Deleting sleep demo ..."
+    "${SCRIPT_DIR}/install-sleep-demo.sh" --delete-sleep true -c kubectl
     echo "Deleting bookinfo demo..."
     "${SCRIPT_DIR}/install-bookinfo-demo.sh" --delete-bookinfo true -c kubectl
     echo "Deleting error rates demo..."
