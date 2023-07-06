3e0e578500a56b1f8f4c39290b86a8e7fed43ff5
Jay Shaughnessy <jshaughn@redhat.com>
2023-07-06 15:36:55 -0400

    Fix cypress failures after PF update

--- frontend/cypress/integration/common/kiali_about.ts
+++ frontend/cypress/integration/common/kiali_about.ts
@@ -1,24 +1,21 @@
-import { And, Then } from "@badeball/cypress-cucumber-preprocessor";
-
+import { And, Then } from '@badeball/cypress-cucumber-preprocessor';
 
 And('user clicks on Help Button', () => {
-    cy.getBySel('about-help-button').click()
-})
+  cy.getBySel('about-help-button').click();
+});
 
 And('user clicks on About Button', () => {
-    cy.get('li[role="menuitem"]').contains("About").click();
-    
-})
+  cy.get('li[role="none"]').contains('About').click();
+});
 
 Then(`user see Kiali brand`, () => {
-    cy.contains('Kiali').should('be.visible')
-    cy.contains('Kiali Container').should('be.visible')
-    cy.contains('Istio').should('be.visible')
-    cy.contains('Prometheus').should('be.visible')
-    cy.contains('Kubernetes').should('be.visible')
-    cy.contains('Grafana URL').should('be.visible')
-    cy.contains('Jaeger URL').should('be.visible')
-    cy.get('[href="https://www.kiali.io"]').should('have.attr', 'href')
-    cy.get('[href="https://github.com/kiali"]').should('have.attr', 'href')
-    
-})
+  cy.contains('Kiali').should('be.visible');
+  cy.contains('Kiali Container').should('be.visible');
+  cy.contains('Istio').should('be.visible');
+  cy.contains('Prometheus').should('be.visible');
+  cy.contains('Kubernetes').should('be.visible');
+  cy.contains('Grafana URL').should('be.visible');
+  cy.contains('Jaeger URL').should('be.visible');
+  cy.get('[href="https://www.kiali.io"]').should('have.attr', 'href');
+  cy.get('[href="https://github.com/kiali"]').should('have.attr', 'href');
+});
