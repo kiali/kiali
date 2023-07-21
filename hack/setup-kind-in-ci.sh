7edfcfca1f5599f9af7b13c261a2c75456ab6a28
Nick Fox <nfox@redhat.com>
2023-07-21 13:23:01 -0400

    Update kind version (#6301) (#6395)
    Co-authored-by: Fernando Hoyos <122779323+ferhoyos@users.noreply.github.com>

--- hack/setup-kind-in-ci.sh
+++ hack/setup-kind-in-ci.sh
@@ -50,17 +50,13 @@ TARGET_BRANCH="${TARGET_BRANCH:-master}"
 # https://istio.io/latest/docs/releases/supported-releases/
 if [ "${TARGET_BRANCH}" == "v1.48" ]; then
   ISTIO_VERSION="1.13.0"
-elif [ "${TARGET_BRANCH}" == "v1.36" ]; then
-  ISTIO_VERSION="1.10.0"
 fi
 
 KIND_NODE_IMAGE=""
-if [ "${ISTIO_VERSION}" == "1.10.0" ]; then
-  KIND_NODE_IMAGE="kindest/node:v1.21.10@sha256:84709f09756ba4f863769bdcabe5edafc2ada72d3c8c44d6515fc581b66b029c"
-elif [ "${ISTIO_VERSION}" == "1.13.0" ]; then
+if [ "${ISTIO_VERSION}" == "1.13.0" ]; then
   KIND_NODE_IMAGE="kindest/node:v1.23.4@sha256:0e34f0d0fd448aa2f2819cfd74e99fe5793a6e4938b328f657c8e3f81ee0dfb9"
 else
-  KIND_NODE_IMAGE="kindest/node:v1.24.4@sha256:adfaebada924a26c2c9308edd53c6e33b3d4e453782c0063dc0028bdebaddf98"
+  KIND_NODE_IMAGE="kindest/node:v1.24.15@sha256:24473777a1eef985dc405c23ab9f4daddb1352ca23db60b75de9e7c408096491"
 fi
 
 # print out our settings for debug purposes
