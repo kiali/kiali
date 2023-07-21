7932d49248ac6c0686f5179a8f1374263d576197
Hayk Hovsepyan <hhovsepy@redhat.com>
2023-07-21 12:48:07 +0200

    Fix potential flaky test by timeout

--- frontend/cypress/integration/common/label_check.ts
+++ frontend/cypress/integration/common/label_check.ts
@@ -4,11 +4,11 @@ export function activeFilters(count:number){
     })
     .then(($lis) => {
       cy.wrap($lis).should('have.length', count);
-  }); 
+  });
 }
 
 export function showMore(){
-  cy.get('#filter-selection > :nth-child(2)').contains('more').click();
-  cy.get('#filter-selection > :nth-child(2)').contains('more').should('not.exist');
+  cy.get('#filter-selection > :nth-child(2)', { timeout: 1000 }).contains('more').should('be.visible').click();
+  cy.get('#filter-selection > :nth-child(2)', { timeout: 1000 }).contains('more').should('not.exist');
 }
 
