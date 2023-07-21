dca37729803ba806c5e930d873faa0b898139c4b
Hayk Hovsepyan <hhovsepy@redhat.com>
2023-07-21 14:34:29 +0200

    better way

--- frontend/cypress/integration/common/label_check.ts
+++ frontend/cypress/integration/common/label_check.ts
@@ -8,7 +8,8 @@ export function activeFilters(count:number){
 }
 
 export function showMore(){
-  cy.get('#filter-selection > :nth-child(2)', { timeout: 1000 }).contains('more').should('be.visible').click();
+  cy.get('#filter-selection > :nth-child(2)', { timeout: 1000 }).contains('more').should('be.visible');
+  cy.get('#filter-selection > :nth-child(2)').contains('more').click();
   cy.get('#filter-selection > :nth-child(2)', { timeout: 1000 }).contains('more').should('not.exist');
 }
 
