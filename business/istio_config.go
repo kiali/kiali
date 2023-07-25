2099552f73a28e143b22ba68ff16a73e349bb09f
Josune Cordoba <49480155+josunect@users.noreply.github.com>
2023-07-25 13:18:58 +0100

    Update the way kiali identifies Ambient (#6406)

--- business/istio_config.go
+++ business/istio_config.go
@@ -8,7 +8,6 @@ import (
 	"strings"
 	"sync"
 
-	"gopkg.in/yaml.v2"
 	extentions_v1alpha1 "istio.io/client-go/pkg/apis/extensions/v1alpha1"
 	networking_v1alpha3 "istio.io/client-go/pkg/apis/networking/v1alpha3"
 	networking_v1beta1 "istio.io/client-go/pkg/apis/networking/v1beta1"
@@ -1179,19 +1178,11 @@ func (in *IstioConfigService) IsGatewayAPI(cluster string) bool {
 // Check if istio Ambient profile was enabled
 // ATM it is defined in the istio-cni-config configmap
 func (in *IstioConfigService) IsAmbientEnabled() bool {
-	var cniNetwork map[string]any
-	istioConfigMap, err := in.kialiCache.GetConfigMap(config.Get().IstioNamespace, "istio-cni-config")
+	daemonset, err := in.kialiCache.GetDaemonSet(config.Get().IstioNamespace, "ztunnel")
 	if err != nil {
-		log.Debugf("Error getting istio-cni-config configmap: %s ", err.Error())
+		log.Debugf("No ztunnel found in istio namespace: %s ", err.Error())
 	} else {
-		err = yaml.Unmarshal([]byte(istioConfigMap.Data["cni_network_config"]), &cniNetwork)
-		if err != nil {
-			log.Errorf("Error reading istio-cni-config configmap: %s ", err.Error())
-			return false
-		}
-		ambientEnabled, ok := cniNetwork["ambient_enabled"].(bool)
-
-		if ok && ambientEnabled {
+		if daemonset != nil {
 			return true
 		}
 	}
