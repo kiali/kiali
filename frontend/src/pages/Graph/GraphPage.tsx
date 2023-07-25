kiali/kiali

commit bd589b8c25516897cd99275b8a7f2aff44aa0440

Fix bad request issue when trying to prematurely show side-panel.  This (#6087)
also serves as a trivial perf enhancement.

Also:
- add a few catch blocks to prevent uncaught errors on canceled requests.
- add a couple of things to .gitignore to support some dev environments

https://github.com/kiali/kiali/issues/6068

frontend/src/pages/Graph/GraphPage.tsx
@@ -134,7 +134,7 @@ type ReduxProps = {
   showServiceNodes: boolean;
   showTrafficAnimation: boolean;
   showVirtualServices: boolean;
-  startTour: ({ info: TourInfo, stop: number }) => void;
+  startTour: ({ info, stop }) => void;
   summaryData: SummaryData | null;
   trace?: JaegerTrace;
   trafficRates: TrafficRate[];
@@ -513,7 +513,7 @@ export class GraphPage extends React.Component<GraphPageProps, GraphPageState> {
                 </div>
               )}
             </ErrorBoundary>
-            {this.props.summaryData && (
+            {this.props.summaryData && !this.state.graphData.isLoading && (
               <SummaryPanel
                 data={this.props.summaryData}
                 duration={this.state.graphData.fetchParams.duration}

.gitignore
@@ -107,3 +107,9 @@ frontend/src/sass/*
 # Cypress
 frontend/cypress/videos
 frontend/cypress/screenshots
+
+# Do not commit core-ui code to this repo
+core-ui
+
+# Do not commit plugin code to this repo
+ossmc

frontend/src/pages/Graph/SummaryPanelGraph.tsx
@@ -644,6 +644,12 @@ export default class SummaryPanelGraph extends React.Component<SummaryPanelPropT
         )
         .then(() => {
           this.setState({ validationsMap: newValidationsMap });
+        })
+        .catch(err => {
+          if (!err.isCanceled) {
+            console.log('SummaryPanelGraph: Error fetching Ignore fetch validations error (canceled).');
+            return;
+          }
         });
     });
   };
@@ -651,12 +657,21 @@ export default class SummaryPanelGraph extends React.Component<SummaryPanelPropT
   private fetchValidationsChunk(chunk: Namespace[], validationsMap: ValidationsMap) {
     return Promise.all(
       chunk.map(ns => {
-        return API.getNamespaceValidations(ns.name).then(rs => ({ validation: rs.data, ns: ns }));
+        return API.getNamespaceValidations(ns.name)
+          .then(rs => ({ validation: rs.data, ns: ns }))
+          .catch(err => {
+            if (!err.isCanceled) {
+              console.log(`SummaryPanelGraph: Error fetching validation chunk: ${API.getErrorString(err)}`);
+            }
+            return { validation: undefined, ns: undefined };
+          });
       })
     )
       .then(results => {
         results.forEach(result => {
-          validationsMap[result.ns.name] = result.validation;
+          if (result.ns) {
+            validationsMap[result.ns.name] = result.validation;
+          }
         });
       })
       .catch(err => {

commit 1c23aef94ca4e9ac77a8b727caa47651257067d9

Fix regression introduced in https://github.com/kiali/kiali/pull/6087. That PR successfully fixed an issue with caused by premature rendering of the SummaryPanel.  But the overly-delayed rendering caused a resize of the available graph space, which forced a unwanted "fit".  This PR changes the approach, delaying an appropriate amount.
https://github.com/kiali/kiali/issues/6248

frontend/src/pages/Graph/GraphPage.tsx
@@ -507,7 +507,7 @@ class GraphPageComponent extends React.Component<GraphPageProps, GraphPageState>
                 </div>
               )}
             </ErrorBoundary>
-            {this.props.summaryData && !this.state.graphData.isLoading && (
+            {isReady && this.props.summaryData && (
               <SummaryPanel
                 data={this.props.summaryData}
                 duration={this.state.graphData.fetchParams.duration}

frontend/src/pages/Graph/GraphToolbar/GraphToolbar.tsx
@@ -244,7 +244,6 @@ class GraphToolbarComponent extends React.PureComponent<GraphToolbarProps> {
   }
 
   private handleNamespaceReturn = () => {
-    console.log(`isPf=${this.props.isPF}`);
     const route = this.props.isPF ? 'graphpf' : 'graph';
     if (
       !this.props.summaryData ||