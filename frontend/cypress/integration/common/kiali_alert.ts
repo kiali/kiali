2c3344f182847442ac1f0a94194c38e658949488
matejnesuta <matejnetusa@gmail.com>
2023-07-28 14:40:04 +0200

    alert test

---
+++ frontend/cypress/integration/common/kiali_alert.ts
@@ -0,0 +1,16 @@
+import { Then } from '@badeball/cypress-cucumber-preprocessor';
+
+Then(`user should see no Istio Components Status`, () => {
+  cy.intercept({
+    pathname: '**/api/istio/status*',
+    query: {
+      objects: ''
+    }
+  }).as('istioStatus');
+
+  cy.request('GET', '/api/istio/status')
+  cy.wait('@istioStatus');
+  cy.waitForReact();
+  cy.get('[data-test="istio-status-danger"]',{ timeout: 1000 }).should('not.exist');
+  cy.get('[data-test="istio-status-warning"]',{ timeout: 1000 }).should('not.exist');
+});
