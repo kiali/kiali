4f4d6b9850174227439bcbcf742bd9317435d9f1
Nick Fox <nick@nrfox.com>
2023-08-21 09:26:34 -0400

    Apply suggestions from code review
    Co-authored-by: John Mazzitelli <mazz@redhat.com>

--- hack/istio/multicluster/setup-tracing.sh
+++ hack/istio/multicluster/setup-tracing.sh
@@ -5,20 +5,20 @@ set -e
 SCRIPT_DIR="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
 source ${SCRIPT_DIR}/env.sh $*
 
-# This scripts combines traces from the west cluster into the east cluster by sending traces through the istio-ingressgateway.
+# This script combines traces from the west cluster into the east cluster by sending traces through the istio-ingressgateway.
 # It deploys an otel collector into the west cluster which gathers all traces and exports them to the east cluster.
 # It opens a separate port on the istio-ingressgateway for zipkin so that the tracing setup doesn't conflict with
 # the bookinfo gateway/traffic.
 
 # This adds a port to the istio-ingressgateway service. This could probably be done through istioctl and passing the right combination
 # of settings into the istioctl hack script but it's way simpler to just patch the service directly.
-ingress_output=$(kubectl get svc -n istio-system --context "${CLUSTER1_CONTEXT}" istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="zipkin-http")]}')
+ingress_output=$(${CLIENT_EXE} get svc -n istio-system --context "${CLUSTER1_CONTEXT}" istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="zipkin-http")]}')
 # Check if the output is empty
 if [ -z "$ingress_output" ]; then
-    kubectl --context "${CLUSTER1_CONTEXT}" patch Service -n istio-system istio-ingressgateway --type=json -p '[{"op": "add", "path": "/spec/ports/-", "value": {"name": "zipkin-http", "port": 9411, "protocol": "TCP", "targetPort": 8080}}]'
+    ${CLIENT_EXE} --context "${CLUSTER1_CONTEXT}" patch Service -n istio-system istio-ingressgateway --type=json -p '[{"op": "add", "path": "/spec/ports/-", "value": {"name": "zipkin-http", "port": 9411, "protocol": "TCP", "targetPort": 8080}}]'
 fi
 
-kubectl --context "${CLUSTER1_CONTEXT}" apply -f - <<EOF
+${CLIENT_EXE} --context "${CLUSTER1_CONTEXT}" apply -f - <<EOF
 apiVersion: networking.istio.io/v1beta1
 kind: VirtualService
 metadata:
@@ -38,7 +38,7 @@ spec:
           number: 9411
 EOF
 
-kubectl --context "${CLUSTER1_CONTEXT}" apply -f - <<EOF
+${CLIENT_EXE} --context "${CLUSTER1_CONTEXT}" apply -f - <<EOF
 apiVersion: networking.istio.io/v1beta1
 kind: Gateway
 metadata:
@@ -56,7 +56,7 @@ spec:
       protocol: HTTP
 EOF
 
-ISTIO_INGRESS_IP=$(kubectl --context "${CLUSTER1_CONTEXT}" get service -n istio-system istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+ISTIO_INGRESS_IP=$(${CLIENT_EXE} --context "${CLUSTER1_CONTEXT}" get service -n istio-system istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
 # Disable everything except zipkin. We can't rename the service so disable that too and create one ourselves.
 helm --kube-context "${CLUSTER2_CONTEXT}" upgrade --install --namespace istio-system my-opentelemetry-collector open-telemetry/opentelemetry-collector -f - <<EOF
@@ -104,7 +104,7 @@ EOF
 
 # Create a service for the collector named zipkin.istio-system to
 # make it consistent with the normal istio tracing service.
-kubectl --context "${CLUSTER2_CONTEXT}" apply -f - <<EOF
+${CLIENT_EXE} --context "${CLUSTER2_CONTEXT}" apply -f - <<EOF
 apiVersion: v1
 kind: Service
 metadata:
