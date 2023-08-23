2cffc7e89b87af422bfac54dcd50affd006e060e
Leandro Beretta <lea.beretta@gmail.com>
2023-08-23 11:57:58 -0300

    new set of svg for PF graph

--- frontend/src/pages/GraphPF/GraphLegendDataPF.ts
+++ frontend/src/pages/GraphPF/GraphLegendDataPF.ts
@@ -1,39 +1,39 @@
 // Node Shapes
-import workloadImage from '../../assets/img/legend/node.svg';
-import appImage from '../../assets/img/legend/app.svg';
-import serviceImage from '../../assets/img/legend/service.svg';
-import aggregateImage from '../../assets/img/legend/aggregate.svg';
-import serviceEntryImage from '../../assets/img/legend/service-entry.svg';
+import workloadImage from '../../assets/img/legend-pf/node.svg';
+import appImage from '../../assets/img/legend-pf/app.svg';
+import serviceImage from '../../assets/img/legend-pf/service.svg';
+import aggregateImage from '../../assets/img/legend-pf/aggregate.svg';
+import serviceEntryImage from '../../assets/img/legend-pf/service-entry.svg';
 // Node Colors
-import nodeColorNormalImage from '../../assets/img/legend/node-color-normal.svg';
-import nodeColorWarningImage from '../../assets/img/legend/node-color-warning.svg';
-import nodeColorDangerImage from '../../assets/img/legend/node-color-danger.svg';
-import nodeColorIdleImage from '../../assets/img/legend/node-color-idle.svg';
+import nodeColorHealthyImage from '../../assets/img/legend-pf/node-color-healthy.svg';
+import nodeColorWarningImage from '../../assets/img/legend-pf/node-color-warning.svg';
+import nodeColorDangerImage from '../../assets/img/legend-pf/node-color-danger.svg';
+import nodeColorIdleImage from '../../assets/img/legend-pf/node-color-idle.svg';
 // Node Background
-import externalNamespaceImage from '../../assets/img/legend/external-namespace.svg';
-import restrictedNamespaceImage from '../../assets/img/legend/restricted-namespace.svg';
+import externalNamespaceImage from '../../assets/img/legend-pf/external-namespace.svg';
+import restrictedNamespaceImage from '../../assets/img/legend-pf/restricted-namespace.svg';
 // Edges
-import edgeSuccessImage from '../../assets/img/legend/edge-success.svg';
-import edgeDangerImage from '../../assets/img/legend/edge-danger.svg';
-import edgeWarnImage from '../../assets/img/legend/edge-warn.svg';
-import edgeIdlemage from '../../assets/img/legend/edge-idle.svg';
-import edgeTcpImage from '../../assets/img/legend/edge-tcp.svg';
-import edgeMtlsImage from '../../assets/img/legend/mtls-badge.svg';
+import edgeSuccessImage from '../../assets/img/legend-pf/edge-success.svg';
+import edgeDangerImage from '../../assets/img/legend-pf/edge-danger.svg';
+import edgeWarnImage from '../../assets/img/legend-pf/edge-warn.svg';
+import edgeIdlemage from '../../assets/img/legend-pf/edge-idle.svg';
+import edgeTcpImage from '../../assets/img/legend-pf/edge-tcp.svg';
+import edgeMtlsImage from '../../assets/img/legend-pf/mtls-badge.svg';
 // Traffic Animation
-import trafficNormalImage from '../../assets/img/legend/traffic-normal-request.svg';
-import trafficFailedImage from '../../assets/img/legend/traffic-failed-request.svg';
-import trafficTcpImage from '../../assets/img/legend/traffic-tcp.svg';
+import trafficHealthyImage from '../../assets/img/legend-pf/traffic-healthy-request.svg';
+import trafficFailedImage from '../../assets/img/legend-pf/traffic-failed-request.svg';
+import trafficTcpImage from '../../assets/img/legend-pf/traffic-tcp.svg';
 // Badges
-import badgeCircuitBreakerImage from '../../assets/img/legend/node-badge-circuit-breaker.svg';
-import badgeFaultInjectionImage from '../../assets/img/legend/node-badge-fault-injection.svg';
-import badgeGatewaysImage from '../../assets/img/legend/node-badge-gateways.svg';
-import badgeMirroringImage from '../../assets/img/legend/node-badge-mirroring.svg';
-import badgeMissingSidecarImage from '../../assets/img/legend/node-badge-missing-sidecar.svg';
-import badgeRequestTimeoutImage from '../../assets/img/legend/node-badge-request-timeout.svg';
-import badgeTrafficShiftingSourceImage from '../../assets/img/legend/node-badge-traffic-shifting.svg';
-import badgeTrafficSourceImage from '../../assets/img/legend/node-badge-traffic-source.svg';
-import badgeVirtualServicesImage from '../../assets/img/legend/node-badge-virtual-services.svg';
-import badgeWorkloadEntryImage from '../../assets/img/legend/node-badge-workload-entry.svg';
+import badgeCircuitBreakerImage from '../../assets/img/legend-pf/node-badge-circuit-breaker.svg';
+import badgeFaultInjectionImage from '../../assets/img/legend-pf/node-badge-fault-injection.svg';
+import badgeGatewaysImage from '../../assets/img/legend-pf/node-badge-gateways.svg';
+import badgeMirroringImage from '../../assets/img/legend-pf/node-badge-mirroring.svg';
+import badgeMissingSidecarImage from '../../assets/img/legend-pf/node-badge-missing-sidecar.svg';
+import badgeRequestTimeoutImage from '../../assets/img/legend-pf/node-badge-request-timeout.svg';
+import badgeTrafficShiftingSourceImage from '../../assets/img/legend-pf/node-badge-traffic-shifting.svg';
+import badgeTrafficSourceImage from '../../assets/img/legend-pf/node-badge-traffic-source.svg';
+import badgeVirtualServicesImage from '../../assets/img/legend-pf/node-badge-virtual-services.svg';
+import badgeWorkloadEntryImage from '../../assets/img/legend-pf/node-badge-workload-entry.svg';
 
 export interface GraphLegendItem {
   title: string;
@@ -59,7 +59,7 @@ export const legendData: GraphLegendItem[] = [
   {
     title: 'Node Colors',
     data: [
-      { label: 'Normal', icon: nodeColorNormalImage },
+      { label: 'Healthy', icon: nodeColorHealthyImage },
       { label: 'Warn', icon: nodeColorWarningImage },
       { label: 'Danger', icon: nodeColorDangerImage },
       { label: 'Idle', icon: nodeColorIdleImage }
@@ -86,7 +86,7 @@ export const legendData: GraphLegendItem[] = [
   {
     title: 'Traffic Animation',
     data: [
-      { label: 'Normal Request', icon: trafficNormalImage },
+      { label: 'Healthy Request', icon: trafficHealthyImage },
       { label: 'Failed Request', icon: trafficFailedImage },
       { label: 'TCP Traffic', icon: trafficTcpImage }
     ]
