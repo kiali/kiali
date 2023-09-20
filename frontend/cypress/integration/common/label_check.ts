e70c2ccbd484da8005132f237b3398ad690784bb
Hayk Hovsepyan <hhovsepy@redhat.com>
2023-09-20 12:23:57 +0200

    removed commented code

--- frontend/cypress/integration/common/label_check.ts
+++ frontend/cypress/integration/common/label_check.ts
@@ -9,8 +9,5 @@ export function activeFilters(count:number){
 
 export function showMore(){
   cy.get('button[data-ouia-component-type="PF5/OverflowChip"]', { timeout: 1000 }).click();
-  // cy.get('#filter-selection > :nth-child(2)', { timeout: 1000 }).contains('more').should('be.visible');
-  // cy.get('#filter-selection > :nth-child(2)').contains('more').click();
-  // cy.get('#filter-selection > :nth-child(2)', { timeout: 1000 }).contains('more').should('not.exist');
 }
 
