e72e61d5e80907910349a8ca6254f50a61ad9332
Hayk Hovsepyan <hhovsepy@redhat.com>
2023-09-20 11:03:17 +0200

    fixed

--- frontend/cypress/integration/common/label_check.ts
+++ frontend/cypress/integration/common/label_check.ts
@@ -8,7 +8,7 @@ export function activeFilters(count:number){
 }
 
 export function showMore(){
-  cy.get('button[type="PF5/OverflowChip"]', { timeout: 1000 }).click();
+  cy.get('button[data-ouia-component-type="PF5/OverflowChip"]', { timeout: 1000 }).click();
   // cy.get('#filter-selection > :nth-child(2)', { timeout: 1000 }).contains('more').should('be.visible');
   // cy.get('#filter-selection > :nth-child(2)').contains('more').click();
   // cy.get('#filter-selection > :nth-child(2)', { timeout: 1000 }).contains('more').should('not.exist');
