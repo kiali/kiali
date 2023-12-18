335ef4796435264c100346a25102ddb9333488a0
josunect <josune.cordoba@gmail.com>
2023-12-18 10:41:37 +0000

    Update Kiali config

--- hack/setup-kind-in-ci.sh
+++ hack/setup-kind-in-ci.sh
@@ -266,6 +266,8 @@ EOF
     --set external_services.grafana.dashboards[0].name="Istio Mesh Dashboard" \
     --set external_services.tracing.provider="tempo" \
     --set external_services.tracing.url="http://tempo-cr-frontend-query.tempo:3200" \
+    --set external_services.tracing.in_cluster_url="http://tempo-cr-frontend-query.tempo:3200" \
+    --set external_services.tracing.use_grpc="false" \
     --set health_config.rate[0].kind="service" \
     --set health_config.rate[0].name="y-server" \
     --set health_config.rate[0].namespace="alpha" \
