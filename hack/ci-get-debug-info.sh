33989d71b4f5ff97b531d54ae16c4af80e022348
Nick Fox <nfox@redhat.com>
2024-01-03 13:21:14 -0500

    Don't pass context for single cluster tests

--- hack/ci-get-debug-info.sh
+++ hack/ci-get-debug-info.sh
@@ -5,7 +5,7 @@
 #
 
 OUTPUT_DIRECTORY=""
-KUBECTL_CONTEXT="kind-east"
+KUBECTL_CONTEXT=""
 
 # Process command line args
 while [[ $# -gt 0 ]]; do
@@ -27,6 +27,7 @@ Valid command line arguments:
     If not provided, a temporary directory will be created.
   -c|--kubectl-context <context_name>
     Specify the kubectl context to use.
+    If not provided, the current context is used.
   -h|--help:
     Display this help message
 HELPMSG
@@ -45,12 +46,15 @@ if [ -z "$OUTPUT_DIRECTORY" ]; then
   echo "INFO: Output directory not provided. Using temporary directory: $OUTPUT_DIRECTORY"
 fi
 
+context_flag=""
+[ -n "${KUBECTL_CONTEXT}" ] && context_flag="--context ${KUBECTL_CONTEXT}"
+
 # Get debug info and write to a separate file.
-kubectl --context "${KUBECTL_CONTEXT}" logs -l app.kubernetes.io/name=kiali --tail=-1 --all-containers -n istio-system > "${OUTPUT_DIRECTORY}/kiali_logs.txt" || rm "${OUTPUT_DIRECTORY}/kiali_logs.txt"
-kubectl --context "${KUBECTL_CONTEXT}" describe nodes > "${OUTPUT_DIRECTORY}/describe_nodes.txt" || rm "${OUTPUT_DIRECTORY}/describe_nodes.txt"
-kubectl --context "${KUBECTL_CONTEXT}" get pods -l app.kubernetes.io/name=kiali -n istio-system -o yaml > "${OUTPUT_DIRECTORY}/kiali_pods.yaml" || rm "${OUTPUT_DIRECTORY}/kiali_pods.yaml"
-kubectl --context "${KUBECTL_CONTEXT}" describe pods -n metallb-system > "${OUTPUT_DIRECTORY}/describe_metallb_pods.txt" || rm "${OUTPUT_DIRECTORY}/describe_metallb_pods.txt"
-kubectl --context "${KUBECTL_CONTEXT}" logs -p deployments/controller -n metallb-system > "${OUTPUT_DIRECTORY}/metallb_controller_logs.txt" || rm "${OUTPUT_DIRECTORY}/metallb_controller_logs.txt"
-kubectl --context "${KUBECTL_CONTEXT}" logs -p ds/speaker -n metallb-system > "${OUTPUT_DIRECTORY}/metallb_speaker_logs.txt" || rm "${OUTPUT_DIRECTORY}/metallb_speaker_logs.txt"
-kubectl --context "${KUBECTL_CONTEXT}" logs deployments/controller -n metallb-system > "${OUTPUT_DIRECTORY}/metallb_controller_current_logs.txt" || rm "${OUTPUT_DIRECTORY}/metallb_controller_current_logs.txt"
-kubectl --context "${KUBECTL_CONTEXT}" logs ds/speaker -n metallb-system > "${OUTPUT_DIRECTORY}/metallb_speaker_current_logs.txt" || rm "${OUTPUT_DIRECTORY}/metallb_speaker_current_logs.txt"
+kubectl ${context_flag} logs -l app.kubernetes.io/name=kiali --tail=-1 --all-containers -n istio-system > "${OUTPUT_DIRECTORY}/kiali_logs.txt" || rm "${OUTPUT_DIRECTORY}/kiali_logs.txt"
+kubectl ${context_flag} describe nodes > "${OUTPUT_DIRECTORY}/describe_nodes.txt" || rm "${OUTPUT_DIRECTORY}/describe_nodes.txt"
+kubectl ${context_flag} get pods -l app.kubernetes.io/name=kiali -n istio-system -o yaml > "${OUTPUT_DIRECTORY}/kiali_pods.yaml" || rm "${OUTPUT_DIRECTORY}/kiali_pods.yaml"
+kubectl ${context_flag} describe pods -n metallb-system > "${OUTPUT_DIRECTORY}/describe_metallb_pods.txt" || rm "${OUTPUT_DIRECTORY}/describe_metallb_pods.txt"
+kubectl ${context_flag} logs -p deployments/controller -n metallb-system > "${OUTPUT_DIRECTORY}/metallb_controller_logs.txt" || rm "${OUTPUT_DIRECTORY}/metallb_controller_logs.txt"
+kubectl ${context_flag} logs -p ds/speaker -n metallb-system > "${OUTPUT_DIRECTORY}/metallb_speaker_logs.txt" || rm "${OUTPUT_DIRECTORY}/metallb_speaker_logs.txt"
+kubectl ${context_flag} logs deployments/controller -n metallb-system > "${OUTPUT_DIRECTORY}/metallb_controller_current_logs.txt" || rm "${OUTPUT_DIRECTORY}/metallb_controller_current_logs.txt"
+kubectl ${context_flag} logs ds/speaker -n metallb-system > "${OUTPUT_DIRECTORY}/metallb_speaker_current_logs.txt" || rm "${OUTPUT_DIRECTORY}/metallb_speaker_current_logs.txt"
