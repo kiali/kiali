66577b9b496c240b4fff5bb291112b3a9fd189ea
Hayk Hovsepyan <hhovsepy@redhat.com>
2024-01-05 10:53:45 +0100

    Added Reference from K8sHTTPRoute to K8sReferenceGrant (#6994)
    * Added Reference from K8sHTTPRoute to K8sReferenceGrant
    
    * Added tests
    
    * Service reference to HTTPRoute considers ReferenceGrant
    
    * fixed failure
    
    * added tests
    
    * alphabetise

--- kubernetes/filters.go
+++ kubernetes/filters.go
@@ -12,6 +12,7 @@ import (
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
 	k8s_networking_v1 "sigs.k8s.io/gateway-api/apis/v1"
+	k8s_networking_v1beta1 "sigs.k8s.io/gateway-api/apis/v1beta1"
 
 	"github.com/kiali/kiali/config"
 )
@@ -524,14 +525,20 @@ func FilterVirtualServicesByService(allVs []*networking_v1beta1.VirtualService, 
 	return filtered
 }
 
-func FilterK8sHTTPRoutesByService(allRoutes []*k8s_networking_v1.HTTPRoute, namespace string, serviceName string) []*k8s_networking_v1.HTTPRoute {
+func FilterK8sHTTPRoutesByService(allRoutes []*k8s_networking_v1.HTTPRoute, referenceGrants []*k8s_networking_v1beta1.ReferenceGrant, namespace string, serviceName string) []*k8s_networking_v1.HTTPRoute {
 	filtered := []*k8s_networking_v1.HTTPRoute{}
 	for _, route := range allRoutes {
 		appendRoute := serviceName == ""
 		if !appendRoute {
 			for _, rule := range route.Spec.Rules {
 				for _, backendRef := range rule.BackendRefs {
-					if string(backendRef.Name) != "" && FilterByHost(string(backendRef.Name), route.Namespace, serviceName, namespace) {
+					backendRefNamespace := route.Namespace
+					if backendRef.Namespace != nil {
+						backendRefNamespace = string(*backendRef.Namespace)
+					}
+					if string(backendRef.Name) != "" && FilterByHost(string(backendRef.Name), backendRefNamespace, serviceName, namespace) &&
+						// a reference grant should exist to reference service namespace to route namespace, or they are in the same namespace
+						(HasMatchingReferenceGrant(route.Namespace, namespace, K8sActualHTTPRouteType, ServiceType, referenceGrants) || route.Namespace == namespace) {
 						appendRoute = true
 					}
 				}
--- models/service.go
+++ models/service.go
@@ -5,6 +5,7 @@ import (
 	core_v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	k8s_networking_v1 "sigs.k8s.io/gateway-api/apis/v1"
+	k8s_networking_v1beta1 "sigs.k8s.io/gateway-api/apis/v1beta1"
 
 	"github.com/kiali/kiali/config"
 	"github.com/kiali/kiali/kubernetes"
@@ -72,21 +73,22 @@ type ServiceDefinitionList struct {
 }
 
 type ServiceDetails struct {
-	Service          Service                               `json:"service"`
-	Cluster          string                                `json:"cluster"`
-	IstioSidecar     bool                                  `json:"istioSidecar"`
-	Endpoints        Endpoints                             `json:"endpoints"`
-	VirtualServices  []*networking_v1beta1.VirtualService  `json:"virtualServices"`
-	DestinationRules []*networking_v1beta1.DestinationRule `json:"destinationRules"`
-	K8sHTTPRoutes    []*k8s_networking_v1.HTTPRoute        `json:"k8sHTTPRoutes"`
-	ServiceEntries   []*networking_v1beta1.ServiceEntry    `json:"serviceEntries"`
-	IstioPermissions ResourcePermissions                   `json:"istioPermissions"`
-	Workloads        WorkloadOverviews                     `json:"workloads"`
+	Cluster            string                                   `json:"cluster"`
+	DestinationRules   []*networking_v1beta1.DestinationRule    `json:"destinationRules"`
+	Endpoints          Endpoints                                `json:"endpoints"`
+	IstioPermissions   ResourcePermissions                      `json:"istioPermissions"`
+	IstioSidecar       bool                                     `json:"istioSidecar"`
+	K8sHTTPRoutes      []*k8s_networking_v1.HTTPRoute           `json:"k8sHTTPRoutes"`
+	K8sReferenceGrants []*k8s_networking_v1beta1.ReferenceGrant `json:"k8sReferenceGrants"`
+	Service            Service                                  `json:"service"`
+	ServiceEntries     []*networking_v1beta1.ServiceEntry       `json:"serviceEntries"`
+	VirtualServices    []*networking_v1beta1.VirtualService     `json:"virtualServices"`
+	Workloads          WorkloadOverviews                        `json:"workloads"`
 	// Services with same app labels (different versions or a single version)
-	SubServices   []*ServiceOverview `json:"subServices"`
 	Health        ServiceHealth      `json:"health"`
-	Validations   IstioValidations   `json:"validations"`
 	NamespaceMTLS MTLSStatus         `json:"namespaceMTLS"`
+	SubServices   []*ServiceOverview `json:"subServices"`
+	Validations   IstioValidations   `json:"validations"`
 }
 
 type (
