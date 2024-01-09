a335c7c8e3d3d54464c5ec376abce26db6fd0952
Leandro Beretta <lea.beretta@gmail.com>
2024-01-09 10:54:59 -0300

    adding group customizations. Changing the focus style to a simpler way, easy for removing the customizations in PF files

---
+++ frontend/src/pages/GraphPF/NodeDecorator.tsx
@@ -0,0 +1,38 @@
+import { Tooltip } from '@patternfly/react-core';
+import {
+  DEFAULT_DECORATOR_RADIUS,
+  Decorator,
+  Node,
+  TopologyQuadrant,
+  getDefaultShapeDecoratorCenter,
+  observer
+} from '@patternfly/react-topology';
+import { IconType } from 'config/Icons';
+import React from 'react';
+
+interface Props {
+  element: Node;
+  icon: IconType;
+  quadrant: TopologyQuadrant;
+  tooltip?: string;
+}
+
+const NodeDecoratorInner: React.FC<Props> = ({ element, quadrant, icon, tooltip }) => {
+  const { x, y } = getDefaultShapeDecoratorCenter(quadrant, element);
+  const decoratorRef = React.useRef<SVGAElement | null>(null);
+
+  return (
+    <Tooltip triggerRef={decoratorRef} content={!!tooltip ? tooltip : icon.text}>
+      <Decorator
+        innerRef={decoratorRef}
+        x={x}
+        y={y}
+        radius={DEFAULT_DECORATOR_RADIUS}
+        showBackground
+        icon={React.createElement(icon.icon)}
+      />
+    </Tooltip>
+  );
+};
+
+export const NodeDecorator = observer(NodeDecoratorInner);
