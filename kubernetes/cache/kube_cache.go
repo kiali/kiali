9c15b3d82f5d719995dbaffbe4e7de9529bafb84
Hayk Hovsepyan <hhovsepy@redhat.com>
2024-01-17 16:03:24 +0100

    better approach of checking K8s Gateway Listers

--- kubernetes/cache/kube_cache.go
+++ kubernetes/cache/kube_cache.go
@@ -160,8 +160,11 @@ type kubeCache struct {
 	client             kubernetes.ClientInterface
 	clusterCacheLister *cacheLister
 	clusterScoped      bool
-	nsCacheLister      map[string]*cacheLister
-	refreshDuration    time.Duration
+	// used in methods before calling Gateway API listers
+	// added because of potential nil issue when CRDs are applied after Kiali pod starts
+	hasGatewayAPIStarted bool
+	nsCacheLister        map[string]*cacheLister
+	refreshDuration      time.Duration
 	// Stops the cluster scoped informers when a refresh is necessary.
 	// Close this channel to stop the cluster-scoped informers.
 	stopClusterScopedChan chan struct{}
@@ -405,6 +408,7 @@ func (c *kubeCache) createGatewayInformers(namespace string) gateway.SharedInfor
 
 		lister.k8sreferencegrantLister = sharedInformers.Gateway().V1beta1().ReferenceGrants().Lister()
 		lister.cachesSynced = append(lister.cachesSynced, sharedInformers.Gateway().V1beta1().ReferenceGrants().Informer().HasSynced)
+		c.hasGatewayAPIStarted = true
 	}
 	return sharedInformers
 }
@@ -1468,26 +1472,9 @@ func (c *kubeCache) GetTelemetries(namespace, labelSelector string) ([]*v1alpha1
 
 func (c *kubeCache) isK8sGatewayListerInit(namespace string) bool {
 	// potential issue can happen when CRDs are created after Kiali start
-	if namespace == metav1.NamespaceAll {
-		if c.clusterScoped {
-			if c.clusterCacheLister.k8sgatewayLister == nil {
-				log.Info(K8sGatewayAPIMessage)
-				return false
-			}
-		} else {
-			for _, nsCacheLister := range c.nsCacheLister {
-				if nsCacheLister.k8sgatewayLister == nil {
-					log.Info(K8sGatewayAPIMessage)
-					return false
-				}
-			}
-		}
-	} else {
-		if c.getCacheLister(namespace).k8sgatewayLister == nil {
-			log.Info(K8sGatewayAPIMessage)
-			return false
-		}
-
+	if !c.hasGatewayAPIStarted {
+		log.Info(K8sGatewayAPIMessage)
+		return false
 	}
 	return true
 }
