d96f615d34ec884036b8389cd220a88334d69801
Fernando Hoyos <fhoyosle@redhat.com>
2024-01-25 16:21:01 +0100

    Format precommit bash script

--- frontend/precommit.sh
+++ frontend/precommit.sh
@@ -13,39 +13,34 @@ fi
 #### GO Formatting ####
 go_files=$(git diff --staged --name-only --diff-filter=AM | grep '\.go$' | grep -v '^vendor')
 
-if [ -n "$go_files" ]
-then
-go_unformatted=$(gofmt -l $go_files)
+if [ -n "$go_files" ]; then
+  go_unformatted=$(gofmt -l $go_files)
 fi
 
-if [ -n "$go_unformatted" ]
-then
-echo "Formatting staged GO files:"
-gofmt -w $go_unformatted
-echo $go_unformatted | tr " " "\n"
+if [ -n "$go_unformatted" ]; then
+  echo "Formatting staged GO files:"
+  gofmt -w $go_unformatted
+  echo $go_unformatted | tr " " "\n"
 fi
 
 #### YAML Formatting ####
 yaml_files=$(git diff --staged --name-only HEAD --diff-filter=AM | grep -E '\.yml|\.yaml')
 
-if [ -n "$yaml_files" ]
-then
-yaml_unformatted=$(yamlfmt -dry -quiet $yaml_files 2>&1 | sed 's/^.*://')
+if [ -n "$yaml_files" ]; then
+  yaml_unformatted=$(yamlfmt -dry -quiet $yaml_files 2>&1 | sed 's/^.*://')
 fi
 
-if [ -n "$yaml_unformatted" ]
-then
-echo "Formatting staged YAML files:"
-echo $yaml_unformatted | tr " " "\n"
+if [ -n "$yaml_unformatted" ]; then
+  echo "Formatting staged YAML files:"
+  echo $yaml_unformatted | tr " " "\n"
 
-# Remove trailing whitespaces to avoid yaml formatter bug (https://github.com/google/yamlfmt/issues/153)
-sed -i 's/[ \t]*$//' $yaml_unformatted
-yamlfmt $yaml_unformatted
+  # Remove trailing whitespaces to avoid yaml formatter bug (https://github.com/google/yamlfmt/issues/153)
+  sed -i 's/[ \t]*$//' $yaml_unformatted
+  yamlfmt $yaml_unformatted
 fi
 
 #### Git commit check ####
-if [ -n "$yaml_unformatted" ] || [ -n "$go_unformatted" ]
-then
-echo "Some files have been formatted - the git commit is aborted."
-exit 1
+if [ -n "$yaml_unformatted" ] || [ -n "$go_unformatted" ]; then
+  echo "Some files have been formatted - the git commit is aborted."
+  exit 1
 fi
