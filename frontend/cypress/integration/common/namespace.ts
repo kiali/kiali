4e74e0c197c50e53f6fc4553de758903980761d0
matejnesuta <matejnetusa@gmail.com>
2024-02-07 16:04:52 +0100

    uhhhhh

--- frontend/cypress/integration/common/namespace.ts
+++ frontend/cypress/integration/common/namespace.ts
@@ -3,12 +3,11 @@ import { ensureKialiFinishedLoading } from './transition';
 
 When('user selects the {string} namespace', (namespace: string) => {
   cy.getBySel('namespace-dropdown').click();
-  if (namespace === ""){
-    cy.get('input[type="checkbox"]').each(($el) => {
+  if (namespace === '') {
+    cy.get('input[type="checkbox"]').each($el => {
       cy.wrap($el).uncheck();
     });
-  }
-  else{
+  } else {
     cy.get(`input[type="checkbox"][value="${namespace}"]`).check();
   }
   cy.getBySel('namespace-dropdown').click();
