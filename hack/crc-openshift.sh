bd046a96d5a77bd2b4fe2a133f2364baa437c318
John Mazzitelli <mazz@redhat.com>
2024-02-28 14:21:06 -0500

    [hack] bump CRC to use OS 4.14

--- hack/crc-openshift.sh
+++ hack/crc-openshift.sh
@@ -270,10 +270,10 @@ SCRIPT_ROOT="$( cd "$(dirname "$0")" ; pwd -P )"
 cd ${SCRIPT_ROOT}
 
 # The default version of the crc tool to be downloaded
-DEFAULT_CRC_DOWNLOAD_VERSION="2.23.0"
+DEFAULT_CRC_DOWNLOAD_VERSION="2.32.0"
 
 # The default version of the crc bundle - this is typically the version included with the CRC download
-DEFAULT_CRC_LIBVIRT_DOWNLOAD_VERSION="4.13.3"
+DEFAULT_CRC_LIBVIRT_DOWNLOAD_VERSION="4.14.8"
 
 # The default virtual CPUs assigned to the CRC VM
 DEFAULT_CRC_CPUS="5"
