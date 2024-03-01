9f8ad1c3c8299795cacfec0ac6d8632a2808560e
Fernando Hoyos <fhoyosle@redhat.com>
2024-03-01 11:34:07 +0100

    Load translation from public URL

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -1,11 +1,15 @@
 import i18n from 'i18next';
-import Backend from 'i18next-http-backend';
+import HttpBackend, { HttpBackendOptions } from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
 
 i18n
-  .use(Backend) // loads translations from server
+  .use(HttpBackend) // loads translations from server
   .use(initReactI18next) // passes i18n down to react-i18next
-  .init({
+  .init<HttpBackendOptions>({
+    backend: {
+      loadPath: `${process.env.PUBLIC_URL}/locales/{{lng}}/{{ns}}.json`
+    },
+
     fallbackLng: 'en',
 
     interpolation: {
