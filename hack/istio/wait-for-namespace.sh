9f43675a69658b4bcc796c672a9003b1c7dce2e0
Pavel Marek <86114326+ScriptingShrimp@users.noreply.github.com>
2024-03-13 16:49:29 +0100

    added support of kubectl (#7190)

--- hack/istio/wait-for-namespace.sh
+++ hack/istio/wait-for-namespace.sh
@@ -11,39 +11,39 @@ if [ $# -eq 0 ]
     exit 1
 fi
 
-# process command line args
+helpmsg() {
+  cat <<HELP
+Valid command line arguments:
+   -ce|--client-exe <path to kubectl> The 'kubectl' or 'oc' command, if not in PATH then must be a full path. Default: oc
+   -n|--namespaces <name>: all of the namespaces we want to patch operator and wait for
+   -h|--help : this message
+HELP
+}
+
 while [[ $# -gt 0 ]]; do
   key="$1"
   case $key in
-    -n|--namespace)
-      shift;
-      NAMESPACES=( "$@" )
-      break;
-      ;;
-    -h|--help)
-      cat <<HELPMSG
-Valid command line arguments:
-  -n|--namespaces <name>: all of the namespaces we want to patch operator and wait for
-  -h|--help : this message
-HELPMSG
-      exit 1
-      ;;
-    *)
-      echo "Unknown argument [$key]. Aborting."
-      exit 1
-      ;;
+    -ce|--client-exe)             CLIENT_EXE="$2";            shift;shift; ;;
+    -n|--namespace)               NAMESPACES="$2";            shift;shift; ;;
+    -h|--help)                    helpmsg;                    exit 1       ;;
+    *) echo "Unknown argument: [$key]. Aborting."; helpmsg; exit 1 ;;
   esac
 done
 
+# set up some of our defaults
+CLIENT_EXE=${CLIENT_EXE:-kubectl}
+CLIENT_EXE="$(which ${CLIENT_EXE} 2>/dev/null || echo "invalid kubectl: ${CLIENT_EXE}")"
+echo "Using CLIENT_EXE: $CLIENT_EXE"
+
 for NAMESPACE in ${NAMESPACES[@]}; do
-  oc patch kiali kiali -n kiali-operator --type=json '-p=[{"op": "add", "path": "/spec/deployment/accessible_namespaces/0", "value":"'$NAMESPACE'"}]'
+  ${CLIENT_EXE} patch kiali kiali -n kiali-operator --type=json '-p=[{"op": "add", "path": "/spec/deployment/accessible_namespaces/0", "value":"'$NAMESPACE'"}]'
 done
 
-oc wait --for=condition=Successful kiali/kiali --timeout=120s -n kiali-operator
+${CLIENT_EXE} wait --for=condition=Successful kiali/kiali --timeout=120s -n kiali-operator
 
 for NAMESPACE in ${NAMESPACES[@]}; do
-  oc wait --for=condition=Ready pods --all -n "$NAMESPACE" --timeout 60s || true
-  oc wait --for=condition=Ready pods --all -n "$NAMESPACE" --timeout 60s
+  ${CLIENT_EXE} wait --for=condition=Ready pods --all -n "$NAMESPACE" --timeout 60s || true
+  ${CLIENT_EXE} wait --for=condition=Ready pods --all -n "$NAMESPACE" --timeout 60s
 done
 
 sleep 80
