8483a8ccbbf2933c22bf959e7b0a8b6f58ed93d6
Fernando Hoyos <fhoyosle@redhat.com>
2024-03-26 16:56:21 +0100

    Check Kiali accessible namespaces in wait-for-namespace.sh script

--- hack/istio/wait-for-namespace.sh
+++ hack/istio/wait-for-namespace.sh
@@ -35,15 +35,35 @@ CLIENT_EXE=${CLIENT_EXE:-kubectl}
 CLIENT_EXE="$(which ${CLIENT_EXE} 2>/dev/null || echo "invalid kubectl: ${CLIENT_EXE}")"
 echo "Using CLIENT_EXE: $CLIENT_EXE"
 
-for NAMESPACE in ${NAMESPACES[@]}; do
-  ${CLIENT_EXE} patch kiali kiali -n kiali-operator --type=json '-p=[{"op": "add", "path": "/spec/deployment/accessible_namespaces/0", "value":"'$NAMESPACE'"}]'
-done
+KIALI_CR_NAMESPACE_NAME="$(${CLIENT_EXE} get kiali --all-namespaces -o jsonpath='{.items[*].metadata.namespace}')"
+KIALI_CR_NAMESPACE="$(echo ${KIALI_CR_NAMESPACE_NAME} | cut -d: -f1)"
+KIALI_CR_NAME="$(echo ${KIALI_CR_NAMESPACE_NAME} | cut -d: -f2)"
+ACCESSIBLE_NAMESPACES="$(${CLIENT_EXE} get kiali $KIALI_CR_NAME -n $KIALI_CR_NAMESPACE -o jsonpath='{.spec.deployment.accessible_namespaces}')"
 
-${CLIENT_EXE} wait --for=condition=Successful kiali/kiali --timeout=120s -n kiali-operator
+# All namespaces are accessible, no need to add namespaces access to Kiali CR
+if [ "${ACCESSIBLE_NAMESPACES}" != "**" ]; then
+  # accessible_namespaces field is not defined in the Kiali CR, initializing value to empty array
+  if [ "${ACCESSIBLE_NAMESPACES}" == "" ]; then
+    ${CLIENT_EXE} patch kiali $KIALI_CR_NAME -n $KIALI_CR_NAMESPACE --type=merge -p '{"spec": {"deployment": {"accessible_namespaces": []}}}'
+  fi
+
+  for NAMESPACE in ${NAMESPACES[@]}; do
+    ${CLIENT_EXE} patch kiali $KIALI_CR_NAME -n $KIALI_CR_NAMESPACE --type=json '-p=[{"op": "add", "path": "/spec/deployment/accessible_namespaces/-", "value":"'$NAMESPACE'"}]'
+  done
+
+  echo -n "Waiting for operator to finish reconciling the CR named [$KIALI_CR_NAME] located in namespace [$KIALI_CR_NAMESPACE]"
+  while [ "$KIALI_CR_REASON" != "Successful" -o "$KIALI_CR_STATUS" != "True" ]; do
+    sleep 1
+    echo -n "."
+    KIALI_CR_REASON="$(oc get kiali $KIALI_CR_NAME -n $KIALI_CR_NAMESPACE -o jsonpath='{.status.conditions[?(@.message=="Awaiting next reconciliation")].reason}')"
+    KIALI_CR_STATUS="$(oc get kiali $KIALI_CR_NAME -n $KIALI_CR_NAMESPACE -o jsonpath='{.status.conditions[?(@.message=="Awaiting next reconciliation")].status}')"
+  done
+  echo
+  echo "Done reconciling"
+fi
 
 for NAMESPACE in ${NAMESPACES[@]}; do
   ${CLIENT_EXE} wait --for=condition=Ready pods --all -n "$NAMESPACE" --timeout 60s || true
-  ${CLIENT_EXE} wait --for=condition=Ready pods --all -n "$NAMESPACE" --timeout 60s
 done
 
 sleep 80
