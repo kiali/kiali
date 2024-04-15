82769bedf6b71f5eabc6bf4c500babaaf04a5646
Nick Fox <nfox@redhat.com>
2024-04-15 14:54:22 -0400

    Bump linter version

--- make/Makefile.build.mk
+++ make/Makefile.build.mk
@@ -91,7 +91,7 @@ test-integration: test-integration-setup
 
 ## lint-install: Installs golangci-lint
 lint-install:
-	curl -sfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $$(${GO} env GOPATH)/bin v1.52.2
+	curl -sfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $$(${GO} env GOPATH)/bin v1.57.2
 
 ## lint: Runs golangci-lint
 # doc.go is ommited for linting, because it generates lots of warnings.
