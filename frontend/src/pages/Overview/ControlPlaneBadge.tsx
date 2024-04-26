f02630aa4a5cc8108792af1a0e1200012244f52c
Jay Shaughnessy <jshaughn@redhat.com>
2024-04-26 09:03:35 -0400

    Add mesh page link to control plane badge

--- frontend/src/pages/Overview/ControlPlaneBadge.tsx
+++ frontend/src/pages/Overview/ControlPlaneBadge.tsx
@@ -1,12 +1,13 @@
-import { Label } from '@patternfly/react-core';
+import { Label, Tooltip } from '@patternfly/react-core';
 import * as React from 'react';
 import { IstioStatusInline } from '../../components/IstioStatus/IstioStatusInline';
-import { serverConfig } from '../../config';
+import { config, serverConfig } from '../../config';
 import { AmbientBadge } from '../../components/Ambient/AmbientBadge';
 import { RemoteClusterBadge } from './RemoteClusterBadge';
 import { isRemoteCluster } from './OverviewCardControlPlaneNamespace';
 import { useTranslation } from 'react-i18next';
 import { I18N_NAMESPACE } from 'types/Common';
+import { Link } from 'react-router-dom';
 
 type Props = {
   annotations?: { [key: string]: string };
@@ -19,9 +20,17 @@ export const ControlPlaneBadge: React.FC<Props> = (props: Props) => {
   // so don't display istio status badge for those.
   return (
     <>
-      <Label style={{ marginLeft: '0.5rem' }} color="green" isCompact>
-        {t('Control plane')}
-      </Label>
+      <Tooltip
+        content={
+          <Link id="mesh" to={config.about.mesh.url}>
+            {config.about.mesh.linkText}
+          </Link>
+        }
+      >
+        <Label style={{ marginLeft: '0.5rem' }} color="green" isCompact>
+          {t('Control plane')}
+        </Label>
+      </Tooltip>
 
       {isRemoteCluster(props.annotations) && <RemoteClusterBadge />}
 
