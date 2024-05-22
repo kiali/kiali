7b0b0983a8821a618d74de4cc0df2508263a7a70
Fernando Hoyos <fhoyosle@redhat.com>
2024-05-22 19:47:27 +0200

    Mesh legend

--- frontend/src/pages/Mesh/MeshHelpTour.tsx
+++ frontend/src/pages/Mesh/MeshHelpTour.tsx
@@ -1,56 +1,60 @@
 import { PopoverPosition } from '@patternfly/react-core';
 import { TourStopInfo, TourInfo } from 'components/Tour/TourStop';
 import { MeshShortcuts } from './toolbar/MeshShortcuts';
+import { t } from 'utils/I18nUtils';
 
 export const MeshTourStops: { [name: string]: TourStopInfo } = {
   ContextualMenu: {
-    name: 'Contextual Menu',
-    description: 'Click kebab menu on a node label to see the contextual menu options for the node.',
+    name: t('Contextual Menu'),
+    description: t('Click kebab menu on a node label to see the contextual menu options for the node.'),
     position: PopoverPosition.left,
     distance: 250
   },
   Display: {
-    name: 'Display',
-    description: 'Set display options for the mesh.',
+    name: t('Display'),
+    description: t('Set display options for the mesh.'),
     position: PopoverPosition.rightStart
   },
   Find: {
-    name: 'Find and Hide',
-    description:
-      'Highlight or Hide mesh elements via typed expressions. Click the dropdown for preset Find or Hide expressions. Click the Find/Hide help icon for details on the expression language.',
+    name: t('Find and Hide'),
+    description: t(
+      'Highlight or Hide mesh elements via typed expressions. Click the dropdown for preset Find or Hide expressions. Click the Find/Hide help icon for details on the expression language.'
+    ),
     position: PopoverPosition.bottom
   },
   Layout: {
-    name: 'Layout selection',
-    description:
-      'Select the layout for the mesh. Different layouts work best with different meshes. Find the layout that works best. Other buttons here provide zoom and fit-to-screen options.',
+    name: t('Layout selection'),
+    description: t(
+      'Select the layout for the mesh. Different layouts work best with different meshes. Find the layout that works best. Other buttons here provide zoom and fit-to-screen options.'
+    ),
     position: PopoverPosition.right
   },
   Legend: {
-    name: 'Legend',
-    description: 'Display the legend to learn about what the different shapes, colors and backgrounds mean.',
+    name: t('Legend'),
+    description: t('Display the legend to learn about what the different shapes, colors and backgrounds mean.'),
     position: PopoverPosition.rightEnd
   },
   Mesh: {
-    name: 'Mesh',
-    description: 'Click on a node or edge to see its summary and emphasize its end-to-end paths.',
+    name: t('Mesh'),
+    description: t('Click on a node or edge to see its summary and emphasize its end-to-end paths.'),
     position: PopoverPosition.left,
     distance: 250
   },
   Shortcuts: {
-    name: 'Shortcuts',
+    name: t('Shortcuts'),
     htmlDescription: MeshShortcuts,
     position: PopoverPosition.leftStart
   },
   TargetPanel: {
-    name: 'Side Panel',
-    description: 'The Side Panel shows details about the currently selected node or edge, otherwise the whole mesh.',
+    name: t('Side Panel'),
+    description: t('The Side Panel shows details about the currently selected node or edge, otherwise the whole mesh.'),
     position: PopoverPosition.left
   },
   TimeRange: {
-    name: 'Time Range',
-    description:
-      'Select how often to refresh the mesh and how much historical metric data is used for metric charts. For example "Last 5m" means use the most recent 5 minutes of request metric data.',
+    name: t('Time Range'),
+    description: t(
+      'Select how often to refresh the mesh and how much historical metric data is used for metric charts. For example "Last 5m" means use the most recent 5 minutes of request metric data.'
+    ),
     position: PopoverPosition.bottomEnd
   }
 };
@@ -65,7 +69,7 @@ export const MeshTour: TourInfo = {
     MeshTourStops.Mesh,
     MeshTourStops.TargetPanel,
     // MeshTourStops.ContextualMenu,
-    MeshTourStops.Layout
-    // MeshTourStops.Legend
+    MeshTourStops.Layout,
+    MeshTourStops.Legend
   ]
 };
--- frontend/src/pages/Mesh/MeshLegendData.ts
+++ frontend/src/pages/Mesh/MeshLegendData.ts
@@ -1,109 +1,69 @@
 // Node Shapes
-import workloadImage from '../../assets/img/legend-pf/node.svg';
-import appImage from '../../assets/img/legend-pf/app.svg';
-import serviceImage from '../../assets/img/legend-pf/service.svg';
-import aggregateImage from '../../assets/img/legend-pf/aggregate.svg';
-import serviceEntryImage from '../../assets/img/legend-pf/service-entry.svg';
+import dataPlaneImage from '../../assets/img/mesh/node-dataplane.svg';
+import infraNodeImage from '../../assets/img/mesh/node-infra.svg';
+
 // Node Colors
 import nodeColorHealthyImage from '../../assets/img/legend-pf/node-color-healthy.svg';
-import nodeColorWarningImage from '../../assets/img/legend-pf/node-color-warning.svg';
 import nodeColorDangerImage from '../../assets/img/legend-pf/node-color-danger.svg';
-import nodeColorIdleImage from '../../assets/img/legend-pf/node-color-idle.svg';
-// Node Background
-import externalNamespaceImage from '../../assets/img/legend-pf/external-namespace.svg';
-import restrictedNamespaceImage from '../../assets/img/legend-pf/restricted-namespace.svg';
+
 // Edges
-import edgeSuccessImage from '../../assets/img/legend-pf/edge-success.svg';
-import edgeDangerImage from '../../assets/img/legend-pf/edge-danger.svg';
-import edgeWarnImage from '../../assets/img/legend-pf/edge-warn.svg';
-import edgeIdlemage from '../../assets/img/legend-pf/edge-idle.svg';
-import edgeTcpImage from '../../assets/img/legend-pf/edge-tcp.svg';
-import edgeMtlsImage from '../../assets/img/legend-pf/mtls-badge.svg';
-// Traffic Animation
-import trafficHealthyImage from '../../assets/img/legend-pf/traffic-healthy-request.svg';
-import trafficFailedImage from '../../assets/img/legend-pf/traffic-failed-request.svg';
-import trafficTcpImage from '../../assets/img/legend-pf/traffic-tcp.svg';
-// Badges
-import badgeCircuitBreakerImage from '../../assets/img/legend-pf/node-badge-circuit-breaker.svg';
-import badgeFaultInjectionImage from '../../assets/img/legend-pf/node-badge-fault-injection.svg';
-import badgeGatewaysImage from '../../assets/img/legend-pf/node-badge-gateways.svg';
-import badgeMirroringImage from '../../assets/img/legend-pf/node-badge-mirroring.svg';
-import badgeMissingSidecarImage from '../../assets/img/legend-pf/node-badge-missing-sidecar.svg';
-import badgeRequestTimeoutImage from '../../assets/img/legend-pf/node-badge-request-timeout.svg';
-import badgeTrafficShiftingSourceImage from '../../assets/img/legend-pf/node-badge-traffic-shifting.svg';
-import badgeTrafficSourceImage from '../../assets/img/legend-pf/node-badge-traffic-source.svg';
-import badgeVirtualServicesImage from '../../assets/img/legend-pf/node-badge-virtual-services.svg';
-import badgeWorkloadEntryImage from '../../assets/img/legend-pf/node-badge-workload-entry.svg';
+import edgeSuccessImage from '../../assets/img/mesh/edge-healthy.svg';
+import edgeDangerImage from '../../assets/img/mesh/edge-unhealthy.svg';
+
+// Node Background
+import dataplane from '../../assets/img/mesh/dataplane.svg';
+import jaegerLogo from '../../assets/img/mesh/jaeger.svg';
+import prometheusLogo from '../../assets/img/mesh/prometheus.svg';
+import grafanaLogo from '../../assets/img/mesh/grafana.svg';
+import istioLogo from '../../assets/img/mesh/istio.svg';
+import kialiLogo from '../../assets/img/mesh/kiali.svg';
+import tempoLogo from '../../assets/img/mesh/tempo.svg';
+
+import { t } from 'utils/I18nUtils';
 
 export interface MeshLegendItem {
-  title: string;
   data: MeshLegendItemRow[];
+  title: string;
 }
 
 export interface MeshLegendItemRow {
-  label: string;
   icon: string;
+  label: string;
+  logo: boolean;
 }
 
 export const legendData: MeshLegendItem[] = [
   {
-    title: 'Node Shapes',
+    title: t('Node Shapes'),
     data: [
-      { label: 'Workload', icon: workloadImage },
-      { label: 'App', icon: appImage },
-      { label: 'Operation', icon: aggregateImage },
-      { label: 'Service', icon: serviceImage },
-      { label: 'Service Entry', icon: serviceEntryImage }
+      { label: t('Infra node'), icon: infraNodeImage, logo: false },
+      { label: t('Data Plane'), icon: dataPlaneImage, logo: false }
     ]
   },
   {
-    title: 'Node Colors',
+    title: t('Node Colors'),
     data: [
-      { label: 'Healthy', icon: nodeColorHealthyImage },
-      { label: 'Warn', icon: nodeColorWarningImage },
-      { label: 'Unhealthy', icon: nodeColorDangerImage },
-      { label: 'Idle', icon: nodeColorIdleImage }
+      { label: t('Healthy'), icon: nodeColorHealthyImage, logo: false },
+      { label: t('Unhealthy'), icon: nodeColorDangerImage, logo: false }
     ]
   },
   {
-    title: 'Node Background',
+    title: t('Edges'),
     data: [
-      { label: 'Unselected Namespace', icon: externalNamespaceImage },
-      { label: 'Restricted / External', icon: restrictedNamespaceImage }
+      { label: t('Failure'), icon: edgeDangerImage, logo: false },
+      { label: t('Healthy'), icon: edgeSuccessImage, logo: false }
     ]
   },
   {
-    title: 'Edges',
+    title: t('Node Background'),
     data: [
-      { label: 'Failure', icon: edgeDangerImage },
-      { label: 'Degraded', icon: edgeWarnImage },
-      { label: 'Healthy', icon: edgeSuccessImage },
-      { label: 'TCP Connection', icon: edgeTcpImage },
-      { label: 'Idle', icon: edgeIdlemage },
-      { label: 'mTLS (badge)', icon: edgeMtlsImage }
-    ]
-  },
-  {
-    title: 'Traffic Animation',
-    data: [
-      { label: 'Healthy Request', icon: trafficHealthyImage },
-      { label: 'Failed Request', icon: trafficFailedImage },
-      { label: 'TCP Traffic', icon: trafficTcpImage }
-    ]
-  },
-  {
-    title: 'Node Badges',
-    data: [
-      { label: 'Circuit Breaker', icon: badgeCircuitBreakerImage },
-      { label: 'Fault Injection', icon: badgeFaultInjectionImage },
-      { label: 'Gateway', icon: badgeGatewaysImage },
-      { label: 'Mirroring', icon: badgeMirroringImage },
-      { label: 'Missing Sidecar', icon: badgeMissingSidecarImage },
-      { label: 'Request Timeout', icon: badgeRequestTimeoutImage },
-      { label: 'Traffic Shifting / TCP Traffic Shifting', icon: badgeTrafficShiftingSourceImage },
-      { label: 'Traffic Source', icon: badgeTrafficSourceImage },
-      { label: 'Virtual Service / Request Routing', icon: badgeVirtualServicesImage },
-      { label: 'Workload Entry', icon: badgeWorkloadEntryImage }
+      { label: t('Data Plane'), icon: dataplane, logo: true },
+      { label: t('Grafana'), icon: grafanaLogo, logo: true },
+      { label: t('Jaeger'), icon: jaegerLogo, logo: true },
+      { label: t('Kiali'), icon: kialiLogo, logo: true },
+      { label: t('Istio'), icon: istioLogo, logo: true },
+      { label: t('Prometheus'), icon: prometheusLogo, logo: true },
+      { label: t('Tempo'), icon: tempoLogo, logo: true }
     ]
   }
 ];
