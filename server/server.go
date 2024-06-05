d974c5c562fd5c90c4759abb55bd938229bc925e
josunect <josune.cordoba@gmail.com>
2024-06-05 13:00:23 +0100

    Remove TODO

--- server/server.go
+++ server/server.go
@@ -86,7 +86,6 @@ func NewServer(controlPlaneMonitor business.ControlPlaneMonitor,
 	// The /debug/pprof/profiler by default needs a write timeout larger than 30s. But also, you can pass in &seconds=XY on the pprof URL
 	// and ask for any profile to extend to those number of seconds you specify, which could be larger than 30s.
 	// To limit the damage this may cause with large write timeouts, we only increase the timeout to 1 minute.
-	// TODO: We could make this configurable in the future. See: https://github.com/kiali/kiali/pull/7108#issuecomment-1932982697
 	writeTimeout := conf.Server.Timeout * time.Second
 	if conf.Server.Profiler.Enabled && writeTimeout < 60 {
 		writeTimeout = 1 * time.Minute
