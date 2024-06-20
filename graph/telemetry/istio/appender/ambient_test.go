ab39ee3abe7bbdc0c48df6e2a996b257b72c99a9
Josune Cordoba <49480155+josunect@users.noreply.github.com>
2024-06-20 13:34:34 +0100

    Fix graph issue with excluded namespaces (#7449)
    * Don't fail if node is not accessible for graph

--- graph/telemetry/istio/appender/ambient_test.go
+++ graph/telemetry/istio/appender/ambient_test.go
@@ -18,6 +18,7 @@ const (
 	defaultCluster = "Kubernetes"
 	appName        = "productpage"
 	appNamespace   = "ambientNamespace"
+	kubeNamespace  = "kube-system"
 )
 
 func setupWorkloadEntries(t *testing.T) *business.Layer {
@@ -66,9 +67,21 @@ func setupWorkloadEntries(t *testing.T) *business.Layer {
 			},
 		}}
 
+	k8spod5 := &core_v1.Pod{
+		ObjectMeta: meta_v1.ObjectMeta{
+			Name:        "workloadC",
+			Namespace:   kubeNamespace,
+			Labels:      map[string]string{"apps": "workloadB", "version": "v2"},
+			Annotations: map[string]string{"sidecar.istio.io/status": "{\"version\":\"\",\"initContainers\":[\"istio-init\",\"enable-core-dump\"],\"containers\":[\"istio-proxy\"],\"volumes\":[\"istio-envoy\",\"istio-certs\"]}"}},
+		Spec: core_v1.PodSpec{
+			Containers: []core_v1.Container{
+				{Name: "workloadB", Image: "whatever"},
+			},
+		}}
+
 	ns := &core_v1.Namespace{ObjectMeta: meta_v1.ObjectMeta{Name: appNamespace}}
 
-	k8s := kubetest.NewFakeK8sClient(k8spod1, k8spod2, k8spod3, k8spod4, ns)
+	k8s := kubetest.NewFakeK8sClient(k8spod1, k8spod2, k8spod3, k8spod4, k8spod5, ns)
 	conf := config.NewConfig()
 	conf.ExternalServices.Istio.IstioAPIEnabled = false
 	conf.KubernetesConfig.ClusterName = defaultCluster
@@ -88,8 +101,6 @@ func workloadEntriesTrafficMap() map[string]*graph.Node {
 
 	// VersionedApp graph
 	trafficMap := make(map[string]*graph.Node)
-
-	// 1 service, 3 workloads. v1 and v2 are workload entries. v3 is a waypoint proxy but with no labels. v4 has the right labels.
 
 	// Service node
 	n0, _ := graph.NewNode(defaultCluster, appNamespace, appName, appNamespace, "", "", "", graph.GraphTypeVersionedApp)
@@ -112,6 +123,38 @@ func workloadEntriesTrafficMap() map[string]*graph.Node {
 	n0.AddEdge(n2).Metadata[graph.ProtocolKey] = graph.HTTP.Name
 	n0.AddEdge(n3).Metadata[graph.ProtocolKey] = graph.HTTP.Name
 	n0.AddEdge(n4).Metadata[graph.ProtocolKey] = graph.HTTP.Name
+	// Need to put some metadata in here to ensure it gets counted as a workload
+
+	return trafficMap
+}
+
+func workloadEntriesTrafficMapExcludedNs() map[string]*graph.Node {
+	conf := config.NewConfig()
+	conf.KubernetesConfig.ClusterName = defaultCluster
+	config.Set(conf)
+
+	// VersionedApp graph
+	trafficMap := workloadEntriesTrafficMap()
+
+	// 1 service, 3 workloads. v1 and v2 are workload entries. v3 is a waypoint proxy but with no labels. v4 has the right labels.
+
+	// Service node
+	n0, _ := graph.NewNode(defaultCluster, kubeNamespace, "kube-dns", kubeNamespace, "", "", "", graph.GraphTypeVersionedApp)
+	// v1 Workload
+	n1, _ := graph.NewNode(defaultCluster, kubeNamespace, "kube-dns", kubeNamespace, "corens-v1", appName, "v1", graph.GraphTypeVersionedApp)
+	// v2 Workload
+	n2, _ := graph.NewNode(defaultCluster, kubeNamespace, "kube-dns", kubeNamespace, "kube-apiserver", appName, "v2", graph.GraphTypeVersionedApp)
+	// v3 Workload with waypoint name
+	n3, _ := graph.NewNode(defaultCluster, kubeNamespace, "kube-dns", kubeNamespace, "kube-proxy", appName, "v2", graph.GraphTypeVersionedApp)
+
+	trafficMap[n0.ID] = n0
+	trafficMap[n1.ID] = n1
+	trafficMap[n2.ID] = n2
+	trafficMap[n3.ID] = n3
+
+	n0.AddEdge(n1).Metadata[graph.ProtocolKey] = graph.HTTP.Name
+	n0.AddEdge(n2).Metadata[graph.ProtocolKey] = graph.HTTP.Name
+	n0.AddEdge(n3).Metadata[graph.ProtocolKey] = graph.HTTP.Name
 	// Need to put some metadata in here to ensure it gets counted as a workload
 
 	return trafficMap
@@ -159,6 +202,37 @@ func TestIsWaypoint(t *testing.T) {
 	a.AppendGraph(trafficMap, globalInfo, namespaceInfo)
 
 	assert.Equal(5, len(trafficMap))
+
+	waypointWorkloadID, _, _ := graph.Id(defaultCluster, appNamespace, appName, appNamespace, "namespace-istio-waypoint", appName, "v2", graph.GraphTypeVersionedApp)
+	waypointNode, found := trafficMap[waypointWorkloadID]
+	assert.True(found)
+	assert.Contains(waypointNode.Metadata, graph.IsWaypoint)
+
+	fakeWaypointWorkloadID, _, _ := graph.Id(defaultCluster, appNamespace, appName, appNamespace, "fake-istio-waypoint", appName, "v2", graph.GraphTypeVersionedApp)
+	fakeWaypointNode, found := trafficMap[fakeWaypointWorkloadID]
+	assert.True(found)
+	assert.NotContains(fakeWaypointNode.Metadata, graph.IsWaypoint)
+}
+
+func TestIsWaypointExcludedNs(t *testing.T) {
+	assert := require.New(t)
+
+	businessLayer := setupWorkloadEntries(t)
+	trafficMap := workloadEntriesTrafficMapExcludedNs()
+
+	globalInfo := graph.NewAppenderGlobalInfo()
+	globalInfo.Business = businessLayer
+	namespaceInfo := graph.NewAppenderNamespaceInfo(appNamespace)
+
+	assert.Equal(9, len(trafficMap))
+
+	// Run the appender...
+
+	a := AmbientAppender{ShowWaypoints: true}
+
+	a.AppendGraph(trafficMap, globalInfo, namespaceInfo)
+
+	assert.Equal(9, len(trafficMap))
 
 	waypointWorkloadID, _, _ := graph.Id(defaultCluster, appNamespace, appName, appNamespace, "namespace-istio-waypoint", appName, "v2", graph.GraphTypeVersionedApp)
 	waypointNode, found := trafficMap[waypointWorkloadID]
