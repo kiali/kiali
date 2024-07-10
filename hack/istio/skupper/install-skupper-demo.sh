35c6f54708914f8f4b99d000cb27f3e5884f72a3
Jay Shaughnessy <jshaughn@redhat.com>
2024-07-10 09:24:53 -0400

    Update hack/istio/skupper/install-skupper-demo.sh
    Co-authored-by: John Mazzitelli <mazz@redhat.com>

--- hack/istio/skupper/install-skupper-demo.sh
+++ hack/istio/skupper/install-skupper-demo.sh
@@ -216,7 +216,7 @@ minikube_install_basic_demo() {
   infomsg "Updating Bookinfo traffic-generator route ..."
   ${CLIENT_EXE} patch configmap traffic-generator-config -n bookinfo --type merge -p '{"data":{"route":"http://productpage:9080/productpage"}}'
   infomsg "Restarting Bookinfo traffic-generator pod ..."
-  ${CLIENT_EXE} -n bookinfo get pods --no-headers=true | awk '/kiali-traffic-generator.*/{print $1}'| xargs  ${CLIENT_EXE} -n bookinfo delete pod
+  ${CLIENT_EXE} kubectl delete pod -n bookinfo -l app=kiali-traffic-generator
 
   if [ "${KIALI_VERSION}" == "dev" ]; then
     infomsg "Installing Kiali ..."
