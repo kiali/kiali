83794f24bb1d813574857d53f0cdbeb5e911e50b
josunect <josune.cordoba@gmail.com>
2024-08-16 13:28:49 +0100

    Update bookinfo script for Istio 1.23 and waypoints

--- hack/istio/functions.sh
+++ hack/istio/functions.sh
@@ -14,3 +14,30 @@ is_cluster_wide() {
       return 1
   fi
 }
+
+# Returns 0 if the istio version is greater than specified, 0 otherwise.
+is_istio_version_eq_greater_than() {
+  local expected_version=$1
+  local istio_version=$(${ISTIOCTL} version)
+  istio_parsed_version=$(echo "$istio_version" | grep "client version" | awk '{print $3}' | cut -d'-' -f1)
+
+  istio_expected_version=$(echo "$expected_version" | cut -d'-' -f1)
+
+  IFS='.' read -r major minor patch <<< "$istio_parsed_version"
+  IFS='.' read -r major_expected minor_expected patch_expected <<< "$istio_expected_version"
+  IFS=' '
+  if [ "${major}" -lt "${major_expected}" ]; then
+    return 1
+  else
+    if [ "${major}" -eq "${major_expected}" ]; then
+      if [ "${minor}" -lt "${minor_expected}" ]; then
+        return 0
+      else
+        return 1
+      fi
+    else
+      return 0
+    fi
+  fi
+
+}
