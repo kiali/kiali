cc269b53bf4e18a0110b7aaefa3201e573b59045
Jay Shaughnessy <jshaughn@redhat.com>
2024-10-08 15:15:08 -0400

    Provide "Shortcuts" specific to PF graph

---
+++ frontend/src/pages/Graph/GraphToolbar/GraphShortcutsPF.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import { Chip } from '@patternfly/react-core';
+
+interface Shortcut {
+  description: string;
+  shortcut: string;
+}
+
+const shortcuts: Shortcut[] = [
+  { shortcut: 'Mouse wheel', description: 'Zoom graph' },
+  { shortcut: 'Click + Drag', description: 'Pan graph' },
+  { shortcut: 'Node Click', description: 'Select node + side panel' },
+  { shortcut: 'Edge Click', description: 'Select edge + side panel' },
+  { shortcut: 'Right click', description: 'Contextual menu' },
+  { shortcut: 'Kebab click', description: 'Contextual menu' },
+  { shortcut: 'Kebab: Node Graph', description: 'Drill into node detail graph' }
+];
+
+const makeShortcut = (shortcut: Shortcut): React.ReactNode => {
+  return (
+    <div style={{ display: 'flex', marginBottom: '10px' }}>
+      <div style={{ flex: '45%' }}>
+        <Chip isReadOnly>{shortcut.shortcut}</Chip>
+      </div>
+
+      <div style={{ flex: '55%' }}>{shortcut.description}</div>
+    </div>
+  );
+};
+
+export const GraphShortcutsPF = (): React.ReactNode => (
+  <>
+    {shortcuts.map(
+      (s: Shortcut): React.ReactNode => {
+        return makeShortcut(s);
+      }
+    )}
+  </>
+);
