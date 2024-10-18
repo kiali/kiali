e8b36fe8cf7c6eedb09f510dec122d91395d3fc5
Leandro Beretta <lea.beretta@gmail.com>
2024-10-18 09:21:16 -0300

    remove experimental cypress settings

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -22,8 +22,6 @@ export default defineConfig({
     omitFiltered: true,
     filterSpecs: true
   },
-  experimentalMemoryManagement: true,
-  numTestsKeptInMemory: 1,
   e2e: {
     baseUrl: 'http://localhost:3000',
     async setupNodeEvents(
