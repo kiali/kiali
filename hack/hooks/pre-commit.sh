3b4475f91d991192f5fb7521b316e7e812657af3
Leandro Beretta <lea.beretta@gmail.com>
2024-10-18 10:25:57 -0300

    fix

--- hack/hooks/pre-commit.sh
+++ hack/hooks/pre-commit.sh
@@ -23,6 +23,22 @@ if [ -n "$go_unformatted" ]; then
   echo $go_unformatted | tr " " "\n"
 fi
 
+#### YAML Formatting ####
+yaml_files=$(git diff --staged --name-only HEAD --diff-filter=AM | grep -E '\.yml|\.yaml')
+
+if [ -n "$yaml_files" ]; then
+  yaml_unformatted=$(yamlfmt -dry -quiet $yaml_files 2>&1 | sed 's/^.*://')
+fi
+
+if [ -n "$yaml_unformatted" ]; then
+  echo "Formatting staged YAML files:"
+  echo $yaml_unformatted | tr " " "\n"
+
+  # Remove trailing whitespaces to avoid yaml formatter bug (https://github.com/google/yamlfmt/issues/153)
+  sed -i 's/[ \t]*$//' $yaml_unformatted
+  yamlfmt $yaml_unformatted
+fi
+
 #### I18N missing statements ####
 cd frontend
 
