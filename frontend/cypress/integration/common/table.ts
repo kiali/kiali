2989fd83bc215077127489b98ca301c9b0e838c8
Fernando Hoyos <fhoyosle@redhat.com>
2024-11-19 12:15:31 +0100

    Click on the button instead of th to fix the test issue

--- frontend/cypress/integration/common/table.ts
+++ frontend/cypress/integration/common/table.ts
@@ -298,21 +298,21 @@ When('user sorts the list by column {string} in {string} order', (column: string
     // 3. aria-sort attribute is descending: click once for ascending.
     if ($el.attr('aria-sort') === 'none') {
       if (order === SortOrder.Ascending) {
-        cy.wrap($el).click();
+        cy.wrap($el).find('button').click();
         cy.wrap($el).should('have.attr', 'aria-sort', SortOrder.Ascending);
       } else if (order === SortOrder.Descending) {
-        cy.wrap($el).click();
-        cy.wrap($el).click();
+        cy.wrap($el).find('button').click();
+        cy.wrap($el).find('button').click();
         cy.wrap($el).should('have.attr', 'aria-sort', SortOrder.Descending);
       }
     } else if ($el.attr('aria-sort') === SortOrder.Ascending) {
       if (order === SortOrder.Descending) {
-        cy.wrap($el).click();
+        cy.wrap($el).find('button').click();
         cy.wrap($el).should('have.attr', 'aria-sort', SortOrder.Descending);
       }
     } else if ($el.attr('aria-sort') === SortOrder.Descending) {
       if (order === SortOrder.Ascending) {
-        cy.wrap($el).click();
+        cy.wrap($el).find('button').click();
         cy.wrap($el).should('have.attr', 'aria-sort', SortOrder.Ascending);
       }
     }
