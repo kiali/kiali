e10f95cac4b453d7a1108c31a75179da19318a12
Hayk Hovsepyan <hhovsepy@redhat.com>
2024-11-26 16:00:22 +0100

    Removed controller tests from integration target (#7959)

--- make/Makefile.build.mk
+++ make/Makefile.build.mk
@@ -84,8 +84,6 @@ test-integration: test-integration-setup .ensure-envtest-bin-dir-exists
 	@echo Running Integration tests
 	cd tests/integration/tests && ${GO} test ${GO_TEST_FLAGS} -v -timeout 30m 2>&1 | tee >(go-junit-report > ../junit-rest-report.xml) ../int-test.log
 	@echo Test results can be found here: $$(ls -1 ${ROOTDIR}/tests/integration/junit-rest-report.xml)
-	@echo Running controller integration tests
-	@cd tests/integration/controller && ${GO} test -v
 
 test-integration-controller: .ensure-envtest-bin-dir-exists
 	@echo Running controller integration tests
