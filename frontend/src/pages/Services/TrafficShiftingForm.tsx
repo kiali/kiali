84857dc63ff9fba0111c5e6e66620b8a8027ecf8
mamyers <mamyers@redhat.com>
2024-12-04 15:54:36 -0500

    Routed to new page for Traffic Shifting

--- frontend/src/pages/Services/TrafficShiftingForm.tsx
+++ frontend/src/pages/Services/TrafficShiftingForm.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
-import { DestinationRule, K8sHTTPRoute, VirtualService } from 'types/IstioObjects';
+// import { DestinationRule, K8sHTTPRoute, VirtualService } from 'types/IstioObjects';
 import { Title, TitleSizes } from '@patternfly/react-core';
+import { WorkloadWeight } from 'components/IstioWizards/TrafficShifting';
 
-// Define the Props type
-interface Props {
+export type TrafficShiftingHead = {
   key: string;
-  virtualServices: VirtualService[];
-  destinationRules: DestinationRule[];
-  k8sHTTPRoutes: K8sHTTPRoute[];
-  serviceWizard: (serviceWizard: string) => void;
+  showValid: boolean;
+  workloads: WorkloadWeight[];
+
+  
+}
+
+type Props = {
+  // key: string;
+  // virtualServices: VirtualService[];
+  // destinationRules: DestinationRule[];
+  // k8sHTTPRoutes: K8sHTTPRoute[];
+  // serviceWizard: (serviceWizard: string) => void;
 }
 
 // Functional component with Props type
-export const TrafficShiftingHead: React.FC<Props> = ({}) => {
+export const TrafficShiftingForm: React.FC<Props> = (_props: Props) => {
   return (
     <Title headingLevel="h2" size={TitleSizes['3xl']}>
       Traffic Shifting
