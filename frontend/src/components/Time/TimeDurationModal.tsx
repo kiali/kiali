38377e2ffd4283e23ffca4de1555248ce9f07afd
josunect <josune.cordoba@gmail.com>
2024-12-10 21:21:10 +0000

    Time duration modal handle url

--- frontend/src/components/Time/TimeDurationModal.tsx
+++ frontend/src/components/Time/TimeDurationModal.tsx
@@ -77,6 +77,7 @@ export const TimeDurationModal: React.FC<Props> = (props: Props) => {
     if (getUrlTimeRange() !== undefined) {
       dispatch(UserSettingsActions.setTimeRange(getUrlTimeRange()));
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handleCancel = (): void => {
