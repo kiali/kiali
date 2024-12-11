68145a8f7c98de2326d8dd81c4def9718142f7e2
mamyers <mamyers@redhat.com>
2024-12-11 15:52:37 -0500

    Adding new ServiceWizardNewPage, and making some changes

--- frontend/src/pages/Services/TrafficShiftingForm.tsx
+++ frontend/src/pages/Services/TrafficShiftingForm.tsx
@@ -1,23 +1,51 @@
 import React from 'react';
 // import { DestinationRule, K8sHTTPRoute, VirtualService } from 'types/IstioObjects';
 import { Title, TitleSizes } from '@patternfly/react-core';
-import { WorkloadWeight } from 'components/IstioWizards/TrafficShifting';
 
-export type TrafficShiftingHead = {
-  key: string;
-  showValid: boolean;
-  workloads: WorkloadWeight[];
-
-  
+export type TrafficShiftingState = {
+  addWorkloadSelector: any;
+  workloadSelectorValid: any;
+  action: any;
+  rules: any;
+  resetState: boolean
+  locked: boolean;
+  mirrored: boolean;
+  weight: number;
+  change: boolean;
 }
 
 type Props = {
-  // key: string;
-  // virtualServices: VirtualService[];
-  // destinationRules: DestinationRule[];
-  // k8sHTTPRoutes: K8sHTTPRoute[];
-  // serviceWizard: (serviceWizard: string) => void;
-}
+  objectGVK(objectGVK: any): unknown;
+  activeNamespaces: any;
+  trafficShifting: TrafficShiftingState;
+  locked: boolean; 
+  mirrored: boolean;
+  weight: number;
+  change: (trafficShiffting: TrafficShiftingState) => void;
+};
+
+export const initTrafficShifting = (): TrafficShiftingState => ({
+  resetState: true,
+  weight: newWeight, 
+  locked: false, 
+  mirrored: true,
+  change: true,
+
+});
+
+export const isTrafficShiftingStateValid = (ap: TrafficShiftingState): boolean => {
+  const workloadSelectorRule = ap.addWorkloadSelector ? ap.workloadSelectorValid : true;
+  const denyRule = ap.action == ap.rules.length > 0;
+
+  return workloadSelectorRule && denyRule;
+};
+// type Props = {
+//   // key: string;
+//   // virtualServices: VirtualService[];
+//   // destinationRules: DestinationRule[];
+//   // k8sHTTPRoutes: K8sHTTPRoute[];
+//   // serviceWizard: (serviceWizard: string) => void;
+// }
 
 // Functional component with Props type
 export const TrafficShiftingForm: React.FC<Props> = (_props: Props) => {
@@ -27,3 +55,7 @@ export const TrafficShiftingForm: React.FC<Props> = (_props: Props) => {
     </Title>
   );
 };
+function newWeight(_workloadName: string, _newWeight: number): void {
+  throw new Error('Function not implemented.');
+}
+
