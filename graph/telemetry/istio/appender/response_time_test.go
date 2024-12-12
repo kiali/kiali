ed77d340ac68f87adcd7cd21fcd624360b19b5c9
Jay Shaughnessy <jshaughn@redhat.com>
2024-12-11 19:29:46 -0500

    Add some tests

--- graph/telemetry/istio/appender/response_time_test.go
+++ graph/telemetry/istio/appender/response_time_test.go
@@ -14,8 +14,11 @@ import (
 func TestResponseTimeP95(t *testing.T) {
 	assert := assert.New(t)
 
-	q0 := `round(histogram_quantile(0.95, sum(rate(istio_request_duration_milliseconds_bucket{reporter=~"destination|waypoint",destination_service_namespace="bookinfo"}[60s])) by (le,source_cluster,source_workload_namespace,source_workload,source_canonical_service,source_canonical_revision,destination_cluster,destination_service_namespace,destination_service,destination_service_name,destination_workload_namespace,destination_workload,destination_canonical_service,destination_canonical_revision,request_protocol)) > 0,0.001)`
-	q0m0 := model.Metric{
+	q0 := `round(histogram_quantile(0.95, sum(rate(istio_request_duration_milliseconds_bucket{reporter="source",source_workload_namespace!="bookinfo",destination_service_namespace="bookinfo"}[60s])) by (le,source_cluster,source_workload_namespace,source_workload,source_canonical_service,source_canonical_revision,destination_cluster,destination_service_namespace,destination_service,destination_service_name,destination_workload_namespace,destination_workload,destination_canonical_service,destination_canonical_revision,request_protocol)) > 0,0.001)`
+	v0 := model.Vector{}
+
+	q1 := `round(histogram_quantile(0.95, sum(rate(istio_request_duration_milliseconds_bucket{reporter=~"destination|waypoint",destination_service_namespace="bookinfo"}[60s])) by (le,source_cluster,source_workload_namespace,source_workload,source_canonical_service,source_canonical_revision,destination_cluster,destination_service_namespace,destination_service,destination_service_name,destination_workload_namespace,destination_workload,destination_canonical_service,destination_canonical_revision,request_protocol)) > 0,0.001)`
+	q1m0 := model.Metric{
 		"source_cluster":                 config.DefaultClusterID,
 		"source_workload_namespace":      "istio-system",
 		"source_workload":                "ingressgateway-unknown",
@@ -30,100 +33,6 @@ func TestResponseTimeP95(t *testing.T) {
 		"destination_canonical_service":  "productpage",
 		"destination_canonical_revision": "v1",
 		"request_protocol":               "http"}
-	q0m1 := model.Metric{
-		"source_cluster":                 config.DefaultClusterID,
-		"source_workload_namespace":      "bookinfo",
-		"source_workload":                "productpage-v1",
-		"source_canonical_service":       "productpage",
-		"source_canonical_revision":      "v1",
-		"destination_cluster":            config.DefaultClusterID,
-		"destination_service_namespace":  "bookinfo",
-		"destination_service":            "reviews.bookinfo.svc.cluster.local",
-		"destination_service_name":       "reviews",
-		"destination_workload_namespace": "bookinfo",
-		"destination_workload":           "reviews-v1",
-		"destination_canonical_service":  "reviews",
-		"destination_canonical_revision": "v1",
-		"request_protocol":               "http"}
-	q0m2 := model.Metric{
-		"source_cluster":                 config.DefaultClusterID,
-		"source_workload_namespace":      "bookinfo",
-		"source_workload":                "productpage-v1",
-		"source_canonical_service":       "productpage",
-		"source_canonical_revision":      "v1",
-		"destination_cluster":            config.DefaultClusterID,
-		"destination_service_namespace":  "bookinfo",
-		"destination_service":            "reviews.bookinfo.svc.cluster.local",
-		"destination_service_name":       "reviews",
-		"destination_workload_namespace": "bookinfo",
-		"destination_workload":           "reviews-v2",
-		"destination_canonical_service":  "reviews",
-		"destination_canonical_revision": "v2",
-		"request_protocol":               "http"}
-	q0m3 := model.Metric{
-		"source_cluster":                 config.DefaultClusterID,
-		"source_workload_namespace":      "bookinfo",
-		"source_workload":                "reviews-v1",
-		"source_canonical_service":       "reviews",
-		"source_canonical_revision":      "v1",
-		"destination_cluster":            config.DefaultClusterID,
-		"destination_service_namespace":  "bookinfo",
-		"destination_service":            "ratings.bookinfo.svc.cluster.local",
-		"destination_service_name":       "ratings",
-		"destination_workload_namespace": "bookinfo",
-		"destination_workload":           "ratings-v1",
-		"destination_canonical_service":  "ratings",
-		"destination_canonical_revision": "v1",
-		"request_protocol":               "http"}
-	q0m4 := model.Metric{
-		"source_cluster":                 config.DefaultClusterID,
-		"source_workload_namespace":      "bookinfo",
-		"source_workload":                "reviews-v2",
-		"source_canonical_service":       "reviews",
-		"source_canonical_revision":      "v2",
-		"destination_cluster":            config.DefaultClusterID,
-		"destination_service_namespace":  "bookinfo",
-		"destination_service":            "ratings.bookinfo.svc.cluster.local",
-		"destination_service_name":       "ratings",
-		"destination_workload_namespace": "bookinfo",
-		"destination_workload":           "ratings-v1",
-		"destination_canonical_service":  "ratings",
-		"destination_canonical_revision": "v1",
-		"request_protocol":               "http"}
-	v0 := model.Vector{
-		&model.Sample{
-			Metric: q0m0,
-			Value:  0.010},
-		&model.Sample{
-			Metric: q0m1,
-			Value:  0.020},
-		&model.Sample{
-			Metric: q0m2,
-			Value:  0.020},
-		&model.Sample{
-			Metric: q0m3,
-			Value:  0.030}, // same edge reported by outgoing (q1), this > value should be preferred
-		&model.Sample{
-			Metric: q0m4,
-			Value:  0.030}, // same edge reported by outgoing (q1), this > value should be preferred
-	}
-
-	q1 := `round(histogram_quantile(0.95, sum(rate(istio_request_duration_milliseconds_bucket{reporter=~"source|waypoint",source_workload_namespace="bookinfo"}[60s])) by (le,source_cluster,source_workload_namespace,source_workload,source_canonical_service,source_canonical_revision,destination_cluster,destination_service_namespace,destination_service,destination_service_name,destination_workload_namespace,destination_workload,destination_canonical_service,destination_canonical_revision,request_protocol)) > 0,0.001)`
-	q1m0 := model.Metric{
-		"source_cluster":                 config.DefaultClusterID,
-		"source_workload_namespace":      "bookinfo",
-		"source_workload":                "productpage-v1",
-		"source_canonical_service":       "productpage",
-		"source_canonical_revision":      "v1",
-		"destination_cluster":            config.DefaultClusterID,
-		"destination_service_namespace":  "bookinfo",
-		"destination_service":            "reviews.bookinfo.svc.cluster.local",
-		"destination_service_name":       "reviews",
-		"destination_workload_namespace": "bookinfo",
-		"destination_workload":           "reviews-v1",
-		"destination_canonical_service":  "reviews",
-		"destination_canonical_revision": "v1",
-		"request_protocol":               "http"}
 	q1m1 := model.Metric{
 		"source_cluster":                 config.DefaultClusterID,
 		"source_workload_namespace":      "bookinfo",
@@ -135,11 +44,26 @@ func TestResponseTimeP95(t *testing.T) {
 		"destination_service":            "reviews.bookinfo.svc.cluster.local",
 		"destination_service_name":       "reviews",
 		"destination_workload_namespace": "bookinfo",
+		"destination_workload":           "reviews-v1",
+		"destination_canonical_service":  "reviews",
+		"destination_canonical_revision": "v1",
+		"request_protocol":               "http"}
+	q1m2 := model.Metric{
+		"source_cluster":                 config.DefaultClusterID,
+		"source_workload_namespace":      "bookinfo",
+		"source_workload":                "productpage-v1",
+		"source_canonical_service":       "productpage",
+		"source_canonical_revision":      "v1",
+		"destination_cluster":            config.DefaultClusterID,
+		"destination_service_namespace":  "bookinfo",
+		"destination_service":            "reviews.bookinfo.svc.cluster.local",
+		"destination_service_name":       "reviews",
+		"destination_workload_namespace": "bookinfo",
 		"destination_workload":           "reviews-v2",
 		"destination_canonical_service":  "reviews",
 		"destination_canonical_revision": "v2",
 		"request_protocol":               "http"}
-	q1m2 := model.Metric{
+	q1m3 := model.Metric{
 		"source_cluster":                 config.DefaultClusterID,
 		"source_workload_namespace":      "bookinfo",
 		"source_workload":                "reviews-v1",
@@ -154,7 +78,86 @@ func TestResponseTimeP95(t *testing.T) {
 		"destination_canonical_service":  "ratings",
 		"destination_canonical_revision": "v1",
 		"request_protocol":               "http"}
-	q1m3 := model.Metric{
+	q1m4 := model.Metric{
+		"source_cluster":                 config.DefaultClusterID,
+		"source_workload_namespace":      "bookinfo",
+		"source_workload":                "reviews-v2",
+		"source_canonical_service":       "reviews",
+		"source_canonical_revision":      "v2",
+		"destination_cluster":            config.DefaultClusterID,
+		"destination_service_namespace":  "bookinfo",
+		"destination_service":            "ratings.bookinfo.svc.cluster.local",
+		"destination_service_name":       "ratings",
+		"destination_workload_namespace": "bookinfo",
+		"destination_workload":           "ratings-v1",
+		"destination_canonical_service":  "ratings",
+		"destination_canonical_revision": "v1",
+		"request_protocol":               "http"}
+	v1 := model.Vector{
+		&model.Sample{
+			Metric: q1m0,
+			Value:  0.010},
+		&model.Sample{
+			Metric: q1m1,
+			Value:  0.020},
+		&model.Sample{
+			Metric: q1m2,
+			Value:  0.020},
+		&model.Sample{
+			Metric: q1m3,
+			Value:  0.030}, // same edge reported by outgoing (q1), this > value should be preferred
+		&model.Sample{
+			Metric: q1m4,
+			Value:  0.030}, // same edge reported by outgoing (q1), this > value should be preferred
+	}
+
+	q2 := `round(histogram_quantile(0.95, sum(rate(istio_request_duration_milliseconds_bucket{reporter=~"source|waypoint",source_workload_namespace="bookinfo"}[60s])) by (le,source_cluster,source_workload_namespace,source_workload,source_canonical_service,source_canonical_revision,destination_cluster,destination_service_namespace,destination_service,destination_service_name,destination_workload_namespace,destination_workload,destination_canonical_service,destination_canonical_revision,request_protocol)) > 0,0.001)`
+	q2m0 := model.Metric{
+		"source_cluster":                 config.DefaultClusterID,
+		"source_workload_namespace":      "bookinfo",
+		"source_workload":                "productpage-v1",
+		"source_canonical_service":       "productpage",
+		"source_canonical_revision":      "v1",
+		"destination_cluster":            config.DefaultClusterID,
+		"destination_service_namespace":  "bookinfo",
+		"destination_service":            "reviews.bookinfo.svc.cluster.local",
+		"destination_service_name":       "reviews",
+		"destination_workload_namespace": "bookinfo",
+		"destination_workload":           "reviews-v1",
+		"destination_canonical_service":  "reviews",
+		"destination_canonical_revision": "v1",
+		"request_protocol":               "http"}
+	q2m1 := model.Metric{
+		"source_cluster":                 config.DefaultClusterID,
+		"source_workload_namespace":      "bookinfo",
+		"source_workload":                "productpage-v1",
+		"source_canonical_service":       "productpage",
+		"source_canonical_revision":      "v1",
+		"destination_cluster":            config.DefaultClusterID,
+		"destination_service_namespace":  "bookinfo",
+		"destination_service":            "reviews.bookinfo.svc.cluster.local",
+		"destination_service_name":       "reviews",
+		"destination_workload_namespace": "bookinfo",
+		"destination_workload":           "reviews-v2",
+		"destination_canonical_service":  "reviews",
+		"destination_canonical_revision": "v2",
+		"request_protocol":               "http"}
+	q2m2 := model.Metric{
+		"source_cluster":                 config.DefaultClusterID,
+		"source_workload_namespace":      "bookinfo",
+		"source_workload":                "reviews-v1",
+		"source_canonical_service":       "reviews",
+		"source_canonical_revision":      "v1",
+		"destination_cluster":            config.DefaultClusterID,
+		"destination_service_namespace":  "bookinfo",
+		"destination_service":            "ratings.bookinfo.svc.cluster.local",
+		"destination_service_name":       "ratings",
+		"destination_workload_namespace": "bookinfo",
+		"destination_workload":           "ratings-v1",
+		"destination_canonical_service":  "ratings",
+		"destination_canonical_revision": "v1",
+		"request_protocol":               "http"}
+	q2m3 := model.Metric{
 		"source_cluster":                 config.DefaultClusterID,
 		"source_workload_namespace":      "bookinfo",
 		"source_workload":                "reviews-v2",
@@ -170,18 +173,18 @@ func TestResponseTimeP95(t *testing.T) {
 		"destination_canonical_revision": "v1",
 		"request_protocol":               "http"}
 
-	v1 := model.Vector{
+	v2 := model.Vector{
 		&model.Sample{
-			Metric: q1m0,
+			Metric: q2m0,
 			Value:  0.020},
 		&model.Sample{
-			Metric: q1m1,
+			Metric: q2m1,
 			Value:  0.020},
 		&model.Sample{
-			Metric: q1m2,
+			Metric: q2m2,
 			Value:  0.040}, // same edge reported by incoming (q0), this > value should get ignored
 		&model.Sample{
-			Metric: q1m3,
+			Metric: q2m3,
 			Value:  0.040}, // same edge reported by incoming (q0), this > value should get ignored
 	}
 
@@ -192,6 +195,7 @@ func TestResponseTimeP95(t *testing.T) {
 	}
 	mockQuery(api, q0, &v0)
 	mockQuery(api, q1, &v1)
+	mockQuery(api, q2, &v2)
 
 	trafficMap := responseTimeTestTraffic()
 	ingressID, _, _ := graph.Id(config.DefaultClusterID, "istio-system", "", "istio-system", "ingressgateway-unknown", "ingressgateway", graph.Unknown, graph.GraphTypeVersionedApp)
@@ -207,8 +211,9 @@ func TestResponseTimeP95(t *testing.T) {
 		InjectServiceNodes: true,
 		Namespaces: map[string]graph.NamespaceInfo{
 			"bookinfo": {
-				Name:     "bookinfo",
-				Duration: duration,
+				Name:      "bookinfo",
+				Duration:  duration,
+				IsAmbient: true,
 			},
 		},
 		Quantile:  0.95,
@@ -221,7 +226,7 @@ func TestResponseTimeP95(t *testing.T) {
 		},
 	}
 
-	appender.appendGraph(trafficMap, graph.NamespaceInfo{Name: "bookinfo", IsAmbient: false}, client)
+	appender.appendGraph(trafficMap, graph.NamespaceInfo{Name: "bookinfo", IsAmbient: true}, client)
 
 	ingress, ok = trafficMap[ingressID]
 	assert.Equal(true, ok)
