c62650e09bda346cdce24eb8d835deeeafea277a
Jay Shaughnessy <jshaughn@redhat.com>
2025-01-14 15:40:55 -0500

    Convert new MeshSettings component to hook - add I18N support

--- frontend/src/pages/Mesh/toolbar/MeshSettings.tsx
+++ frontend/src/pages/Mesh/toolbar/MeshSettings.tsx
@@ -26,11 +26,11 @@ import {
   titleStyle
 } from 'styles/DropdownStyles';
 import { KialiDispatch } from 'types/Redux';
-import { KialiCrippledFeatures } from 'types/ServerConfig';
 import { serverConfig } from '../../../config';
 import { infoStyle } from 'styles/IconStyle';
 import { INITIAL_MESH_STATE } from 'reducers/MeshDataState';
 import { MeshToolbarActions } from 'actions/MeshToolbarActions';
+import { useKialiTranslation } from 'utils/I18nUtils';
 
 type ReduxStateProps = {
   showGateways: boolean;
@@ -48,8 +48,6 @@ type MeshSettingsProps = ReduxStateProps &
     disabled: boolean;
   };
 
-type MeshSettingsState = { crippledFeatures?: KialiCrippledFeatures; isOpen: boolean };
-
 interface DisplayOptionType {
   iconClassName?: string;
   iconColor?: string;
@@ -63,118 +61,24 @@ interface DisplayOptionType {
 
 const marginBottom = 20;
 
-class MeshSettingsComponent extends React.PureComponent<MeshSettingsProps, MeshSettingsState> {
-  constructor(props: MeshSettingsProps) {
-    super(props);
+const MeshSettingsComponent: React.FC<MeshSettingsProps> = (props: MeshSettingsProps) => {
+  const [isOpen, setIsOpen] = React.useState<boolean>(false);
+  const { t } = useKialiTranslation();
 
-    this.state = {
-      isOpen: false
-    };
-
-    // Let URL override current redux state at construction time. Update URL as needed.
-    this.handleURLBool(
-      URLParam.MESH_GATEWAYS,
-      INITIAL_MESH_STATE.toolbarState.showGateways,
-      props.showGateways,
-      props.toggleGateways
-    );
-
-    this.handleURLBool(
-      URLParam.MESH_WAYPOINTS,
-      INITIAL_MESH_STATE.toolbarState.showWaypoints,
-      props.showWaypoints,
-      props.toggleWaypoints
-    );
-  }
-
-  componentDidUpdate(prev: MeshSettingsProps): void {
-    // ensure redux state and URL are aligned
-    this.alignURLBool(
-      URLParam.MESH_GATEWAYS,
-      INITIAL_MESH_STATE.toolbarState.showGateways,
-      prev.showGateways,
-      this.props.showGateways
-    );
-
-    this.alignURLBool(
-      URLParam.MESH_WAYPOINTS,
-      INITIAL_MESH_STATE.toolbarState.showWaypoints,
-      prev.showWaypoints,
-      this.props.showWaypoints
-    );
-  }
-
-  private handleURLBool = (
-    param: URLParam,
-    paramDefault: boolean,
-    reduxValue: boolean,
-    reduxToggle: () => void
-  ): void => {
-    const urlValue = HistoryManager.getBooleanParam(param);
-
-    if (urlValue !== undefined) {
-      if (urlValue !== reduxValue) {
-        reduxToggle();
-      }
-    } else if (reduxValue !== paramDefault) {
-      HistoryManager.setParam(param, String(reduxValue));
-    }
-  };
-
-  private alignURLBool = (param: URLParam, paramDefault: boolean, prev: boolean, curr: boolean): void => {
-    if (prev === curr) {
-      return;
-    }
-
-    if (curr === paramDefault) {
-      HistoryManager.deleteParam(param);
-    } else {
-      HistoryManager.setParam(param, String(curr));
-    }
-  };
-
-  render(): React.ReactNode {
-    return (
-      <Dropdown
-        toggle={(toggleRef: React.Ref<MenuToggleElement>) => (
-          <MenuToggle
-            ref={toggleRef}
-            id="display-settings"
-            onClick={() => this.onToggle(!this.state.isOpen)}
-            isExpanded={this.state.isOpen}
-            isDisabled={this.props.disabled}
-          >
-            Display
-          </MenuToggle>
-        )}
-        isOpen={this.state.isOpen}
-        onOpenChange={(isOpen: boolean) => this.onToggle(isOpen)}
-      >
-        <DropdownList>{this.getMenuOptions()}</DropdownList>
-      </Dropdown>
-    );
-  }
-
-  private onToggle = (isOpen: boolean): void => {
-    this.setState({
-      isOpen
-    });
-  };
-
-  private getMenuOptions = (): React.ReactNode => {
+  const getMenuOptions = (): React.ReactNode => {
     // map our attributes from redux
-    const { showGateways, showWaypoints } = this.props;
+    const { showGateways, showWaypoints } = props;
 
     // map our dispatchers for redux
-    const { toggleGateways, toggleWaypoints } = this.props;
+    const { toggleGateways, toggleWaypoints } = props;
 
     const visibilityOptions: DisplayOptionType[] = [
       {
         id: 'filterGateways',
         isChecked: showGateways,
-        labelText: 'Gateways',
+        labelText: t('Gateways'),
         onChange: toggleGateways,
-        tooltip: <div style={{ textAlign: 'left' }}>When enabled, include gateways in the mesh topology.</div>
+        tooltip: <div style={{ textAlign: 'left' }}>t('When enabled, include gateways in the mesh topology.')</div>
       }
     ];
 
@@ -182,12 +86,11 @@ class MeshSettingsComponent extends React.PureComponent<MeshSettingsProps, MeshS
       visibilityOptions.push({
         id: 'filterWaypoints',
         isChecked: showWaypoints,
-        labelText: 'Waypoints',
+        labelText: t('Waypoints'),
         onChange: toggleWaypoints,
         tooltip: (
           <div style={{ textAlign: 'left' }}>
-            <div>Show waypoint proxies workloads.</div>
-            <div>When enabled in an Ambient environment, include waypoints in the mesh topology.</div>
+            <div>t(When enabled in an Ambient environment, include waypoints in the mesh topology.')</div>
           </div>
         )
       });
@@ -207,7 +110,7 @@ class MeshSettingsComponent extends React.PureComponent<MeshSettingsProps, MeshS
                 <Checkbox
                   id={item.id}
                   isChecked={item.isChecked}
-                  isDisabled={this.props.disabled || item.isDisabled}
+                  isDisabled={props.disabled || item.isDisabled}
                   label={item.labelText}
                   onChange={item.onChange}
                 />
@@ -224,7 +127,100 @@ class MeshSettingsComponent extends React.PureComponent<MeshSettingsProps, MeshS
       </BoundingClientAwareComponent>
     );
   };
-}
+
+  return (
+    <Dropdown
+      toggle={(toggleRef: React.Ref<MenuToggleElement>) => (
+        <MenuToggle
+          ref={toggleRef}
+          id="display-settings"
+          onClick={() => setIsOpen(!isOpen)}
+          isExpanded={isOpen}
+          isDisabled={props.disabled}
+        >
+          Display
+        </MenuToggle>
+      )}
+      isOpen={isOpen}
+      onOpenChange={(isOpen: boolean) => setIsOpen(isOpen)}
+    >
+      <DropdownList>{getMenuOptions()}</DropdownList>
+    </Dropdown>
+  );
+};
+
+const withURLAwareness = (
+  MeshSettingsComponent: React.FC<MeshSettingsProps>
+): React.ComponentClass<MeshSettingsProps> => {
+  return class extends React.Component<MeshSettingsProps> {
+    constructor(props: MeshSettingsProps) {
+      super(props);
+
+      // Let URL override current redux state at construction time. Update URL as needed.
+      this.handleURLBool(
+        URLParam.MESH_GATEWAYS,
+        INITIAL_MESH_STATE.toolbarState.showGateways,
+        props.showGateways,
+        props.toggleGateways
+      );
+
+      this.handleURLBool(
+        URLParam.MESH_WAYPOINTS,
+        INITIAL_MESH_STATE.toolbarState.showWaypoints,
+        props.showWaypoints,
+        props.toggleWaypoints
+      );
+    }
+
+    private handleURLBool = (
+      param: URLParam,
+      paramDefault: boolean,
+      reduxValue: boolean,
+      reduxToggle: () => void
+    ): void => {
+      const urlValue = HistoryManager.getBooleanParam(param);
+
+      if (urlValue !== undefined) {
+        if (urlValue !== reduxValue) {
+          reduxToggle();
+        }
+      } else if (reduxValue !== paramDefault) {
+        HistoryManager.setParam(param, String(reduxValue));
+      }
+    };
+
+    private alignURLBool = (param: URLParam, paramDefault: boolean, prev: boolean, curr: boolean): void => {
+      if (prev === curr) {
+        return;
+      }
+
+      if (curr === paramDefault) {
+        HistoryManager.deleteParam(param);
+      } else {
+        HistoryManager.setParam(param, String(curr));
+      }
+    };
+
+    componentDidUpdate(prev: MeshSettingsProps): void {
+      this.alignURLBool(
+        URLParam.MESH_GATEWAYS,
+        INITIAL_MESH_STATE.toolbarState.showGateways,
+        prev.showGateways,
+        this.props.showGateways
+      );
+      this.alignURLBool(
+        URLParam.MESH_WAYPOINTS,
+        INITIAL_MESH_STATE.toolbarState.showWaypoints,
+        prev.showWaypoints,
+        this.props.showWaypoints
+      );
+    }
+
+    render(): React.ReactNode {
+      return <MeshSettingsComponent {...this.props} />;
+    }
+  };
+};
 
 // Allow Redux to map sections of our global app state to our props
 const mapStateToProps = (state: KialiAppState): ReduxStateProps => ({
@@ -240,5 +236,4 @@ const mapDispatchToProps = (dispatch: KialiDispatch): ReduxDispatchProps => {
   };
 };
 
-// hook up to Redux for our State to be mapped to props
-export const MeshSettings = connect(mapStateToProps, mapDispatchToProps)(MeshSettingsComponent);
+export const MeshSettings = connect(mapStateToProps, mapDispatchToProps)(withURLAwareness(MeshSettingsComponent));
