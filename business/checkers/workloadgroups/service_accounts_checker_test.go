f21e4ee970f0531a87ed7cc3ebc69f96cec4d801
Hayk Hovsepyan <hhovsepy@redhat.com>
2025-01-15 09:47:21 +0100

    lint

--- business/checkers/workloadgroups/service_accounts_checker_test.go
+++ business/checkers/workloadgroups/service_accounts_checker_test.go
@@ -3,11 +3,12 @@ package authorization
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+
 	"github.com/kiali/kiali/config"
 	"github.com/kiali/kiali/models"
 	"github.com/kiali/kiali/tests/data"
 	"github.com/kiali/kiali/tests/testutils/validations"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestPresentServiceAccount(t *testing.T) {
