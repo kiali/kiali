dd432afc0e140b872c445cf4eacd1052d4886417
Fernando Hoyos <122779323+ferhoyos@users.noreply.github.com>
2025-02-06 15:50:04 +0100

    Set apiVersion of ReferenceGrant to v1beta1 (#8134)

--- kubernetes/types.go
+++ kubernetes/types.go
@@ -71,7 +71,7 @@ var (
 	K8sGatewayClasses  = K8sNetworkingGroupVersionV1.WithKind(K8sGatewayClassType)
 	K8sGRPCRoutes      = K8sNetworkingGroupVersionV1.WithKind(K8sGRPCRouteType)
 	K8sHTTPRoutes      = K8sNetworkingGroupVersionV1.WithKind(K8sHTTPRouteType)
-	K8sReferenceGrants = K8sNetworkingGroupVersionV1.WithKind(K8sReferenceGrantType)
+	K8sReferenceGrants = K8sNetworkingGroupVersionV1Beta1.WithKind(K8sReferenceGrantType)
 	K8sTCPRoutes       = K8sNetworkingGroupVersionV1Alpha2.WithKind(K8sTCPRouteType)
 	K8sTLSRoutes       = K8sNetworkingGroupVersionV1Alpha2.WithKind(K8sTLSRouteType)
 
