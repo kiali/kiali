9bcb3c632dec2b00e6a9955624a78c653f876c2f
Hayk Hovsepyan <hhovsepy@redhat.com>
2025-02-19 12:50:00 +0100

    better approach in clusters component status, fixed tests

--- frontend/src/components/IstioStatus/__tests__/IstioStatusList.test.tsx
+++ frontend/src/components/IstioStatus/__tests__/IstioStatusList.test.tsx
@@ -3,25 +3,30 @@ import { shallow } from 'enzyme';
 import { ComponentStatus, Status } from '../../../types/IstioStatus';
 import { IstioStatusList } from '../IstioStatusList';
 import { shallowToJson } from 'enzyme-to-json';
+import { CLUSTER_DEFAULT } from '../../../types/Graph';
 
 it('lists all the components grouped', () => {
   const components: ComponentStatus[] = [
     {
+      cluster: CLUSTER_DEFAULT,
       name: 'grafana',
       status: Status.NotFound,
       is_core: false
     },
     {
+      cluster: CLUSTER_DEFAULT,
       name: 'prometheus',
       status: Status.Unhealthy,
       is_core: false
     },
     {
+      cluster: CLUSTER_DEFAULT,
       name: 'istiod',
       status: Status.NotFound,
       is_core: true
     },
     {
+      cluster: CLUSTER_DEFAULT,
       name: 'istio-egressgateway',
       status: Status.Unhealthy,
       is_core: true
