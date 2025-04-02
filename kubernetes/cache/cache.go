847b04898f86113f4092ea98e179ad285db2e2c0
Jay Shaughnessy <jshaughn@redhat.com>
2025-04-02 15:35:25 -0400

    lower the exp times, CI is failing intermittently. We can change it to be higher when the CRD has these in config, and then set lower limits just for CI.

--- kubernetes/cache/cache.go
+++ kubernetes/cache/cache.go
@@ -23,9 +23,9 @@ import (
 
 const (
 	ambientCheckExpirationTime = 10 * time.Minute
-	gatewayExpirationTime      = 4 * time.Minute
+	gatewayExpirationTime      = 3 * time.Minute
 	meshExpirationTime         = 20 * time.Second
-	waypointExpirationTime     = 4 * time.Minute
+	waypointExpirationTime     = 3 * time.Minute
 )
 
 const (
