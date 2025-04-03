014c4e1aafdd9cbfb9c95b887fe4289161b350bd
Jay Shaughnessy <jshaughn@redhat.com>
2025-04-03 15:43:49 -0400

    Refactor gateway multi_match_checker for efficiency
    kiali8211

--- business/checkers/gateways/multi_match_checker.go
+++ business/checkers/gateways/multi_match_checker.go
@@ -5,7 +5,6 @@ import (
 	"strconv"
 	"strings"
 
-	api_networking_v1 "istio.io/api/networking/v1"
 	networking_v1 "istio.io/client-go/pkg/apis/networking/v1"
 
 	"k8s.io/apimachinery/pkg/labels"
@@ -13,15 +12,13 @@ import (
 	"github.com/kiali/kiali/config"
 	"github.com/kiali/kiali/kubernetes"
 	"github.com/kiali/kiali/models"
-	"github.com/kiali/kiali/util/intutil"
 )
 
 type MultiMatchChecker struct {
-	Cluster         string
-	Conf            *config.Config
-	Gateways        []*networking_v1.Gateway
-	existingList    map[string][]Host
-	hostRegexpCache map[string]regexp.Regexp
+	Cluster   string
+	Conf      *config.Config
+	Gateways  []*networking_v1.Gateway
+	regexpMap map[string]regexp.Regexp
 }
 
 const (
@@ -31,71 +28,101 @@ const (
 )
 
 type Host struct {
-	GatewayRuleName string
-	Hostname        string
+	Gateway         *networking_v1.Gateway
 	HostIndex       int
-	Namespace       string
-	Port            int
+	Name            string
 	ServerIndex     int
 	TargetNamespace string
 }
 
+// hostMatchKey groups hosts that match already match on Port and Selector, so
+// that we can perform matching on smaller sets of hosts
+type hostMatchKey = struct {
+	Port     uint32
+	Selector string
+}
+
+// hostMatchMap groups hosts that minimally match on the hostMatchKey fields
+type hostMatchMap map[hostMatchKey][]Host
+
 // Check validates that no two gateways share the same host+port combination
 func (m MultiMatchChecker) Check() models.IstioValidations {
+	hostMap := hostMatchMap{}
+	hostKey := hostMatchKey{}
 	validations := models.IstioValidations{}
-	m.existingList = map[string][]Host{}
-	m.hostRegexpCache = map[string]regexp.Regexp{}
+	m.regexpMap = map[string]regexp.Regexp{}
 
-	for _, g := range m.Gateways {
-		gatewayRuleName := g.Name
-		gatewayNamespace := g.Namespace
-
-		selectorString := ""
-		if len(g.Spec.Selector) > 0 {
-			selectorString = labels.Set(g.Spec.Selector).String()
+	// first, collect all of the gateway hosts and group together those that need to be checked
+	// against each other.
+	for _, gw := range m.Gateways {
+		hostKey.Selector = ""
+		if len(gw.Spec.Selector) > 0 {
+			hostKey.Selector = labels.Set(gw.Spec.Selector).String()
 		}
-		for i, server := range g.Spec.Servers {
+
+		for serverIndex, server := range gw.Spec.Servers {
 			if server == nil {
 				continue
 			}
-			hosts := parsePortAndHostnames(server)
-			for hi, host := range hosts {
-				host.ServerIndex = i
-				host.HostIndex = hi
-				host.GatewayRuleName = gatewayRuleName
-				host.Namespace = gatewayNamespace
-				// Hostname can be given in <target-namespace>/Hostname syntax
-				host.TargetNamespace = targetNamespaceAll
-				namespaceAndHost := strings.Split(host.Hostname, "/")
+
+			hostKey.Port = 0
+			if server.Port != nil {
+				hostKey.Port = server.Port.Number
+			}
+
+			for hostIndex, serverHost := range server.Hosts {
+				host := Host{
+					Gateway:         gw,
+					HostIndex:       hostIndex,
+					Name:            serverHost,
+					ServerIndex:     serverIndex,
+					TargetNamespace: targetNamespaceAll,
+				}
+				// host can be given in <target-namespace>/host syntax
+				namespaceAndHost := strings.Split(serverHost, "/")
 				if len(namespaceAndHost) > 1 {
-					host.Hostname = namespaceAndHost[1]
+					host.Name = namespaceAndHost[1]
 					host.TargetNamespace = namespaceAndHost[0]
 					// replace targetNamespaceCurrent with GW namespace to simplify duplicate checking
 					if host.TargetNamespace == targetNamespaceCurrent {
-						host.TargetNamespace = gatewayNamespace
+						host.TargetNamespace = gw.Namespace
 					}
 				}
-				duplicate, dhosts := m.findMatch(host, selectorString)
-				if duplicate {
-					// The above is referenced by each one below..
-					currentHostValidation := createError(host.GatewayRuleName, host.Namespace, m.Cluster, host.ServerIndex, host.HostIndex)
-					existingHosts := make(map[string]bool)
-					for i := 0; i < len(dhosts); i++ {
-						dh := dhosts[i]
-						// we skip CurrentHostValidation
-						// skip duplicate references when one gateway has several duplicate hosts
-						if (dh.Namespace == gatewayNamespace && dh.GatewayRuleName == gatewayRuleName) || existingHosts[dh.Namespace+"/"+dh.GatewayRuleName] {
-							continue
-						}
-						existingHosts[dh.Namespace+"/"+dh.GatewayRuleName] = true
-						refValidation := createError(dh.GatewayRuleName, dh.Namespace, m.Cluster, dh.ServerIndex, dh.HostIndex)
-						refValidation = refValidation.MergeReferences(currentHostValidation)
-						currentHostValidation = currentHostValidation.MergeReferences(refValidation)
-						validations = validations.MergeValidations(refValidation)
-					}
-					validations = validations.MergeValidations(currentHostValidation)
+				if hosts, found := hostMap[hostKey]; found {
+					hostMap[hostKey] = append(hosts, host)
+				} else {
+					hostMap[hostKey] = []Host{host}
 				}
-				m.existingList[selectorString] = append(m.existingList[selectorString], host)
+			}
+		}
+	}
+
+	// second, look for matches for each grouped set of hosts
+	for _, hosts := range hostMap {
+		for i, host := range hosts {
+			if len(hosts) == i+1 {
+				break
+			}
+			hasDuplicates, duplicates := m.findMatch(host, hosts[i+1:])
+			if hasDuplicates {
+				hostValidationError := newValidationError(host.Gateway.Name, host.Gateway.Namespace, m.Cluster, host.ServerIndex, host.HostIndex)
+				existingHosts := make(map[string]bool)
+
+				for _, duplicate := range duplicates {
+					existingHostKey := duplicate.Gateway.Namespace + "/" + duplicate.Gateway.Name
+
+					// skip duplicate references when one gateway has several duplicate hosts
+					if (duplicate.Gateway.Namespace == host.Gateway.Namespace && duplicate.Gateway.Name == host.Gateway.Name) || existingHosts[existingHostKey] {
+						continue
+					}
+
+					existingHosts[existingHostKey] = true
+					refValidation := newValidationError(duplicate.Gateway.Name, duplicate.Gateway.Namespace, m.Cluster, duplicate.ServerIndex, duplicate.HostIndex)
+					refValidation = refValidation.MergeReferences(hostValidationError)
+					hostValidationError = hostValidationError.MergeReferences(refValidation)
+					validations = validations.MergeValidations(refValidation)
+				}
+				validations = validations.MergeValidations(hostValidationError)
 			}
 		}
 	}
@@ -103,13 +130,13 @@ func (m MultiMatchChecker) Check() models.IstioValidations {
 	return validations
 }
 
-func createError(gatewayRuleName, namespace, cluster string, serverIndex, hostIndex int) models.IstioValidations {
-	key := models.IstioValidationKey{Name: gatewayRuleName, Namespace: namespace, ObjectGVK: kubernetes.Gateways, Cluster: cluster}
+func newValidationError(gatewayName, namespace, cluster string, serverIndex, hostIndex int) models.IstioValidations {
+	key := models.IstioValidationKey{Name: gatewayName, Namespace: namespace, ObjectGVK: kubernetes.Gateways, Cluster: cluster}
 	checks := models.Build("gateways.multimatch",
 		"spec/servers["+strconv.Itoa(serverIndex)+"]/hosts["+strconv.Itoa(hostIndex)+"]")
 	rrValidation := &models.IstioValidation{
 		Cluster:   cluster,
-		Name:      gatewayRuleName,
+		Name:      gatewayName,
 		ObjectGVK: key.ObjectGVK,
 		Valid:     true,
 		Checks: []*models.IstioCheck{
@@ -120,72 +147,52 @@ func createError(gatewayRuleName, namespace, cluster string, serverIndex, hostIn
 	return models.IstioValidations{key: rrValidation}
 }
 
-func parsePortAndHostnames(serverDef *api_networking_v1.Server) []Host {
-	var port int
-	if serverDef.Port != nil {
-		if n, e := intutil.Convert(serverDef.Port.Number); e == nil {
-			port = n
-		}
-	}
-	if len(serverDef.Hosts) > 0 {
-		hosts := make([]Host, 0, len(serverDef.Hosts))
-		for _, hostname := range serverDef.Hosts {
-			hosts = append(hosts, Host{
-				Port:     port,
-				Hostname: hostname,
-			})
-		}
-		return hosts
-	}
-	return nil
-}
-
 // findMatch uses a linear search with regexp to check for matching gateway host + port combinations. If this becomes a bottleneck for performance, replace with a graph or trie algorithm.
-func (m MultiMatchChecker) findMatch(host Host, selector string) (bool, []Host) {
+func (m MultiMatchChecker) findMatch(host Host, otherHosts []Host) (bool, []Host) {
 	duplicates := make([]Host, 0)
+	skipWild := m.Conf.KialiFeatureFlags.Validations.SkipWildcardGatewayHosts
 
-	for groupSelector, hostGroup := range m.existingList {
-		if groupSelector != selector {
+	for _, other := range otherHosts {
+		// only compare hosts that share the target namespace or hosts where at least one is exported to all namespaces
+		if host.TargetNamespace != other.TargetNamespace && host.TargetNamespace != targetNamespaceAll && other.TargetNamespace != targetNamespaceAll {
+			continue
+		}
+		// wildcardMatches will always match unless SkipWildcardGatewayHosts is set 'true'
+		if host.Name == wildCardMatch || other.Name == wildCardMatch {
+			if !skipWild {
+				duplicates = append(duplicates, host)
+				duplicates = append(duplicates, other)
+			}
 			continue
 		}
 
-		for _, h := range hostGroup {
-			// only compare hosts that share the target namespace or hosts where at least one of the pair is exported to all namespaces
-			if h.TargetNamespace == targetNamespaceAll || host.TargetNamespace == targetNamespaceAll || h.TargetNamespace == host.TargetNamespace {
-				if h.Port == host.Port {
-					// wildcardMatches will always match unless SkipWildcardGatewayHosts is set 'true'
-					if host.Hostname == wildCardMatch || h.Hostname == wildCardMatch {
-						if !m.Conf.KialiFeatureFlags.Validations.SkipWildcardGatewayHosts {
-							duplicates = append(duplicates, host)
-							duplicates = append(duplicates, h)
-						}
-						continue
-					}
+		// DNS is case-insensitive
+		hostName := strings.ToLower(host.Name)
+		otherName := strings.ToLower(other.Name)
 
-					// DNS is case-insensitive
-					current := strings.ToLower(host.Hostname)
-					previous := strings.ToLower(h.Hostname)
+		// avoid regex if it's simple equality
+		if hostName == otherName {
+			duplicates = append(duplicates, host)
+			duplicates = append(duplicates, other)
+			continue
+		}
 
-					// lazily compile hostname Regex
-					currentRegexp, ok := m.hostRegexpCache[current]
-					if !ok {
-						currentRegexp = *regexpFromHostname(current)
-						m.hostRegexpCache[current] = currentRegexp
-					}
-					previousRegexp, ok := m.hostRegexpCache[previous]
-					if !ok {
-						previousRegexp = *regexpFromHostname(previous)
-						m.hostRegexpCache[previous] = previousRegexp
-					}
+		// lazily compile hostname Regex
+		hostRegexp, ok := m.regexpMap[hostName]
+		if !ok {
+			hostRegexp = *regexpFromHostname(hostName)
+			m.regexpMap[hostName] = hostRegexp
+		}
+		otherRegexp, ok := m.regexpMap[otherName]
+		if !ok {
+			otherRegexp = *regexpFromHostname(otherName)
+			m.regexpMap[otherName] = otherRegexp
+		}
 
-					if currentRegexp.MatchString(previous) ||
-						previousRegexp.MatchString(current) {
-						duplicates = append(duplicates, host)
-						duplicates = append(duplicates, h)
-						continue
-					}
-				}
-			}
+		if hostRegexp.MatchString(otherName) || otherRegexp.MatchString(hostName) {
+			duplicates = append(duplicates, host)
+			duplicates = append(duplicates, other)
+			continue
 		}
 	}
 	return len(duplicates) > 0, duplicates
