5f651a73b91b097f02b5ad77c810044607c0af0c
Jay Shaughnessy <jshaughn@redhat.com>
2025-04-11 16:31:55 -0400

    Update hack/setup-kind-in-ci.sh

--- hack/setup-kind-in-ci.sh
+++ hack/setup-kind-in-ci.sh
@@ -340,11 +340,11 @@ setup_kind_tempo() {
     --set health_config.rate[0].tolerance[0].code="5xx" \
     --set health_config.rate[0].tolerance[0].degraded=2 \
     --set health_config.rate[0].tolerance[0].failure=100 \
-    --set kiali_internal.cache_expiration.ambient_check="1m" \
-    --set kiali_internal.cache_expiration.gateway="1m" \
-    --set kiali_internal.cache_expiration.istio_status="30s" \
-    --set kiali_internal.cache_expiration.mesh="20s" \
-    --set kiali_internal.cache_expiration.waypoint="1m" \
+    --set kiali_internal.cache_expiration.ambient_check="10s" \
+    --set kiali_internal.cache_expiration.gateway="10s" \
+    --set kiali_internal.cache_expiration.istio_status="15s" \
+    --set kiali_internal.cache_expiration.mesh="10s" \
+    --set kiali_internal.cache_expiration.waypoint="10s" \
     kiali-server \
     "${HELM_CHARTS_DIR}"/_output/charts/kiali-server-*.tgz
   
