71d98392203778151924fbb6687869fcc758a4c4
josunect <jcordoba@redhat.com>
2025-04-14 14:27:56 +0100

    Update kiali internal settings

--- hack/istio/multicluster/deploy-kiali.sh
+++ hack/istio/multicluster/deploy-kiali.sh
@@ -189,6 +189,11 @@ deploy_kiali() {
     --set health_config.rate[0].tolerance[0].code="5xx"
     --set health_config.rate[0].tolerance[0].degraded=2
     --set health_config.rate[0].tolerance[0].failure=100
+    --set kiali_internal.cache_expiration.ambient_check="10m"
+    --set kiali_internal.cache_expiration.gateway="3m"
+    --set kiali_internal.cache_expiration.istio_status="0"
+    --set kiali_internal.cache_expiration.mesh="10s"
+    --set kiali_internal.cache_expiration.waypoint="3m"
     --set deployment.ingress.enabled=${ingress_enabled}
     --set deployment.service_type=${service_type}
     --set server.web_port=${web_port}
