2c7e2abcd33680f8320919435fde6b569b7cdb69
John Mazzitelli <mazz@redhat.com>
2025-05-09 20:21:29 -0400

    group all graph APIs in same log group name

--- handlers/graph.go
+++ handlers/graph.go
@@ -64,7 +64,7 @@ func GraphNamespaces(
 		defer handlePanic(r.Context(), w)
 
 		// prepare the logger in a context, and replace the request context with ours that has our logger in it
-		r = log.AddGroupToLoggerInRequestContext(r, log.GraphNamespaceLogName)
+		r = log.AddGroupToLoggerInRequestContext(r, log.GraphLogName)
 
 		// TODO: getLayer and its downstream call chain has our logger in the request context now; it just needs to extract and use it (which it does not today)
 		business, err := getLayer(r, conf, kialiCache, clientFactory, cpm, prom, traceClientLoader, grafana, discovery)
@@ -92,7 +92,7 @@ func GraphNode(
 		defer handlePanic(r.Context(), w)
 
 		// prepare the logger in a context, and replace the request context with ours that has our logger in it
-		r = log.AddGroupToLoggerInRequestContext(r, log.GraphNodeLogName)
+		r = log.AddGroupToLoggerInRequestContext(r, log.GraphLogName)
 
 		business, err := getLayer(r, conf, kialiCache, clientFactory, cpm, prom, traceClientLoader, grafana, discovery)
 		graph.CheckError(err)
