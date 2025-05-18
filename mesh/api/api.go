afb26b024bfb8d1125303815ea419995830a2ac0
John Mazzitelli <mazz@redhat.com>
2025-05-18 13:15:25 -0400

    get mesh graph generation metrics (#8431)
    fixes: https://github.com/kiali/kiali/issues/8429

--- mesh/api/api.go
+++ mesh/api/api.go
@@ -19,6 +19,7 @@ import (
 	"github.com/kiali/kiali/mesh/config/common"
 	"github.com/kiali/kiali/mesh/generator"
 	"github.com/kiali/kiali/observability"
+	"github.com/kiali/kiali/prometheus/internalmetrics"
 )
 
 // GraphMesh generates a mesh graph using the provided options
@@ -37,9 +38,6 @@ func GraphMesh(
 		observability.Attribute("package", "api"),
 	)
 	defer end()
-	// time how long it takes to generate this graph
-	// promtimer := internalmetrics.GetMeshGraphGenerationTimePrometheusTimer()
-	// defer promtimer.ObserveDuration()
 
 	// Create a 'global' object to store the business. Global only to the request.
 	globalInfo := mesh.NewGlobalInfo()
@@ -50,6 +48,9 @@ func GraphMesh(
 	globalInfo.Grafana = grafana
 	globalInfo.KialiCache = kialiCache
 	globalInfo.IstioStatusGetter = &business.IstioStatus
+
+	promtimer := internalmetrics.GetGraphGenerationTimePrometheusTimer("mesh", "mesh", false)
+	promtimer.ObserveDuration()
 
 	code, config = graphMesh(ctx, globalInfo, o)
 
@@ -73,8 +74,8 @@ func graphMesh(ctx context.Context, globalInfo *mesh.GlobalInfo, o mesh.Options)
 func generateGraph(meshMap mesh.MeshMap, o mesh.Options) (int, interface{}) {
 	log.Tracef("Generating config for [%s] graph...", o.ConfigVendor)
 
-	//	promtimer := internalmetrics.GetMeshGraphMarshalTimePrometheusTimer()
-	//	defer promtimer.ObserveDuration()
+	promtimer := internalmetrics.GetGraphMarshalTimePrometheusTimer("mesh", "mesh", false)
+	defer promtimer.ObserveDuration()
 
 	var vendorConfig interface{}
 	switch o.ConfigVendor {
