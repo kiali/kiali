f0bf7df8dd2d499792f7eda6a897567fff583e59
mkralik3 <mkralik@redhat.com>
2025-05-19 16:02:13 +0200

    Do not install Gateway API when they are already in the cluster

--- hack/istio/install-istio-via-istioctl.sh
+++ hack/istio/install-istio-via-istioctl.sh
@@ -576,8 +576,9 @@ else
       echo "Gateway API Version is not specified, taking the latest released version"
       K8S_GATEWAY_API_VERSION=`curl --head --silent "https://github.com/kubernetes-sigs/gateway-api/releases/latest" | grep "location: " | awk '{print $2}' | sed "s/.*tag\///g" | cat -v | sed "s/\^M//g"`
     fi
-    echo "Installing Gateway API version ${K8S_GATEWAY_API_VERSION}"
-    ${CLIENT_EXE} apply -k "github.com/kubernetes-sigs/gateway-api/config/crd/experimental?ref=${K8S_GATEWAY_API_VERSION}"
+    echo "Verifying that Gateway API is installed; if it is not then Gateway API version ${K8S_GATEWAY_API_VERSION} will be installed now."
+    $CLIENT_EXE get crd gateways.gateway.networking.k8s.io &> /dev/null || \
+      { $CLIENT_EXE kustomize "github.com/kubernetes-sigs/gateway-api/config/crd?ref=${K8S_GATEWAY_API_VERSION}" | $CLIENT_EXE apply -f -; }
   fi
 
   # Do some OpenShift specific things
