b52c567ee23ab813695aa6d531ddf4f5e2d05e3a
Hayk Hovsepyan <hhovsepy@redhat.com>
2025-05-21 15:31:43 +0200

    Added tests, removed old ones

--- kubernetes/filters_test.go
+++ kubernetes/filters_test.go
@@ -19,62 +19,6 @@ import (
 	"github.com/kiali/kiali/config"
 )
 
-func TestFilterPodsForEndpoints(t *testing.T) {
-	assert := assert.New(t)
-
-	endpoints := core_v1.Endpoints{
-		Subsets: []core_v1.EndpointSubset{
-			{
-				Addresses: []core_v1.EndpointAddress{
-					{
-						TargetRef: &core_v1.ObjectReference{
-							Name: "pod-1",
-							Kind: "Pod",
-						},
-					},
-					{
-						TargetRef: &core_v1.ObjectReference{
-							Name: "pod-2",
-							Kind: "Pod",
-						},
-					},
-					{
-						TargetRef: &core_v1.ObjectReference{
-							Name: "other",
-							Kind: "Other",
-						},
-					},
-					{},
-				},
-			},
-			{
-				Addresses: []core_v1.EndpointAddress{
-					{
-						TargetRef: &core_v1.ObjectReference{
-							Name: "pod-3",
-							Kind: "Pod",
-						},
-					},
-				},
-			},
-		},
-	}
-
-	pods := []core_v1.Pod{
-		{ObjectMeta: meta_v1.ObjectMeta{Name: "pod-1"}},
-		{ObjectMeta: meta_v1.ObjectMeta{Name: "pod-2"}},
-		{ObjectMeta: meta_v1.ObjectMeta{Name: "pod-3"}},
-		{ObjectMeta: meta_v1.ObjectMeta{Name: "pod-999"}},
-		{ObjectMeta: meta_v1.ObjectMeta{Name: "other"}},
-	}
-
-	filtered := FilterPodsByEndpoints(&endpoints, pods)
-	assert.Len(filtered, 3)
-	assert.Equal("pod-1", filtered[0].Name)
-	assert.Equal("pod-2", filtered[1].Name)
-	assert.Equal("pod-3", filtered[2].Name)
-}
-
 func TestFilterGateways(t *testing.T) {
 	assert := assert.New(t)
 
--- models/endpoint.go
+++ models/endpoint.go
@@ -12,18 +12,18 @@ type Endpoint struct {
 func GetEndpointsFromPods(pods []core_v1.Pod) *Endpoints {
 	endpointPodAddresses := Endpoints{}
 	for _, pod := range pods {
-		ep := Endpoint{
-			Addresses: make(Addresses, 0),
-			Ports:     make(Ports, 0),
-		}
 		if pod.Status.PodIP != "" { // make sure Pod's IP address is not empty
+			ep := Endpoint{
+				Addresses: make(Addresses, 0),
+				Ports:     make(Ports, 0),
+			}
 			ep.Addresses = append(ep.Addresses, Address{
 				Kind: pod.Kind,
 				Name: pod.Name,
 				IP:   pod.Status.PodIP,
 			})
+			endpointPodAddresses = append(endpointPodAddresses, ep)
 		}
-		endpointPodAddresses = append(endpointPodAddresses, ep)
 	}
 
 	return &endpointPodAddresses
