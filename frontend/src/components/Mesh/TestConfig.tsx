1353cefb3f830d8d4440cff705f69e9fbb5c78c1
josunect <jcordoba@redhat.com>
2025-06-10 16:46:49 +0100

    styles

--- frontend/src/components/Mesh/TestConfig.tsx
+++ frontend/src/components/Mesh/TestConfig.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { useKialiTranslation } from '../../utils/I18nUtils';
-import { Button, ButtonVariant } from '@patternfly/react-core';
+import { Button, ButtonVariant, Tooltip, TooltipPosition } from '@patternfly/react-core';
 import AceEditor from 'react-ace';
 import { Theme } from '../../types/Common';
 import { istioAceEditorStyle } from '../../styles/AceEditorStyle';
@@ -9,22 +9,61 @@ import { getKialiTheme } from '../../utils/ThemeUtils';
 import { dump } from 'js-yaml';
 import ReactAce from 'react-ace/lib/ace';
 import { TracingInfo } from '../../types/TracingInfo';
+import { ValidationTypes } from 'types/IstioObjects';
+import { kialiStyle } from '../../styles/StyleUtils';
+import { Validation } from '../Validations/Validation';
 
 type CheckModalProps = {
   tracingInfo?: TracingInfo;
 };
+
+const healthStatusStyle = kialiStyle({
+  marginLeft: '0.5rem'
+});
 
 export const TestConfig: React.FC<CheckModalProps> = (props: CheckModalProps) => {
   const { t } = useKialiTranslation();
   const aceEditorRef = React.useRef<ReactAce | null>(null);
 
   const theme = getKialiTheme();
+  const [configResult, setConfigResult] = React.useState<string | null>(null);
 
   const getConfigToString = dump(props.tracingInfo, {
     noRefs: true,
     skipInvalid: true,
     ...yamlDumpOptions
   });
+
+  const showResult = (): React.ReactElement => {
+    let healthSeverity: ValidationTypes;
+    switch (configResult) {
+      case 'Ok':
+        healthSeverity = ValidationTypes.Correct;
+        break;
+      case 'Error':
+        healthSeverity = ValidationTypes.Warning;
+        break;
+      default:
+        healthSeverity = ValidationTypes.Error;
+    }
+
+    return (
+      <Tooltip
+        aria-label={t('Health status')}
+        position={TooltipPosition.right}
+        enableFlip={true}
+        content={<>{t('Configuration returned valid results')}</>}
+      >
+        <span className={healthStatusStyle}>
+          <Validation severity={healthSeverity} />
+        </span>
+      </Tooltip>
+    );
+  };
+
+  const handleTestConfig = (): void => {
+    setConfigResult('Ok');
+  };
 
   return (
     <div style={{ paddingTop: '10px' }}>
@@ -40,9 +79,10 @@ export const TestConfig: React.FC<CheckModalProps> = (props: CheckModalProps) =>
         setOptions={aceOptions ?? { foldStyle: 'markbegin' }}
         value={getConfigToString}
       />
-      <Button style={{ marginTop: '10px' }} variant={ButtonVariant.secondary}>
-        Test Config
+      <Button style={{ marginTop: '10px' }} variant={ButtonVariant.secondary} onClick={handleTestConfig}>
+        {t('Test Config')}
       </Button>
+      {configResult ? showResult() : ''}
     </div>
   );
 };
