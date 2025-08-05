cafa76481fc1ea6712f6cc235cd3efaffa4cacf8
Jay Shaughnessy <jshaughn@redhat.com>
2025-08-05 09:26:21 -0400

    remove return

--- hack/istio/multicluster/install-external-kiali.sh
+++ hack/istio/multicluster/install-external-kiali.sh
@@ -271,5 +271,4 @@ fi
 if [ "${BOOKINFO_ENABLED}" == "true" ]; then
   echo "Installing bookinfo demo in namespace [${BOOKINFO_NAMESPACE}] on [${CLUSTER2_CONTEXT}]"
   source ${SCRIPT_DIR}/../install-bookinfo-demo.sh --client-exe "${CLIENT_EXE}" --istio-dir "${ISTIO_DIR}" --istio-namespace "${ISTIO_NAMESPACE}" --namespace "${BOOKINFO_NAMESPACE}" --minikube-profile "${CLUSTER2_CONTEXT}" --namespace "${BOOKINFO_NAMESPACE}" --minikube-profile "${CLUSTER2_CONTEXT}"
-  return 0
 fi
