e17ce318685ba421e78729597839e2c52b45cb30
mamyers <mamyers@redhat.com>
2025-09-10 15:27:11 -0400

    Added link to Network traffic (hopefully)

--- frontend/src/pages/Graph/SummaryPanelNode.tsx
+++ frontend/src/pages/Graph/SummaryPanelNode.tsx
@@ -74,6 +74,7 @@ export type SummaryPanelNodeProps = Omit<SummaryPanelPropType, 'kiosk'> & {
 export type SummaryPanelNodeComponentProps = ReduxProps &
   SummaryPanelNodeProps & {
     gateways: string[] | null;
+    netObsUrl?: string;
     onKebabToggled?: (isOpen: boolean) => void;
     peerAuthentications: PeerAuthentication[] | null;
     serviceDetails: ServiceDetailsInfo | null | undefined;
@@ -233,12 +234,18 @@ export class SummaryPanelNodeComponent extends React.Component<SummaryPanelNodeC
               {secondBadge}
               <div className={nodeInfoStyle}>
                 <NetworkTrafficBadge namespace={nodeData.namespace} />
-                <KialiPageLink
-                  href={`/graph/namespaces?namespaces=${encodeURIComponent(nodeData.namespace)}`}
-                  cluster={nodeData.cluster}
-                >
-                  Network Traffic
-                </KialiPageLink>
+                {this.props.netObsUrl ? (
+                  <a href={this.props.netObsUrl} target="_blank" rel="noopener noreferrer">
+                    Network Traffic <KialiIcon.ExternalLink />
+                  </a>
+                ) : (
+                  <KialiPageLink
+                    href={`/graph/namespaces?namespaces=${encodeURIComponent(nodeData.namespace)}`}
+                    cluster={nodeData.cluster}
+                  >
+                    Network Traffic
+                  </KialiPageLink>
+                )}
               </div>
               {!nodeData.isWaypoint && (
                 <div className={nodeInfoStyle}>
@@ -587,7 +594,7 @@ export const SummaryPanelNode: React.FC<SummaryPanelNodeProps> = (props: Summary
   const rankResult = useKialiSelector(state => state.graph.rankResult);
   const showRank = useKialiSelector(state => state.graph.toolbarState.showRank);
   const updateTime = useKialiSelector(state => state.graph.updateTime);
-  // const externalServices = useKialiSelector(state => state.statusState.externalServices);
+  const externalServices = useKialiSelector(state => state.statusState.externalServices);
 
   const [isKebabOpen, setIsKebabOpen] = React.useState<boolean>(false);
 
@@ -605,6 +612,12 @@ export const SummaryPanelNode: React.FC<SummaryPanelNodeProps> = (props: Summary
     setIsKebabOpen(isOpen);
   };
 
+  const netObs = externalServices?.find(s => s.name && s.url && s.name.toLowerCase().includes('observ'));
+  const netObsBase = netObs?.url ? netObs.url.replace(/\/$/, '') : undefined;
+  const netObsUrl = netObsBase
+    ? `${netObsBase}#/?view=topology&namespace=${encodeURIComponent(nodeData.namespace)}`
+    : undefined;
+
   return (
     <SummaryPanelNodeComponent
       tracingState={tracingState}
@@ -614,7 +627,7 @@ export const SummaryPanelNode: React.FC<SummaryPanelNodeProps> = (props: Summary
       serviceDetails={isServiceDetailsLoading ? undefined : serviceDetails}
       gateways={gateways}
       peerAuthentications={peerAuthentications}
-      // externalServices={externalServices}
+      netObsUrl={netObsUrl}
       onKebabToggled={handleKebabToggled}
       {...props}
     />
