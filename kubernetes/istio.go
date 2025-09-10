c08ec990006a825f73ad4c489faced8c713874df
Jay Shaughnessy <jshaughn@redhat.com>
2025-09-10 16:49:14 -0400

    pr feedback: nick - apply the suggested approach to avoid all-namespace fetches when clusterwise access is not available

--- kubernetes/istio.go
+++ kubernetes/istio.go
@@ -12,7 +12,6 @@ import (
 	security_v1 "istio.io/client-go/pkg/apis/security/v1"
 	istio "istio.io/client-go/pkg/clientset/versioned"
 	apps_v1 "k8s.io/api/apps/v1"
-	core_v1 "k8s.io/api/core/v1"
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	inferenceapiclient "sigs.k8s.io/gateway-api-inference-extension/client-go/clientset/versioned"
 	k8s_networking_v1 "sigs.k8s.io/gateway-api/apis/v1"
@@ -21,7 +20,6 @@ import (
 	"github.com/kiali/kiali/config"
 	"github.com/kiali/kiali/log"
 	"github.com/kiali/kiali/util/httputil"
-	"github.com/kiali/kiali/util/sliceutil"
 )
 
 const (
@@ -326,35 +324,34 @@ func DestinationRuleHasMTLSEnabled(destinationRule *networking_v1.DestinationRul
 	return false, ""
 }
 
-// ClusterNameFromIstiod attempts to resolve the cluster info of the "home" cluster where kiali is running
+// ClusterNameFromIstiod attempts to resolve the clusterName of the "home" cluster where kiali is running,
 // by inspecting the istiod deployment. Assumes that the istiod deployment is in the same cluster as the kiali pod.
 func ClusterNameFromIstiod(conf *config.Config, k8s ClientInterface) (string, error) {
-	// Loop through all namespaces to find istiod deployments. We don't perform a single query for all namespaces
-	// because that works only if Kiali has cluster-wide access.
-	namespaces, err := k8s.GetNamespaces("")
-	if err != nil {
-		return "", err
-	}
-
-	namespaceNames := sliceutil.Map(namespaces, func(ns core_v1.Namespace) string {
-		return ns.Name
-	})
-
 	var istiodDeployments []apps_v1.Deployment
-	for _, namespaceName := range namespaceNames {
-		deployments, err := k8s.GetDeployments(namespaceName, meta_v1.ListOptions{LabelSelector: "app=istiod"})
+	if conf.AllNamespacesAccessible() {
+		deployments, err := k8s.GetDeployments("", meta_v1.ListOptions{LabelSelector: "app=istiod"})
 		if err != nil {
-			log.Warningf("Failed to get deployments from namespace %s: %v", namespaceName, err)
-			continue
+			log.Warningf("Failed to get istiod deployments: %v", err)
+		} else {
+			istiodDeployments = deployments
 		}
-		istiodDeployments = append(istiodDeployments, deployments...)
+	} else {
+		// this should always include the controlplane namespace(s) if the discovery selectors are correctly set up
+		for _, ns := range conf.Deployment.AccessibleNamespaces {
+			deployments, err := k8s.GetDeployments(ns, meta_v1.ListOptions{LabelSelector: "app=istiod"})
+			if err != nil {
+				log.Warningf("Failed to get istiod deployments from namespace %s: %v", ns, err)
+				continue
+			}
+			istiodDeployments = append(istiodDeployments, deployments...)
+		}
 	}
 
 	if len(istiodDeployments) == 0 {
-		return "", fmt.Errorf("istiod deployment not found in any namespace (searched namespaces: %v)", namespaceNames)
+		return "", fmt.Errorf("no istiod deployment found, no clusterName determined")
 	}
 
-	// Just take the first one since they should all have the same cluster id.
+	// Just take the first one since they should all have the same clusterID.
 	istiodDeployment := &istiodDeployments[0]
 
 	istiodContainers := istiodDeployment.Spec.Template.Spec.Containers
