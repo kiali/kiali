730002281b3fef0753b5b3239aa0dc7a54269e88
Nick Fox <6226732+nrfox@users.noreply.github.com>
2025-09-18 15:49:05 -0400

    Backport keycloak settings

--- hack/keycloak.sh
+++ hack/keycloak.sh
@@ -4,8 +4,16 @@
 
 set -e
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+cd ${SCRIPT_DIR}/..
+
+# Not exposed as a flag yet but could be.
+KEYCLOAK_VERSION="26.3.4"
+
 KEYCLOAK_CERTS_DIR=""
 KEYCLOAK_EXTERNAL_IP=""
+SET_LIMIT_MEMORY=""
+SET_REQUESTS_MEMORY=""
 
 infomsg() {
   echo "[INFO] ${1}"
@@ -19,6 +27,10 @@ while [[ $# -gt 0 ]]; do
     deploy) _CMD="deploy"; shift ;;
     -kcd|--keycloak-certs-dir)    KEYCLOAK_CERTS_DIR="$2";    shift;shift; ;;
     -kip|--keycloak-external-ip)    KEYCLOAK_EXTERNAL_IP="$2";    shift;shift; ;;
+    -slm|--set-limit-memory)
+      SET_LIMIT_MEMORY="$2"; shift; shift ;;
+    -srm|--set-requests-memory)
+      SET_REQUESTS_MEMORY="$2"; shift; shift ;;
     -h|--help)
       cat <<HELPMSG
 
@@ -31,6 +43,10 @@ Valid options:
   -kip|--keycloak-external-ip
       External IP address for the keycloak service.
       Required for the 'deploy' command.
+  -slm|--set-limit-memory
+      Add --set resources.limits.memory <value> to the helm command. Ex. resources.limits.memory=1Gi
+  -srm|--set-requests-memory
+      Add --set resources.requests.memory <valor>  to the helm command. Ex. resources.requests.memory=1Gi
 
 The command must be one of:
   create-ca:        create the root CA for keycloak.
@@ -106,30 +122,33 @@ EOF
     -extensions v3_req -extfile "${KEYCLOAK_CERTS_DIR}"/req.cnf
   
   # create kube secret from the certs
-  kubectl create secret tls keycloak-tls --cert="${KEYCLOAK_CERTS_DIR}"/cert.pem --key="${KEYCLOAK_CERTS_DIR}"/key.pem -n keycloak
+  kubectl get secret keycloak-tls -n keycloak || kubectl create secret tls keycloak-tls --cert="${KEYCLOAK_CERTS_DIR}"/cert.pem --key="${KEYCLOAK_CERTS_DIR}"/key.pem -n keycloak
 
-  echo "Creating keycloak deployment"
-  helm upgrade --install --wait --timeout 15m \
-  --namespace keycloak \
-   keycloak oci://registry-1.docker.io/bitnamicharts/keycloak --version 24.3.2 \
-  --reuse-values --values - <<EOF
-auth:
-  createAdminUser: true
-  adminUser: admin
-  adminPassword: admin
-  managementUser: manager
-  managementPassword: manager
-proxyAddressForwarding: true
-postgresql:
-  enabled: true
-tls:
-  enabled: true
-  usePem: true
-  existingSecret: keycloak-tls
-service:
-  type: LoadBalancer
-EOF
+  HELM_MEMORY_ARGS=""
+  if [ -n "$SET_LIMIT_MEMORY" ]; then
+    HELM_MEMORY_ARGS="$HELM_MEMORY_ARGS --set resources.limits.memory=$SET_LIMIT_MEMORY"
+  fi
+  if [ -n "$SET_REQUESTS_MEMORY" ]; then
+    HELM_MEMORY_ARGS="$HELM_MEMORY_ARGS --set resources.requests.memory=$SET_REQUESTS_MEMORY"
+  fi
 
+  kubectl apply -n keycloak -f https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/${KEYCLOAK_VERSION}/kubernetes/keycloaks.k8s.keycloak.org-v1.yml
+  kubectl apply -n keycloak -f https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/${KEYCLOAK_VERSION}/kubernetes/keycloakrealmimports.k8s.keycloak.org-v1.yml
+  kubectl apply -n keycloak -f https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/${KEYCLOAK_VERSION}/kubernetes/kubernetes.yml
+
+  kubectl apply -n keycloak -f "${SCRIPT_DIR}/keycloak/postgresql.yaml"
+  kubectl apply -n keycloak -f "${SCRIPT_DIR}/keycloak/keycloak-service.yaml"
+  kubectl apply -n keycloak -f "${SCRIPT_DIR}/keycloak/keycloak-secrets.yaml"
+  keycloak_yaml=$(cat "${SCRIPT_DIR}/keycloak/keycloak-cr.yaml" | yq ".spec.hostname.hostname = \"${KEYCLOAK_EXTERNAL_ADDRESS}\"")
+  if [ -n "$SET_REQUESTS_MEMORY" ]; then
+    keycloak_yaml=$(echo "${keycloak_yaml}" | yq ".spec.resources.requests.memory = \"${SET_REQUESTS_MEMORY}\"")
+  fi
+  if [ -n "$SET_LIMIT_MEMORY" ]; then
+    keycloak_yaml=$(echo "${keycloak_yaml}" | yq ".spec.resources.limits.memory = \"${SET_LIMIT_MEMORY}\"")
+  fi
+  kubectl apply -n keycloak -f - <<< "${keycloak_yaml}"
+
+  kubectl wait --for=condition=Ready keycloak/keycloak -n keycloak --timeout=600s
   echo "Keycloak deployed. Waiting for the keycloak ingress to be ready..."
   kubectl wait --for=jsonpath='{.status.loadBalancer.ingress}' -n keycloak service/keycloak
 fi
