e9477ae36d9e69e7a7b1403a7476f2ff333d26a6
Nick Fox <nfox@redhat.com>
2025-09-18 12:49:37 -0400

    Migrate to keycloak operator from helm chart

--- hack/keycloak.sh
+++ hack/keycloak.sh
@@ -4,6 +4,12 @@
 
 set -e
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+cd ${SCRIPT_DIR}/..
+
+# Not exposed as a flag yet but could be.
+KEYCLOAK_VERSION="26.3.4"
+
 KEYCLOAK_CERTS_DIR=""
 KEYCLOAK_EXTERNAL_IP=""
 SET_LIMIT_MEMORY=""
@@ -116,7 +122,7 @@ EOF
     -extensions v3_req -extfile "${KEYCLOAK_CERTS_DIR}"/req.cnf
   
   # create kube secret from the certs
-  kubectl create secret tls keycloak-tls --cert="${KEYCLOAK_CERTS_DIR}"/cert.pem --key="${KEYCLOAK_CERTS_DIR}"/key.pem -n keycloak
+  kubectl get secret keycloak-tls -n keycloak || kubectl create secret tls keycloak-tls --cert="${KEYCLOAK_CERTS_DIR}"/cert.pem --key="${KEYCLOAK_CERTS_DIR}"/key.pem -n keycloak
 
   HELM_MEMORY_ARGS=""
   if [ -n "$SET_LIMIT_MEMORY" ]; then
@@ -125,28 +131,24 @@ EOF
   if [ -n "$SET_REQUESTS_MEMORY" ]; then
     HELM_MEMORY_ARGS="$HELM_MEMORY_ARGS --set resources.requests.memory=$SET_REQUESTS_MEMORY"
   fi
-  helm upgrade --install --wait --timeout 15m \
-  --namespace keycloak \
-  keycloak oci://registry-1.docker.io/bitnamicharts/keycloak --version 24.3.2 \
-  $HELM_MEMORY_ARGS \
-  --reuse-values --values - <<EOF
-auth:
-  createAdminUser: true
-  adminUser: admin
-  adminPassword: admin
-  managementUser: manager
-  managementPassword: manager
-proxyAddressForwarding: true
-postgresql:
-  enabled: true
-tls:
-  enabled: true
-  usePem: true
-  existingSecret: keycloak-tls
-service:
-  type: LoadBalancer
-EOF
 
+  kubectl apply -n keycloak -f https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/${KEYCLOAK_VERSION}/kubernetes/keycloaks.k8s.keycloak.org-v1.yml
+  kubectl apply -n keycloak -f https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/${KEYCLOAK_VERSION}/kubernetes/keycloakrealmimports.k8s.keycloak.org-v1.yml
+  kubectl apply -n keycloak -f https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/${KEYCLOAK_VERSION}/kubernetes/kubernetes.yml
+
+  kubectl apply -n keycloak -f "${SCRIPT_DIR}/keycloak/postgresql.yaml"
+  kubectl apply -n keycloak -f "${SCRIPT_DIR}/keycloak/keycloak-service.yaml"
+  kubectl apply -n keycloak -f "${SCRIPT_DIR}/keycloak/keycloak-secrets.yaml"
+  keycloak_yaml=$(cat "${SCRIPT_DIR}/keycloak/keycloak-cr.yaml" | yq ".spec.hostname.hostname = \"${KEYCLOAK_EXTERNAL_ADDRESS}\"")
+  if [ -n "$SET_REQUESTS_MEMORY" ]; then
+    keycloak_yaml=$(echo "${keycloak_yaml}" | yq ".spec.resources.requests.memory = \"${SET_REQUESTS_MEMORY}\"")
+  fi
+  if [ -n "$SET_LIMIT_MEMORY" ]; then
+    keycloak_yaml=$(echo "${keycloak_yaml}" | yq ".spec.resources.limits.memory = \"${SET_LIMIT_MEMORY}\"")
+  fi
+  kubectl apply -n keycloak -f - <<< "${keycloak_yaml}"
+
+  kubectl wait --for=condition=Ready keycloak/keycloak -n keycloak --timeout=600s
   echo "Keycloak deployed. Waiting for the keycloak ingress to be ready..."
   kubectl wait --for=jsonpath='{.status.loadBalancer.ingress}' -n keycloak service/keycloak
 fi
