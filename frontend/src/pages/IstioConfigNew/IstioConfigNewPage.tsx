6268dd318025ea108717d9cadecb78904264f52d
Meng-Yuan Huang <myh@live.com>
2025-09-23 17:25:57 +0800

    Fix scroll in IstioConfigNewPage (#8751) (#8752)
    * Fix scroll in IstioConfigNewPage (#8751)
    
    * Update frontend/src/pages/IstioConfigNew/IstioConfigNewPage.tsx
    
    ---------
    
    Co-authored-by: Hayk Hovsepyan <haik.hovsepian@gmail.com>

--- frontend/src/pages/IstioConfigNew/IstioConfigNewPage.tsx
+++ frontend/src/pages/IstioConfigNew/IstioConfigNewPage.tsx
@@ -492,178 +492,187 @@ const IstioConfigNewPageComponent: React.FC<Props> = (props: Props) => {
   const isFormValid = isNameValid && isNamespacesValid && isClustersValid && isIstioFormValid();
 
   return (
-    <>
+    <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
       <div>
         <DefaultSecondaryMasthead showClusterSelector={false} hideNamespaceSelector={true} />
       </div>
 
-      <RenderContent>
-        <Form className={formPadding} isHorizontal={true}>
-          <FormGroup label={t('Namespaces')} isRequired={true} fieldId="namespaces">
-            <NamespaceDropdown disabled={false} />
+       <div style={{ flexGrow: 1, overflowY: 'auto' }}>
+        <RenderContent>
+          <Form className={formPadding} isHorizontal={true}>
+            <FormGroup label={t('Namespaces')} isRequired={true} fieldId="namespaces">
+              <NamespaceDropdown disabled={false} />
 
-            {!isValid(isNamespacesValid) && (
-              <FormHelperText>
-                <HelperText>
-                  <HelperTextItem>{t('An Istio Config resource needs at least one namespace selected')}</HelperTextItem>
-                </HelperText>
-              </FormHelperText>
-            )}
-          </FormGroup>
-
-          {isMultiCluster && (
-            <FormGroup label={t('Clusters')} isRequired={true} fieldId="clusters">
-              <ClusterDropdown />
-
-              {!isValid(isClustersValid) && (
+              {!isValid(isNamespacesValid) && (
                 <FormHelperText>
                   <HelperText>
-                    <HelperTextItem>{t('An Istio Config resource needs at least one cluster selected')}</HelperTextItem>
+                    <HelperTextItem>
+                      {t('An Istio Config resource needs at least one namespace selected')}
+                    </HelperTextItem>
                   </HelperText>
                 </FormHelperText>
               )}
             </FormGroup>
-          )}
 
-          <FormGroup label={t('Name')} isRequired={true} fieldId="name">
-            <TextInput
-              value={name}
-              isRequired={true}
-              type="text"
-              id="name"
-              aria-describedby={t('name')}
-              name="name"
-              onChange={onNameChange}
-              validated={isValid(isNameValid)}
-            />
+            {isMultiCluster && (
+              <FormGroup label={t('Clusters')} isRequired={true} fieldId="clusters">
+                <ClusterDropdown />
 
-            {!isValid(isNameValid) && (
-              <FormHelperText>
-                <HelperText>
-                  <HelperTextItem>{t('A valid {{kind}} name is required', props.objectGVK.Kind)}</HelperTextItem>
-                </HelperText>
-              </FormHelperText>
+                {!isValid(isClustersValid) && (
+                  <FormHelperText>
+                    <HelperText>
+                      <HelperTextItem>
+                        {t('An Istio Config resource needs at least one cluster selected')}
+                      </HelperTextItem>
+                    </HelperText>
+                  </FormHelperText>
+                )}
+              </FormGroup>
             )}
-          </FormGroup>
 
-          {getGVKTypeString(props.objectGVK) === getGVKTypeString(gvkType.AuthorizationPolicy) && (
-            <AuthorizationPolicyForm authorizationPolicy={authorizationPolicy} onChange={onChangeAuthorizationPolicy} />
-          )}
+            <FormGroup label={t('Name')} isRequired={true} fieldId="name">
+              <TextInput
+                value={name}
+                isRequired={true}
+                type="text"
+                id="name"
+                aria-describedby={t('name')}
+                name="name"
+                onChange={onNameChange}
+                validated={isValid(isNameValid)}
+              />
 
-          {getGVKTypeString(props.objectGVK) === getGVKTypeString(gvkType.Gateway) && (
-            <GatewayForm gateway={gateway} onChange={onChangeGateway} />
-          )}
+              {!isValid(isNameValid) && (
+                <FormHelperText>
+                  <HelperText>
+                    <HelperTextItem>{t('A valid {{kind}} name is required', props.objectGVK.Kind)}</HelperTextItem>
+                  </HelperText>
+                </FormHelperText>
+              )}
+            </FormGroup>
 
-          {getGVKTypeString(props.objectGVK) === getGVKTypeString(gvkType.K8sGateway) && (
-            <K8sGatewayForm k8sGateway={k8sGateway} onChange={onChangeK8sGateway} />
-          )}
+            {getGVKTypeString(props.objectGVK) === getGVKTypeString(gvkType.AuthorizationPolicy) && (
+              <AuthorizationPolicyForm
+                authorizationPolicy={authorizationPolicy}
+                onChange={onChangeAuthorizationPolicy}
+              />
+            )}
 
-          {getGVKTypeString(props.objectGVK) === getGVKTypeString(gvkType.K8sReferenceGrant) && (
-            <K8sReferenceGrantForm k8sReferenceGrant={k8sReferenceGrant} onChange={onChangeK8sReferenceGrant} />
-          )}
+            {getGVKTypeString(props.objectGVK) === getGVKTypeString(gvkType.Gateway) && (
+              <GatewayForm gateway={gateway} onChange={onChangeGateway} />
+            )}
 
-          {getGVKTypeString(props.objectGVK) === getGVKTypeString(gvkType.PeerAuthentication) && (
-            <PeerAuthenticationForm peerAuthentication={peerAuthentication} onChange={onChangePeerAuthentication} />
-          )}
+            {getGVKTypeString(props.objectGVK) === getGVKTypeString(gvkType.K8sGateway) && (
+              <K8sGatewayForm k8sGateway={k8sGateway} onChange={onChangeK8sGateway} />
+            )}
 
-          {getGVKTypeString(props.objectGVK) === getGVKTypeString(gvkType.RequestAuthentication) && (
-            <RequestAuthenticationForm
-              requestAuthentication={requestAuthentication}
-              onChange={onChangeRequestAuthentication}
-            />
-          )}
+            {getGVKTypeString(props.objectGVK) === getGVKTypeString(gvkType.K8sReferenceGrant) && (
+              <K8sReferenceGrantForm k8sReferenceGrant={k8sReferenceGrant} onChange={onChangeK8sReferenceGrant} />
+            )}
 
-          {getGVKTypeString(props.objectGVK) === getGVKTypeString(gvkType.ServiceEntry) && (
-            <ServiceEntryForm serviceEntry={serviceEntry} onChange={onChangeServiceEntry} />
-          )}
+            {getGVKTypeString(props.objectGVK) === getGVKTypeString(gvkType.PeerAuthentication) && (
+              <PeerAuthenticationForm peerAuthentication={peerAuthentication} onChange={onChangePeerAuthentication} />
+            )}
 
-          {getGVKTypeString(props.objectGVK) === getGVKTypeString(gvkType.Sidecar) && (
-            <SidecarForm sidecar={sidecar} onChange={onChangeSidecar} />
-          )}
+            {getGVKTypeString(props.objectGVK) === getGVKTypeString(gvkType.RequestAuthentication) && (
+              <RequestAuthenticationForm
+                requestAuthentication={requestAuthentication}
+                onChange={onChangeRequestAuthentication}
+              />
+            )}
 
-          <FormGroup fieldId="labels" label="Labels">
-            <div className={editStyle}>
-              <Labels labels={labels} expanded={true} />
+            {getGVKTypeString(props.objectGVK) === getGVKTypeString(gvkType.ServiceEntry) && (
+              <ServiceEntryForm serviceEntry={serviceEntry} onChange={onChangeServiceEntry} />
+            )}
 
+            {getGVKTypeString(props.objectGVK) === getGVKTypeString(gvkType.Sidecar) && (
+              <SidecarForm sidecar={sidecar} onChange={onChangeSidecar} />
+            )}
+
+            <FormGroup fieldId="labels" label="Labels">
+              <div className={editStyle}>
+                <Labels labels={labels} expanded={true} />
+
+                <Button
+                  className={editButton}
+                  type="button"
+                  variant="link"
+                  isInline
+                  onClick={() => onLabelsWizardToggle(true)}
+                  data-test={'edit-labels'}
+                >
+                  {t('Edit')}
+                  <KialiIcon.PencilAlt className={editIcon} />
+                </Button>
+              </div>
+
+              <WizardLabels
+                showAnotationsWizard={showLabelsWizard}
+                type={'labels'}
+                onChange={labels => onAddLabels(labels)}
+                onClose={() => onLabelsWizardToggle(false)}
+                labels={labels}
+                canEdit={true}
+              />
+            </FormGroup>
+
+            <FormGroup fieldId="annotations" label={t('Annotations')}>
+              <div className={editStyle}>
+                <Labels labels={annotations} type={'annotations'} expanded={true} />
+
+                <Button
+                  className={editButton}
+                  type="button"
+                  variant="link"
+                  isInline
+                  onClick={() => onAnnotationsWizardToggle(true)}
+                  data-test={'edit-annotations'}
+                >
+                  {t('Edit')}
+                  <KialiIcon.PencilAlt className={editIcon} />
+                </Button>
+              </div>
+
+              <WizardLabels
+                showAnotationsWizard={showAnnotationsWizard}
+                type={'annotations'}
+                onChange={annotations => onAddAnnotations(annotations)}
+                onClose={() => onAnnotationsWizardToggle(false)}
+                labels={annotations}
+                canEdit={true}
+              />
+            </FormGroup>
+
+            <ActionGroup>
               <Button
-                className={editButton}
-                type="button"
-                variant="link"
-                isInline
-                onClick={() => onLabelsWizardToggle(true)}
-                data-test={'edit-labels'}
+                variant={ButtonVariant.primary}
+                isDisabled={!isFormValid}
+                onClick={() => openPreview()}
+                data-test={'preview'}
               >
-                {t('Edit')}
-                <KialiIcon.PencilAlt className={editIcon} />
+                {t('Preview')}
               </Button>
-            </div>
 
-            <WizardLabels
-              showAnotationsWizard={showLabelsWizard}
-              type={'labels'}
-              onChange={labels => onAddLabels(labels)}
-              onClose={() => onLabelsWizardToggle(false)}
-              labels={labels}
-              canEdit={true}
-            />
-          </FormGroup>
-
-          <FormGroup fieldId="annotations" label={t('Annotations')}>
-            <div className={editStyle}>
-              <Labels labels={annotations} type={'annotations'} expanded={true} />
-
-              <Button
-                className={editButton}
-                type="button"
-                variant="link"
-                isInline
-                onClick={() => onAnnotationsWizardToggle(true)}
-                data-test={'edit-annotations'}
-              >
-                {t('Edit')}
-                <KialiIcon.PencilAlt className={editIcon} />
+              <Button variant={ButtonVariant.secondary} onClick={() => backToList()}>
+                {t('Cancel')}
               </Button>
-            </div>
+            </ActionGroup>
+          </Form>
 
-            <WizardLabels
-              showAnotationsWizard={showAnnotationsWizard}
-              type={'annotations'}
-              onChange={annotations => onAddAnnotations(annotations)}
-              onClose={() => onAnnotationsWizardToggle(false)}
-              labels={annotations}
-              canEdit={true}
-            />
-          </FormGroup>
-
-          <ActionGroup>
-            <Button
-              variant={ButtonVariant.primary}
-              isDisabled={!isFormValid}
-              onClick={() => openPreview()}
-              data-test={'preview'}
-            >
-              {t('Preview')}
-            </Button>
-
-            <Button variant={ButtonVariant.secondary} onClick={() => backToList()}>
-              {t('Cancel')}
-            </Button>
-          </ActionGroup>
-        </Form>
-
-        <IstioConfigPreview
-          isOpen={showPreview}
-          items={itemsPreview}
-          downloadPrefix={props.objectGVK.Kind}
-          title={t('Preview new istio objects')}
-          opTarget={'create'}
-          disableAction={!canCreate}
-          ns={props.activeNamespaces.map(n => n.name).join(',')}
-          onConfirm={items => onPreviewConfirm(items)}
-          onClose={() => setShowPreview(false)}
-        />
-      </RenderContent>
-    </>
+          <IstioConfigPreview
+            isOpen={showPreview}
+            items={itemsPreview}
+            downloadPrefix={props.objectGVK.Kind}
+            title={t('Preview new istio objects')}
+            opTarget={'create'}
+            disableAction={!canCreate}
+            ns={props.activeNamespaces.map(n => n.name).join(',')}
+            onConfirm={items => onPreviewConfirm(items)}
+            onClose={() => setShowPreview(false)}
+          />
+        </RenderContent>
+      </div>
+    </div>
   );
 };
 
