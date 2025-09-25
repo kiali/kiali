8cea08baf1f55b8f65a4be8ed85762a481e0af14
Hayk Hovsepyan <hhovsepy@redhat.com>
2025-09-25 15:10:51 +0200

    lint upgrade2

--- make/Makefile.build.mk
+++ make/Makefile.build.mk
@@ -95,7 +95,7 @@ test-integration-controller: .ensure-envtest-bin-dir-exists
 
 ## lint-install: Installs golangci-lint
 lint-install:
-	curl -sfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $$(${GO} env GOPATH)/bin v1.62.0
+	curl -sfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $$(${GO} env GOPATH)/bin v1.64.8
 
 ## lint: Runs golangci-lint
 # doc.go is ommited for linting, because it generates lots of warnings.
