560e7ed695aa8ed2319b4c032b7e44f5db4cceba
Jay Shaughnessy <jshaughn@redhat.com>
2025-11-13 14:51:36 -0500

    Disable graph caching for upstream CI tests to ensure that a returned graph always reflects the most recent config and/or data.

--- hack/istio/multicluster/deploy-kiali.sh
+++ hack/istio/multicluster/deploy-kiali.sh
@@ -228,6 +228,7 @@ deploy_kiali() {
           --set kiali_internal.cache_expiration.istio_status="0"
           --set kiali_internal.cache_expiration.mesh="10s"
           --set kiali_internal.cache_expiration.waypoint="2m"
+          --set kiali_internal.graph_cache.enabled="false"
         )
   fi
 
