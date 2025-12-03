2b04bd3de681974cd8fc6f678234827f8c73ff55
Alberto Gutierrez <aljesusg@gmail.com>
2025-12-03 14:23:27 +0100

    Fix toolbar Graph colors (#8936)

--- frontend/src/styles/GraphStyle.ts
+++ frontend/src/styles/GraphStyle.ts
@@ -1,9 +1,7 @@
-import { PFColors } from 'components/Pf/PfColors';
 import { kialiStyle } from './StyleUtils';
 
 export const toolbarActiveStyle = kialiStyle({
   top: '0.25rem',
-  color: PFColors.Active,
   $nest: {
     '& svg': {
       width: '1.25rem',
