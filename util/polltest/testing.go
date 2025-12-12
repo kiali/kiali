328ba157f26db15ccf5a75cbafa06e908e2e4a3a
John Mazzitelli <mazz@redhat.com>
2025-12-12 10:41:39 -0500

    use poll util

--- util/polltest/testing.go
+++ util/polltest/testing.go
@@ -13,7 +13,19 @@ import (
 func PollForCondition(t *testing.T, timeout time.Duration, condition func() bool) bool {
 	t.Helper()
 
-	const interval = 50 * time.Millisecond
+	return PollForConditionInterval(t, timeout, 50*time.Millisecond, condition)
+}
+
+// PollForConditionInterval polls until a condition is met or timeout is reached, checking
+// the condition at the given interval.
+// Returns true if condition was met, false if timeout occurred.
+func PollForConditionInterval(t *testing.T, timeout time.Duration, interval time.Duration, condition func() bool) bool {
+	t.Helper()
+
+	if interval <= 0 {
+		t.Fatalf("interval must be > 0, got: %v", interval)
+	}
+
 	iterations := int(timeout / interval)
 	for i := 0; i < iterations; i++ {
 		time.Sleep(interval)
