06b6ab59532eb142a94e67b19f1f3c7044af84ba
Fernando Hoyos <122779323+ferhoyos@users.noreply.github.com>
2026-01-14 20:13:59 +0100

    Set Istio version to Kiali target branch (#9062)
    * Set Istio version to Kiali target branch
    
    * Debug API response
    
    * Revert "Debug API response"
    
    This reverts commit c6ac9154431577104922afe026cc8167148939d8.

--- hack/setup-kind-in-ci.sh
+++ hack/setup-kind-in-ci.sh
@@ -144,18 +144,22 @@ TARGET_BRANCH="${TARGET_BRANCH:-master}"
 # https://kiali.io/docs/installation/installation-guide/prerequisites/
 # https://istio.io/latest/docs/releases/supported-releases/
 if [ -z "${ISTIO_VERSION}" ]; then
-  if [ "${TARGET_BRANCH}" == "v1.65" ]; then
-    ISTIO_VERSION="1.16.0"
-  elif [ "${TARGET_BRANCH}" == "v1.73" ]; then
-    ISTIO_VERSION="1.18.0"
+  if [ "${TARGET_BRANCH}" == "v1.73" ]; then
+    ISTIO_VERSION="1.18.7"
+  elif [ "${TARGET_BRANCH}" == "v2.4" ]; then
+    ISTIO_VERSION="1.23.6"
+  elif [ "${TARGET_BRANCH}" == "v2.11" ]; then
+    ISTIO_VERSION="1.26.8"
   fi
 fi
 
 KIND_NODE_IMAGE=""
-if [ "${ISTIO_VERSION}" == "1.16.0" ]; then
-  KIND_NODE_IMAGE="kindest/node:v1.23.4@sha256:0e34f0d0fd448aa2f2819cfd74e99fe5793a6e4938b328f657c8e3f81ee0dfb9"
-elif [ "${ISTIO_VERSION}" == "v1.18.0" ]; then
-  KIND_NODE_IMAGE="kindest/node:v1.27.3@sha256:3966ac761ae0136263ffdb6cfd4db23ef8a83cba8a463690e98317add2c9ba72"
+if [ "${ISTIO_VERSION}" == 1.18.* ]; then
+  KIND_NODE_IMAGE="kindest/node:v1.27.16@sha256:2d21a61643eafc439905e18705b8186f3296384750a835ad7a56cb574b35af8"
+elif [[ "${ISTIO_VERSION}" == 1.23.* ]]; then
+  KIND_NODE_IMAGE="kindest/node:v1.30.13@sha256:397209b3d947d154f6641f2d0ce8d473732bd91c87d9575ade99049aa33cd648"
+elif [[ "${ISTIO_VERSION}" == 1.26.* ]]; then
+  KIND_NODE_IMAGE="kindest/node:v1.33.7@sha256:d26ef333bdb2cbe9862a0f7c3803ecc7b4303d8cea8e814b481b09949d353040"
 fi
 
 if [ -z "${HELM_CHARTS_DIR}" ]; then
