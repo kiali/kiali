3d6cd8107b49691223f7dce9e285bd04f3dd6a38
Hayk Hovsepyan <hhovsepy@redhat.com>
2026-01-27 15:57:19 +0100

    Type and Health header icons alligned

---
+++ frontend/src/pages/Namespaces/NamespaceStyle.ts
@@ -0,0 +1,7 @@
+import { kialiStyle } from 'styles/StyleUtils';
+
+export const headerWithHelpStyle = kialiStyle({
+  alignItems: 'center',
+  display: 'flex',
+  gap: '0.5rem'
+});
--- frontend/src/pages/Namespaces/TypeHeader.tsx
+++ frontend/src/pages/Namespaces/TypeHeader.tsx
@@ -1,44 +1,42 @@
 import * as React from 'react';
-import { Tooltip, TooltipPosition } from '@patternfly/react-core';
-import { KialiIcon } from '../../config/KialiIcon';
+import { Button, ButtonVariant, Popover, PopoverPosition } from '@patternfly/react-core';
 import { useKialiTranslation } from 'utils/I18nUtils';
-import { kialiStyle } from '../../styles/StyleUtils';
-
-const infoIconStyle = kialiStyle({
-  marginLeft: '0.5rem',
-  cursor: 'pointer'
-});
+import { KialiIcon } from '../../config/KialiIcon';
+import { headerWithHelpStyle } from './NamespaceStyle';
 
 export const TypeHeader: React.FC = () => {
   const { t } = useKialiTranslation();
 
-  const tooltipContent = (
+  const popoverBody = (
     <div style={{ textAlign: 'left' }}>
       <div>
-        <strong>{t('Namespace type')}</strong>
-      </div>
-      <div style={{ marginTop: '0.5rem' }}>
         <div>
-          <strong>{t('CP')}</strong> - {t('Control plane')}: {t('Istio control plane')}
+          <strong>{t('CP')}</strong> - {t('Control plane')}: {t('Istio control plane.')}
         </div>
         <div>
-          <strong>{t('DP')}</strong> - {t('Data plane')}: {t('Namespace is part of the mesh')}
+          <strong>{t('DP')}</strong> - {t('Data plane')}: {t('Namespace is part of the mesh.')}
         </div>
         <div>
-          <strong>Empty</strong> - {t('Namespace is not part of the mesh')}
+          <strong>Empty</strong> - {t('Namespace is not part of the mesh.')}
         </div>
       </div>
     </div>
   );
 
   return (
-    <>
-      {t('Type')}
-      <Tooltip content={tooltipContent} position={TooltipPosition.top}>
-        <span className={infoIconStyle}>
-          <KialiIcon.Info />
-        </span>
-      </Tooltip>
-    </>
+    <div className={headerWithHelpStyle}>
+      <span>{t('Type')}</span>
+      <Popover
+        aria-label={t('Namespace type information')}
+        headerContent={<span>{t('Namespace type')}</span>}
+        bodyContent={popoverBody}
+        position={PopoverPosition.top}
+        triggerAction="hover"
+      >
+        <Button variant={ButtonVariant.link} isInline>
+          <KialiIcon.Help />
+        </Button>
+      </Popover>
+    </div>
   );
 };
