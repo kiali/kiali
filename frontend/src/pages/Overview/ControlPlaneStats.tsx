687a6d01bee6942bbbebc9888e7c568c3d3458c6
Fernando Hoyos <122779323+ferhoyos@users.noreply.github.com>
2026-02-02 12:48:30 +0100

    Add popovers with health details to Overview page cards (#9174)
    * Add popovers with health details to Overview page cards
    
    * Fix typo
    
    * Translate health status label

--- frontend/src/pages/Overview/ControlPlaneStats.tsx
+++ frontend/src/pages/Overview/ControlPlaneStats.tsx
@@ -1,13 +1,35 @@
 import * as React from 'react';
-import { Card, CardBody, CardFooter, CardHeader, CardTitle, Spinner } from '@patternfly/react-core';
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+  Popover,
+  PopoverPosition,
+  Spinner
+} from '@patternfly/react-core';
 import { Link } from 'react-router-dom-v5-compat';
-import { PFColors } from 'components/Pf/PfColors';
 import { KialiIcon } from 'config/KialiIcon';
 import { Paths } from 'config';
 import { t } from 'utils/I18nUtils';
 import { useControlPlanes } from 'hooks/controlPlanes';
 import { Status } from 'types/IstioStatus';
-import { cardStyle, cardBodyStyle, linkStyle, iconStyle, statsContainerStyle, statItemStyle } from './OverviewStyles';
+import { PFBadge, PFBadges } from 'components/Pf/PfBadges';
+import { PFColors } from 'components/Pf/PfColors';
+import {
+  cardStyle,
+  cardBodyStyle,
+  clickableStyle,
+  iconStyle,
+  linkStyle,
+  popoverHeaderStyle,
+  popoverItemStatusStyle,
+  popoverItemStyle,
+  statItemStyle,
+  statsContainerStyle
+} from './OverviewStyles';
+import { classes } from 'typestyle';
 
 export const ControlPlaneStats: React.FC = () => {
   const { controlPlanes, isLoading } = useControlPlanes();
@@ -16,6 +38,23 @@ export const ControlPlaneStats: React.FC = () => {
   const total = controlPlanes.length;
   const healthy = controlPlanes.filter(cp => cp.status === Status.Healthy).length;
   const unhealthy = total - healthy;
+
+  // Get control planes with issues
+  const controlPlanesWithIssues = controlPlanes.filter(cp => cp.status !== Status.Healthy);
+
+  const popoverContent = (
+    <>
+      {controlPlanesWithIssues.map(cp => (
+        <div key={`${cp.cluster.name}-${cp.istiodName}`} className={popoverItemStyle}>
+          <span>
+            <PFBadge badge={PFBadges.Istio} size="sm" />
+            <Link to={`/${Paths.MESH}?cluster=${cp.cluster.name}`}>{cp.istiodName}</Link>
+          </span>
+          <span className={popoverItemStatusStyle}>{cp.status}</span>
+        </div>
+      ))}
+    </>
+  );
 
   return (
     <Card className={cardStyle}>
@@ -36,10 +75,21 @@ export const ControlPlaneStats: React.FC = () => {
               </div>
             )}
             {unhealthy > 0 && (
-              <div className={statItemStyle}>
-                <span>{unhealthy}</span>
-                <KialiIcon.Error />
-              </div>
+              <Popover
+                aria-label={t('Control planes with issues')}
+                position={PopoverPosition.right}
+                headerContent={
+                  <span className={popoverHeaderStyle}>
+                    <KialiIcon.ExclamationTriangle /> {t('Control planes')}
+                  </span>
+                }
+                bodyContent={popoverContent}
+              >
+                <div className={classes(statItemStyle, clickableStyle)}>
+                  <span className={linkStyle}>{unhealthy}</span>
+                  <KialiIcon.ExclamationTriangle />
+                </div>
+              </Popover>
             )}
           </div>
         )}
