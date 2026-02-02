2e6f1ddab0f20f01c90d934b7706764af6c12500
Hayk Hovsepyan <hhovsepy@redhat.com>
2026-02-02 12:53:10 +0100

    Namespaces UX improvements, n/a style, label filters (#9163)
    * Namespaces UX improvements, n/a style, label filters

--- frontend/src/pages/Namespaces/NamespaceStyle.ts
+++ frontend/src/pages/Namespaces/NamespaceStyle.ts
@@ -5,3 +5,16 @@ export const headerWithHelpStyle = kialiStyle({
   display: 'flex',
   gap: '0.5rem'
 });
+
+export const namespaceNaIconStyle = kialiStyle({
+  color: 'var(--pf-t--global--icon--color--disabled)'
+});
+
+export const statusIconStyle = kialiStyle({
+  display: 'inline-block',
+  marginRight: '0.5rem'
+});
+
+export const statusTextStyle = kialiStyle({
+  display: 'inline-block'
+});
