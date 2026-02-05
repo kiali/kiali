a448ef67b7026dda7a31286f9bd822eea891f17a
josunect <jcordoba@redhat.com>
2026-02-05 13:08:26 +0000

    Remove unused styles

--- frontend/src/pages/WorkloadList/WorkloadListPage.tsx
+++ frontend/src/pages/WorkloadList/WorkloadListPage.tsx
@@ -28,12 +28,6 @@ import { RefreshIntervalManual, RefreshIntervalPause } from 'config/Config';
 import { HistoryManager } from 'app/History';
 import { endPerfTimer, startPerfTimer } from '../../utils/PerformanceUtils';
 import { setAIContext } from 'helpers/ChatAI';
-import { kialiStyle } from 'styles/StyleUtils';
-
-const refreshStyle = kialiStyle({
-  marginLeft: '0.4rem',
-  marginRight: '0.4rem'
-});
 
 type WorkloadListPageState = FilterComponent.State<WorkloadListItem> & {
   loaded: boolean;
