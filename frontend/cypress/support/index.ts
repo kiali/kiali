4e1ca3b9a493004c33eedfdb33b1c9f96a00a667
Fernando Hoyos <122779323+ferhoyos@users.noreply.github.com>
2026-03-05 15:17:17 +0100

    Remove react-cypress-selector to support React 18 (#9301) (#9309)
    * Remove react-cypress-selector to support React 18
    
    * Replace additional cypress-react-selector commands

--- frontend/cypress/support/index.ts
+++ frontend/cypress/support/index.ts
@@ -13,7 +13,5 @@
 // https://on.cypress.io/configuration
 // ***********************************************************
 
-import 'cypress-react-selector';
-
 // Import commands.js using ES2015 syntax:
 import './commands';
