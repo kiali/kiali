93bca7d7186d73877a8bd14996fa86d70ed134fb
Fernando Hoyos <fhoyosle@redhat.com>
2026-03-24 19:16:23 +0100

    Ensure correct yarn version in frontend folder

--- make/Makefile.ui.mk
+++ make/Makefile.ui.mk
@@ -5,7 +5,7 @@
 ## run-frontend: Run the frontend UI in a local development server. Set YARN_START_URL to update package.json.
 # The 'proxy' field will be set to the YARN_START_URL value (or empty if not provided).
 # The proxy field will be automatically cleaned up when yarn start exits.
-run-frontend:
+run-frontend: .ensure-yarn-version
 	sed -i -e "2 i \ \ \"proxy\": \"${YARN_START_URL}\"," -e "/\"proxy\":/d" ${ROOTDIR}/frontend/package.json
 	@echo "'yarn start' will use this proxy setting: $$(grep proxy ${ROOTDIR}/frontend/package.json || echo 'No proxy configured')"
 	@cleanup() { \
@@ -22,20 +22,20 @@ run-frontend:
 yarn-start: run-frontend
 
 ## cypress-run: Runs all the cypress frontend integration tests locally without the GUI (i.e. headless).
-cypress-run:
+cypress-run: .ensure-yarn-version
 	@cd ${ROOTDIR}/frontend && yarn cypress:run --headless --config numTestsKeptInMemory=0,video=false
 
-cypress-selected:
+cypress-selected: .ensure-yarn-version
 	@cd ${ROOTDIR}/frontend && yarn cypress:run:selected --headless --config numTestsKeptInMemory=0,video=false
 
 ## cypress-gui: Opens the cypress GUI letting you pick which frontend integration tests to run locally.
-cypress-gui:
+cypress-gui: .ensure-yarn-version
 	@cd ${ROOTDIR}/frontend && yarn cypress
 
 ## perf-tests-run: Runs the frontend perf tests locally without the GUI.
-perf-tests-run:
+perf-tests-run: .ensure-yarn-version
 	@cd ${ROOTDIR}/frontend && yarn cypress:run:perf --headless --config numTestsKeptInMemory=0,video=false
 
 ## perf-tests-gui: Runs the frontend perf tests locally with the GUI.
-perf-tests-gui:
+perf-tests-gui: .ensure-yarn-version
 	@cd ${ROOTDIR}/frontend && yarn cypress:perf
