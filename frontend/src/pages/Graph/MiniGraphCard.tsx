46885174f84a7b67af2008f2ee95df39467fc38e
Jay Shaughnessy <jshaughn@redhat.com>
2026-05-05 13:34:39 -0400

    Update frontend/src/pages/Graph/MiniGraphCard.tsx
    Co-authored-by: Josune Cordoba <49480155+josunect@users.noreply.github.com>

--- frontend/src/pages/Graph/MiniGraphCard.tsx
+++ frontend/src/pages/Graph/MiniGraphCard.tsx
@@ -429,7 +429,18 @@ class MiniGraphCardComponent extends React.Component<MiniGraphCardProps, MiniGra
     const selected = selectAnd(elems(this.state.graphRefs!.getController()).nodes, [
       { prop: 'isSelected', op: 'truthy' }
     ]);
-    const focusSelector = selected.length > 0 ? `${URLParam.FOCUS_SELECTOR}=${encodeURI(selected[0].getId())}` : '';
+   const params = new URLSearchParams();
+    params.set('graphType', graphType);
+    params.set('injectServiceNodes', 'true');
+    params.set('namespaces', namespace);
+    if (targetNamespace.cluster) {
+      params.set(URLParam.CLUSTERNAME, targetNamespace.cluster);
+    }
+    if (selected.length > 0) {
+      params.set(URLParam.FOCUS_SELECTOR, selected[0].getId());
+    }
+
+    const graphUrl = `/graph/namespaces?${params.toString()}`;
 
     const graphUrl = `/graph/namespaces?graphType=${graphType}&injectServiceNodes=true&namespaces=${namespace}&${focusSelector}`;
 
