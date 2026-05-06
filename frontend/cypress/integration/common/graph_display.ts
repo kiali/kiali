7d358d34abd74267e23d3cf699ffa7ab40d1eee7
Fernando Hoyos <fhoyosle@redhat.com>
2026-05-06 13:22:53 +0200

    Fix prometheus cypress test for OSSMC

--- frontend/cypress/integration/common/graph_display.ts
+++ frontend/cypress/integration/common/graph_display.ts
@@ -590,7 +590,7 @@ Given('prometheus is reported as disabled in the config', () => {
   // the /api/config call that the Kiali frontend makes on load.
   // This works in any suite (local binary, operator, Helm) without requiring
   // a server restart or CR/ConfigMap patch.
-  cy.intercept(`${Cypress.config('baseUrl')}/api/config`, request => {
+  cy.intercept('**/api/config', request => {
     request.reply(response => {
       response.body['prometheus'] = {
         ...response.body['prometheus'],
@@ -601,7 +601,7 @@ Given('prometheus is reported as disabled in the config', () => {
     });
   }).as('configWithPrometheusDisabled');
   // Visit the graph page immediately so the intercept is active for the config load
-  cy.visit(`${Cypress.config('baseUrl')}/console/graph/namespaces?refresh=0`);
+  cy.visit({ url: `${Cypress.config('baseUrl')}/console/graph/namespaces?refresh=0` });
   cy.wait('@configWithPrometheusDisabled');
 });
 
